a4c29e62e88e3705cbb8d216770e1eb0b9790cbb
Mario Lassnig <mario@lassnig.net>
2012-03-05 11:43:15 +0100

    fixed doc
    Change-Id: I171969a5ec81873f852b9c79a2afd9f2c80d013d

--- lib/rucio/auth/authenticate.py
+++ lib/rucio/auth/authenticate.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env /Users/Mario/Development/CERN/rucio/.venv/bin/python
+#!/usr/bin/env python
 """
 @copyright: European Organization for Nuclear Research (CERN)
 @contact: U{ph-adp-ddm-lab@cern.ch<mailto:ph-adp-ddm-lab@cern.ch>}
@@ -10,7 +10,14 @@ U{http://www.apache.org/licenses/LICENSE-2.0}
 - Mario Lassnig, <mario.lassnig@cern.ch>, CERN PH-ADP-CO, 2012
 """
 
+import logging
+import uuid
 import web
+
+logger = logging.getLogger("rucio.authentication")
+sh = logging.StreamHandler()
+sh.setLevel(logging.DEBUG)
+logger.addHandler(sh)
 
 urls = (
     '/authenticate', 'Authenticate',
@@ -19,19 +26,72 @@ urls = (
 
 
 class Authenticate:
+    """Authenticate a Rucio account"""
 
     def GET(self):
-        return "new token"
+        """Get an authentication token for an account.
 
-    application = web.application(urls, globals()).wsgifunc()
+        HTTP Request Header:
+            Rucio-Account: the account name string to authenticate.
+            Rucio-Username: the username string to verify.
+            Rucio-Password: the password string to verify.
+
+        HTTP Response Header:
+            Rucio-Auth-Token: 32-bit hex token encoded as a string
+
+        HTTP Error Headers:
+            200 OK
+            401 Unauthorized
+
+        Example Request:
+            curl -v -X GET -H "Rucio-Account: ddmlab" -H "Rucio-Username: testuser" -H "Rucio-Password: testpassword" http://localhost/authenticate
+        """
+
+        web.header('Content-Type', 'application/octet-stream')
+
+        account = web.ctx.env.get('HTTP_RUCIO_ACCOUNT')
+        username = web.ctx.env.get('HTTP_RUCIO_USERNAME')
+        password = web.ctx.env.get('HTTP_RUCIO_PASSWORD')
+
+        if account is None:
+            raise web.BadRequest()
+
+        if username == 'testuser' and password == 'testpassword':
+            web.header('Rucio-Auth-Token', str(uuid.uuid4()).replace('-', ''))
+            return ""
+        else:
+            raise web.Unauthorized()
+
+    def PUT(self):
+        web.header('Content-Type', 'application/octet-stream')
+        raise web.BadRequest()
+
+    def POST(self):
+        web.header('Content-Type', 'application/octet-stream')
+        raise web.BadRequest()
+
+    def DELETE(self):
+        web.header('Content-Type', 'application/octet-stream')
+        raise web.BadRequest()
 
 
 class Validate:
 
     def GET(self):
-        return "validated"
+        web.header('Content-Type', 'application/octet-stream')
+        raise web.BadRequest()
 
-    application = web.application(urls, globals()).wsgifunc()
+    def PUT(self):
+        web.header('Content-Type', 'application/octet-stream')
+        raise web.BadRequest()
+
+    def POST(self):
+        web.header('Content-Type', 'application/octet-stream')
+        raise web.BadRequest()
+
+    def DELETE(self):
+        web.header('Content-Type', 'application/octet-stream')
+        raise web.BadRequest()
 
 
 app = web.application(urls, globals())
