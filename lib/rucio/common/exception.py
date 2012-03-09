a678d7fa430effe6772d078a61ea3f34400b4c85
Thomas Beermann <thomas.beermann@cern.ch>
2012-03-09 14:28:47 +0100

    now it should work
    Change-Id: I8c77beca2337413d8b0584654221ff62d48b9b0c

---
+++ lib/rucio/common/exception.py
@@ -0,0 +1,42 @@
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Thomas Beermann,  <thomas.beermann@cern.ch> , 2012
+
+class RucioException(Exception):
+    """
+    To correctly use this class, inherit from it and define
+    a 'message' property. That message will get printf'd
+    with the keyword arguments provided to the constructor.
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.message = "An unknown exception occurred"
+        try:
+            self._error_string = self.message % kwargs
+        except Exception:
+            # at least get the core message out if something happened
+            self._error_string = self.message
+        if len(args) > 0:
+            # If there is a non-kwarg parameter, assume it's the error
+            # message or reason description and tack it on to the end
+            # of the exception message
+            # Convert all arguments into their string representations...
+            args = ["%s" % arg for arg in args]
+            self._error_string = (self._error_string +
+                                  "\nDetails: %s" % '\n'.join(args))
+
+    def __str__(self):
+        return self._error_string
+
+
+class NotFound(RucioException):
+    message = "An object with the specified identifier was not found."
+
+
+class Duplicate(RucioException):
+    message = "An object with the same identifier already exists."
