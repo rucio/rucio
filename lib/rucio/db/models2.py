f9e8ebbdf7e5e27af3b21060b1e1e10b8d8a0588
Martin Barisits <martin.barisits@cern.ch>
2012-03-12 16:55:37 +0100

    Conflict resolved (hopefully)
    Change-Id: I0b09e6f1649e4767edd4b5bfc3a18638e9fcc1b9

--- lib/rucio/db/models2.py
+++ lib/rucio/db/models2.py
@@ -2,8 +2,7 @@
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # You may not use this file except in compliance with the License.
-# You may obtain a copy of the License at:
-# http://www.apache.org/licenses/LICENSE-2.0
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 #
 # Authors:
 # - Angelos Molfetas <angelos.molfetas@cern.ch>, 2011
