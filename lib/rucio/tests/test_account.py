a5b0e7874c7b8c635f1abf4b86eff8fe9388d73b
Thomas Beermann <thomas.beermann@cern.ch>
2012-03-14 08:23:38 +0100

    merged conflicts
    Change-Id: I338d4f055331a03d57c4f47f53ed734d1fe3d103

--- lib/rucio/tests/test_account.py
+++ lib/rucio/tests/test_account.py
@@ -2,72 +2,82 @@
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # You may not use this file except in compliance with the License.
-# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Authors:
-# - Thomas Beermann,  <thomas.beermann@cern.ch> , 2012
+# - Thomas Beermann, <thomas.beermann@cern.ch>, 2012
 
+import json
 
-import datetime
-import random
-import logging
-import unittest
-
+from paste.fixture import TestApp
 from nose.tools import *
 
-from rucio.db import api
-from rucio.common import exception
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from rucio.db import models1 as models
+from rucio.web.server.account import app
 
 
-logger = logging.getLogger('rucio.test.test_account')
-sh = logging.StreamHandler()
-sh.setLevel(logging.DEBUG)
-logger.addHandler(sh)
-
-
-class TestAccountApi(unittest.TestCase):
-
-    """
-    Test cases for the account api
-    """
+class TestAccount():
 
     def setUp(self):
-        super(TestAccountApi, self).setUp()
-        api.configure_db()
-
-    def testCreateAccount(self):
-        """
-        tests that newly created accounts are added correctly to the database
-        """
-        test_accounts = []
-        test_accounts.append(('tbeerman', 'user'))
-        test_accounts.append(('vgaronne', 'user'))
-        test_accounts.append(('mlassnig', 'user'))
-
-        for account in test_accounts:
-            api.create_account(account[0], account[1])
-
-        for account in test_accounts:
-            test_account = api.get_account(account[0])
-            self.assertEquals(account[0], test_account.account)
-            self.assertEquals(account[1], test_account.type)
-
-    @raises(exception.Duplicate)
-    def testDuplicateException(self):
-        """
-        tests if DuplicateException is correctly thrown when adding an already existing account
-        """
-        account = api.list_accounts()[0]
-
-        api.create_account(account.account, account.type)
-
-    @raises(exception.NotFound)
-    def testNotFoundException(self):
-        """
-        tests if NotFoundException is correclty thrown when querying for a not existing account
-        """
-        api.get_account('some_random_account')
+        engine = create_engine('sqlite:////tmp/rucio.db', echo=True)
+        models.register_models(engine)
 
     def tearDown(self):
-        """Clear the test environment"""
-        super(TestAccountApi, self).tearDown()
+        engine = create_engine('sqlite:////tmp/rucio.db', echo=True)
+        models.unregister_models(engine)
+
+    def test_create_user_success(self):
+        """ send a POST to create a new user """
+        mw = []
+        headers = {'Rucio-Type': 'user'}
+        r = TestApp(app.wsgifunc(*mw)).post('/account/testuser', headers=headers, expect_errors=True)
+        assert_equal(r.status, 201)
+
+    def test_create_user_failure(self):
+        """ send a POST with an existing user to test the error """
+        mw = []
+        headers = {'Rucio-Type': 'user'}
+        r1 = TestApp(app.wsgifunc(*mw)).post('/account/testuser', headers=headers, expect_errors=True)
+        r2 = TestApp(app.wsgifunc(*mw)).post('/account/testuser', headers=headers, expect_errors=True)
+        assert_equal(r2.status, 500)
+
+    def test_get_user_success(self):
+        """ send a GET to retrieve the infos of the new user """
+        mw = []
+        headers1 = {'Rucio-Type': 'user'}
+        headers2 = {}
+        r1 = TestApp(app.wsgifunc(*mw)).post('/account/testuser', headers=headers1, expect_errors=True)
+        r2 = TestApp(app.wsgifunc(*mw)).get('/account/testuser', headers=headers2, expect_errors=True)
+        body = json.loads(r2.body)
+        assert_equal(body['account'], 'testuser')
+        assert_equal(r2.status, 200)
+
+    def test_get_user_failure(self):
+        """ send a GET with a wrong user test the error """
+        mw = []
+        headers2 = {}
+        r = TestApp(app.wsgifunc(*mw)).get('/account/wronguser', headers=headers2, expect_errors=True)
+        assert_equal(r.status, 500)
+
+    def test_del_user_success(self):
+        """ send a DELETE to disable the new user """
+        mw = []
+        headers1 = {'Rucio-Type': 'user'}
+        headers2 = {}
+        r1 = TestApp(app.wsgifunc(*mw)).post('/account/testuser', headers=headers1, expect_errors=True)
+        r2 = TestApp(app.wsgifunc(*mw)).delete('/account/testuser', headers=headers2, expect_errors=True)
+        r3 = TestApp(app.wsgifunc(*mw)).get('/account/testuser', headers=headers2, expect_errors=True)
+        body = json.loads(r3.body)
+        assert_true(body['deleted'])
+        assert_equal(r2.status, 200)
+
+    def test_del_user_failure(self):
+        """ send a DELETE with a wrong user to test the error """
+        mw = []
+        headers = {}
+        r = TestApp(app.wsgifunc(*mw)).delete('/account/wronguser', headers=headers, expect_errors=True)
+        assert_equal(r.status, 500)
