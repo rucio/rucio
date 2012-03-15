0c7605f3b3b9d0790fcb2dfd855518f814ce2aa6
Mario Lassnig <mario.lassnig@cern.ch>
2012-03-15 17:18:44 +0100

    deliberate failure to test automated build checker, try 5
    Change-Id: Iaf96219053ca0f0186d78d8c5cd3aa80cebdc64b

--- lib/rucio/web/rest/authentication.py
+++ lib/rucio/web/rest/authentication.py
@@ -8,7 +8,7 @@
 # Authors:
 # - Mario Lassnig, <mario.lassnig@cern.ch>, 2012
 
-import webbbbb
+import webbbbbb
 
 from rucio.api.authentication import get_auth_token_user_pass
 from rucio.api.authentication import get_auth_token_kerberos
