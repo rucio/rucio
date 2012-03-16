5e454988d7f7b8e27b841e5a085618f2b6203d7a
Vincent Garonne <vgaronne@gmail.com>
2012-03-16 10:22:25 +0100

    fixed uuid
    Change-Id: If8b332aae91c9ec3d259c65dfbb6d5a53cff2993
    Icf4339681ee812fc1978e624af4c94dc5afa6aa0

---
+++ lib/rucio/common/utils.py
@@ -0,0 +1,18 @@
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Vincent Garonne, <vincent.garonne@cern.ch>, 2012
+
+"""
+Rucio utilities.
+"""
+
+import uuid
+
+
+def generate_uuid():
+    return str(uuid.uuid4()).replace('-', '').lower()
