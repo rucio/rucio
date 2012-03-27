9b4ffc906da88b1d0d422c85910285c05a736b08
Vincent Garonne <vgaronne@gmail.com>
2012-03-27 21:47:08 +0200

    First iteration on CLI&Client implementation
    Change-Id: I313d371e4a9cba5bd273c79954aa1a884104d1ac

---
+++ lib/rucio/tests/test_bin_rucio.py
@@ -0,0 +1,30 @@
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Vincent Garonne, <vincent.garonne@cern.ch>, 2012
+
+from paste.fixture import TestApp
+from nose.tools import *
+
+from rucio import version
+from rucio.tests.utils import execute
+
+
+class TestBinRucio():
+
+    def setUp(self):
+        # setup http
+        pass
+
+    def tearDown(self):
+        # teardown http
+        pass
+
+    def test_rucio_version(self):
+        cmd = 'bin/rucio --version'
+        exitcode, out, err = execute(cmd)
+        assert_equal(out, 'rucio %s\n' % version.version_string())
---
+++ lib/rucio/tests/test_clients.py
@@ -0,0 +1,26 @@
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Vincent Garonne, <vincent.garonne@cern.ch>, 2012
+
+from paste.fixture import TestApp
+from nose.tools import *
+
+from rucio import client
+
+
+class TestRucioClient():
+
+    def setUp(self):
+        self.client = client.Client("0.0.0.0")
+
+    def tearDown(self):
+        # teardown http
+        pass
+
+    def test_add_dataset(self):
+        self.client.add_dataset()
