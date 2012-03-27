28cf15ae1c44e9ff7c7a35adb4d685f948334c14
Vincent Garonne <vgaronne@gmail.com>
2012-03-27 21:47:08 +0200

    First iteration on CLI&Client implementation
    Change-Id: I313d371e4a9cba5bd273c79954aa1a884104d1ac

---
+++ lib/rucio/tests/utils.py
@@ -0,0 +1,38 @@
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Vincent Garonne, <vincent.garonne@cern.ch>, 2012
+
+"""Common utilities used in testing"""
+
+import subprocess
+
+
+def execute(cmd):
+    """
+    Executes a command in a subprocess. Returns a tuple
+    of (exitcode, out, err), where out is the string output
+    from stdout and err is the string output from stderr when
+    executing the command.
+
+    :param cmd: Command string to execute
+    """
+
+    process = subprocess.Popen(cmd,
+                               shell=True,
+                               stdin=subprocess.PIPE,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    out = ''
+    err = ''
+    exitcode = 0
+
+    result = process.communicate()
+    (out, err) = result
+    exitcode = process.returncode
+
+    return exitcode, out, err
