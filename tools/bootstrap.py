2346f90d383017c6debeb8ebaf970fbaa61c4fb4
Mario Lassnig <mario.lassnig@cern.ch>
2012-06-20 15:15:29 +0200

    Bootstrap the database

---
+++ tools/bootstrap.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Mario Lassnig, <mario.lassnig@cern.ch>, 2012
+
+# Run this once to set up the database.
+#   PYTHONPATH=/opt/rucio/.venv/lib/python2.7/site-packages/rucio python tools/bootstrap.py
+#
+# Verify for default SQLite:
+#   for i in `sqlite3 /tmp/rucio.db ".tables"`; do echo $i:; sqlite3 /tmp/rucio.db "select * from $i"; echo; done
+
+from rucio.db.session import build_database, create_root_account
+
+if __name__ == '__main__':
+    build_database()
+    create_root_account()
