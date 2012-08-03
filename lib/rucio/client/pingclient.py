0035d5bdb31bb54752a6eac222815379cbfad071
Vincent Garonne <vincent.garonne@cern.ch>
2012-08-03 19:01:26 +0200

    few fixes
    Change-Id: I1f8ed97384a34cca5863673e5a5950f783be78bf

--- lib/rucio/client/pingclient.py
+++ lib/rucio/client/pingclient.py
@@ -5,7 +5,7 @@
 # You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 #
 # Authors:
-# - Thomas Beermann, <thomas.beermann@cern.ch>, 2012
+# - Vincent Garonne, <vincent.garonne@cern.ch>, 2012
 
 from json import loads
 from requests import get
