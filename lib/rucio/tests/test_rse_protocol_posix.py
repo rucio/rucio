e7ac29f086b5a74bd26afc0ac13cc4994890bd72
Ralph Vigne <ralph.vigne@cern.ch>
2012-09-27 15:27:14 +0200

    download now works with LFNs and PFNs ; rebase/merge ; scope is now used to create separate folder when downloading files
    Change-Id: Ieb96146895777b69152116a49042515f87fee8b3

--- lib/rucio/tests/test_rse_protocol_posix.py
+++ lib/rucio/tests/test_rse_protocol_posix.py
@@ -20,7 +20,7 @@ from rucio.rse import rsemanager
 from rsemgr_api_test import MgrTestCases
 
 
-class xTestRsePOSIX():
+class TestRsePOSIX():
     tmpdir = None
 
     @classmethod
@@ -41,10 +41,10 @@ class xTestRsePOSIX():
         os.mkdir(prefix)
         os.system('dd if=/dev/urandom of=%s/data.raw bs=1024 count=1024' % prefix)
         for f in MgrTestCases.files_remote:
-            #path =  storage.lfn2uri({'filename': f, 'scope': 'test'})
-            #dirs = os.path.dirname(path)
-            #if not os.path.exists(dirs):
-            #    os.makedirs(dirs)
+            path = storage.lfn2uri({'filename': f, 'scope': 'test'})
+            dirs = os.path.dirname(path)
+            if not os.path.exists(dirs):
+                os.makedirs(dirs)
             shutil.copy('%s/data.raw' % prefix, storage.lfn2uri({'filename': f, 'scope': 'test'}))
 
     @classmethod
@@ -62,26 +62,35 @@ class xTestRsePOSIX():
 
     # Mgr-Tests: GET
     def test_multi_get_mgr_ok(self):
-        """POSIX (RSE/PROTOCOLS): Get multiple files from storage (Success)"""
+        """POSIX (RSE/PROTOCOLS): Get multiple files from storage providing LFNs and PFNs (Success)"""
         self.mtc.test_multi_get_mgr_ok()
 
-    def test_get_mgr_ok_single(self):
-        """POSIX (RSE/PROTOCOLS): Get a single file from storage (Success)"""
-        self.mtc.test_get_mgr_ok_single()
+    def test_get_mgr_ok_single_lfn(self):
+        """POSIX (RSE/PROTOCOLS): Get a single file from storage providing LFN (Success)"""
+        self.mtc.test_get_mgr_ok_single_lfn()
+
+    def test_get_mgr_ok_single_pfn(self):
+        """POSIX (RSE/PROTOCOLS): Get a single file from storage providing PFN (Success)"""
+        self.mtc.test_get_mgr_ok_single_pfn()
 
     @raises(exception.SourceNotFound)
     def test_get_mgr_SourceNotFound_multi(self):
-        """POSIX (RSE/PROTOCOLS): Get multiple files from storage (SourceNotFound)"""
+        """POSIX (RSE/PROTOCOLS): Get multiple files from storage providing LFNs and PFNs (SourceNotFound)"""
         self.mtc.test_get_mgr_SourceNotFound_multi()
 
     @raises(exception.SourceNotFound)
-    def test_get_mgr_SourceNotFound_single(self):
-        """POSIX (RSE/PROTOCOLS): Get a single file from storage (SourceNotFound)"""
-        self.mtc.test_get_mgr_SourceNotFound_single()
+    def test_get_mgr_SourceNotFound_single_lfn(self):
+        """POSIX (RSE/PROTOCOLS): Get a single file from storage providing LFN (SourceNotFound)"""
+        self.mtc.test_get_mgr_SourceNotFound_single_lfn()
+
+    @raises(exception.SourceNotFound)
+    def test_get_mgr_SourceNotFound_single_pfn(self):
+        """POSIX (RSE/PROTOCOLS): Get a single file from storage providing PFN (SourceNotFound)"""
+        self.mtc.test_get_mgr_SourceNotFound_single_pfn()
 
     # Mgr-Tests: PUT
     def test_put_mgr_ok_multi(self):
-        """POSIX (RSE/PROTOCOLS): Put multiple files to storage (Success)"""
+        """POSIX (RSE/PROTOCOLS): Put multiple files to storage providing LFNs and PFNs (Success)"""
         self.mtc.test_put_mgr_ok_multi()
 
     def test_put_mgr_ok_single(self):
