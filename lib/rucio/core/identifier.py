2435ea19d917a51f6a57913682f748b755344675
Mario Lassnig <mario.lassnig@cern.ch>
2012-11-21 17:38:24 +0100

    fix for wrong child_type in DID aggregation ; removed print statement ; renamed variables
    Change-Id: I443d99917f176b1fb83172005cd65dffd849d911

--- lib/rucio/core/identifier.py
+++ lib/rucio/core/identifier.py
@@ -19,39 +19,39 @@ from rucio.db.session import get_session
 session = get_session()
 
 
-def list_replicas(scope, name):
+def list_replicas(scope, did):
     """
-    List file replicas for a did.
+    List file replicas for a data identifier.
 
     :param scope: The scope name.
-    :param dsn: The name.
+    :param did: The data identifier.
 
     """
-    query = session.query(models.DataIdentifier).filter_by(scope=scope, name=name, deleted=False)
+    query = session.query(models.DataIdentifier).filter_by(scope=scope, did=did, deleted=False)
     try:
         did = query.one()
     except NoResultFound:
-        raise exception.DataIdentifierNotFound("Data identifier '%(scope)s:%(name)s' not found" % locals())
+        raise exception.DataIdentifierNotFound("Data identifier '%(scope)s:%(did)s' not found" % locals())
 
     if did.type == models.DataIdType.FILE:
-        files = [(scope, name), ]
+        files = [(scope, did), ]
     else:
         files = list()
-        dids = [(scope, name), ]
+        dids = [(scope, did), ]
         while dids:
             s, n = dids.pop()
-            query = session.query(models.DataIdentifierAssociation).filter_by(scope=s, name=n, deleted=False)
-            for did in query:
-                if did.child_type == models.DataIdType.FILE:
-                    file = (did.child_scope, did.child_name)
+            query = session.query(models.DataIdentifierAssociation).filter_by(scope=s, did=n, deleted=False)
+            for tmp_did in query:
+                if tmp_did.child_type == models.DataIdType.FILE:
+                    file = (tmp_did.child_scope, tmp_did.child_name)
                     if file not in files:
                         files.append(file)
                 else:
-                    dids.append((did.child_scope, did.child_name))
+                    dids.append((tmp_did.child_scope, tmp_did.child_name))
 
     replicas = {}
     for s, n in files:
-        query = session.query(models.RSEFileAssociation).filter_by(scope=s, name=n, deleted=False)
+        query = session.query(models.RSEFileAssociation).filter_by(scope=s, did=n, deleted=False)
         rows = list()
         for replica in query:
             rows.append({'size': replica.size, 'rse': replica.rse.rse, 'checksum': replica.checksum, 'pfn': replica.pfn})
@@ -64,98 +64,100 @@ def add_identifier(scope, did, sources, issuer):
     Add dataset/container
 
     :param scope: The scope name.
-    :param name: The name.
+    :param did: The data identifier.
     :param sources: The content.
     :param issuer: The issuer account.
-
     """
+
     data_type = None
-    # Check content
+
+    # Get the correct child data type
+    #
+    # TODO: Disallow putting files into containers
+    #
     for source in sources:
         query = session.query(models.DataIdentifier).filter_by(scope=source['scope'], did=source['did'], deleted=False)
         try:
             tmp_did = query.one()
             if tmp_did.type == models.DataIdType.FILE:
-                tmp_data_type = models.DataIdType.DATASET
-            elif tmp_did.type == models.DataIdType.DATASET or tmp_did.type == models.DataIdType.CONTAINER:
-                tmp_data_type = models.DataIdType.CONTAINER
+                source['type'] = models.DataIdType.FILE
+                data_type = models.DataIdType.DATASET
+            elif tmp_did.type == models.DataIdType.DATASET:
+                source['type'] = models.DataIdType.DATASET
+                data_type = models.DataIdType.CONTAINER
+            elif tmp_did.type == models.DataIdType.CONTAINER:
+                source['type'] = models.DataIdType.CONTAINER
+                data_type = models.DataIdType.CONTAINER
 
-            if not data_type:
-                data_type = tmp_data_type
-            elif tmp_data_type != tmp_data_type:
-                # To change
-                raise exception.DataIdentifierNotFound("Can not mixed data identifiers" % locals())
         except NoResultFound:
             raise exception.DataIdentifierNotFound("Data identifier '%(scope)s:%(did)s' not found" % source)
 
-    # Insert dataset/container
+    # Insert new data identifier with correct type
     new_did = models.DataIdentifier(scope=scope, did=did, owner=issuer, type=data_type)
     try:
         new_did.save(session=session)
     except IntegrityError, e:
-        # needs to parse the exception string
-        print e
         session.rollback()
+        raise e
 
-    # Insert content
+    # Insert content with correct type
     for source in sources:
         try:
-            new_child = models.DataIdentifierAssociation(scope=scope, did=did, child_scope=source['scope'], child_did=source['did'], type=data_type, child_type=did.type)
+            new_child = models.DataIdentifierAssociation(scope=scope, did=did, child_scope=source['scope'], child_did=source['did'], type=data_type, child_type=source['type'])
             new_child.save(session=session)
         except IntegrityError, e:
-           # needs to parse the exception string
-            print e
             session.rollback()
+            raise e
 
     session.commit()
 
 
-def list_content(scope, name):
+def list_content(scope, did):
     """
     List dataset/container contents.
 
     :param scope: The scope name.
-    :param dsn: The name.
+    :param did: The data identifier.
 
     """
-    query = session.query(models.DataIdentifier).filter_by(scope=scope, name=name, deleted=False)
+    query = session.query(models.DataIdentifier).filter_by(scope=scope, did=did, deleted=False)
     try:
-        did = query.one()
+        query.one()
     except NoResultFound:
-        raise exception.DataIdentifierNotFound("Data identifier '%(scope)s:%(name)s' not found" % locals())
+        raise exception.DataIdentifierNotFound("Data identifier '%(scope)s:%(did)s' not found" % locals())
 
     dids = list()
-    query = session.query(models.DataIdentifierAssociation).filter_by(scope=scope, name=name, deleted=False)
-    for did in query:
-        dids.append({'name': did.child_name, 'scope': did.child_scope})
+    query = session.query(models.DataIdentifierAssociation).filter_by(scope=scope, did=did, deleted=False)
+    for tmp_did in query:
+        dids.append({'name': tmp_did.child_name, 'scope': tmp_did.child_scope})
     return dids
 
 
-def list_files(scope, name):
+def list_files(scope, did):
     """
     List container/dataset file contents.
 
     :param scope: The scope name.
-    :param dsn: The name.
+    :param did: The data identifier.
 
     """
-    query = session.query(models.DataIdentifier).filter_by(scope=scope, name=name, deleted=False)
+    query = session.query(models.DataIdentifier).filter_by(scope=scope, did=did, deleted=False)
     try:
         did = query.one()
     except NoResultFound:
-        raise exception.DataIdentifierNotFound("Data identifier '%(scope)s:%(name)s' not found" % locals())
+        raise exception.DataIdentifierNotFound("Data identifier '%(scope)s:%(did)s' not found" % locals())
 
     files = list()
-    dids = [(scope, name), ]
+    dids = [(scope, did), ]
     while dids:
         s, n = dids.pop()
-        query = session.query(models.DataIdentifierAssociation).filter_by(scope=s, name=n, deleted=False)
-        for did in query:
-            if did.child_type == models.DataIdType.FILE:
-                file = {'scope': did.child_scope, 'name': did.child_name}
+        query = session.query(models.DataIdentifierAssociation).filter_by(scope=s, did=n, deleted=False)
+        for tmp_did in query:
+            if tmp_did.child_type == models.DataIdType.FILE:
+                file = {'scope': tmp_did.child_scope, 'name': tmp_did.child_name}
                 if file not in files:
                     files.append(file)
             else:
-                dids.append((did.child_scope, did.child_name))
+                dids.append((tmp_did.child_scope, tmp_did.child_name))
 
     return files
