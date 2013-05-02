be7f3696d79fd7cdfe4963604d62581866e8989d
Luis Rodrigues <lfrodrigues@gmail.com>
2013-05-02 11:21:12 +0200

    initial monitor implementation ; [RUCIO-267] reordered imports
    Change-Id: Ifbdd41b26070c24817d00b95777c3390599d036d

---
+++ lib/rucio/core/monitor.py
@@ -0,0 +1,28 @@
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Luis Rodrigues, <luis.rodrigues@cern.ch>, 2013
+
+from pystatsd import Client
+
+from rucio.common.config import config_get
+
+server = config_get('monitor', 'carbon_server')
+port = config_get('monitor', 'carbon_port')
+scope = config_get('monitor', 'user_scope')
+pystatsd_client = Client(host=server, port=port, prefix=scope)
+
+
+def record(timeseries, delta=1):
+    """
+    Updates one or more timeseries counters by arbitrary amounts
+
+    :param timeseries: The timeseries or a list of timeseries to be updated.
+    :param delta: The increment for the timeseries, by default increment by 1.
+    """
+    pystatsd_client.update_stats(timeseries, delta)
