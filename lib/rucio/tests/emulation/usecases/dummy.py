9fd1992e7402be1bcb94601c2e9df3d3c1b807d3
Ralph Vigne <ralph.vigne@cern.ch>
2013-05-27 21:59:04 +0200

    [RUCIO-287] emulation supports now runtime configuration changes; no more time frame support; ; runtime changes of configuration update interval
    Change-Id: Ia7dc9b55a62ab7b3df606497af36035530bd8241

---
+++ lib/rucio/tests/emulation/usecases/dummy.py
@@ -0,0 +1,50 @@
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#              http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Ralph Vigne, <ralph.vigne@cern.ch>, 2013
+# - Vincent Garonne, <vincent.garonne@cern.ch>, 2013
+
+from rucio.tests.emulation.ucemulator import UCEmulator
+
+
+class UseCaseDefinition(UCEmulator):
+    """
+        Implements all TZero use cases.
+    """
+
+    @UCEmulator.UseCase
+    def UC1(self):
+        """
+            Registers a new empty dataset using the add_identifier method.
+
+            :param tse: time series element of the current time frame
+        """
+        print 'Perform UA1'
+
+    @UCEmulator.UseCase
+    def UC2(self):
+        """
+            Registers file replicas for a dataset. The number of files is provided
+            as 'no_of_files' in the tse object. This number applied to a gauss-distribution
+            function to derive the actual number of files added to the dataset.
+
+        """
+        #self.time_it(self.some_method, kwargs={'arg1': some_other_information, 'arg2': some_more})
+        print 'Perform UC2'
+        pass
+
+    def setup(self, ctx):
+        """
+            Sets up shared information/objects between the use cases and creates between one
+            and ten empty datasets for the UC_TZ_REGISTER_APPEND use case.
+
+            :param cfg: the context of etc/emulation.cfg
+        """
+        print '\tSetup cfg - param'
+        ctx.run = 0
+        print '\t%s' % ctx
