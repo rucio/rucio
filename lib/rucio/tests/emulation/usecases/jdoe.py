82bd1a580689f4f8de33141265224dd7f2ae9c0f
Thomas Beermann <thomas.beermann@cern.ch>
2013-07-12 15:20:50 +0200

    wildcard and metadata search within the emulation framework
    Change-Id: Iec4e972ca1a66c1af9e3c07e201b554364e4c2dc

---
+++ lib/rucio/tests/emulation/usecases/jdoe.py
@@ -0,0 +1,122 @@
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#              http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Thomas Beermann, <thomas.beermann@cern.ch>, 2013
+
+from json import loads
+from random import uniform
+from time import time
+
+from rucio.client.didclient import DIDClient
+from rucio.core import monitor
+from rucio.tests.emulation.ucemulator import UCEmulator
+
+
+def random_pick(some_list, probabilities):
+    x = uniform(0, 1)
+    cumulative_probability = 0.0
+    for item, item_probability in zip(some_list, probabilities):
+        cumulative_probability += item_probability
+        if x < cumulative_probability:
+            break
+    return item
+
+
+class UseCaseDefinition(UCEmulator):
+    """
+        Implements Jdoe use cases.
+    """
+
+    @UCEmulator.UseCase
+    def LIST_DIDS_METADATA(self, scope, metadata):
+        jdoe_account = 'jdoe'
+        client = DIDClient(account=jdoe_account)
+
+        print 'run with: ' + str(metadata)
+
+        start = time()
+        with monitor.record_timer_block('jdoe.list_dids_metadata'):
+            dids = [did for did in client.list_dids(scope=scope, filters=metadata, type='dataset')]
+
+        duration = time() - start
+        cnt = len(dids)
+        print 'got %d dids' % cnt
+
+        monitor.record_counter('jdoe.list_dids_metadata.num_results', cnt)
+        if cnt != 0:
+            monitor.record_counter('jdoe.list_dids_metadata.time_per_did', duration / cnt)
+
+        return {'no_datasets': cnt}
+
+    def LIST_DIDS_METADATA_input(self, ctx):
+        scope = random_pick(ctx.scopes, ctx.scope_probs)
+
+        metadata = ctx.metadata[scope][ctx.metadata_counter[scope]]
+        ctx.metadata_counter[scope] = (ctx.metadata_counter[scope] + 1) % ctx.metadata_len[scope]
+
+        scope = metadata['project']
+        metadata.pop('name', None)
+
+        print 'metadata picked: ' + str(metadata)
+
+        return {'scope': scope,
+                'metadata': metadata}
+
+    def LIST_DIDS_WILDCARD(self, scope, wildcard):
+        jdoe_account = 'jdoe'
+        client = DIDClient(account=jdoe_account)
+
+        print 'run with: ' + str(wildcard)
+        start = time()
+        with monitor.record_timer_block('jdoe.list_dids_wildcard'):
+            dids = [did for did in client.list_dids(scope=scope, filters=wildcard, type='dataset')]
+
+        duration = time() - start
+        cnt = len(dids)
+        print 'got %d dids' % cnt
+
+        monitor.record_counter('jdoe.list_dids_wildcard.num_results', cnt)
+        if cnt != 0:
+            monitor.record_counter('jdoe.list_dids_wildcard.time_per_did', duration / cnt)
+
+        return {'no_datasets': cnt}
+
+    def LIST_DIDS_WILDCARD_input(self, ctx):
+        scope = random_pick(ctx.scopes, ctx.scope_probs)
+
+        metadata = ctx.metadata[scope][ctx.metadata_counter[scope]]
+        ctx.metadata_counter[scope] = (ctx.metadata_counter[scope] + 1) % ctx.metadata_len[scope]
+
+        scope = metadata['project']
+        wildcard = {}
+        wildcard['name'] = metadata['name']
+
+        print 'wildcard picked: ' + str(wildcard)
+
+        return {'scope': scope,
+                'wildcard': wildcard}
+
+    def setup(self, ctx):
+        ctx.metadata = {}
+        ctx.metadata_len = {}
+        ctx.metadata_counter = {}
+        print ctx
+        for scope in ctx.scopes:
+            ctx.metadata[scope] = []
+            ctx.metadata_counter[scope] = 0
+            input_file = open(ctx.input_files[scope], 'r')
+            cnt = 0
+            for line in input_file:
+                metadata = loads(line.strip())
+                ctx.metadata[scope].append(metadata)
+                cnt += 1
+            ctx.metadata_len[scope] = cnt
+
+        print 'Jdoe emulation starting'
+        for scope in ctx.scopes:
+            print '%s: %d patterns' % (scope, ctx.metadata_len[scope])
