bd71c60c96bc5f57c89aa2a104a6c287894e0c37
WeiJen Chang <e4523744@gmail.com>
2013-07-22 15:36:05 +0000

    added xrootd protocol support ; a new version without using pyxrootd because of some bugs
    Change-Id: I3997f3de5f622d0aec783ae94b2d79943e0ba353

---
+++ lib/rucio/rse/protocols/xrootd.py
@@ -0,0 +1,220 @@
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - WeiJen Chang, <wchang@cern.ch>, 2013
+
+import os
+
+from rucio.common import exception
+from rucio.rse.protocols import protocol
+from urlparse import urlparse
+from rucio.common.utils import execute
+
+
+class Default(protocol.RSEProtocol):
+    """ Implementing access to RSEs using the XRootD protocol."""
+
+    def __init__(self, props):
+        """ Initializes the object with information about the referred RSE.
+
+            :param props Properties derived from the RSE Repository
+        """
+        self.rse = props
+        self.scheme = self.rse['scheme']
+        self.hostname = self.rse['hostname']
+        self.port = str(self.rse['port'])
+
+    def get_path(self, lfn, scope):
+        """ Transforms the physical file name into the local URI in the referred RSE.
+            Suitable for sites implementoing the RUCIO naming convention.
+
+            :param lfn: filename
+            :param scope: scope
+
+            :returns: RSE specific URI of the physical file
+        """
+        return '%s/%s/%s' % (self.rse['prefix'], scope, lfn)
+
+    def path2pfn(self, path):
+        """
+            Returns a fully qualified PFN for the file referred by path.
+
+            :param path: The path to the file.
+
+            :returns: Fully qualified PFN.
+
+        """
+        if not path.startswith('xroot'):
+            return '%s://%s:%s/%s' % (self.scheme, self.hostname, self.port, path)
+        else:
+            return path
+
+    def exists(self, pfn):
+        """ Checks if the requested file is known by the referred RSE.
+
+            :param pfn Physical file name
+
+            :returns: True if the file exists, False if it doesn't
+
+            :raise  ServiceUnavailable
+        """
+        try:
+            cmd = 'xrdfs %s:%s stat %s' % (self.hostname, self.port, pfn)
+            status, out, err = execute(cmd)
+            if not status == 0:
+                return False
+        except Exception as e:
+            raise exception.ServiceUnavailable(e)
+
+        return True
+
+    def connect(self, credentials):
+        """ Establishes the actual connection to the referred RSE.
+
+            :param credentials Provides information to establish a connection
+                to the referred storage system. For S3 connections these are
+                access_key, secretkey, host_base, host_bucket, progress_meter
+                and skip_existing.
+
+            :raises RSEAccessDenied
+        """
+        try:
+            cmd = 'xrdfs %s:%s query stats %s:%s' % (self.hostname, self.port, self.hostname, self.port)
+            status, out, err = execute(cmd)
+            if not status == 0:
+                raise exception.RSEAccessDenied(err)
+        except Exception as e:
+            raise exception.RSEAccessDenied(e)
+
+    def close(self):
+        """ Closes the connection to RSE."""
+        pass
+
+    def get(self, pfn, dest):
+        """ Provides access to files stored inside connected the RSE.
+
+            :param pfn Physical file name of requested file
+            :param dest Name and path of the files when stored at the client
+
+            :raises DestinationNotAccessible, ServiceUnavailable, SourceNotFound
+        """
+        path = self.path2pfn(pfn)
+
+        if not self.exists(pfn):
+            raise exception.SourceNotFound()
+        try:
+            cmd = 'xrdcp -f %s %s' % (path, dest)
+            status, out, err = execute(cmd)
+            if not status == 0:
+                raise exception.RucioException(err)
+        except Exception as e:
+            raise exception.ServiceUnavailable(e)
+
+    def put(self, filename, target, source_dir):
+        """
+            Allows to store files inside the referred RSE.
+
+            :param source: path to the source file on the client file system
+            :param target: path to the destination file on the storage
+            :param source_dir: Path where the to be transferred files are stored in the local file system
+
+            :raises DestinationNotAccessible: if the destination storage was not accessible.
+            :raises ServiceUnavailable: if some generic error occured in the library.
+            :raises SourceNotFound: if the source file was not found on the referred storage.
+        """
+        source_url = '%s/%s' % (source_dir, filename)
+        path = self.path2pfn(target)
+
+        if not os.path.exists(source_url):
+            raise exception.SourceNotFound()
+        try:
+            cmd = 'xrdcp -f %s %s' % (source_url, path)
+            status, out, err = execute(cmd)
+            if not status == 0:
+                raise exception.RucioException(err)
+        except Exception as e:
+            raise exception.ServiceUnavailable(e)
+
+    def delete(self, path):
+        """
+            Deletes a file from the connected RSE.
+
+            :param path: path to the to be deleted file
+
+            :raises ServiceUnavailable: if some generic error occured in the library.
+            :raises SourceNotFound: if the source file was not found on the referred storage.
+        """
+        if not self.exists(path):
+            raise exception.SourceNotFound()
+        try:
+            cmd = 'xrdfs %s:%s rm %s' % (self.hostname, self.port, path)
+            status, out, err = execute(cmd)
+            if not status == 0:
+                raise exception.RucioException(err)
+        except Exception as e:
+            raise exception.ServiceUnavailable(e)
+
+    def rename(self, path, new_path):
+        """ Allows to rename a file stored inside the connected RSE.
+
+            :param path: path to the current file on the storage
+            :param new_path: path to the new file on the storage
+
+            :raises DestinationNotAccessible: if the destination storage was not accessible.
+            :raises ServiceUnavailable: if some generic error occured in the library.
+            :raises SourceNotFound: if the source file was not found on the referred storage.
+        """
+        if not self.exists(path):
+            raise exception.SourceNotFound()
+        try:
+            cmd = 'xrdfs %s:%s mv %s %s' % (self.hostname, self.port, path, new_path)
+            status, out, err = execute(cmd)
+            if not status == 0:
+                raise exception.RucioException(err)
+        except Exception as e:
+            raise exception.ServiceUnavailable(e)
+
+    def split_pfn(self, pfn):
+        """
+            Splits the given PFN into the parts known by the protocol. During parsing the PFN is also checked for
+            validity on the given RSE with the given protocol.
+
+            :param pfn: a fully qualified PFN
+
+            :returns: a dict containing all known parts of the PFN for the protocol e.g. scheme, hostname, port, prefix, path, filename
+
+            :raises RSEFileNameNotSupported: if the provided PFN doesn't match with the protocol settings
+        """
+        parsed = urlparse(pfn)
+        ret = dict()
+        ret['scheme'] = parsed.scheme
+        ret['hostname'] = parsed.netloc.partition(':')[0]
+        ret['port'] = int(parsed.netloc.partition(':')[2]) if parsed.netloc.partition(':')[2] != '' else 0
+        ret['path'] = parsed.path
+
+        # Protect against 'lazy' defined prefixes for RSEs in the repository
+        self.rse['prefix'] = '' if self.rse['prefix'] is None else self.rse['prefix']
+        if not self.rse['prefix'].startswith('/'):
+            self.rse['prefix'] = '/' + self.rse['prefix']
+        if not self.rse['prefix'].endswith('/'):
+            self.rse['prefix'] += '/'
+
+        if self.rse['hostname'] != ret['hostname']:
+            raise exception.RSEFileNameNotSupported('Invalid hostname: provided \'%s\', expected \'%s\'' % (ret['hostname'], self.rse['hostname']))
+        if self.rse['port'] != ret['port']:
+            raise exception.RSEFileNameNotSupported('Invalid port: provided \'%s\', expected \'%s\'' % (ret['port'], self.rse['port']))
+        if not ret['path'].startswith('/' + self.rse['prefix']):
+            raise exception.RSEFileNameNotSupported('Invalid prefix: provided \'%s\', expected \'%s\'' % ('/'.join(ret['path'].split('/')[0:len(self.rse['prefix'].split('/')) - 1]),
+                                                                                                          self.rse['prefix']))  # len(...)-1 due to the leading '/
+
+        # Spliting parsed.path into prefix, path, filename
+        ret['prefix'] = self.rse['prefix']
+        ret['path'] = ret['path'].partition(self.rse['prefix'])[2]
+        ret['filename'] = ret['path'].split('/')[-1]
+        ret['path'] = ret['path'].partition(ret['filename'])[0]
+        return ret
---
+++ lib/rucio/tests/test_rse_protocol_xrootd.py
@@ -0,0 +1,251 @@
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - WeiJen Chang, <wchang@cern.ch>, 2013
+
+import json
+import os
+import shutil
+import tempfile
+
+from uuid import uuid4 as uuid
+
+from nose.tools import raises
+
+from rucio.common import exception
+from rucio.rse import rsemanager
+from rsemgr_api_test import MgrTestCases
+from rucio.common.utils import execute
+
+
+class TestRseXROOTD():
+    tmpdir = None
+    user = None
+
+    @classmethod
+    def setupClass(cls):
+        """XROOTD (RSE/PROTOCOLS): Creating necessary directories and files """
+        # Creating local files
+        cls.tmpdir = tempfile.mkdtemp()
+        cls.user = uuid()
+
+        with open("%s/data.raw" % cls.tmpdir, "wb") as out:
+            out.seek((1024 * 1024) - 1)  # 1 MB
+            out.write('\0')
+        for f in MgrTestCases.files_local:
+            shutil.copy('%s/data.raw' % cls.tmpdir, '%s/%s' % (cls.tmpdir, f))
+
+        storage = rsemanager.RSEMgr()
+        with open('etc/rse_repository.json') as f:
+            data = json.load(f)
+        prefix = data['WJ-XROOTD']['protocols']['supported']['xroot']['prefix']
+        hostname = data['WJ-XROOTD']['protocols']['supported']['xroot']['hostname']
+        port = data['WJ-XROOTD']['protocols']['supported']['xroot']['port']
+
+        try:
+            os.mkdir(prefix)
+        except Exception, e:
+            print e
+
+        os.system('dd if=/dev/urandom of=%s/data.raw bs=1024 count=1024' % prefix)
+        cls.static_file = 'xroot://%s:%d/%s/data.raw' % (hostname, port, prefix)
+        cmd = 'xrdcp %s/data.raw %s' % (prefix, cls.static_file)
+        execute(cmd)
+
+        for f in MgrTestCases.files_remote:
+            tmp = storage.parse_pfn('WJ-XROOTD', storage.lfn2pfn('WJ-XROOTD', {'filename': f, 'scope': 'user.%s' % cls.user}))
+            path = 'xroot://%s:%d/%s%s%s' % (tmp['hostname'], tmp['port'], tmp['prefix'], tmp['path'], tmp['filename'])
+            cmd = 'xrdcp %s/data.raw %s' % (prefix, path)
+            execute(cmd)
+
+    @classmethod
+    def teardownClass(cls):
+        """XROOTD (RSE/PROTOCOLS): Removing created directorie s and files"""
+        with open('etc/rse_repository.json') as f:
+            data = json.load(f)
+        prefix = data['WJ-XROOTD']['protocols']['supported']['xroot']['prefix']
+        hostname = data['WJ-XROOTD']['protocols']['supported']['xroot']['hostname']
+
+        shutil.rmtree(prefix)
+        shutil.rmtree(cls.tmpdir)
+
+        clean_raw = '%s/data.raw' % prefix
+        list_files_cmd_user = 'xrdfs %s ls %s/user.%s' % (hostname, prefix, cls.user)
+        clean_files = str(execute(list_files_cmd_user)[1]).split('\n')
+        list_files_cmd_group = 'xrdfs %s ls %s/group.%s' % (hostname, prefix, cls.user)
+        clean_files += str(execute(list_files_cmd_group)[1]).split('\n')
+        clean_files.append(clean_raw)
+        for files in clean_files:
+            clean_cmd = 'xrdfs %s rm %s' % (hostname, files)
+            execute(clean_cmd)
+
+        clean_prefix = '%s' % prefix
+        list_directory = 'xrdfs %s ls %s' % (hostname, prefix)
+        clean_directory = str(execute(list_directory)[1]).split('\n')
+        clean_directory.append(clean_prefix)
+        for directory in clean_directory:
+            clean_cmd = 'xrdfs %s rmdir %s' % (hostname, directory)
+            execute(clean_cmd)
+
+    def setup(self):
+        """XROOTD (RSE/PROTOCOLS): Creating Mgr-instance """
+        self.tmpdir = TestRseXROOTD.tmpdir
+        self.rse_id = 'WJ-XROOTD'
+        self.mtc = MgrTestCases(self.tmpdir, 'WJ-XROOTD', TestRseXROOTD.user, TestRseXROOTD.static_file)
+
+    # Mgr-Tests: GET
+    def test_multi_get_mgr_ok(self):
+        """XROOTD (RSE/PROTOCOLS): Get multiple files from storage providing LFNs and PFNs (Success)"""
+        self.mtc.test_multi_get_mgr_ok()
+
+    def test_get_mgr_ok_single_lfn(self):
+        """XROOTD (RSE/PROTOCOLS): Get a single file from storage providing LFN (Success)"""
+        self.mtc.test_get_mgr_ok_single_lfn()
+
+    def test_get_mgr_ok_single_pfn(self):
+        """XROOTD (RSE/PROTOCOLS): Get a single file from storage providing PFN (Success)"""
+        self.mtc.test_get_mgr_ok_single_pfn()
+
+    @raises(exception.SourceNotFound)
+    def test_get_mgr_SourceNotFound_multi(self):
+        """XROOTD (RSE/PROTOCOLS): Get multiple files from storage providing LFNs and PFNs (SourceNotFound)"""
+        self.mtc.test_get_mgr_SourceNotFound_multi()
+
+    @raises(exception.SourceNotFound)
+    def test_get_mgr_SourceNotFound_single_lfn(self):
+        """XROOTD (RSE/PROTOCOLS): Get a single file from storage providing LFN (SourceNotFound)"""
+        self.mtc.test_get_mgr_SourceNotFound_single_lfn()
+
+    @raises(exception.SourceNotFound)
+    def test_get_mgr_SourceNotFound_single_pfn(self):
+        """XROOTD (RSE/PROTOCOLS): Get a single file from storage providing PFN (SourceNotFound)"""
+        self.mtc.test_get_mgr_SourceNotFound_single_pfn()
+
+    # Mgr-Tests: PUT
+    def test_put_mgr_ok_multi(self):
+        """XROOTD (RSE/PROTOCOLS): Put multiple files to storage providing LFNs and PFNs (Success)"""
+        self.mtc.test_put_mgr_ok_multi()
+
+    def test_put_mgr_ok_single(self):
+        """XROOTD (RSE/PROTOCOLS): Put a single file to storage (Success)"""
+        self.mtc.test_put_mgr_ok_single()
+
+    @raises(exception.SourceNotFound)
+    def test_put_mgr_SourceNotFound_multi(self):
+        """XROOTD (RSE/PROTOCOLS): Put multiple files to storage (SourceNotFound)"""
+        self.mtc.test_put_mgr_SourceNotFound_multi()
+
+    @raises(exception.SourceNotFound)
+    def test_put_mgr_SourceNotFound_single(self):
+        """XROOTD (RSE/PROTOCOLS): Put a single file to storage (SourceNotFound)"""
+        self.mtc.test_put_mgr_SourceNotFound_single()
+
+    @raises(exception.FileReplicaAlreadyExists)
+    def test_put_mgr_FileReplicaAlreadyExists_multi(self):
+        """XROOTD (RSE/PROTOCOLS): Put multiple files to storage (FileReplicaAlreadyExists)"""
+        self.mtc.test_put_mgr_FileReplicaAlreadyExists_multi()
+
+    @raises(exception.FileReplicaAlreadyExists)
+    def test_put_mgr_FileReplicaAlreadyExists_single(self):
+        """XROOTD (RSE/PROTOCOLS): Put a single file to storage (FileReplicaAlreadyExists)"""
+        self.mtc.test_put_mgr_FileReplicaAlreadyExists_single()
+
+    # MGR-Tests: DELETE
+    def test_delete_mgr_ok_multi(self):
+        """XROOTD (RSE/PROTOCOLS): Delete multiple files from storage (Success)"""
+        self.mtc.test_delete_mgr_ok_multi()
+
+    def test_delete_mgr_ok_single(self):
+        """XROOTD (RSE/PROTOCOLS): Delete a single file from storage (Success)"""
+        self.mtc.test_delete_mgr_ok_single()
+
+    @raises(exception.SourceNotFound)
+    def test_delete_mgr_SourceNotFound_multi(self):
+        """XROOTD (RSE/PROTOCOLS): Delete multiple files from storage (SourceNotFound)"""
+        self.mtc.test_delete_mgr_SourceNotFound_multi()
+
+    @raises(exception.SourceNotFound)
+    def test_delete_mgr_SourceNotFound_single(self):
+        """XROOTD (RSE/PROTOCOLS): Delete a single file from storage (SourceNotFound)"""
+        self.mtc.test_delete_mgr_SourceNotFound_single()
+
+    # MGR-Tests: EXISTS
+    def test_exists_mgr_ok_multi(self):
+        """XROOTD (RSE/PROTOCOLS): Check multiple files on storage (Success)"""
+        self.mtc.test_exists_mgr_ok_multi()
+
+    def test_exists_mgr_ok_single_lfn(self):
+        """XROOTD (RSE/PROTOCOLS): Check a single file on storage using LFN (Success)"""
+        self.mtc.test_exists_mgr_ok_single_lfn()
+
+    def test_exists_mgr_ok_single_pfn(self):
+        """XROOTD (RSE/PROTOCOLS): Check a single file on storage using PFN (Success)"""
+        self.mtc.test_exists_mgr_ok_single_pfn()
+
+    def test_exists_mgr_false_multi(self):
+        """XROOTD (RSE/PROTOCOLS): Check multiple files on storage (Fail)"""
+        self.mtc.test_exists_mgr_false_multi()
+
+    def test_exists_mgr_false_single(self):
+        """XROOTD (RSE/PROTOCOLS): Check a single file on storage using LFN (Fail)"""
+        self.mtc.test_exists_mgr_false_single_lfn()
+
+    def test_exists_mgr_false_single_pfn(self):
+        """XROOTD (RSE/PROTOCOLS): Check a single file on storage using PFN (Fail)"""
+        self.mtc.test_exists_mgr_false_single_pfn()
+
+    # MGR-Tests: RENAME
+    def test_rename_mgr_ok_multi(self):
+        """XROOTD (RSE/PROTOCOLS): Rename multiple files on storage (Success)"""
+        self.mtc.test_rename_mgr_ok_multi()
+
+    def test_rename_mgr_ok_single_lfn(self):
+        """XROOTD (RSE/PROTOCOLS): Rename a single file on storage using LFN (Success)"""
+        self.mtc.test_rename_mgr_ok_single_lfn()
+
+    def test_rename_mgr_ok_single_pfn(self):
+        """XROOTD (RSE/PROTOCOLS): Rename a single file on storage using PFN (Success)"""
+        self.mtc.test_rename_mgr_ok_single_pfn()
+
+    @raises(exception.FileReplicaAlreadyExists)
+    def test_rename_mgr_FileReplicaAlreadyExists_multi(self):
+        """XROOTD (RSE/PROTOCOLS): Rename multiple files on storage (FileReplicaAlreadyExists)"""
+        self.mtc.test_rename_mgr_FileReplicaAlreadyExists_multi()
+
+    @raises(exception.FileReplicaAlreadyExists)
+    def test_rename_mgr_FileReplicaAlreadyExists_single_lfn(self):
+        """XROOTD (RSE/PROTOCOLS): Rename a single file on storage using LFN(FileReplicaAlreadyExists)"""
+        self.mtc.test_rename_mgr_FileReplicaAlreadyExists_single_lfn()
+
+    @raises(exception.FileReplicaAlreadyExists)
+    def test_rename_mgr_FileReplicaAlreadyExists_single_pfn(self):
+        """XROOTD (RSE/PROTOCOLS): Rename a single file on storage using PFN (FileReplicaAlreadyExists)"""
+        self.mtc.test_rename_mgr_FileReplicaAlreadyExists_single_pfn()
+
+    @raises(exception.SourceNotFound)
+    def test_rename_mgr_SourceNotFound_multi(self):
+        """XROOTD (RSE/PROTOCOLS): Rename multiple files on storage (SourceNotFound)"""
+        self.mtc.test_rename_mgr_SourceNotFound_multi()
+
+    @raises(exception.SourceNotFound)
+    def test_rename_mgr_SourceNotFound_single_lfn(self):
+        """XROOTD (RSE/PROTOCOLS): Rename a single file on storage using LFN (SourceNotFound)"""
+        self.mtc.test_rename_mgr_SourceNotFound_single_lfn()
+
+    @raises(exception.SourceNotFound)
+    def test_rename_mgr_SourceNotFound_single_pfn(self):
+        """XROOTD (RSE/PROTOCOLS): Rename a single file on storage using PFN (SourceNotFound)"""
+        self.mtc.test_rename_mgr_SourceNotFound_single_pfn()
+
+    def test_change_scope_mgr_ok_single_lfn(self):
+        """XROOTD (RSE/PROTOCOLS): Change the scope of a single file on storage using LFN (Success)"""
+        self.mtc.test_change_scope_mgr_ok_single_lfn()
+
+    def test_change_scope_mgr_ok_single_pfn(self):
+        """XROOTD (RSE/PROTOCOLS): Change the scope of a single file on storage using PFN (Success)"""
+        self.mtc.test_change_scope_mgr_ok_single_pfn()
