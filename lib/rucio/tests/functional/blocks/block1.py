a3e47dfe6e766db6c542685ce35bad88d525467f
Luis Rodrigues <luis.rodrigues@cern.ch>
2013-09-04 12:47:50 +0200

    initial implementation of functional test
    Change-Id: Id39b6a55534e41a5e80a06919c990930c085a38d

---
+++ lib/rucio/tests/functional/blocks/block1.py
@@ -0,0 +1,12 @@
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Luis Rodrigues, <luis.rodrigues@cern.ch>, 2013
+
+
+# client2.add_replica(rse_src, scope_name, did_name, did_bytes)
+# client2.add_replication_rule(dids=[{'scope': scope_name, 'name': did_name}], account=account_name, copies=2, rse_expression='%s|%s'%(rse1, rse2), grouping='NONE')
