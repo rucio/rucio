2db5f3f867b51b9b53f170c27f897b81a0dfc67f
Mario Lassnig <mario.lassnig@cern.ch>
2013-11-06 16:31:32 +0100

    transfer: loggify consumer
    Change-Id: I2a4d3a4ccd53c5193acab1ea7796eda4977a4bd8

--- lib/rucio/daemons/conveyor/consumer.py
+++ lib/rucio/daemons/conveyor/consumer.py
@@ -12,17 +12,23 @@
 Conveyor is a daemon to manage file transfers.
 """
 
-import datetime
+import logging
 import threading
 import time
 
 import json
 import stomp
 
-from rucio.db.constants import FTSState, RequestState
+from rucio.common.config import config_get, config_get_int
 from rucio.core.monitor import record_counter
 from rucio.core.request import set_request_state
-from rucio.common.config import config_get, config_get_int
+from rucio.db.constants import FTSState, RequestState
+
+logging.getLogger("requests").setLevel(logging.CRITICAL)
+
+logging.basicConfig(filename='%s/%s.log' % (config_get('common', 'logdir'), __name__),
+                    level=getattr(logging, config_get('common', 'loglevel').upper()),
+                    format='%(asctime)s\t%(process)d\t%(levelname)s\t%(message)s')
 
 graceful_stop = threading.Event()
 
@@ -34,16 +40,16 @@ class Consumer(object):
 
     def on_error(self, headers, message):
         record_counter('daemons.conveyor.consumer.error')
-        print '[%s %s] ERROR: %s' % (self.__broker, datetime.datetime.now(), message)
+        logging.error('[%s] %s' % (self.__broker, message))
 
     def on_message(self, headers, message):
-        print '[%s %s] MESSAGE: %s' % (self.__broker, datetime.datetime.now(), message['file_state'])
         record_counter('daemons.conveyor.consumer.message')
+        logging.debug('[%s] %s' % (self.__broker, message))
+
         msg = json.loads(message[:-1])  # message always ends with an unparseable EOT character
 
         if msg['job_metadata'] != '':
             if msg['job_state'] == FTSState.FINISHED:
-                print 'job finished'
                 set_request_state(msg['job_metadata']['request_id'], RequestState.DONE)
 
 
@@ -52,7 +58,7 @@ def consumer(once=False, process=0, total_processes=1, thread=0, total_threads=1
     Main loop to consume messages from the FTS3 producer.
     """
 
-    print 'consumer: starting'
+    logging.info('consumer starting')
 
     brokers = []
     try:
@@ -67,7 +73,7 @@ def consumer(once=False, process=0, total_processes=1, thread=0, total_threads=1
                                       ssl_key_file=config_get('messaging-fts3', 'ssl_key_file'),
                                       ssl_cert_file=config_get('messaging-fts3', 'ssl_cert_file')))
 
-    print 'consumer: started'
+    logging.info('consumer started')
 
     while not graceful_stop.is_set():
 
@@ -75,19 +81,17 @@ def consumer(once=False, process=0, total_processes=1, thread=0, total_threads=1
 
             if not conn.is_connected():
 
-                print 'consumer: connecting to', conn._Connection__host_and_ports[0][0]
+                logging.info('connecting to %s' % conn._Connection__host_and_ports[0][0])
                 record_counter('daemons.messaging.fts3.reconnect.%s' % conn._Connection__host_and_ports[0][0].split('.')[0])
 
                 conn.set_listener('rucio-messaging-fts3', Consumer(broker=conn._Connection__host_and_ports[0]))
                 conn.start()
                 conn.connect(headers={'client-id': 'rucio-messaging-fts3'}, wait=True)
-                conn.subscribe(destination=config_get('messaging-fts3', 'destination'),
-                               ack='auto',)
-                               #headers={'selector': 'vo = \'atlas\''})
+                conn.subscribe(destination=config_get('messaging-fts3', 'destination'), ack='auto')
 
         time.sleep(1)
 
-    print 'consumer: graceful stop requested'
+    logging.info('graceful stop requested')
 
     for conn in conns:
         try:
@@ -95,7 +99,7 @@ def consumer(once=False, process=0, total_processes=1, thread=0, total_threads=1
         except:
             pass
 
-    print 'consumer: graceful stop done'
+    logging.info('graceful stop done')
 
 
 def stop(signum=None, frame=None):
@@ -111,12 +115,12 @@ def run(once=False, process=0, total_processes=1, total_threads=1):
     Starts up the messenger threads
     """
 
-    print 'main: starting threads'
+    logging.info('starting consumer threads')
     threads = [threading.Thread(target=consumer, kwargs={'process': process, 'total_processes': total_processes, 'thread': i, 'total_threads': total_threads}) for i in xrange(0, total_threads)]
 
     [t.start() for t in threads]
 
-    print 'main: waiting for interrupts'
+    logging.info('main: waiting for interrupts')
 
     # Interruptible joins require a timeout.
     while len(threads) > 0:
