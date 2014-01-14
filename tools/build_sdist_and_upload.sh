7fa31c85edcd8d4fbe1090dbd36de1ece4ff6040
Vincent Garonne <vgaronne@gmail.com>
2014-01-14 15:59:33 +0100

    updated deps
    Change-Id: I7b5494c526991a4698386a881e5f9a38efd81513

--- tools/build_sdist_and_upload.sh
+++ tools/build_sdist_and_upload.sh
@@ -12,3 +12,4 @@
 python setup.py --release register -r http://atlas-pip.cern.ch/ sdist upload -r http://atlas-pip.cern.ch/
 /bin/cp setup_rucio_client.py setup.py
 python setup.py --release register -r http://atlas-pip.cern.ch/ sdist upload -r http://atlas-pip.cern.ch/
+/bin/cp setup_rucio.py setup.py
