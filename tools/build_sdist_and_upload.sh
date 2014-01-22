61e13809f7a297398531aecfddaa9ffe3ea64c9f
Vincent Garonne <vincent.garonne@cern.ch>
2014-01-22 15:22:41 +0100

    Removed sqla warnings ; small fixes;cleanup; updated del/add replica permission ; small fixes;cleanup; updated del/add replica permission ; small fixes;cleanup; updated del/add replica permission
    Change-Id: Ie9a37433a6e73b4da377777d75ae1b5f4eb942db

--- tools/build_sdist_and_upload.sh
+++ tools/build_sdist_and_upload.sh
@@ -6,12 +6,12 @@
 # You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 #
 # Authors:
-# Vincent Garonne, <vincent.garonne@cern.ch>, 2012
+# Vincent Garonne, <vincent.garonne@cern.ch>, 2012-2014
 
 /bin/cp setup_rucio.py setup.py
-python setup.py --release register sdist upload
-#python setup.py --release register -r http://atlas-pip.cern.ch/ sdist upload -r http://atlas-pip.cern.ch/
+#python setup.py --release register sdist upload
+python setup.py --release register -r http://atlas-pip.cern.ch/ sdist upload -r http://atlas-pip.cern.ch/
 /bin/cp setup_rucio_client.py setup.py
-python setup.py --release register sdist upload
-#python setup.py --release register -r http://atlas-pip.cern.ch/ sdist upload -r http://atlas-pip.cern.ch/
+#python setup.py --release register sdist upload
+python setup.py --release register -r http://atlas-pip.cern.ch/ sdist upload -r http://atlas-pip.cern.ch/
 /bin/cp setup_rucio.py setup.py
