193199269d2d952723573f9bf7d031628e6ec5f9
Mario Lassnig <mario.lassnig@cern.ch>
2014-02-13 16:08:59 +0100

    updated pip docs
    Change-Id: I86f6bef42f90f5460c631c23855a6942397d7a88

--- tools/build_sdist_and_upload.sh
+++ tools/build_sdist_and_upload.sh
@@ -7,11 +7,12 @@
 #
 # Authors:
 # Vincent Garonne, <vincent.garonne@cern.ch>, 2012-2014
+# Mario Lassnig, <mario.lassnig@cern.ch>, 2014
 
 /bin/cp setup_rucio.py setup.py
 python setup.py --release register sdist upload
-#python setup.py --release register -r http://atlas-pip.cern.ch/ sdist upload -r http://atlas-pip.cern.ch/
+#python setup.py --release register -r https://voatlasrucio-pip.cern.ch/ sdist upload -r https://voatlasrucio-pip.cern.ch/
 /bin/cp setup_rucio_client.py setup.py
 python setup.py --release register sdist upload
-#python setup.py --release register -r http://atlas-pip.cern.ch/ sdist upload -r http://atlas-pip.cern.ch/
+#python setup.py --release register -r https://voatlasrucio-pip.cern.ch/ sdist upload -r https://voatlasrucio-pip.cern.ch/
 /bin/cp setup_rucio.py setup.py
