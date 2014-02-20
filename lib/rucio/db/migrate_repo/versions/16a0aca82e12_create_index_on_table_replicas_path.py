fc2647a9a0c3667c45973cfcb651c5eafcca689f
Vincent Garonne <vgaronne@gmail.com>
2014-02-20 10:45:59 +0100

    Added alembic support for schema versioning ; What Mario said ; Added  etc/alembic.ini.template ; Upd. alembic version ; Restrutured repo
    Change-Id: If821f8862aa23f4a54d78f105f6995a87671837f

---
+++ lib/rucio/db/migrate_repo/versions/16a0aca82e12_create_index_on_table_replicas_path.py
@@ -0,0 +1,30 @@
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Vincent Garonne, <vincent.garonne@cern.ch>, 2014
+
+"""create index on table replicas(path)
+
+Revision ID: 16a0aca82e12
+Revises: None
+Create Date: 2014-02-20 09:35:24.044458
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '16a0aca82e12'
+down_revision = None
+
+from alembic import op
+
+
+def upgrade():
+    op.create_index('REPLICAS_PATH_IDX', 'replicas', ['path'])
+
+
+def downgrade():
+    op.drop_index('REPLICAS_PATH_IDX')
