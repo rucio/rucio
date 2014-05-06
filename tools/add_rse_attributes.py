3218e16eed56432048253f34084a35d4d37bf2e8
David Cameron <d.g.cameron@gmail.com>
2014-05-06 13:42:53 +0200

    collector tool to add RSE attributes
    Change-Id: Id5d5e89378125711f0b83e01f0ea8d438c1f85bc

---
+++ tools/add_rse_attributes.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#                       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Vincent Garonne, <vincent.garonne@cern.ch>, 2013-2014
+# - David Cameron, <david.cameron@cern.ch>, 2014
+
+import json
+import requests
+import sys
+
+from rucio.client import Client
+from rucio.common.exception import RucioException, RSENotFound
+
+UNKNOWN = 3
+CRITICAL = 2
+WARNING = 1
+OK = 0
+
+# Takes DDM endpoint information from AGIS and adds selected attributes to RSEs
+if __name__ == '__main__':
+
+    url = 'http://atlas-agis-api.cern.ch/request/ddmendpoint/query/list/?json'
+    resp = requests.get(url=url)
+    data = json.loads(resp.content)
+
+    c = Client()
+    for rse in data:
+
+        # Only use active endpoints in AGIS
+        if rse['state'] != 'ACTIVE':
+            continue
+
+        # Check if RSE exists
+        try:
+            c.get_rse(rse['name'])
+        except RSENotFound:
+            continue
+
+        print rse['name']
+
+        try:
+            c.add_rse_attribute(rse['name'], 'tier', str(rse['tier_level']))
+            c.add_rse_attribute(rse['name'], 'istape', str(rse['is_tape']))
+            c.add_rse_attribute(rse['name'], 'cloud', str(rse['cloud']))
+            c.add_rse_attribute(rse['name'], 'spacetoken', str(rse['token']))
+        except RucioException as e:
+            print str(e)
+
+    sys.exit(OK)
