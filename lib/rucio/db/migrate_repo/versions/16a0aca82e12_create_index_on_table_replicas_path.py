227e65c0365a1f2c6833ae84fa2297560af81dfd
Vincent Garonne <vgaronne@gmail.com>
2014-06-11 09:12:50 +0200

    Added index hint ; oups ; oups
    Change-Id: Ifd01108056332ae95cb217d4db92f5188423bbb6

--- lib/rucio/db/migrate_repo/versions/16a0aca82e12_create_index_on_table_replicas_path.py
+++ lib/rucio/db/migrate_repo/versions/16a0aca82e12_create_index_on_table_replicas_path.py
@@ -23,8 +23,8 @@ from alembic import op
 
 
 def upgrade():
-    op.create_index('REPLICAS_PATH_IDX', 'replicas', ['path'])
+    op.create_index('TOKENS_ACCOUNT_EXPIRED_AT_IDX', 'tokens', ['path'])
 
 
 def downgrade():
-    op.drop_index('REPLICAS_PATH_IDX')
+    op.drop_index('TOKENS_ACCOUNT_EXPIRED_AT_IDX')
