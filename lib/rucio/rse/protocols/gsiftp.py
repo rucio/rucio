652ad4d67e60e8d40f06676deb3b70e776aa5a7f
Vincent Garonne <vincent.garonne@cern.ch>
2014-06-27 09:24:50 +0200

    Added support to gsiftp urls ; fixed comment
    Change-Id: I1c81f70bfffe10b72d1be879052cbcc90711fbc9

---
+++ lib/rucio/rse/protocols/gsiftp.py
@@ -0,0 +1,22 @@
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Vincent Garonne, <vincent.garonne@cern.ch>, 2014
+
+from rucio.rse.protocols import protocol
+
+
+class Default(protocol.RSEProtocol):
+    """ Implementing access to RSEs using the local filesystem."""
+
+    def __init__(self, protocol_attr, rse_settings):
+        """ Initializes the object with information about the referred RSE.
+
+            :param props Properties derived from the RSE Repository
+        """
+        super(Default, self).__init__(protocol_attr, rse_settings)
