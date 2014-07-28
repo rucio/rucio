f1586d1ba6c7a0252c0abe0513e419b59f718d87
Thomas Beermann <thomas.beermann@cern.ch>
2014-07-28 11:11:45 +0200

    added new account chooser ; refactored templates to have base with header/footer
    Change-Id: Ie4e60e148ffd3137c2a724f93a722b2b62b8a588

--- lib/rucio/web/ui/views/main.py
+++ lib/rucio/web/ui/views/main.py
@@ -34,19 +34,25 @@ def index(request):
     token = None
     js_token = None
     js_account = None
+    accounts = None
     rucio_ui_version = version.version_string()
 
     if request.is_secure():
         renv = request.environ
+        # try to get and check the rucio session token from cookie
         session_token = request.COOKIES.get('x-rucio-auth-token')
         validate_token = authentication.validate_auth_token(session_token)
 
+        # if there is no session token or if invalid: get a new one.
         if validate_token is None:
+            # get all accounts for an identity. Needed for account switcher in UI.
+            accounts = identity.list_accounts_for_identity(renv['SSL_CLIENT_S_DN'], 'x509')
             try:
                 try:
+                    # try to get the default account for the identity.
                     def_account = identity.get_default_account(renv['SSL_CLIENT_S_DN'], 'x509')
                 except IdentityError:
-                    accounts = identity.list_accounts_for_identity(renv['SSL_CLIENT_S_DN'], 'x509')
+                    # if there is no default account used the first from the list off accounts.
                     def_account = accounts[0]
 
                 token = authentication.get_auth_token_x509(def_account,
@@ -56,18 +62,19 @@ def index(request):
             except:
                 return render_to_response('problem.html', locals())
 
+            # write the token and account to javascript variables, that will be used in the HTML templates.
             js_token = __to_js('token', token)
             js_account = __to_js('account', def_account)
-            account = def_account
         else:
             js_token = __to_js('token', session_token)
             js_account = __to_js('account', validate_token['account'])
-            account = validate_token['account']
+            accounts = identity.list_accounts_for_identity(session_token.split('-')[1], 'x509')
 
     else:
         return render_to_response('problem.html', locals())
 
     response = render_to_response('index.html', locals())
+    # if there was no valid session token write the new token to a cookie.
     if session_token is None:
         response.set_cookie('x-rucio-auth-token', value=token, max_age=3600)
 
