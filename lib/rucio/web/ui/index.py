5db283bb3874562dcc8a3784fcef352883476fea
Thomas Beermann <thomas.beermann@cern.ch>
2014-08-08 11:24:13 +0200

    - changed rucio ui from django to web.py - added new JS RucioClient class ; removed white spaces
    Change-Id: Iacc1735f40840b8373f90f215739bb4a66325c21

---
+++ lib/rucio/web/ui/index.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Thomas Beermann, <thomas.beermann@cern.ch>, 2014
+
+from os.path import dirname, join
+
+from web import application, template
+
+from rucio.web.ui.common.utils import check_token
+
+
+urls = (
+    '', 'Index'
+    )
+
+
+class Index():
+    def GET(self):
+        render = template.render(join(dirname(__file__), 'templates/'))
+        return check_token(render.index())
+
+
+"""----------------------
+   Web service startup
+----------------------"""
+
+app = application(urls, globals())
+application = app.wsgifunc()
