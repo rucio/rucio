fce08c6a50757410958a1a68f92c0c97a55ffa73
Mario Lassnig <mario.lassnig@cern.ch>
2014-09-24 13:30:23 +0200

    [RUCIO-600] FTS3 Host CA checking disabled
    Change-Id: If878653f54a21c3e0f0cd3d7abfdb1b6651053db

--- tools/delegate_proxy_fts3.py
+++ tools/delegate_proxy_fts3.py
@@ -33,12 +33,14 @@ if '--prod' in sys.argv:
     h = 'https://fts3.cern.ch:8446'
 elif '--devel' in sys.argv:
     h = 'https://fts3-devel.cern.ch:8446'
+elif '--raltest' in sys.argv:
+    h = 'https://fts3-test.gridpp.rl.ac.uk:8446'
 print h
 
-w = requests.get('%s/whoami' % h, verify=v, cert=c).json()
+w = requests.get('%s/whoami' % h, verify=False, cert=c).json()
 pprint.pprint(w)
 
-b = requests.get('%s/delegation/%s' % (h, w['delegation_id']), verify=v, cert=c).json()
+b = requests.get('%s/delegation/%s' % (h, w['delegation_id']), verify=False, cert=c).json()
 pprint.pprint(b)
 bt = parser.parse('1970-01-01 00:00:01')
 if b is not None:
@@ -47,15 +49,15 @@ at = parser.parse('1970-01-01 00:00:00')
 
 while at < bt:
     with open('/tmp/fts3request.pem', 'w') as fd:
-        fd.write(requests.get('%s/delegation/%s/request' % (h, w['delegation_id']), verify=v, cert=c).text)
+        fd.write(requests.get('%s/delegation/%s/request' % (h, w['delegation_id']), verify=False, cert=c).text)
 
     commands.getstatusoutput('/bin/echo -n > /etc/pki/CA/index.txt')
     commands.getstatusoutput('/bin/echo "00" > /etc/pki/CA/serial')
     commands.getstatusoutput('/usr/bin/openssl ca -in /tmp/fts3request.pem -preserveDN -days 365 -cert %s -keyfile %s -md sha1 -out /tmp/fts3proxy.pem -subj "%s" -policy policy_anything -batch' % (c, c, s))
     commands.getstatusoutput('/bin/cat /tmp/fts3proxy.pem %s > /tmp/fts3full.pem' % c)
 
-    print requests.put('%s/delegation/%s/credential' % (h, w['delegation_id']), verify=v, cert=c, data=open('/tmp/fts3full.pem', 'r')).text
-    a = requests.get('%s/delegation/%s' % (h, w['delegation_id']), verify=v, cert=c).json()
+    print requests.put('%s/delegation/%s/credential' % (h, w['delegation_id']), verify=False, cert=c, data=open('/tmp/fts3full.pem', 'r')).text
+    a = requests.get('%s/delegation/%s' % (h, w['delegation_id']), verify=False, cert=c).json()
     pprint.pprint(a)
     if a is not None:
         at = parser.parse(a['termination_time'])
