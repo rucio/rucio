727aa548a4429b85899037f4b8aa4e27911912cc
Cedric Serfon <cedric.serfon@cern.ch>
2014-10-06 10:12:26 +0200

    [RUCIO-341], [RUCIO-497], [RUCIO-606] Improvements/fixes on subscriptions ; Martin's comments ; Fix ; Fix
    Change-Id: I0de5f1f9605579a00ab9eab11e9d0ca95d894302

--- lib/rucio/client/subscriptionclient.py
+++ lib/rucio/client/subscriptionclient.py
@@ -24,22 +24,21 @@ class SubscriptionClient(BaseClient):
     def __init__(self, rucio_host=None, auth_host=None, account=None, ca_cert=None, auth_type=None, creds=None, timeout=None):
         super(SubscriptionClient, self).__init__(rucio_host, auth_host, account, ca_cert, auth_type, creds, timeout)
 
-    def add_subscription(self, name, filter, replication_rules, subscription_policy, lifetime, retroactive, dry_run):
+    def add_subscription(self, name, account, filter, replication_rules, comments, lifetime, retroactive, dry_run):
         """
         Adds a new subscription which will be verified against every new added file and dataset
 
-        :param account: Account identifier
-        :type account:  String
         :param name: Name of the subscription
         :type:  String
+        :param account: Account identifier
+        :type account:  String
         :param filter: Dictionary of attributes by which the input data should be filtered
                        **Example**: ``{'dsn': 'data11_hi*.express_express.*,data11_hi*physics_MinBiasOverlay*', 'account': 'tzero'}``
         :type filter:  Dict
         :param replication_rules: Replication rules to be set : Dictionary with keys copies, rse_expression, weight, rse_expression
         :type replication_rules:  Dict
-        :param subscription_policy: Name of an advanced subscription policy, which allows more advanced operations
-                                    **Example**: ``'data_export'``
-        :type subscription_policy:  String
+        :param comments: Comments for the subscription
+        :type comments:  String
         :param lifetime: Subscription's lifetime (days); False if subscription has no lifetime
         :type lifetime:  Integer or False
         :param retroactive: Flag to know if the subscription should be applied on previous data
@@ -47,17 +46,17 @@ class SubscriptionClient(BaseClient):
         :param dry_run: Just print the subscriptions actions without actually executing them (Useful if retroactive flag is set)
         :type dry_run:  Boolean
         """
-        path = self.SUB_BASEURL + '/' + name
+        path = self.SUB_BASEURL + '/' + account + '/' + name
         url = build_url(choice(self.list_hosts), path=path)
         if filter and type(filter) != dict:
             raise TypeError('filter should be a dict')
         if replication_rules and type(replication_rules) != list:
             raise TypeError('replication_rules should be a list')
-        data = dumps({'filter': filter, 'replication_rules': replication_rules, 'subscription_policy': subscription_policy,
+        data = dumps({'filter': filter, 'replication_rules': replication_rules, 'comments': comments,
                       'lifetime': lifetime, 'retroactive': retroactive, 'dry_run': dry_run})
         r = self._send_request(url, type='POST', data=data)
         if r.status_code == codes.created:
-            return True
+            return r.text
         else:
             exc_cls, exc_msg = self._get_exception(r.headers, r.status_code)
             raise exc_cls(exc_msg)
@@ -75,11 +74,11 @@ class SubscriptionClient(BaseClient):
         :rtype:   Dict
         :raises: exception.NotFound if subscription is not found
         """
-        if not account:
-            account = '*'
-        if not name:
-            name = '*'
-        path = self.SUB_BASEURL + '/' + account + '/' + name
+        path = self.SUB_BASEURL
+        if account:
+            path += + '/' + account
+            if name:
+                path += '/' + name
         url = build_url(choice(self.list_hosts), path=path)
         r = self._send_request(url, type='GET')
         if r.status_code == codes.ok:
@@ -88,20 +87,21 @@ class SubscriptionClient(BaseClient):
             exc_cls, exc_msg = self._get_exception(r.headers, r.status_code)
             raise exc_cls(exc_msg)
 
-    def update_subscription(self, name, filter=None, replication_rules=None, subscription_policy=None, lifetime=None, retroactive=None, dry_run=None):
+    def update_subscription(self, name, account=None, filter=None, replication_rules=None, comments=None, lifetime=None, retroactive=None, dry_run=None):
         """
         Updates a subscription
 
         :param name: Name of the subscription
         :type:  String
+        :param account: Account identifier
+        :type account:  String
         :param filter: Dictionary of attributes by which the input data should be filtered
                        **Example**: ``{'dsn': 'data11_hi*.express_express.*,data11_hi*physics_MinBiasOverlay*', 'account': 'tzero'}``
         :type filter:  Dict
         :param replication_rules: Replication rules to be set : Dictionary with keys copies, rse_expression, weight, rse_expression
         :type replication_rules:  Dict
-        :param subscription_policy: Name of an advanced subscription policy, which allows more advanced operations
-                                    **Example**: ``'data_export'``
-        :type subscription_policy:  String
+        :param comments: Comments for the subscription
+        :type comments:  String
         :param lifetime: Subscription's lifetime (days); False if subscription has no lifetime
         :type lifetime:  Integer or False
         :param retroactive: Flag to know if the subscription should be applied on previous data
@@ -110,13 +110,15 @@ class SubscriptionClient(BaseClient):
         :type dry_run:  Boolean
         :raises: exception.NotFound if subscription is not found
         """
-        path = self.SUB_BASEURL + '/' + name
+        if not account:
+            account = self.account
+        path = self.SUB_BASEURL + '/' + account + '/' + name
         url = build_url(choice(self.list_hosts), path=path)
         if filter and type(filter) != dict:
             raise TypeError('filter should be a dict')
         if replication_rules and type(replication_rules) != list:
             raise TypeError('replication_rules should be a list')
-        data = dumps({'filter': filter, 'replication_rules': replication_rules, 'subscription_policy': subscription_policy,
+        data = dumps({'filter': filter, 'replication_rules': replication_rules, 'comments': comments,
                       'lifetime': lifetime, 'retroactive': retroactive, 'dry_run': dry_run})
         r = self._send_request(url, type='PUT', data=data)
         if r.status_code == codes.created:
