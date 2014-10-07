48e7cbf4c668097903015dbb675f54ddd2d05f80
Martin Barisits <martin.barisits@cern.ch>
2014-10-07 16:07:58 +0200

    [RUCIO-679] Judge repairer moves rules to SUSPENDED after 2 weeks
    Change-Id: I56b990b778a1b38556721b53c87970cdaa8e9fe1

---
+++ lib/rucio/db/migrate_repo/versions/102efcf145f4_added_stuck_at_column_to_rules.py
@@ -0,0 +1,31 @@
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Martin Barisits, <martin.barisits@cern.ch>, 2014
+
+"""Added stuck_at column to rules
+
+Revision ID: 102efcf145f4
+Revises: 4207be2fd914
+Create Date: 2014-10-07 13:31:25.347076
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '102efcf145f4'
+down_revision = '4207be2fd914'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column('rules', sa.Column('stuck_at', sa.DateTime))
+
+
+def downgrade():
+    op.drop_column('rules', 'stuck_at')
