8dd330b4a1070c613c294105382321e9e4dcc211
Mario Lassnig <mario.lassnig@cern.ch>
2014-10-08 16:27:54 +0200

    [RUCIO-697] hermes: multi process thread support
    Change-Id: I0c54130e4c4f11491215d8b2c741b1ea817fb027

--- lib/rucio/core/message.py
+++ lib/rucio/core/message.py
@@ -13,6 +13,7 @@
 import json
 
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.sql.expression import bindparam, text
 
 from rucio.common.exception import InvalidObject, RucioException
 from rucio.db.models import Message
@@ -38,11 +39,15 @@ def add_message(event_type, payload, session=None):
 
 
 @transactional_session
-def retrieve_messages(limit=100, session=None):
+def retrieve_messages(bulk=1000, process=None, total_processes=None, thread=None, total_threads=None, session=None):
     """
-    Retrieve up to $limit messages.
+    Retrieve up to $bulk messages.
 
-    :param limit: Number of messages as an int.
+    :param bulk: Number of messages as an integer.
+    :param process: Identifier of the caller process as an integer.
+    :param total_processes: Maximum number of processes as an integer.
+    :param thread: Identifier of the caller thread as an integer.
+    :param total_threads: Maximum number of threads as an integer.
     :param session: The database session to use.
 
     :returns messages: List of dictionaries {id, created_at, event_type, payload}
@@ -51,7 +56,31 @@ def retrieve_messages(limit=100, session=None):
     messages = []
 
     try:
-        query = session.query(Message.id, Message.created_at, Message.event_type, Message.payload).order_by(Message.created_at).limit(limit)
+        query = session.query(Message.id,
+                              Message.created_at,
+                              Message.event_type,
+                              Message.payload).order_by(Message.created_at)
+
+        if (total_processes-1) > 0:
+            if session.bind.dialect.name == 'oracle':
+                bindparams = [bindparam('process_number', process), bindparam('total_processes', total_processes-1)]
+                query = query.filter(text('ORA_HASH(id, :total_processes) = :process_number', bindparams=bindparams))
+            elif session.bind.dialect.name == 'mysql':
+                query = query.filter('mod(md5(id), %s) = %s' % (total_processes-1, process))
+            elif session.bind.dialect.name == 'postgresql':
+                query = query.filter('mod(abs((\'x\'||md5(id))::bit(32)::int), %s) = %s' % (total_processes-1, process))
+
+        if (total_threads-1) > 0:
+            if session.bind.dialect.name == 'oracle':
+                bindparams = [bindparam('thread_number', thread), bindparam('total_threads', total_threads-1)]
+                query = query.filter(text('ORA_HASH(id, :total_threads) = :thread_number', bindparams=bindparams))
+            elif session.bind.dialect.name == 'mysql':
+                query = query.filter('mod(md5(id), %s) = %s' % (total_threads-1, thread))
+            elif session.bind.dialect.name == 'postgresql':
+                query = query.filter('mod(abs((\'x\'||md5(id))::bit(32)::int), %s) = %s' % (total_threads-1, thread))
+
+        query = query.limit(bulk)
+
         for id, created_at, event_type, payload in query:
             messages.append({'id': id,
                              'created_at': created_at,
@@ -73,7 +102,9 @@ def delete_messages(ids, session=None):
 
     try:
         for id in ids:
-            session.query(Message).filter_by(id=id).delete(synchronize_session=False)
+            session.query(Message).\
+                filter_by(id=id).\
+                delete(synchronize_session=False)
     except IntegrityError, e:
         raise RucioException(e.args)
 
