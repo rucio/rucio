3fd3f525cc23be613ff69be1542792d863c7b0c5
Mario Lassnig <mario.lassnig@cern.ch>
2014-10-31 18:02:07 +0100

    [RUCIO-767] get request by DID and RSE for webui ; martins comments
    Change-Id: I698acff6fda8aefb8341a421feaa8f29c0e13834

---
+++ lib/rucio/web/rest/request.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Mario Lassnig, <mario.lassnig@cern.ch>, 2014
+
+import datetime
+import json
+
+from logging import getLogger, StreamHandler, DEBUG
+
+from web import application, ctx, loadhook, header
+
+from rucio.api import request
+from rucio.common.utils import generate_http_error
+from rucio.web.rest.common import rucio_loadhook, RucioController, exception_wrapper
+
+
+logger = getLogger("rucio.request")
+sh = StreamHandler()
+sh.setLevel(DEBUG)
+logger.addHandler(sh)
+
+urls = ('/(.+)/(.+)/(.+)', 'RequestGet',)
+
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, datetime.datetime):
+        serial = obj.isoformat()
+        return serial
+
+
+class RequestGet(RucioController):
+    """ REST API to get requests. """
+
+    @exception_wrapper
+    def GET(self, scope, name, rse):
+        """
+        List request for given DID to a destination RSE.
+
+        HTTP Success:
+            200 OK
+
+        HTTP Error:
+            401 Unauthorized
+            404 Request Not Found
+        """
+
+        header('Content-Type', 'application/json')
+
+        try:
+            return json.dumps(request.get_request_by_did(scope=scope,
+                                                         name=name,
+                                                         rse=rse,
+                                                         issuer=ctx.env.get('issuer')),
+                              default=json_serial)
+        except:
+            raise generate_http_error(404, 'RequestNotFound', 'No request found for DID %s:%s at RSE %s' % (scope,
+                                                                                                            name,
+                                                                                                            rse))
+
+"""----------------------
+   Web service startup
+----------------------"""
+
+app = application(urls, globals())
+app.add_processor(loadhook(rucio_loadhook))
+application = app.wsgifunc()
