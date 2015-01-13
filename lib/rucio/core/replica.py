9b250318ac3d9c8a1f9dc2e5d1122c3adfbba7c9
Vincent Garonne <vgaronne@gmail.com>
2015-01-13 16:57:48 +0100

    [RUCIO-822] Support for immediate obsolete data deletion
    Change-Id: I46a5b7394a07db115c3ba6ffc1ef734b3827436a

--- lib/rucio/core/replica.py
+++ lib/rucio/core/replica.py
@@ -649,8 +649,10 @@ def list_unlocked_replicas(rse, limit, bytes=None, rse_id=None, worker_number=No
     neededSpace = bytes
     totalbytes = 0
     for (scope, name, bytes, tombstone) in query.yield_per(1000):
+
         if tombstone != OBSOLETE and neededSpace is not None and totalbytes >= neededSpace:
             break
+
         d = {'scope': scope, 'name': name, 'bytes': bytes}
         rows.append(d)
         if tombstone != OBSOLETE:
