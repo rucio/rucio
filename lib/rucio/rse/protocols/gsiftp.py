13061f7087ae643d5cb66e8ae6dc6a2a84c0471a
Wen Guan <wguan.icedew@gmail.com>
2015-02-13 14:34:10 +0100

    [RUCIO-1219] add function to check gsiftp site usage
    Change-Id: Ia6845d28ec1e6965e68c993c2fe40580ed843497

--- lib/rucio/rse/protocols/gsiftp.py
+++ lib/rucio/rse/protocols/gsiftp.py
@@ -7,7 +7,12 @@
 #
 # Authors:
 # - Vincent Garonne, <vincent.garonne@cern.ch>, 2014
+# - Wen Guan, <wen.guan@cern.ch>, 2015
 
+import json
+import os
+
+from rucio.common import exception
 from rucio.rse.protocols import protocol
 
 
@@ -20,3 +25,49 @@ class Default(protocol.RSEProtocol):
             :param props Properties derived from the RSE Repository
         """
         super(Default, self).__init__(protocol_attr, rse_settings)
+
+    def connect(self):
+        """
+        Establishes the actual connection to the referred RSE.
+        If we decide to use gfal, init should be done here.
+
+        :raises RSEAccessDenied
+        """
+        pass
+
+    def close(self):
+        """
+        Closes the connection to RSE.
+        """
+        pass
+
+    def get_space_usage(self):
+        """
+        Get RSE space usage information.
+
+        :returns: a list with dict containing 'totalsize' and 'unusedsize'
+
+        :raises ServiceUnavailable: if some generic error occured in the library.
+        """
+        endpoint_path = ''.join([self.attributes['scheme'], '://', self.attributes['hostname'], ':', str(self.attributes['port']), '/atlas/dq2/site-size'])
+        dest = '/tmp/rucio-gsiftp-site-size'
+
+        try:
+            import gfal2
+            if os.path.exists(dest):
+                os.remove(dest)
+            ctx = gfal2.creat_context()
+            params = ctx.transfer_parameters()
+            params.timeout = 60
+            ret = ctx.filecopy(params, str(endpoint_path), 'file:' + dest)
+            if ret == 0:
+                data_file = open(dest)
+                data = json.load(data_file)
+                data_file.close()
+                totalsize = data['sizes']['total']
+                availablesize = data['sizes']['available']
+                unusedsize = totalsize - availablesize
+            return totalsize, unusedsize
+        except Exception as e:
+            print e
+            raise exception.ServiceUnavailable(e)
