857d99f4d68d00f2e880cb38033f5cb7f24c39fa
Vincent Garonne <vgaronne@gmail.com>
2015-02-14 09:11:54 +0100

    Revert "[RUCIO-1185] Added filter options for rules."
    This reverts commit 4e22ed72167dcbd9c19772a3378d40a0fc573bbe.
    
    Change-Id: Ic314268d5514872706a8ae178ab74b0f08f47048

--- lib/rucio/core/rule.py
+++ lib/rucio/core/rule.py
@@ -17,7 +17,6 @@ import sys
 from copy import deepcopy
 from datetime import datetime, timedelta
 from re import match
-from time import strptime
 
 from sqlalchemy.exc import IntegrityError, StatementError
 from sqlalchemy.orm.exc import NoResultFound
@@ -370,19 +369,12 @@ def list_rules(filters={}, session=None):
     if filters:
         for (k, v) in filters.items():
             if k == 'state' and isinstance(v, basestring):
-                query = query.filter(models.ReplicationRule.state == RuleState.from_string(v))
-            elif k == 'did_type' and isinstance(v, basestring):
-                query = query.filter(models.ReplicationRule.did_type == DIDType.from_string(v))
-            elif k == 'grouping' and isinstance(v, basestring):
-                query = query.filter(models.ReplicationRule.grouping == RuleGrouping.from_string(v))
-            elif k == 'created_before' and isinstance(v, basestring):
-                dt = strptime(v, '%Y-%m-%d %H:%M:%S')
-                query = query.filter(models.ReplicationRule.created_at < dt)
-            elif k == 'created_after' and isinstance(v, basestring):
-                dt = strptime(v, '%Y-%m-%d %H:%M:%S')
-                query = query.filter(models.ReplicationRule.created_at > dt)
-            else:
-                query = query.filter(getattr(models.ReplicationRule, k) == v)
+                v = RuleState.from_string(v)
+            if k == 'did_type' and isinstance(v, basestring):
+                v = DIDType.from_string(v)
+            if k == 'grouping' and isinstance(v, basestring):
+                v = RuleGrouping.from_string(v)
+            query = query.filter(getattr(models.ReplicationRule, k) == v)
 
     try:
         for rule in query.yield_per(5):
