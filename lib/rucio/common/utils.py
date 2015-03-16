b467e0c0ce2810238543b983f7277ffb84581f66
Vincent Garonne <vgaronne@gmail.com>
2015-03-16 13:27:56 +0100

    [RUCIO-1330] Added method to check if process(pid) exists ; added sanity_check method ; Started using heartbeats values in undertaker
    Change-Id: I7d4e2e27b2402288c85854873a0f5b2e0693e1e1

--- lib/rucio/common/utils.py
+++ lib/rucio/common/utils.py
@@ -14,7 +14,9 @@ Rucio utilities.
 """
 
 import datetime
+import errno
 import json
+import os
 import re
 import subprocess
 import zlib
@@ -391,3 +393,33 @@ def clean_surls(surls):
         res.append(surl)
     res.sort()
     return res
+
+
+def pid_exists(pid):
+    """
+    Check whether pid exists in the current process table.
+    UNIX only.
+    """
+    if pid < 0:
+        return False
+    if pid == 0:
+        # According to "man 2 kill" PID 0 refers to every process
+        # in the process group of the calling process.
+        # On certain systems 0 is a valid PID but we have no way
+        # to know that in a portable fashion.
+        raise ValueError('invalid PID 0')
+    try:
+        os.kill(pid, 0)
+    except OSError as err:
+        if err.errno == errno.ESRCH:
+            # ESRCH == No such process
+            return False
+        elif err.errno == errno.EPERM:
+            # EPERM clearly means there's a process to deny access to
+            return True
+        else:
+            # According to "man 2 kill" possible error values are
+            # (EINVAL, EPERM, ESRCH)
+            raise
+    else:
+        return True
