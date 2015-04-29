72e716b01977f49313948938161d794abb42f022
Ralph Vigne <ralph.vigne@cern.ch>
2015-04-29 21:29:13 +0200

    [RUCIO-000] minor changes in http monitoring scripts ; fixed account reporting
    Change-Id: Ie6b98c147db09f726a93fbb869720a4d9d834e74

--- tools/hadoop/http-monitoring-reports/RucioServerLogs2Graphite.py
+++ tools/hadoop/http-monitoring-reports/RucioServerLogs2Graphite.py
@@ -13,6 +13,7 @@ import re
 import socket
 import time
 import sys
+import traceback
 
 from collections import defaultdict
 from collections import OrderedDict
@@ -34,7 +35,8 @@ mapper = OrderedDict({'time': 1,  # (01) Date/Time [YYYY-MM-DD HH:MM:SS] => [%{%
                       #  'protocol_version':  14,  # (10) First line of request => \"%r\"
                       'account': 17,  # (11) "Rucio Account" => \"%{X-Rucio-Auth-Token}i\"
                       #  'certificate':  16,  # (11) "Rucio Account" => \"%{X-Rucio-Auth-Token}i\"
-                      #  'useragent':  17   # (12) User Agent, including also the script name (as derived from __main__) e.g. "my-script rucio-clients/0.2.13" => \"%{User-Agent}i\"
+                      #  'useragent':  17,   # (12) User Agent
+                      #  'scriptID':  18   # (12) Name of the script calling the clients
                       })
 
 
@@ -45,7 +47,7 @@ GRAPHITE_HOST = 'rucio-graphite-int.cern.ch'
 GRAPHITE_PORT = 2003
 GRAPHITE_SCOPE = 'rucio.http-monitoring'
 
-pattern = re.compile("^\\[(.*):(.*?)\\]\t((.*?)\\..*?)\t(\\S+)\t(\\S+)\t(\\S+)\t(\\S+)\t(\\S+)\t(\\S+)\t(\\S+)\t\"((\\S+)\\s+(\\S+)\\s+(\\S+))\"\t\"((.*?)-/.*?)\"\t\"(.*?)\"$")
+pattern = re.compile("^\\[(.*):(.*?)\\]\t((.*?)\\..*?)\t(\\S+)\t(.+?)\t(\\S+)\t(\\S+)\t(\\S+)\t(\\S+)\t(\\S+)\t\"((\\S+)\\s+(\\S+)\\s+(\\S+))\"\t\"((.*?)-/?.*?)\"\t\"(.*?)\"\t\\S+$")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 timestamp = 0
@@ -55,28 +57,53 @@ cnt_ts = 0
 cnt_unkonwn_account = 0
 cnt_no_match = 0
 cnt_match = 0
+cnt_redirect = 0
+cnt_misc = 0
 
 for line in sys.stdin:
     try:
-        attr = pattern.match(line).group(*key_mapper)
+        attr = pattern.match(line)
+        if attr is None:
+            if '/redirect/' in line:
+                cnt_redirect += 1
+                continue
+            if '/ping' in line:
+                cnt_misc += 1
+                continue
+            if '/server-status' in line:
+                cnt_misc += 1
+                continue
+            cnt_no_match += 1
+            continue
+        attr = attr.group(*key_mapper)
         if not re.match('[0-9]{,4}-[0-9]{,2}-[0-9]{,2} [0-9]{,2}:[0-9]{,2}', attr[keys.index('time')]):
             cnt_ts += 1
             continue
         if attr[keys.index('account')] == '':
+            if '/redirect/' in line:
+                cnt_redirect += 1
+                continue
+            if '/ping' in line:
+                cnt_misc += 1
+                continue
+            if '/server-status' in line:
+                cnt_misc += 1
+                continue
             cnt_unkonwn_account += 1
             continue
         new_minute = (timestamp < int(time.mktime(time.strptime(attr[keys.index('time')], '%Y-%m-%d %H:%M'))))
     except Exception:
+        traceback.print_exc()
         cnt_no_match += 1
         continue
     cnt_match += 1
     if new_minute:
         # Report stats to Graphite
-        message = "%s.%s.%s.%s" % (GRAPHITE_SCOPE,
-                                   attr[keys.index('host')],
-                                   attr[keys.index('account')],
-                                   attr[keys.index('http_verb')])
         for metric in events:
+            message = "%s.%s.%s.%s" % (GRAPHITE_SCOPE,
+                                       metric[0],
+                                       metric[1],
+                                       metric[2])
             for value in events[metric].keys():
                 sock.sendto("%s.%s %s %s\n" % (message, value, events[metric][value], timestamp), (GRAPHITE_HOST, GRAPHITE_PORT))
         # Reset stats
@@ -92,6 +119,7 @@ for line in sys.stdin:
                                       'response_time_in_micros.upper': None,
                                       'response_time_in_micros.sum': 0})
         timestamp = int(time.mktime(time.strptime(attr[keys.index('time')], '%Y-%m-%d %H:%M')))
+
     # Update stats entry
     e = events[(attr[keys.index('host')], attr[keys.index('account')], attr[keys.index('http_verb')])]
     for metric in ['request_size', 'response_size', 'response_time_in_micros']:
@@ -100,4 +128,4 @@ for line in sys.stdin:
         e[metric + '.lower'] = int(attr[keys.index(metric)]) if (e[metric + '.lower'] > attr[keys.index(metric)]) or (e[metric + '.lower'] is None) else e[metric + '.lower']
         e[metric + '.upper'] = int(attr[keys.index(metric)]) if (e[metric + '.upper'] < attr[keys.index(metric)]) else e[metric + '.upper']  # In case of None, True. Thus no None check
 
-print 'Matched: %s\t Unknown accounts: %s\tFailed matching lines: %s\tFailed matching timestamp: %s' % (cnt_match, cnt_unkonwn_account, cnt_no_match, cnt_ts)
+print 'Matched: %s\t Unknown accounts: %s\tFailed matching lines: %s\tFailed matching timestamp: %s\tRerdirects: %s\tMisc: %s' % (cnt_match, cnt_unkonwn_account, cnt_no_match, cnt_ts, cnt_redirect, cnt_misc)
