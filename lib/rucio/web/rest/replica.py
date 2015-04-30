00072981b3677d5358712fe8643a111fbb86ee20
Vincent Garonne <vincent.garonne@cern.ch>
2015-04-30 11:31:35 +0200

    [RUCIO-000] Fixed Content-Type header
    Change-Id: I1e0e7c18cfcf157f01b7ae7d7e75ddf3ee077561

--- lib/rucio/web/rest/replica.py
+++ lib/rucio/web/rest/replica.py
@@ -607,6 +607,7 @@ class DatasetReplicas(RucioController):
         :returns: A dictionary containing all replicas information.
         :returns: A metalink description of replicas if metalink(4)+xml is specified in Accept:
         """
+        header('Content-Type', 'application/x-json-stream')
         try:
             for row in list_dataset_replicas(scope=scope, name=name):
                 yield dumps(row, cls=APIEncoder) + '\n'
