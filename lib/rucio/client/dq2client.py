465d0134949f5dcd9b50db2bec9803c3b510b845
Vincent Garonne <vgaronne@gmail.com>
2015-05-06 09:50:59 +0200

    [RUCIO-1495] Moved to SafeConfigParser ; pylint fixes ; More pylint fixes... ; More pylint fixes
    Change-Id: I433036e7b13f4ae8585a9e739ed1a802d57be5b8

--- lib/rucio/client/dq2client.py
+++ lib/rucio/client/dq2client.py
@@ -1,3 +1,4 @@
+
 '''
   Copyright European Organization for Nuclear Research (CERN)
 
@@ -17,6 +18,7 @@
 Compatibility Wrapper for DQ2 and Rucio.
      http://svnweb.cern.ch/world/wsvn/dq2/trunk/dq2.clients/lib/dq2/clientapi/DQ2.py
 '''
+#pylint: skip-file
 
 import copy
 import hashlib
