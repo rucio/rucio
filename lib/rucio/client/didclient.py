e9cebc2e50c595420868c6fba88247c2d8ffd1d8
Vincent Garonne <vgaronne@gmail.com>
2015-05-08 16:39:55 +0200

    [RUCIO-000] More pylint fixes
    Change-Id: I5b6f992ce80b581fe1038eeac0ec15758bcb9cbb

--- lib/rucio/client/didclient.py
+++ lib/rucio/client/didclient.py
@@ -1,17 +1,19 @@
-# Copyright European Organization for Nuclear Research (CERN)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# You may not use this file except in compliance with the License.
-# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-#
-# Authors:
-# - Vincent Garonne, <vincent.garonne@cern.ch>, 2012-2015
-# - Mario Lassnig, <mario.lassnig@cern.ch>, 2012-2013
-# - Thomas Beermann, <thomas.beermann@cern.ch> 2013
-# - Yun-Pin Sun, <yun-pin.sun@cern.ch>, 2013
-# - Cedric Serfon, <cedric.serfon@cern.ch>, 2014-2015
-# - Ralph Vigne, <ralph.vigne@cern.ch>, 2015
-# - Martin Barisits, <martin.barisits@cern.ch>, 2014-2015
+'''
+  Copyright European Organization for Nuclear Research (CERN)
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  You may not use this file except in compliance with the License.
+  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+  Authors:
+  - Vincent Garonne, <vincent.garonne@cern.ch>, 2012-2015
+  - Mario Lassnig, <mario.lassnig@cern.ch>, 2012-2013
+  - Thomas Beermann, <thomas.beermann@cern.ch> 2013
+  - Yun-Pin Sun, <yun-pin.sun@cern.ch>, 2013
+  - Cedric Serfon, <cedric.serfon@cern.ch>, 2014-2015
+  - Ralph Vigne, <ralph.vigne@cern.ch>, 2015
+  - Martin Barisits, <martin.barisits@cern.ch>, 2014-2015
+'''
 
 from json import dumps
 from requests.status_codes import codes
