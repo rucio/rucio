9fc51c98583074fc46a678549d6c1659c5f04185
Cedric Serfon <cedric.serfon@cern.ch>
2015-07-24 13:01:17 +0200

    [RUCIO-1730] Add unavailable source in rse_usage

--- etc/sql/oracle/scheduler.sql
+++ etc/sql/oracle/scheduler.sql
@@ -26,3 +26,21 @@ END LOOP;
 END;', number_of_arguments=>0, start_date=>NULL, repeat_interval=>'Freq=Minutely;Interval=30', end_date=>NULL);
 END;
 --/
+
+
+--/
+BEGIN
+    dbms_scheduler.create_job(job_name=>'ATLAS_RUCIO.UPDATE_RSE_USAGE', job_class=>'DEFAULT_JOB_CLASS', comments=>NULL, auto_drop=>TRUE, job_type=>'plsql_block', job_action=>
+'BEGIN
+FOR usage IN (SELECT /*+ INDEX(R REPLICAS_STATE_IDX ) */ rse_id, SUM(bytes) AS bytes , COUNT(*) AS files
+              FROM atlas_rucio.replicas r WHERE (CASE WHEN state != ''A'' THEN rse_id END) IS NOT NULL
+              AND (state=''U'' or state=''C'') AND tombstone IS NULL GROUP BY rse_id)
+LOOP
+        MERGE INTO atlas_rucio.rse_usage USING DUAL ON (RSE_USAGE.rse_id = usage.rse_id and source = ''unavailable'')
+        WHEN MATCHED THEN UPDATE SET used=usage.bytes, files=usage.files, updated_at=sysdate
+        WHEN NOT MATCHED THEN INSERT (rse_id, source, used, files, updated_at, created_at) VALUES (usage.rse_id, ''unavailable'', usage.bytes, usage.files, sysdate, sysdate);
+        COMMIT;
+END LOOP;
+END;', number_of_arguments=>0, start_date=>NULL, repeat_interval=>'Freq=Minutely;Interval=30', end_date=>NULL);
+END;
+--/
