e9feb64386cfd01c43f059c4d0482a06bff752e6
Vincent Garonne <vgaronne@gmail.com>
2015-08-26 11:55:51 +0200

    [RUCIO-1820] Updated deps + fixes alembic for sqlite
    Change-Id: I03985871d6e9e8845ef94c3d9741d92ab36d503a

--- lib/rucio/db/migrate_repo/versions/32c7d2783f7e_create_bad_replicas_table.py
+++ lib/rucio/db/migrate_repo/versions/32c7d2783f7e_create_bad_replicas_table.py
@@ -20,7 +20,7 @@ Create Date: 2015-02-13 16:22:05.154112
 revision = '32c7d2783f7e'
 down_revision = '384b96aa0f60'
 
-from alembic import op
+from alembic import op, context
 import sqlalchemy as sa
 from rucio.db.types import GUID
 
@@ -36,12 +36,13 @@ def upgrade():
                     sa.Column('updated_at', sa.DateTime),
                     sa.Column('created_at', sa.DateTime))
 
-    op.create_primary_key('BAD_REPLICAS_STATE_PK', 'bad_replicas', ['scope', 'name', 'rse_id', 'created_at'])
-    op.create_check_constraint('BAD_REPLICAS_SCOPE_NN', 'bad_replicas', 'scope is not null')
-    op.create_check_constraint('BAD_REPLICAS_NAME_NN', 'bad_replicas', 'name is not null')
-    op.create_check_constraint('BAD_REPLICAS_RSE_ID_NN', 'bad_replicas', 'rse_id is not null')
-    op.create_foreign_key('BAD_REPLICAS_ACCOUNT_FK', 'bad_replicas', 'accounts', ['account'], ['account'])
-    op.create_index('BAD_REPLICAS_STATE_IDX', 'bad_replicas', ['rse_id', 'state'])
+    if context.get_context().dialect.name not in ('sqlite'):
+        op.create_primary_key('BAD_REPLICAS_STATE_PK', 'bad_replicas', ['scope', 'name', 'rse_id', 'created_at'])
+        op.create_check_constraint('BAD_REPLICAS_SCOPE_NN', 'bad_replicas', 'scope is not null')
+        op.create_check_constraint('BAD_REPLICAS_NAME_NN', 'bad_replicas', 'name is not null')
+        op.create_check_constraint('BAD_REPLICAS_RSE_ID_NN', 'bad_replicas', 'rse_id is not null')
+        op.create_foreign_key('BAD_REPLICAS_ACCOUNT_FK', 'bad_replicas', 'accounts', ['account'], ['account'])
+        op.create_index('BAD_REPLICAS_STATE_IDX', 'bad_replicas', ['rse_id', 'state'])
 
 
 def downgrade():
