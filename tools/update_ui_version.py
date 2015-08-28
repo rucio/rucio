1e74a503c6a098358aece5592225a71558938ba5
Thomas Beermann <thomas.beermann@cern.ch>
2015-08-28 16:21:43 +0200

    [RUCIO-1824] add version param to ui includes to force cache reload on change
    Change-Id: I14f54a135a19a0964c5839ff34858cdd9a40b6d1

---
+++ tools/update_ui_version.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Thomas Beermann, <thomas.beermann@cern.ch>, 2015
+#
+# script to update the version parameter for RucioUI Javascript and CSS includes
+
+from hashlib import md5
+from re import sub
+from sys import argv
+
+
+files = ['/static/rucio.js', '/static/base.js', '/static/rucio.css']
+
+
+def md5_file(filename):
+    hash = md5()
+    with open(filename) as f:
+        for chunk in iter(lambda: f.read(4096), ""):
+            hash.update(chunk)
+    return hash.hexdigest()
+
+with open(argv[1], 'r') as f:
+    orig_file = f.read()
+
+new_file = orig_file
+
+for f in files:
+    md5_sum = md5_file('/opt/rucio/lib/rucio/web/ui' + f)
+    regex = r"(?<=" + f + "\?version=)[^\"]+"
+
+    new_file = sub(regex, md5_sum, new_file)
+
+with open(argv[1], 'w') as f:
+    f.write(new_file)
