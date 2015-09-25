9851154993743576800c105646665be5b7beffdd
Vincent Garonne <vincent.garonne@cern.ch>
2015-09-25 09:55:11 +0200

    [RUCIO-1877] Change rucio.db layout
    Change-Id: Ic8c7666668560c0e0f7bf7eaac3023e58e05c48e

---
+++ lib/rucio/db/sqla/enum.py
@@ -0,0 +1,120 @@
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Vincent Garonne, <vincent.garonne@cern.ch>, 2013
+# - Mario Lassnig, <mario.lassnig@cern.ch>, 2013-2014
+
+'''
+Class to handle enum type with sqlachelmy.
+ref. http://techspot.zzzeek.org/2011/01/14/the-enum-recipe/
+
+'''
+
+import uuid
+
+from sqlalchemy.types import SchemaType, TypeDecorator, Enum
+
+from rucio.common.exception import InvalidType
+
+
+class EnumSymbol(object):
+    """Define a fixed symbol tied to a parent class."""
+
+    def __init__(self, cls_, name, value, description):
+        self.cls_ = cls_
+        self.name = name
+        self.value = value
+        self.description = description
+
+    def __reduce__(self):
+        """Allow unpickling to return the symbol
+        linked to the DeclEnum class."""
+        return getattr, (self.cls_, self.name)
+
+    def __iter__(self):
+        return iter([self.value, self.description])
+
+    def __repr__(self):
+        return "%s" % self.description
+
+
+class EnumMeta(type):
+    """Generate new DeclEnum classes."""
+
+    def __init__(cls, classname, bases, dict_):
+        cls._reg = reg = cls._reg.copy()
+        cls._syms = syms = cls._syms.copy()
+        for k, v in dict_.items():
+            if isinstance(v, tuple):
+                sym = reg[v[0]] = syms[v[1]] = EnumSymbol(cls, k, *v)
+                setattr(cls, k, sym)
+        return type.__init__(cls, classname, bases, dict_)
+
+    def __iter__(cls):
+        return iter(cls._reg.values())
+
+
+class DeclEnum(object):
+    """Declarative enumeration."""
+
+    __metaclass__ = EnumMeta
+    _reg = {}
+    _syms = {}
+
+    @classmethod
+    def from_string(cls, value):
+        try:
+            return cls._reg[value]
+        except KeyError:
+            raise ValueError("Invalid value for %r: %r" % (cls.__name__, value))
+
+    @classmethod
+    def from_sym(cls, value):
+        try:
+            return cls._syms[value.upper()]
+        except KeyError:
+            raise ValueError("Invalid value for %r: %r" % (cls.__name__, value))
+
+    @classmethod
+    def values(cls):
+        return cls._reg.keys()
+
+    @classmethod
+    def db_type(cls, name=None, default=None):
+        return DeclEnumType(enum=cls, name=name, default=default)
+
+
+class DeclEnumType(SchemaType, TypeDecorator):
+
+    def __init__(self, enum, name=None, default=None):
+        self.enum = enum
+
+        # Workaround SQLAlchemy not propagating Enum names properly
+
+        if name is None:
+            self.impl = Enum(*enum.values(), name='RUCIO_ENUM_' + str(uuid.uuid4())[:6])
+        else:
+            self.impl = Enum(*enum.values(), name=name)
+
+    def _set_table(self, table, column):
+        self.impl._set_table(table, column)
+
+    def copy(self):
+        return DeclEnumType(self.enum)
+
+    def process_bind_param(self, value, dialect):
+        try:
+            if value is None:
+                return None
+            return value.value
+        except AttributeError:
+            raise InvalidType('Invalid value/type %s for %s' % (value, self.enum))
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return None
+        return self.enum.from_string(value.strip())
---
+++ lib/rucio/db/sqla/util.py
@@ -0,0 +1,142 @@
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Vincent Garonne, <vincent.garonne@cern.ch>, 2013-2014
+# - Mario Lassnig, <mario.lassnig@cern.ch>, 2013-2014
+# - Martin Barisits, <martin.barisits@cern.ch>, 2014
+
+from traceback import format_exc
+
+from alembic import command
+from alembic.config import Config
+
+from sqlalchemy.engine import reflection
+from sqlalchemy.schema import MetaData, Table, DropTable, ForeignKeyConstraint, DropConstraint
+
+from rucio.common.config import config_get
+from rucio.core.account_counter import create_counters_for_new_account
+from rucio.db.sqla import session, models
+from rucio.db.sqla.constants import AccountStatus, AccountType, IdentityType
+
+
+def build_database(echo=True, tests=False):
+    """ Applies the schema to the database. Run this command once to build the database. """
+    engine = session.get_engine(echo=echo)
+    models.register_models(engine)
+
+    # Put the database under version control
+    alembic_cfg = Config(config_get('alembic', 'cfg'))
+    command.stamp(alembic_cfg, "head")
+
+
+def dump_schema():
+    """ Creates a schema dump to a specific database. """
+    engine = session.get_dump_engine()
+    models.register_models(engine)
+
+
+def destroy_database(echo=True):
+    """ Removes the schema from the database. Only useful for test cases or malicious intents. """
+    engine = session.get_engine(echo=echo)
+    models.unregister_models(engine)
+
+
+def drop_everything(echo=True):
+    """ Pre-gather all named constraints and table names, and drop everything. This is better than using metadata.reflect();
+        metadata.drop_all() as it handles cyclical constraints between tables.
+        Ref. http://www.sqlalchemy.org/trac/wiki/UsageRecipes/DropEverything
+    """
+    engine = session.get_engine(echo=echo)
+    conn = engine.connect()
+
+    # the transaction only applies if the DB supports
+    # transactional DDL, i.e. Postgresql, MS SQL Server
+    trans = conn.begin()
+
+    inspector = reflection.Inspector.from_engine(engine)
+
+    # gather all data first before dropping anything.
+    # some DBs lock after things have been dropped in
+    # a transaction.
+    metadata = MetaData()
+
+    tbs = []
+    all_fks = []
+
+    for table_name in inspector.get_table_names():
+        fks = []
+        for fk in inspector.get_foreign_keys(table_name):
+            if not fk['name']:
+                continue
+            fks.append(ForeignKeyConstraint((), (), name=fk['name']))
+        t = Table(table_name, metadata, *fks)
+        tbs.append(t)
+        all_fks.extend(fks)
+
+    for fkc in all_fks:
+        try:
+            print str(DropConstraint(fkc)) + ';'
+            conn.execute(DropConstraint(fkc))
+        except:
+            print format_exc()
+
+    for table in tbs:
+        try:
+            print str(DropTable(table)).strip() + ';'
+            conn.execute(DropTable(table))
+        except:
+            print format_exc()
+
+    trans.commit()
+
+
+def create_root_account():
+    """ Inserts the default root account to an existing database. Make sure to change the default password later. """
+
+    up_id = 'ddmlab'
+    up_pwd = '2ccee6f6dd1bc2269cddd7cd5e47578e98e430539807c36df23fab7dd13e7583'
+    up_email = 'ph-adp-ddm-lab@cern.ch'
+    x509_id = '/C=CH/ST=Geneva/O=CERN/OU=PH-ADP-CO/CN=DDMLAB Client Certificate/emailAddress=ph-adp-ddm-lab@cern.ch'
+    x509_email = 'ph-adp-ddm-lab@cern.ch'
+    gss_id = 'ddmlab@CERN.CH'
+    gss_email = 'ph-adp-ddm-lab@cern.ch'
+
+    try:
+        up_id = config_get('bootstrap', 'userpass_identity')
+        up_pwd = config_get('bootstrap', 'userpass_pwd')
+        up_email = config_get('bootstrap', 'userpass_email')
+        x509_id = config_get('bootstrap', 'x509_identity')
+        x509_email = config_get('bootstrap', 'x509_email')
+        gss_id = config_get('bootstrap', 'gss_identity')
+        gss_email = config_get('bootstrap', 'gss_email')
+    except:
+        pass
+        # print 'Config values are missing (check rucio.cfg{.template}). Using hardcoded defaults.'
+
+    s = session.get_session()
+
+    account = models.Account(account='root', account_type=AccountType.SERVICE, status=AccountStatus.ACTIVE)
+
+    identity1 = models.Identity(identity=up_id, identity_type=IdentityType.USERPASS, password=up_pwd, salt='0', email=up_email)
+    iaa1 = models.IdentityAccountAssociation(identity=identity1.identity, identity_type=identity1.identity_type, account=account.account, is_default=True)
+
+    # X509 authentication
+    identity2 = models.Identity(identity=x509_id, identity_type=IdentityType.X509, email=x509_email)
+    iaa2 = models.IdentityAccountAssociation(identity=identity2.identity, identity_type=identity2.identity_type, account=account.account, is_default=True)
+
+    # GSS authentication
+    identity3 = models.Identity(identity=gss_id, identity_type=IdentityType.GSS, email=gss_email)
+    iaa3 = models.IdentityAccountAssociation(identity=identity3.identity, identity_type=identity3.identity_type, account=account.account, is_default=True)
+
+    # Account counters
+    create_counters_for_new_account(account='root', session=s)
+
+    # Apply
+    s.add_all([account, identity1, identity2, identity3])
+    s.commit()
+    s.add_all([iaa1, iaa2, iaa3])
+    s.commit()
