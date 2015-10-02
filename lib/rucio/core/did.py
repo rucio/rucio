d4bf7e615d022116494928e6dd4b0a1889d3525a
Vincent Garonne <vgaronne@gmail.com>
2015-10-02 14:22:16 +0200

    [RUCIO-1886] Undertaker inserts now in content history
    Change-Id: Ic21b2dd1f582a846040914819f33ed1d53186673

--- lib/rucio/core/did.py
+++ lib/rucio/core/did.py
@@ -442,36 +442,6 @@ def delete_dids(dids, account, session=None):
         parent_content_clause.append(and_(models.DataIdentifierAssociation.child_scope == did['scope'], models.DataIdentifierAssociation.child_name == did['name']))
         rule_id_clause.append(and_(models.ReplicationRule.scope == did['scope'], models.ReplicationRule.name == did['name']))
 
-        content_clause.append(and_(models.DataIdentifierAssociation.scope == did['scope'], models.DataIdentifierAssociation.name == did['name']))
-        collection_replica_clause.append(and_(models.CollectionReplica.scope == did['scope'],
-                                              models.CollectionReplica.name == did['name']))
-        # Archive content
-        q = session.query(models.DataIdentifierAssociation.scope,
-                          models.DataIdentifierAssociation.name,
-                          models.DataIdentifierAssociation.child_scope,
-                          models.DataIdentifierAssociation.child_name,
-                          models.DataIdentifierAssociation.did_type,
-                          models.DataIdentifierAssociation.child_type,
-                          models.DataIdentifierAssociation.bytes,
-                          models.DataIdentifierAssociation.adler32,
-                          models.DataIdentifierAssociation.md5,
-                          models.DataIdentifierAssociation.guid,
-                          models.DataIdentifierAssociation.events,
-                          models.DataIdentifierAssociation.rule_evaluation,
-                          bindparam("did_created_at",  did['created_at']),
-                          models.DataIdentifierAssociation.created_at,
-                          models.DataIdentifierAssociation.updated_at,
-                          bindparam("deleted_at",  datetime.utcnow())).\
-            filter(and_(models.DataIdentifierAssociation.scope == did['scope'],
-                        models.DataIdentifierAssociation.name == did['name']))
-        ins = Insert(table=models.DataIdentifierAssociationHistory, inline=True).\
-            from_select(('scope', 'name', 'child_scope', 'child_name', 'did_type',
-                         'child_type', 'bytes', 'adler32', 'md5', 'guid', 'events',
-                         'rule_evaluation', 'did_created_at', 'created_at', 'updated_at',
-                         'deleted_at'), q)
-        session.execute(ins)
->>>>>>> [RUCIO-1886] Undertaker inserts now in content history
->>>>>>> [RUCIO-1886] Undertaker inserts now in content history
         # Send message for AMI
         add_message('ERASE', {'account': account,
                               'scope': did['scope'],
