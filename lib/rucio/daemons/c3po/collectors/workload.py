dd2276af7f71e6391569c92e88773bf4055c8259
Thomas Beermann <thomas.beermann@cern.ch>
2015-11-05 15:26:48 +0100

    [RUCIO-1953] first simple workload collector
    Change-Id: I428b20280006bf4bda6d749b2de1ea9a6b4d1af4

---
+++ lib/rucio/daemons/c3po/collectors/workload.py
@@ -0,0 +1,81 @@
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Thomas Beermann, <thomas.beermann@cern.ch>, 2015
+
+import logging
+from json import loads
+from requests import get
+from sys import stdout
+from time import time
+
+from rucio.common.config import config_get, config_get_int
+from rucio.daemons.c3po.utils.timeseries import RedisTimeSeries
+
+logging.basicConfig(stream=stdout,
+                    level=getattr(logging, config_get('common', 'loglevel').upper()),
+                    format='%(asctime)s\t%(process)d\t%(levelname)s\t%(message)s')
+
+
+class WorkloadCollector():
+    """
+    Collector to retrieve the workload from PanDA. It stores it as a time series in Redis and provides
+    the average and maximum number of running jobs for a sliding window.
+    """
+    def __init__(self):
+        self._avg_jobs = {}
+        self._max_jobs = {}
+        self._tms = RedisTimeSeries(config_get('c3po-workload', 'redis_host'), config_get_int('c3po-workload', 'redis_port'), config_get_int('c3po-workload', 'window'))
+
+        self._request_headers = {"Accept": "application/json", "Content-Type": "application/json"}
+        self._request_url = config_get('c3po-workload', 'panda_url')
+        self._tms.delete_keys()
+        self.reload_cache()
+
+    def reload_cache(self):
+        self._tms.trim()
+        for key in self._tms.get_keys():
+            site = key.split('_')[1]
+            job_series = self._tms.get_series(site)
+            num_jobs = len(job_series)
+            if num_jobs > 0:
+                self._avg_jobs[site] = sum(job_series) / num_jobs
+                self._max_jobs[site] = max(job_series)
+
+    def collect_workload(self):
+        start = time()
+        resp = get(self._request_url, headers=self._request_headers)
+        logging.debug("PanDA response took %fs" % (time() - start))
+
+        start = time()
+        jobs = loads(resp.text)
+        logging.debug("decoding JSON response took %fs" % (time() - start))
+        sites = {}
+
+        start = time()
+        for job in jobs:
+            if job['computingsite'] not in sites:
+                sites[job['computingsite']] = 0
+            sites[job['computingsite']] += 1
+        for site, jobs in sites.items():
+            self._tms.add_point(site, jobs)
+
+        logging.debug("processing took %fs" % (time() - start))
+        self.reload_cache()
+
+    def get_avg_jobs(self, site):
+        return self._avg_jobs[site]
+
+    def get_max_jobs(self, site):
+        return self._max_jobs[site]
+
+    def get_sites(self):
+        return self._avg_jobs.keys()
+
+    def get_series(self, site):
+        return self._tms.get_series(site)
