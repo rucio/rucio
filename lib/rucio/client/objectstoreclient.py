e7a116198f51d84c495f002dc84b299180b0da56
Wen Guan <wen.guan@cern.ch>
2016-02-05 16:48:12 +0100

    [RUCIO-2065] update objectstore to adapt rse_account.cfg
    Change-Id: Ia17bcae8f162749512efda68ae57f3167ba03b83

--- lib/rucio/client/objectstoreclient.py
+++ lib/rucio/client/objectstoreclient.py
@@ -23,13 +23,14 @@ class ObjectStoreClient(BaseClient):
     def __init__(self, rucio_host=None, auth_host=None, account=None, ca_cert=None, auth_type=None, creds=None, timeout=None, user_agent='rucio-clients'):
         super(ObjectStoreClient, self).__init__(rucio_host, auth_host, account, ca_cert, auth_type, creds, timeout, user_agent)
 
-    def connect(self, url):
+    def connect(self, rse, url):
         """
+        :param rse: RSE name.
         :param url: URL string.
 
         :returns: OK.
         """
-        url = build_url(self.host, path='/'.join([self.OBJECTSTORE_BASEURL, url]))
+        url = build_url(self.host, path='/'.join([self.OBJECTSTORE_BASEURL, url, rse, 'connect']))
         r = self._send_request(url, type='GET')
         if r.status_code == codes.ok:
             return
@@ -37,13 +38,14 @@ class ObjectStoreClient(BaseClient):
         exc_cls, exc_msg = self._get_exception(headers=r.headers, status_code=r.status_code, data=r.content)
         raise exc_cls(exc_msg)
 
-    def get_signed_url(self, url, operation='read'):
+    def get_signed_url(self, url, rse, operation='read'):
         """
         :param url: URL string.
+        :param rse: RSE name.
 
         :returns: A signed URL refering to the file.
         """
-        url = build_url(self.host, path='/'.join([self.OBJECTSTORE_BASEURL, operation, url]))
+        url = build_url(self.host, path='/'.join([self.OBJECTSTORE_BASEURL, url, rse, operation]))
         r = self._send_request(url, type='GET')
         if r.status_code == codes.ok:
             return r.text
@@ -51,13 +53,14 @@ class ObjectStoreClient(BaseClient):
         exc_cls, exc_msg = self._get_exception(headers=r.headers, status_code=r.status_code, data=r.content)
         raise exc_cls(exc_msg)
 
-    def get_signed_urls(self, urls, operation='read'):
+    def get_signed_urls(self, urls, rse, operation='read'):
         """
         :param ruls: List of URLs.
+        :param rse: RSE name.
 
         :returns: URL dictionaries refering to the files.
         """
-        url = build_url(self.host, path='/'.join([self.OBJECTSTORE_BASEURL, operation]))
+        url = build_url(self.host, path='/'.join([self.OBJECTSTORE_BASEURL, rse, operation]))
         headers = {}
         r = self._send_request(url, headers=headers, type='POST', data=json.dumps(urls))
 
@@ -67,13 +70,14 @@ class ObjectStoreClient(BaseClient):
         exc_cls, exc_msg = self._get_exception(headers=r.headers, status_code=r.status_code, data=r.content)
         raise exc_cls(exc_msg)
 
-    def get_metadata(self, urls):
+    def get_metadata(self, urls, rse):
         """
         :param ruls: List of URLs.
+        :param rse: RSE name.
 
         :returns: Dictionary of metadata refering to the files.
         """
-        url = build_url(self.host, path='/'.join([self.OBJECTSTORE_BASEURL, 'info']))
+        url = build_url(self.host, path='/'.join([self.OBJECTSTORE_BASEURL, 'info', rse]))
         headers = {}
         r = self._send_request(url, headers=headers, type='POST', data=json.dumps(urls))
 
@@ -83,14 +87,15 @@ class ObjectStoreClient(BaseClient):
         exc_cls, exc_msg = self._get_exception(headers=r.headers, status_code=r.status_code, data=r.content)
         raise exc_cls(exc_msg)
 
-    def rename(self, pfn, new_pfn):
+    def rename(self, pfn, new_pfn, rse):
         """
         :param rul: URL string.
         :param new_rul: URL string.
+        :param rse: RSE name.
 
         :returns: Dictionary of metadata refering to the files.
         """
-        url = build_url(self.host, path='/'.join([self.OBJECTSTORE_BASEURL, 'rename']))
+        url = build_url(self.host, path='/'.join([self.OBJECTSTORE_BASEURL, 'rename', rse]))
         headers = {}
         urls = {'url': pfn, 'new_url': new_pfn}
         r = self._send_request(url, headers=headers, type='POST', data=json.dumps(urls))
