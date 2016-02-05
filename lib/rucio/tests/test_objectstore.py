874fa4484226c649bec4966cd265453579f9a564
Wen Guan <wguan.icedew@gmail.com>
2016-02-05 16:48:12 +0100

    [RUCIO-2065] update objectstore to adapt rse_account.cfg
    Change-Id: Ia17bcae8f162749512efda68ae57f3167ba03b83

--- lib/rucio/tests/test_objectstore.py
+++ lib/rucio/tests/test_objectstore.py
@@ -19,8 +19,9 @@ from rucio.common import exception
 class TestObjectStoreCommon:
 
     def setup(self):
-        self.url = 's3://cephgw.usatlas.bnl.gov:8443/rucio_bucket/test_public'
-        ret = objectstore.get_signed_urls([self.url], operation='write')
+        self.url = 's3+https://cephgw.usatlas.bnl.gov:8443/rucio_bucket/test_public'
+        self.rse = 'BNL-OSG2_ES'
+        ret = objectstore.get_signed_urls([self.url], rse=self.rse, operation='write')
         if isinstance(ret[self.url], Exception):
             raise ret[self.url]
         command = 'curl --request PUT --upload-file /bin/hostname "%s"' % str(ret[self.url])
@@ -32,11 +33,11 @@ class TestObjectStoreCommon:
 
     def test_connect(self):
         """ OBJECTSTORE (COMMON): Connect """
-        objectstore.connect(self.url)
+        objectstore.connect(self.rse, self.url)
 
     def test_get_signed_urls_read(self):
         """ OBJECTSTORE (COMMON): Get signed urls for read """
-        ret = objectstore.get_signed_urls([self.url], operation='read')
+        ret = objectstore.get_signed_urls([self.url], rse=self.rse, operation='read')
         if isinstance(ret[self.url], Exception):
             raise ret[self.url]
 
@@ -56,7 +57,7 @@ class TestObjectStoreCommon:
 
     def test_get_signed_urls_write(self):
         """ OBJECTSTORE (COMMON): Get signed urls for write """
-        ret = objectstore.get_signed_urls([self.url], operation='write')
+        ret = objectstore.get_signed_urls([self.url], rse=self.rse, operation='write')
         if isinstance(ret[self.url], Exception):
             raise ret[self.url]
 
@@ -78,7 +79,7 @@ class TestObjectStoreCommon:
     def test_get_signed_urls_read_not_exists(self):
         """ OBJECTSTORE (COMMON): Get signed not exist urls for read """
         url = '%s_not_exist' % (self.url)
-        ret = objectstore.get_signed_urls([url], operation='read')
+        ret = objectstore.get_signed_urls([url], rse=self.rse, operation='read')
         if isinstance(ret[url], Exception):
             raise ret[url]
         raise Exception("Respone not as expected: should catch SourceNotFound")
@@ -86,7 +87,7 @@ class TestObjectStoreCommon:
     def test_get_metadata(self):
         """ OBJECTSTORE (COMMON): Get metadata """
         url = self.url
-        ret = objectstore.get_metadata([url])
+        ret = objectstore.get_metadata([url], rse=self.rse)
         if isinstance(ret[url], Exception):
             raise ret[url]
         if 'filesize' not in ret[url]:
@@ -96,11 +97,11 @@ class TestObjectStoreCommon:
         """ OBJECTSTORE (COMMON): Rename """
         url = self.url
         new_url = '%s_new' % url
-        objectstore.rename(url, new_url)
-        ret = objectstore.get_metadata([url])
+        objectstore.rename(url, new_url, rse=self.rse)
+        ret = objectstore.get_metadata([url], rse=self.rse)
         if not isinstance(ret[url], exception.SourceNotFound):
             raise ret[url]
-        ret = objectstore.get_metadata([new_url])
+        ret = objectstore.get_metadata([new_url], rse=self.rse)
         if isinstance(ret[new_url], Exception):
             raise ret[new_url]
         if 'filesize' not in ret[new_url]:
@@ -110,7 +111,7 @@ class TestObjectStoreCommon:
     def test_get_metadata_not_exist(self):
         """ OBJECTSTORE (COMMON): Get metadata for not exist url """
         url = '%s_not_exist' % (self.url)
-        ret = objectstore.get_metadata([url])
+        ret = objectstore.get_metadata([url], rse=self.rse)
         if isinstance(ret[url], Exception):
             raise ret[url]
         raise Exception("Respone not as expected: should catch SourceNotFound")
@@ -121,7 +122,7 @@ class TestObjectStoreCommon:
         for i in range(10):
             url = '%s_%s' % (self.url, i)
             urls.append(url)
-        ret = objectstore.get_signed_urls(urls, operation='write')
+        ret = objectstore.get_signed_urls(urls, rse=self.rse, operation='write')
         for url in urls:
             if isinstance(url, Exception):
                 raise ret[self.url]
@@ -134,7 +135,7 @@ class TestObjectStoreCommon:
             if 'AccessDenied' in output:
                 raise Exception(output)
 
-        ret = objectstore.delete(urls)
+        ret = objectstore.delete(urls, rse=self.rse)
         for url in urls:
             if isinstance(ret[url], Exception):
                 raise ret[url]
@@ -145,7 +146,7 @@ class TestObjectStoreCommon:
         for i in range(10):
             url = '%s_%s' % (self.url, i)
             urls.append(url)
-        ret = objectstore.get_signed_urls(urls, operation='write')
+        ret = objectstore.get_signed_urls(urls, rse=self.rse, operation='write')
         for url in urls:
             if isinstance(url, Exception):
                 raise ret[self.url]
@@ -158,7 +159,7 @@ class TestObjectStoreCommon:
             if 'AccessDenied' in output:
                 raise Exception(output)
 
-        status, output = objectstore.delete_dir(self.url)
+        status, output = objectstore.delete_dir(self.url, rse=self.rse)
         if status:
             raise Exception(output)
 
@@ -167,8 +168,9 @@ class TestObjectStoreClients:
 
     def setup(self):
         self.os_client = ObjectStoreClient()
-        self.url = 's3://cephgw.usatlas.bnl.gov:8443/rucio_bucket/test_public'
-        ret = objectstore.get_signed_urls([self.url], operation='write')
+        self.url = 's3+https://cephgw.usatlas.bnl.gov:8443/rucio_bucket/test_public'
+        self.rse = 'BNL-OSG2_ES'
+        ret = objectstore.get_signed_urls([self.url], rse=self.rse, operation='write')
         if isinstance(ret[self.url], Exception):
             raise ret[self.url]
         command = 'curl --request PUT --upload-file /bin/hostname "%s"' % ret[self.url]
@@ -179,12 +181,12 @@ class TestObjectStoreClients:
             raise Exception(output)
 
     def test_connect(self):
-        """ OBJECTSTORE (COMMON): Connect """
-        self.os_client.connect(self.url)
+        """ OBJECTSTORE (CLIENT): Connect """
+        self.os_client.connect(self.rse, self.url)
 
     def test_get_signed_url_read(self):
         """ OBJECTSTORE (CLIENT): Get signed url for read """
-        ret = self.os_client.get_signed_url(self.url, operation='read')
+        ret = self.os_client.get_signed_url(self.url, rse=self.rse, operation='read')
         if type(ret) not in [str, unicode]:
             raise Exception("Return %s is not as expected.")
 
@@ -204,7 +206,7 @@ class TestObjectStoreClients:
 
     def test_get_signed_url_write(self):
         """ OBJECTSTORE (CLIENT): Get signed url for write """
-        ret = self.os_client.get_signed_url(self.url, operation='write')
+        ret = self.os_client.get_signed_url(self.url, rse=self.rse, operation='write')
         if type(ret) not in [str, unicode]:
             raise Exception("Return %s is not as expected.")
 
@@ -226,12 +228,12 @@ class TestObjectStoreClients:
     def test_get_signed_url_read_not_exists(self):
         """ OBJECTSTORE (CLIENT): Get signed not exist url for read """
         url = '%s_not_exist' % (self.url)
-        self.os_client.get_signed_url(url, operation='read')
+        self.os_client.get_signed_url(url, rse=self.rse, operation='read')
         raise Exception("Respone not as expected: should catch SourceNotFound")
 
     def test_get_signed_urls_read(self):
         """ OBJECTSTORE (CLIENT): Get signed urls for read """
-        ret = self.os_client.get_signed_urls([self.url], operation='read')
+        ret = self.os_client.get_signed_urls([self.url], rse=self.rse, operation='read')
         if isinstance(ret[self.url], Exception):
             raise ret[self.url]
 
@@ -251,7 +253,7 @@ class TestObjectStoreClients:
 
     def test_get_signed_urls_write(self):
         """ OBJECTSTORE (CLIENT): Get signed urls for write """
-        ret = self.os_client.get_signed_urls([self.url], operation='write')
+        ret = self.os_client.get_signed_urls([self.url], rse=self.rse, operation='write')
         if isinstance(ret[self.url], Exception):
             raise ret[self.url]
 
@@ -273,13 +275,13 @@ class TestObjectStoreClients:
     def test_get_signed_urls_read_not_exists(self):
         """ OBJECTSTORE (CLIENT): Get signed not exist urls for read """
         url = '%s_not_exist' % (self.url)
-        self.os_client.get_signed_urls([url], operation='read')
+        self.os_client.get_signed_urls([url], rse=self.rse, operation='read')
         raise Exception("Respone not as expected: should catch SourceNotFound")
 
     def test_get_metadata(self):
         """ OBJECTSTORE (CLIENT): Get metadata """
         url = self.url
-        ret = self.os_client.get_metadata([url])
+        ret = self.os_client.get_metadata([url], rse=self.rse)
         if isinstance(ret[url], Exception):
             raise ret[url]
         if 'filesize' not in ret[url]:
@@ -289,20 +291,20 @@ class TestObjectStoreClients:
     def test_get_metadata_not_exist(self):
         """ OBJECTSTORE (CLIENT): Get metadata for not exist url """
         url = '%s_not_exist' % (self.url)
-        self.os_client.get_metadata([url])
+        self.os_client.get_metadata([url], rse=self.rse)
         raise Exception("Respone not as expected: should catch SourceNotFound")
 
     def test_rename(self):
         """ OBJECTSTORE (CLIENT): Rename """
         url = self.url
         new_url = '%s_new' % url
-        self.os_client.rename(url, new_url)
+        self.os_client.rename(url, new_url, rse=self.rse)
         try:
-            self.os_client.get_metadata([url])
+            self.os_client.get_metadata([url], rse=self.rse)
         except exception.SourceNotFound:
             pass
 
-        ret = self.os_client.get_metadata([new_url])
+        ret = self.os_client.get_metadata([new_url], rse=self.rse)
         if isinstance(ret[new_url], Exception):
             raise ret[new_url]
         if 'filesize' not in ret[new_url]:
