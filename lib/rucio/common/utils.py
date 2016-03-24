93c817160f0e491c48708fb7b0563bd56bf480e7
Vincent Garonne <vincent.garonne@cern.ch>
2016-03-24 09:23:31 +0100

    [RUCIO-2102] Fallback to the getuser if not defined

--- lib/rucio/common/utils.py
+++ lib/rucio/common/utils.py
@@ -22,6 +22,8 @@ import re
 import subprocess
 import zlib
 
+
+from getpass import getuser
 from itertools import izip_longest
 from logging import getLogger, Formatter
 from logging.handlers import RotatingFileHandler
@@ -461,11 +463,19 @@ def get_tmp_dir():
 
     :return: A path.
     """
-    tmp_dir, user = None, pwd.getpwuid(os.getuid()).pw_name
+    user, tmp_dir = None, None
+    try:
+        user = pwd.getpwuid(os.getuid()).pw_name
+    except:
+        pass
+
     for env_var in ('TMP', 'TMPDIR', 'TEMP'):
         if env_var in os.environ:
             tmp_dir = os.environ[env_var]
             break
+
+    if not user:
+        user = getuser()
 
     if not tmp_dir:
         return '/tmp/' + user + '/'
