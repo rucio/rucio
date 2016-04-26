34b69f3ac41a869b6a9fd24373c4a14be78cead8
Martin Barisits <martin.barisits@cern.ch>
2016-04-26 16:25:09 +0200

    [RUCIO-2173] BB8 Prototype

--- lib/rucio/daemons/bb8/bb8.py
+++ lib/rucio/daemons/bb8/bb8.py
@@ -17,10 +17,8 @@ import threading
 import sys
 import os
 
-from datetime import datetime
 
 from rucio.core.heartbeat import live, die, sanity_check
-from rucio.core.rule import get_rule, add_rule, update_rule
 from rucio.common.config import config_get
 
 graceful_stop = threading.Event()
@@ -30,49 +28,13 @@ logging.basicConfig(stream=sys.stdout,
                     format='%(asctime)s\t%(process)d\t%(levelname)s\t%(message)s')
 
 
-def rebalance_rule(parent_rule_id, activity, rse_expression):
-    """
-    Rebalance a replication rule to a new RSE
-
-    :param parent_rule_id:       Replication rule to be rebalanced.
-    :param activity:             Activity to be used for the rebalancing.
-    :param rse_expression:       RSE expression of the new rule.
-    :returns:                    The new child rule id.
-    """
-    parent_rule = get_rule(rule_id=parent_rule_id)
-
-    if parent_rule['expires_at'] is None:
-        lifetime = None
-    else:
-        lifetime = (datetime.utcnow() - parent_rule['expires_at']).days * 24 * 3600 + (datetime.utcnow() - parent_rule['expires_at']).seconds
-
-    child_rule = add_rule(dids=[{'scope': parent_rule['scope'],
-                                 'name': parent_rule['name']}],
-                          account=parent_rule['account'],
-                          coppies=parent_rule['copies'],
-                          rse_expression=rse_expression,
-                          grouping=parent_rule['grouping'],
-                          weight=parent_rule['weight'],
-                          lifetime=lifetime,
-                          locked=parent_rule['locked'],
-                          subscription_id=parent_rule['subscription_id'],
-                          source_replica_expression=None,
-                          activity=activity,
-                          notify=parent_rule['notify'],
-                          purge_replicas=parent_rule['purge_replicas'],
-                          ignore_availability=True,
-                          comment=parent_rule['comment'],
-                          ask_approval=False,
-                          asynchronous=False)[0]
-
-    update_rule(rule_id=parent_rule_id, options={'child_rule_id': child_rule, 'lifetime': 1})
-    return child_rule
-
-
 def rule_rebalancer(once=False):
     """
     Main loop to rebalancer rules automatically
     """
+
+    raise NotImplementedError()
+
     hostname = socket.gethostname()
     pid = os.getpid()
     current_thread = threading.current_thread()
@@ -94,6 +56,8 @@ def stop(signum=None, frame=None):
     Graceful exit.
     """
 
+    raise NotImplementedError()
+
     graceful_stop.set()
 
 
@@ -101,6 +65,8 @@ def run(once=False, threads=1):
     """
     Starts up the Judge-Clean threads.
     """
+
+    raise NotImplementedError()
 
     hostname = socket.gethostname()
     sanity_check(executable='rucio-bb8', hostname=hostname)
