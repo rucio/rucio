d2eaf609c48de6715b223cf86304e013a9634bee
Cedric Serfon <cedric.serfon@cern.ch>
2016-08-30 15:54:54 +0200

    [RUCIO-2416] Manual management of Lifetime Model exceptions in Atropos

--- lib/rucio/common/policy.py
+++ lib/rucio/common/policy.py
@@ -16,8 +16,10 @@ from ConfigParser import NoOptionError, NoSectionError
 from datetime import datetime, timedelta
 from dogpile.cache import make_region
 from dogpile.cache.api import NoValue
+from sqlalchemy.orm.exc import NoResultFound
 
 from rucio.db.sqla import models
+from rucio.db.sqla.session import read_session
 from rucio.common.config import config_get
 from rucio.common.exception import ScratchDiskLifetimeConflict
 from rucio.core.account import has_account_attribute
@@ -55,13 +57,17 @@ def get_lifetime_policy():
     return lifetime_dict
 
 
-def define_eol(scope, name, rses, session):
+@read_session
+def define_eol(scope, name, rses, session=None):
     # Check if on ATLAS managed space
     if [rse for rse in rses if list_rse_attributes(rse=None, rse_id=rse['id'], session=session).get('type') in ['LOCALGROUPDISK', 'LOCALGROUPTAPE', 'GROUPDISK', 'GROUPTAPE']]:
         return None
     # Now check the lifetime policy
-    did = session.query(models.DataIdentifier).filter(models.DataIdentifier.scope == scope,
-                                                      models.DataIdentifier.name == name).one()
+    try:
+        did = session.query(models.DataIdentifier).filter(models.DataIdentifier.scope == scope,
+                                                          models.DataIdentifier.name == name).one()
+    except NoResultFound:
+        return None
     policy_dict = get_lifetime_policy()
     did_type = 'other'
     if scope.startswith('mc'):
@@ -119,12 +125,24 @@ def define_eol(scope, name, rses, session):
                     match_policy = False
                     break
             if match_policy:
-                lifetime_value, extension = int(policy['age']) * 30, int(policy['extension']) * 30
+                if int(policy['age']) > 12:
+                    years = int(int(policy['age']) / 12)
+                    months = int(policy['age']) - years * 12
+                    lifetime_value = 365 * years + 30 * months
+                else:
+                    lifetime_value = int(policy['age']) * 30
+                if int(policy['extension']) > 12:
+                    years = int(int(policy['extension']) / 12)
+                    months = int(policy['extension']) - years * 12
+                    extension = 365 * years + 30 * months
+                else:
+                    extension = int(policy['extension']) * 30
+
                 default_eol_at = did.created_at + timedelta(days=lifetime_value)
                 if default_eol_at > datetime.utcnow():
                     eol_at = default_eol_at
                 elif did.accessed_at:
-                    eol_at = did.accessed_at + timedelta(extension)
+                    eol_at = did.accessed_at + timedelta(days=extension)
                     if eol_at < default_eol_at:
                         eol_at = default_eol_at
                 else:
@@ -134,7 +152,39 @@ def define_eol(scope, name, rses, session):
     return None
 
 
-def get_scratch_policy(account, rses, lifetime, session):
+def get_lifetime_exceptions():
+    lifetime_exceptions = REGION.get('lifetime_exceptions')
+    if type(lifetime_exceptions) is NoValue:
+        exceptions = {}
+        try:
+            lifetime_dir = config_get('lifetime', 'directory')
+        except (NoSectionError, NoOptionError):
+            lifetime_dir = '/opt/rucio/etc/policies'
+        for dtype in ['data', 'mc', 'valid', 'other']:
+            input_file_name = '%s/exceptions_%s.json' % (lifetime_dir, dtype)
+            if os.path.isfile(input_file_name):
+                with open(input_file_name, 'r') as input_file:
+                    for exception in json.load(input_file):
+                        date, extension_date = exception['date'], exception.get('expiration_date')
+                        date = datetime.strptime(date, '%Y-%m-%d')
+                        if not extension_date:
+                            extension_date = date + timedelta(days=90)
+                        else:
+                            extension_date = datetime.strptime(extension_date, '%Y-%m-%d')
+                        for dsn in exception['datasets']:
+                            if dsn in exceptions:
+                                if exceptions[dsn] < extension_date:
+                                    # print 'Lifetime for %s will be extended from %s to %s' % (dsn, exceptions[dsn], extension_date)
+                                    exceptions[dsn] = extension_date
+                            else:
+                                exceptions[dsn] = extension_date
+
+        REGION.set('lifetime_exceptions', exceptions)
+    return exceptions
+
+
+@read_session
+def get_scratch_policy(account, rses, lifetime, session=None):
     vo_name = get_vo()
     if vo_name == 'atlas':
         # Check SCRATCHDISK Policy
