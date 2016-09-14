c5f3bc8e07e496990123ec0cda49884a1d2976d8
Vincent Garonne <vgaronne@gmail.com>
2016-09-14 10:33:48 +0200

    [RUCIO-2438] Configurable permission module

---
+++ lib/rucio/core/permission/__init__.py
@@ -0,0 +1,28 @@
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Vincent Garonne, <vincent.garonne@cern.ch>, 2016
+
+from ConfigParser import NoOptionError, NoSectionError
+
+from rucio.common import config
+
+if config.config_has_section('permission'):
+
+    try:
+        policy = config.config_get('permission', 'policy')
+    except (NoOptionError, NoSectionError) as error:
+        policy = 'generic'
+
+    if policy.lower() == 'generic':
+        from rucio.core.permission.generic import *  # NOQA
+    elif policy.lower() == 'atlas':
+        from rucio.core.permission.atlas import *  # NOQA
+    else:
+        from rucio.core.permission.generic import *  # NOQA
+else:
+    from rucio.core.permission.generic import *  # NOQA
