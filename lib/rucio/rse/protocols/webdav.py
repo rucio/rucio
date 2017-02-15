fe77212ca7518ae63762e973446e104e40b2a9fd
Vincent Garonne <vincent.garonne@cern.ch>
2017-02-15 10:18:36 +0100

    [RUCIO-2530] Reaper reports critical errors for Dav deletion

--- lib/rucio/rse/protocols/webdav.py
+++ lib/rucio/rse/protocols/webdav.py
@@ -8,7 +8,7 @@
 
   Authors:
   - Cedric Serfon <cedric.serfon@cern.ch>, 2012-2016
-  - Vincent Garonne, <vincent.garonne@cern.ch>, 2013
+  - Vincent Garonne, <vincent.garonne@cern.ch>, 2013-2017
   - Sylvain Blunier, <sylvain.blunier@cern.ch>, 2016
 '''
 
