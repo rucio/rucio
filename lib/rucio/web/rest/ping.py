065fa568229d8ace0bf257ba43220cc34f70cc4c
Vincent Garonne <vincent.garonne@cern.ch>
2017-02-28 17:07:06 +0100

    [RUCIO-2588] pylint fixes: try with web.py rest endpoint

--- lib/rucio/web/rest/ping.py
+++ lib/rucio/web/rest/ping.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
-# Copyright European Organization for Nuclear Research (CERN)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# You may not use this file except in compliance with the License.
-# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-#
-# Authors:
-# - Vincent Garonne, <vincent.garonne@cern.ch>, 2012-2013
-# - Thomas Beermann, <thomas.beermann@cern.ch>, 2012
-# - Mario Lassnig, <mario.lassnig@cern.ch>, 2014
+''' Copyright European Organization for Nuclear Research (CERN)
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ You may not use this file except in compliance with the License.
+ You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+ Authors:
+ - Vincent Garonne, <vincent.garonne@cern.ch>, 2012-2017
+ - Thomas Beermann, <thomas.beermann@cern.ch>, 2012
+ - Mario Lassnig, <mario.lassnig@cern.ch>, 2014
+'''
 
 from json import dumps
 from logging import getLogger, StreamHandler, DEBUG
@@ -17,15 +18,19 @@ from web import application, ctx, header
 from rucio import version
 from rucio.web.rest.common import RucioController
 
-logger = getLogger("rucio.rucio")
-sh = StreamHandler()
-sh.setLevel(DEBUG)
-logger.addHandler(sh)
+LOGGER = getLogger("rucio.rucio")
+SH = StreamHandler()
+SH.setLevel(DEBUG)
+LOGGER.addHandler(SH)
 
-urls = ('/?$', 'Ping')
+URLS = ('/?$', 'Ping')
 
 
 class Ping(RucioController):
+    '''
+    Ping class
+    '''
+
     def GET(self):
         """
         .. http:get:: /ping
@@ -55,7 +60,6 @@ class Ping(RucioController):
             :statuscode 200: no error
             :statuscode 500: InternalError
         """
-
         header('Access-Control-Allow-Origin', ctx.env.get('HTTP_ORIGIN'))
         header('Access-Control-Allow-Headers', ctx.env.get('HTTP_ACCESS_CONTROL_REQUEST_HEADERS'))
         header('Access-Control-Allow-Methods', '*')
@@ -69,9 +73,9 @@ class Ping(RucioController):
         return dumps({"version": version.version_string()})
 
 
-"""----------------------
-   Web service startup
-----------------------"""
+#----------------------
+#   Web service startup
+#----------------------
 
-app = application(urls, globals())
-application = app.wsgifunc()
+APP = application(URLS, globals())
+application = APP.wsgifunc()
