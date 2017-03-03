db93383f57b9ec11966fa136a05da985704351b2
Thomas Beermann <thomas.beermann@cern.ch>
2017-03-03 14:39:49 +0100

    [RUCIO-2588] fix

--- lib/rucio/tests/test_hermes.py
+++ lib/rucio/tests/test_hermes.py
@@ -1,15 +1,13 @@
-'''
-  Copyright European Organization for Nuclear Research (CERN)
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  You may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-  http://www.apache.org/licenses/LICENSE-2.0
-
-  Authors:
-  - Mario Lassnig, <mario.lassnig@cern.ch>, 2015
-  - Thomas Beermann, <thomas.beermann@cern.ch>, 2017
-'''
+#  Copyright European Organization for Nuclear Research (CERN)
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Authors:
+#  - Mario Lassnig, <mario.lassnig@cern.ch>, 2015
+#  - Thomas Beermann, <thomas.beermann@cern.ch>, 2017
 
 """
 Hermes Test
