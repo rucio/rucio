b728805f7257be53e9676503d698497b3fb50cd4
Vincent Garonne <vgaronne@gmail.com>
2017-03-14 16:49:12 +0100

    [RUCIO-2588] pyyyyylinteuhhh fixerghhheu

--- lib/rucio/api/permission.py
+++ lib/rucio/api/permission.py
@@ -1,14 +1,16 @@
-# Copyright European Organization for Nuclear Research (CERN)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# You may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#              http://www.apache.org/licenses/LICENSE-2.0
-#
-# Authors:
-# - Angelos Molfetas, <angelos.molfetas@cern.ch>, 2012
-# - Vincent Garonne, <vincent.garonne@cern.ch>, 2012-2013
-# - Mario Lassnig, <mario.lassnig@cern.ch>, 2012
+'''
+  Copyright European Organization for Nuclear Research (CERN)
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  You may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+               http://www.apache.org/licenses/LICENSE-2.0
+
+  Authors:
+  - Angelos Molfetas, <angelos.molfetas@cern.ch>, 2012
+  - Vincent Garonne, <vincent.garonne@cern.ch>, 2012-2017
+  - Mario Lassnig, <mario.lassnig@cern.ch>, 2012
+'''
 
 from rucio.core import permission
 
