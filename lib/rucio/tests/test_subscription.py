9616fa90d3603805490dddec619c0a8e8947a9a5
Cedric Serfon <cedric.serfon@cern.ch>
2017-09-19 15:39:53 +0200

    [RUCIO-2796] Fix comments

--- lib/rucio/tests/test_subscription.py
+++ lib/rucio/tests/test_subscription.py
@@ -1,14 +1,16 @@
-# Copyright European Organization for Nuclear Research (CERN)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# You may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Authors:
-# - Cedric Serfon, <cedric.serfon@cern.ch>, 2013-2015, 2017
-# - Thomas Beermann, <thomas.beermann@cern.ch>, 2014
-# - Martin Barisits, <martin.barisits@cern.ch>, 2015-2016
+"""
+ Copyright European Organization for Nuclear Research (CERN)
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ You may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Authors:
+ - Cedric Serfon, <cedric.serfon@cern.ch>, 2013-2015, 2017
+ - Thomas Beermann, <thomas.beermann@cern.ch>, 2014
+ - Martin Barisits, <martin.barisits@cern.ch>, 2015-2016
+"""
 
 from json import dumps, loads
 
