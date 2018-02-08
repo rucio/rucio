68f0a938d395257cbfa158a447d5bc3d1d09ddc1
Thomas Beermann <thomas.beermann@cern.ch>
2018-02-08 14:57:29 +0100

    REST: first commit with new flask api and sphinx documentation #586

--- lib/rucio/web/rest/common.py
+++ lib/rucio/web/rest/common.py
@@ -8,10 +8,12 @@
   Authors:
   - Vincent Garonne, <vincent.garonne@cern.ch>, 2013 - 2017
   - Mario Lassnig, <mario.lassnig@cern.ch>, 2014
+  - Thomas Beermann, <thomas.beermann@cern.ch>, 2018
 
 REST utilities
 """
 
+from flask import request
 from json import loads
 from time import time
 from traceback import format_exc
@@ -20,8 +22,45 @@ from web.webapi import Created, HTTPError, OK, seeother
 
 from rucio.api.authentication import validate_auth_token
 from rucio.common.exception import RucioException
-from rucio.common.utils import generate_http_error, generate_uuid
+from rucio.common.utils import generate_http_error, generate_http_error_flask, generate_uuid
 from rucio.core.monitor import record_timer
+
+
+def before_flask_request():
+    if request.environ.get('REQUEST_METHOD') == 'OPTIONS':
+        raise "", 200
+
+    auth_token = request.environ.get('HTTP_X_RUCIO_AUTH_TOKEN')
+
+    try:
+        auth = validate_auth_token(auth_token)
+    except RucioException, e:
+        return generate_http_error_flask(500, e.__class__.__name__, e.args[0][0])
+    except Exception, e:
+        print format_exc()
+        return e, 500
+
+    if auth is None:
+        return generate_http_error_flask(401, 'CannotAuthenticate', 'Cannot authenticate with given credentials')
+
+    request.environ['issuer'] = auth.get('account')
+    request.environ['identity'] = auth.get('identity')
+    request.environ['request_id'] = generate_uuid()
+    request.environ['start_time'] = time()
+
+
+def after_flask_request(response):
+    response.headers['Access-Control-Allow-Origin'] = request.environ.get('HTTP_ORIGIN')
+    response.headers['Access-Control-Allow-Headers']= request.environ.get('HTTP_ACCESS_CONTROL_REQUEST_HEADERS')
+    response.headers['Access-Control-Allow-Methods'] = '*'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+
+    if request.environ.get('REQUEST_METHOD') == 'GET':
+        response.headers['Cache-Control'] = 'no-cache, no-store, max-age=0, must-revalidate'
+        response.headers['Cache-Control'] = 'post-check=0, pre-check=0'
+        response.headers['Pragma'] = 'no-cache'
+
+    return response
 
 
 def rucio_loadhook():
---
+++ lib/rucio/web/rest/flaskapi/account.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Thomas Beermann, <thomas.beermann@cern.ch>, 2018
+
+from json import dumps
+
+from rucio.api.scope import list_scopes
+from rucio.web.rest.common import before_flask_request, after_flask_request
+
+from flask import Flask, Blueprint
+from flask.views import MethodView
+
+class Attributes(MethodView):
+    def get(self, account):
+        """Return collection of posts.
+
+        .. :quickref: Posts Collection; Get collection of posts.
+
+        **Example request**:
+
+        .. sourcecode:: http
+
+            GET /posts/ HTTP/1.1
+            Host: example.com
+            Accept: application/json
+
+        **Example response**:
+
+        .. sourcecode:: http
+
+            HTTP/1.1 200 OK
+            Vary: Accept
+            Content-Type: application/json
+
+            [
+              {
+                "post_id": 12345,
+                "author": "/author/123/",
+                "tags": ["sphinx", "rst", "flask"],
+                "title": "Documenting API in Sphinx with httpdomain",
+                "body": "How to..."
+              },
+              {
+                "post_id": 12346,
+                "author": "/author/123/",
+                "tags": ["python3", "typehints", "annotations"],
+                "title": "To typehint or not to typehint that is the question",
+                "body": "Static checking in python..."
+              }
+            ]
+
+        :query sort: sorting order e.g. sort=author,-pub_date
+        :query q: full text search query
+        :resheader Content-Type: application/json
+        :status 200: posts found
+        :returns: :class:`myapp.objects.Post`
+        """
+        return dumps(list_scopes())
+
+    def post(self, account, key):
+        """Add new post.
+
+        .. :quickref: Posts Collection; Add new post to collection.
+    
+        :reqheader Accept: application/json
+        :<json string title: post title
+        :<json string body: post body
+        :<json string author: author id
+        :<json List[string] tags: tags list
+        :>json int: id
+        :resheader Content-Type: application/json
+        :resheader Location: post url
+        :status 201: post created
+        :status 400: malformed request
+        :status 422: invalid parameters
+        """
+        pass
+
+    def delete(self, account, key):
+        pass
+
+
+bp = Blueprint('did', __name__)
+account_view = Attributes.as_view('account')
+bp.add_url_rule('/', view_func=account_view, methods=['GET',])
+bp.add_url_rule('/<account>/<scope>', view_func=account_view, methods=['POST',])
+
+app = Flask(__name__)
+app.register_blueprint(bp)
+app.before_request(before_flask_request)
+app.after_request(after_flask_request)
+
+
+def make_doc():
+    """ Only used for sphinx documentation to add the prefix """
+    doc_app = Flask(__name__)
+    doc_app.register_blueprint(bp, url_prefix='/dids')
+    return doc_app
+
+
+if __name__ == "__main__":
+    app.run()
