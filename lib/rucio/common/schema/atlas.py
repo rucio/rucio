fef3e5f16d1ff48d005b850bb70c06f69b56bb13
Mario Lassnig <mario.lassnig@cern.ch>
2018-05-04 11:17:34 +0200

    schema: add root and webdav functional tests for atlas; Fix #1117

--- lib/rucio/common/schema/atlas.py
+++ lib/rucio/common/schema/atlas.py
@@ -1,18 +1,20 @@
-'''
-  Copyright European Organization for Nuclear Research (CERN)
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  You may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-  http://www.apache.org/licenses/LICENSE-2.0
-
-  Authors:
-  - Vincent Garonne, <vincent.garonne@cern.ch>, 2013-2017
-  - Cedric Serfon, <cedric.serfon@cern.ch>, 2014-2015, 2017
-  - Joaquin Bogado, <joaquin.bogado@cern.ch>, 2015
-  - Mario Lassnig, <mario.lassnig@cern.ch>, 2015
-  - Martin Barisits, <martin.barisits@cern.ch>, 2016
-'''
+# Copyright 2017-2018 CERN for the benefit of the ATLAS collaboration.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors:
+# - Vincent Garonne <vgaronne@gmail.com>, 2017
+# - Mario Lassnig <mario.lassnig@cern.ch>, 2018
 
 from jsonschema import validate, ValidationError
 
@@ -37,7 +39,8 @@ ACCOUNT_TYPE = {"description": "Account type",
 ACTIVITY = {"description": "Activity name",
             "type": "string",
             "enum": ["Data Brokering", "Data Consolidation", "Data rebalancing",
-                     "Debug", "Express", "Functional Test", "Group Subscriptions",
+                     "Debug", "Express", "Functional Test", "Functional Test XrootD",
+                     "Functional Test WebDAV", "Group Subscriptions",
                      "Production Input", "Production Output",
                      "Analysis Input", "Analysis Output", "Staging",
                      "T0 Export", "T0 Tape", "Upload/Download (Job)",
