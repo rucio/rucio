c0016d2e2984ec3dedfb05505dcd6e00b65d6276
Martin Barisits <martin.barisits@cern.ch>
2018-06-13 11:36:27 +0200

    Client: Add a 600s timeout to all client commands; Fix #1204

--- lib/rucio/client/touchclient.py
+++ lib/rucio/client/touchclient.py
@@ -15,6 +15,7 @@
 # Authors:
 # - Thomas Beermann <thomas.beermann@cern.ch>, 2017
 # - Vincent Garonne <vgaronne@gmail.com>, 2018
+# - Martin Barisits <martin.barisits@cern.ch>, 2018
 
 from json import dumps
 from requests import post
@@ -34,7 +35,7 @@ class TouchClient(BaseClient):
     DIDS_BASEURL = 'dids'
     TRACES_BASEURL = 'traces'
 
-    def __init__(self, rucio_host=None, auth_host=None, account=None, ca_cert=None, auth_type=None, creds=None, timeout=None, user_agent='rucio-clients'):
+    def __init__(self, rucio_host=None, auth_host=None, account=None, ca_cert=None, auth_type=None, creds=None, timeout=600, user_agent='rucio-clients'):
         super(TouchClient, self).__init__(rucio_host, auth_host, account, ca_cert, auth_type, creds, timeout, user_agent)
 
     def touch(self, scope, name, rse=None):
