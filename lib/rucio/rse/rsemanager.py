3128a3a6fcf94974c1a06a579d1c5f604b023e1e
Tomas Javurek <tomasjavurek09@gmail.com>
2018-07-25 14:57:02 +0200

    rsemgr: adding gs False Fix - 1364

--- lib/rucio/rse/rsemanager.py
+++ lib/rucio/rse/rsemanager.py
@@ -409,6 +409,7 @@ def upload(rse_settings, lfns, source_dir=None, force_pfn=None, force_scheme=Non
                         protocol_delete.delete('%s.rucio.upload' % list(protocol_delete.lfns2pfns(make_valid_did(lfn)).values())[0])
                     except Exception as e:
                         ret['%s:%s' % (scope, name)] = exception.RSEOperationNotSupported('Unable to remove temporary file %s.rucio.upload: %s' % (pfn, str(e)))
+                        gs = False
                         continue
                 try:  # Try uploading file
                     protocol.put(base_name, '%s.rucio.upload' % pfn, source_dir, transfer_timeout=transfer_timeout)
