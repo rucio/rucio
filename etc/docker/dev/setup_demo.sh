4979de27324af65e8ec8324e902a365e872a73b4
Hannes Hansen <hannes.jakob.hansen@cern.ch>
2018-09-07 15:35:23 +0200

    Patch: fixed access to WebUi and API Fix #1516

---
+++ etc/docker/dev/setup_demo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+# Copyright 2014-2018 CERN for the benefit of the ATLAS collaboration.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Thomas Beermann, <thomas.beermann@cern.ch>, 2018
+
+python /setup_data.py
