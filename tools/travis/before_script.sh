9275d9e8928c7ecd919531aaf64ccc64142320e3
Thomas Beermann <thomas.beermann@cern.ch>
2019-01-21 13:50:41 +0100

    Switch oracle image for travis tests #2010

--- tools/travis/before_script.sh
+++ tools/travis/before_script.sh
@@ -15,9 +15,10 @@
 #
 # Authors:
 # - Vincent Garonne <vgaronne@gmail.com>, 2018
+# - Thomas Beermann <thomas.beermann@cern.ch>, 2019
 
 if [[ $RDBMS == "oracle" ]]; then
-    docker run -d -p 8080:8080 -p 1521:1521 --name=oracle -e processes=1000 -e sessions=1105 -e transactions=1215 -e ORACLE_ALLOW_REMOTE=true sath89/oracle-xe-11g
+    docker run -d -p 8080:8080 -p 1521:1521 --name=oracle -e processes=1000 -e sessions=1105 -e transactions=1215 -e ORACLE_ALLOW_REMOTE=true wnameless/oracle-xe-11g
     docker run --name=activemq -d webcenter/activemq:latest
     sleep 100
     docker run -d --link oracle:oracle --link activemq:activemq --name=rucio rucio/rucio
