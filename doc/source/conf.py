781ba31d492a27b86aa488a89fe5fa98f0e1c16e
Hannes Hansen <hannes.jakob.hansen@cern.ch>
2019-01-23 17:21:02 +0100

    Documentation: added rucio module to PYTHONPATH to fix broken readthedocs build; Fix #1774

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -18,9 +18,9 @@
 #
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
 from mock import Mock as MagicMock
+
+sys.path.insert(len(sys.path) - 1, os.path.abspath('.'))
 
 
 class Mock(MagicMock):
