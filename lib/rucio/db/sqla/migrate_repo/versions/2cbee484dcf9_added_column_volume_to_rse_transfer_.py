51f3276f234f1fe53d89788d4ec28c145ba892d7
Hannes Hansen <hannes.jakob.hansen@cern.ch>
2019-03-11 14:16:47 +0100

    Core & Internals: added grouped FIFO release and volumn check per destination RSE to throttler & tests; Fix #2220

---
+++ lib/rucio/db/sqla/migrate_repo/versions/2cbee484dcf9_added_column_volume_to_rse_transfer_.py
@@ -0,0 +1,47 @@
+# Copyright 2013-2019 CERN for the benefit of the ATLAS collaboration.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors:
+# - Hannes Hansen <hannes.jakob.hansen@cern.ch>, 2019
+
+''' added column volume to rse_transfer_limits '''
+
+import sqlalchemy as sa
+
+from alembic import context
+from alembic.op import (drop_column, add_column)
+
+
+# Alembic revision identifiers
+revision = '2cbee484dcf9'
+down_revision = 'cebad904c4dd'
+
+
+def upgrade():
+    '''
+    Upgrade the database to this revision
+    '''
+
+    if context.get_context().dialect.name in ['oracle', 'mysql', 'postgresql']:
+        schema = context.get_context().version_table_schema if context.get_context().version_table_schema else ''
+        add_column('rse_transfer_limits', sa.Column('volume', sa.BigInteger), schema=schema)
+
+
+def downgrade():
+    '''
+    Downgrade the database to the previous revision
+    '''
+    if context.get_context().dialect.name in ['oracle', 'mysql', 'postgresql']:
+        schema = context.get_context().version_table_schema if context.get_context().version_table_schema else ''
+        drop_column('rse_transfer_limits', 'volume', schema=schema)
