3c0ca0c73757740a339a9130d0f098dabe988b17
Dilaksun Bavarajan <dilaksun.bavarajan@cern.ch>
2019-04-18 13:30:20 +0200

    changes based on review

--- lib/rucio/daemons/conveyor/fts_throttler.py
+++ lib/rucio/daemons/conveyor/fts_throttler.py
@@ -69,17 +69,24 @@ class FTSThrottler(object):
                 logging.warn('could not get the cycle file, cannot perform tuning for this cycle without cycle file, returning')
                 return
 
+            try:
+                tuning_ratio = config_get('conveyor', 'fts_throttler_tuning_ratio')
+            except Exception:
+                logging.warn('could not get the tuning ratio from config, returning')
+                return
+
             rses = result['aggregations']['rse']['buckets']
             cycle_info_dict = {'storages': []}
             for rse in rses:
-                # if a rse has a failure ratio above 25% we tune it.
-                if rse['failure_ratio'].get('value') > 25:
+                # if a rse has a failure ratio above the tuning ratio (percentage) we tune it.
+                if rse['failure_ratio'].get('value') > int(tuning_ratio):
 
                     # rse_info holds the storage name(0) and FTS-host server(1)
                     rse_info = rse['key'].split()
 
                     # Tapes might have other reasons for timeouts which should be treated differently, therefor they are ignored and not tuned for now.
-                    if rse_info[0].endswith('TAPE'):
+                    if rse['storage_type']['hits']['hits'][0]['_source']['payload']['dst-type'] == 'TAPE':
+                        logging.info('%s is a tape storage type, it will not be tuned', rse_info[0])
                         continue
                     # instantiate transfertool for access to get_se_config and set_se_config.
                     t = FTS3Transfertool(rse_info[1])
@@ -90,14 +97,12 @@ class FTSThrottler(object):
 
                     n = rse['failure_ratio'].get('value')
 
-                    print('\033[91m RSE \033[0m' + rse_info[0] + '\033[91m on FTS host \033[0m' + rse_info[1] +
-                          '\033[91m has failure ratio \033[0m' + str(rse['failure_ratio'].get('value')) + '\033[91m on storage \033[0m' + url)
                     logging.info(' RSE ' + rse_info[0] + ' on FTS host ' + rse_info[1] +
                                  ' has failure ratio ' + str(rse['failure_ratio'].get('value')) + ' on storage ' + url)
 
                     try:
                         se = t.get_se_config(url)
-                        print(se)
+                        logging.info('storage settings: %s', se)
                     except KeyError:
                         logging.warn('configuration for storage element was not found, config will be set from default values')
                         # all FTS Host servers have a default reference storage named '*' that holds the default values for all storages that arent listed yet.
@@ -115,9 +120,9 @@ class FTSThrottler(object):
                                                             'tuned_outbound_max_active': int((100 / (100 + n)) * default_storage['se_info']['outbound_max_active']),
                                                             'fts-host': rse_info[1], 'time': str(datetime.datetime.now())})
                         continue
-                    except Exception, error:
+                    except Exception as error:
                         logging.warn('an error occured when trying to get the storage configuration')
-                        print str(error)
+                        logging.warn(str(error))
                         continue
 
                     # Even though we could read the config, we still need to know if the important attributes are empty.
@@ -177,7 +182,7 @@ class FTSThrottler(object):
             storages = cycle_info_dict['storages']
             for storage in storages:
                 t = FTS3Transfertool(storage['fts-host'])
-                print(storage)
+                logging.info('storage information: %s', storage)
                 t.set_se_config(storage['storage'], inbound_max_active=storage['inbound_max_active'], outbound_max_active=storage['outbound_max_active'])
                 logging.info('on storage ' + storage['storage'] + ' outbound_max_active reverted from ' +
                              str(storage['tuned_outbound_max_active']) + ' to ' + str(storage['outbound_max_active']) +
@@ -185,7 +190,7 @@ class FTSThrottler(object):
             logging.info('revert performed')
         return True
 
-    def request_timeout_data(self, destination=True, last_hours=1, transfer_successes_lower_boundary=30, transfer_timeouts_lower_boundary=30, kserver='http://atlas-kibana.mwt2.org:9200/rucio-events-*/_search'):
+    def request_timeout_data(self, destination=True, last_hours=1, transfer_successes_lower_boundary=20, transfer_timeouts_lower_boundary=20, kserver='http://atlas-kibana.mwt2.org:9200/rucio-events-*/_search'):
         """
         requests timeout data using elastic search
         :returns: JSON result of the elastic search query.
@@ -216,6 +221,13 @@ class FTSThrottler(object):
                     },
                     'aggs': {
                         'destination': {
+                            'top_hits': {
+                                '_source': {
+                                },
+                                'size': 1
+                            }
+                        },
+                        'storage_type': {
                             'top_hits': {
                                 '_source': {
                                 },
@@ -282,10 +294,12 @@ class FTSThrottler(object):
             params_dict['aggs']['rse']['terms'] = {'script': """(doc['payload.dst-rse'].empty ? '' : doc['payload.dst-rse'].value) + ' ' + (doc['payload.transfer-endpoint'].empty ? '' : doc['payload.transfer-endpoint'].value)""",
                                                    'size': 1000}
             params_dict['aggs']['rse']['aggs']['destination']['top_hits']['_source'] = {'include': ['payload.dst-url']}
+            params_dict['aggs']['rse']['aggs']['storage_type']['top_hits']['_source'] = {'include': ['payload.dst-type']}
         else:
             params_dict['aggs']['rse']['terms'] = {'script': """(doc['payload.src-rse'].empty ? '' : doc['payload.src-rse'].value) + ' ' + (doc['payload.transfer-endpoint'].empty ? '' : doc['payload.transfer-endpoint'].value)""",
                                                    'size': 1000}
             params_dict['aggs']['rse']['aggs']['destination']['top_hits']['_source'] = {'include': ['payload.src-url']}
+            params_dict['aggs']['rse']['aggs']['storage_type']['top_hits']['_source'] = {'include': ['payload.src-type']}
 
         params_str = json.dumps(params_dict)
         try:
@@ -300,18 +314,19 @@ class FTSThrottler(object):
             return result.json()
         raise Exception('could not get result from %s, status code returned : %s', kserver, result.status_code if result else None)
 
-    def testread(self):
+    def testread(self, tuning_ratio=25):
         """
         Read the failure ratio of storages without tuning
         :returns: filtered JSON response from Elastic search.
+        :param tuning_ratio: integer lower bound for what failing storages you want to read.
         """
         result = self.request_timeout_data()
         if result is not None:
 
             rses = result['aggregations']['rse']['buckets']
             for rse in rses:
-                # if a rse has a failure ratio above 25% we tune it.
-                if rse['failure_ratio'].get('value') > 25:
+                # if a rse has a failure ratio above the tuning ratio we read it.
+                if rse['failure_ratio'].get('value') > tuning_ratio:
 
                     # rse_info holds the storage name(0) and FTS-host server(1)
                     rse_info = rse['key'].split()
@@ -320,16 +335,16 @@ class FTSThrottler(object):
                     # extract FTS storage from dst-url
                     tmp = rse['destination']['hits']['hits'][0]['_source']['payload']['dst-url'].split(':', 2)
                     url = tmp[0] + ':' + tmp[1]
-                    print('\033[91m RSE \033[0m' + rse_info[0] + '\033[91m on FTS host \033[0m' + rse_info[1] + '\033[91m has failure ratio \033[0m' + str(rse['failure_ratio'].get('value')) + '\033[91m on storage \033[0m' + url)
+                    logging.info('\033[91m RSE \033[0m' + rse_info[0] + '\033[91m on FTS host \033[0m' + rse_info[1] + '\033[91m has failure ratio \033[0m' + str(rse['failure_ratio'].get('value')) + '\033[91m on storage \033[0m' + url)
 
                     try:
                         se = t.get_se_config(url)
-                        print(se)
+                        logging.info('storage settings: %s', se)
                     except KeyError:
                         logging.warn('configuration for storage element was not found')
-                    except Exception, error:
+                    except Exception as error:
                         logging.warn('an error occured when trying to get the storage configuration')
-                        print str(error)
+                        logging.warn(str(error))
                         continue
 
             return rses
@@ -354,10 +369,12 @@ def fts_throttler(once=False, cycle_interval=3600):
     prepend_str = 'Thread [%i/%i] : ' % (heart_beat['assign_thread'] + 1, heart_beat['nr_threads'])
     current_time = time.time()
     graceful_stop.wait(10)
+    running_instance = False
 
     while not graceful_stop.is_set():
         heart_beat = heartbeat.live(executable, hostname, pid, hb_thread, older_than=3600)
         if heart_beat['nr_threads'] < 2:
+            running_instance = True
             # this loop cannot be entered by more than one instance at a time.
             while not graceful_stop.is_set():
 
@@ -392,11 +409,11 @@ def fts_throttler(once=False, cycle_interval=3600):
 
     logging.info(prepend_str + 'Throttler - graceful stop requested')
 
-    # before we stop, try to revert the recent tuning. do not revert if there are other instances
-    if heart_beat['nr_threads'] < 2:
+    # before we stop, try to revert, but only if this instance was running the cycles.
+    # ! If the cycle info file information is shared between instances, then this implementation must be changed !
+    if running_instance:
         try:
             FTSThrottler().revert()
-            pass
         except Exception:
             logging.warn('could not revert changes before stopping')
 
