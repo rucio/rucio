c5c7a4f933e0d5349205da0f62289607f5ea645c
Ruturaj <ruturaj2305@gmail.com>
2019-07-19 22:49:36 +0530

    Enhancement: Add new rest endpoint /follow for DIDs
    Enhancement: Syntax fix

--- lib/rucio/api/did.py
+++ lib/rucio/api/did.py
@@ -14,6 +14,7 @@
   - Martin Barisits, <martin.barisits@cern.ch>, 2014-2015
   - Hannes Hansen, <hannes.jakob.hansen@cern.ch>, 2018-2019
   - Andrew Lister, <andrew.lister@stfc.ac.uk>, 2019
+  - Ruturaj Gujar, <ruturaj.gujar23@gmail.com>, 2019
 
   PY3K COMPATIBLE
 '''
