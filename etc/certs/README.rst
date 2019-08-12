392d1262e72e4c41efa2f09c7de5cf614f176c8a
Mario Lassnig <mario.lassnig@cern.ch>
2019-08-12 15:55:01 +0200

    now really with localhost SAN

--- etc/certs/README.rst
+++ etc/certs/README.rst
@@ -1,6 +1,4 @@
 Notice
 ======
 
-These certificates should be kept in sync with the ones in the https://github.com/rucio/containers repository. They expire in 2049, so there's no rush.
-
-If you want to add new hostnames, just extend the relevant entries in the generate.sh and sign with the Rucio Development CA.
+If you update these certificates you must push them over to the https://github.com/rucio/containers repository. They expire in 2049, so there's no rush.
--- etc/certs/generate.sh
+++ etc/certs/generate.sh
@@ -2,10 +2,10 @@
 export PASSPHRASE=123456
 
 # Rucio Development CA
-openssl genrsa -out rucio_ca.key.pem -passout env:PASSPHRASE 2048
-openssl req -x509 -new -batch -key rucio_ca.key.pem -days 9999 -out rucio_ca.pem -subj "/CN=Rucio Development CA" -passin env:PASSPHRASE
-hash=$(openssl x509 -noout -hash -in rucio_ca.pem)
-ln -sf rucio_ca.pem $hash.0
+#openssl genrsa -out rucio_ca.key.pem -passout env:PASSPHRASE 2048
+#openssl req -x509 -new -batch -key rucio_ca.key.pem -days 9999 -out rucio_ca.pem -subj "/CN=Rucio Development CA" -passin env:PASSPHRASE
+#hash=$(openssl x509 -noout -hash -in rucio_ca.pem)
+#ln -sf rucio_ca.pem $hash.0
 
 # User certificate
 openssl req -new -newkey rsa:2048 -nodes -keyout ruciouser.key.pem -subj "/CN=Rucio User" > ruciouser.csr
@@ -19,26 +19,23 @@ openssl x509 -req -days 9999 -CAcreateserial -extfile <(printf "subjectAltName=D
 
 # FTS
 openssl req -new -newkey rsa:2048 -nodes -keyout hostcert_fts.key.pem -subj "/CN=fts" > hostcert_fts.csr
-openssl x509 -req -days 9999 -CAcreateserial -in hostcert_fts.csr -CA rucio_ca.pem -CAkey rucio_ca.key.pem -out hostcert_fts.pem -passin env:PASSPHRASE
+openssl x509 -req -days 9999 -CAcreateserial -extfile <(printf "subjectAltName=DNS:localhost") -in hostcert_fts.csr -CA rucio_ca.pem -CAkey rucio_ca.key.pem -out hostcert_fts.pem -passin env:PASSPHRASE
 
 
 # XrootD Server 1
 openssl req -new -newkey rsa:2048 -nodes -keyout hostcert_xrd1.key.pem -subj "/CN=xrd1" > hostcert_xrd1.csr
-openssl x509 -req -days 9999 -CAcreateserial -in hostcert_xrd1.csr -CA rucio_ca.pem -CAkey rucio_ca.key.pem -out hostcert_xrd1.pem -passin env:PASSPHRASE
+openssl x509 -req -days 9999 -CAcreateserial -extfile <(printf "subjectAltName=DNS:localhost") -in hostcert_xrd1.csr -CA rucio_ca.pem -CAkey rucio_ca.key.pem -out hostcert_xrd1.pem -passin env:PASSPHRASE
 
 
 # XrootD Server 2
 openssl req -new -newkey rsa:2048 -nodes -keyout hostcert_xrd2.key.pem -subj "/CN=xrd2" > hostcert_xrd2.csr
-openssl x509 -req -days 9999 -CAcreateserial -in hostcert_xrd2.csr -CA rucio_ca.pem -CAkey rucio_ca.key.pem -out hostcert_xrd2.pem -passin env:PASSPHRASE
+openssl x509 -req -days 9999 -CAcreateserial -extfile <(printf "subjectAltName=DNS:localhost") -in hostcert_xrd2.csr -CA rucio_ca.pem -CAkey rucio_ca.key.pem -out hostcert_xrd2.pem -passin env:PASSPHRASE
 
 
 # XrootD Server 3
 openssl req -new -newkey rsa:2048 -nodes -keyout hostcert_xrd3.key.pem -subj "/CN=xrd3" > hostcert_xrd3.csr
-openssl x509 -req -days 9999 -CAcreateserial -in hostcert_xrd2.csr -CA rucio_ca.pem -CAkey rucio_ca.key.pem -out hostcert_xrd2.pem -passin env:PASSPHRASE
+openssl x509 -req -days 9999 -CAcreateserial -extfile <(printf "subjectAltName=DNS:localhost") -in hostcert_xrd3.csr -CA rucio_ca.pem -CAkey rucio_ca.key.pem -out hostcert_xrd3.pem -passin env:PASSPHRASE
 
-
-
-rm *.csr
 chmod 0400 *key*
 
 echo
