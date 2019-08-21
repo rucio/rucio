befca9744c0d8e17a21f50c4a9977c36f4879e72
Mario Lassnig <mario.lassnig@cern.ch>
2019-08-21 16:41:29 +0200

    fix wait script

--- tools/travis/before_script.sh
+++ tools/travis/before_script.sh
@@ -22,7 +22,9 @@
 if [[ $RDBMS == "oracle" ]]; then
     docker run -d -p 8080:8080 -p 1521:1521 --name=oracle -e processes=1000 -e sessions=1105 -e transactions=1215 -e ORACLE_ALLOW_REMOTE=true -e ORACLE_DISABLE_ASYNCH_IO=true rucio/oraclexe
     docker run --name=activemq -d webcenter/activemq:latest
-    /usr/local/bin/wait-for-it.sh -h oracle -p 1521 -t 100
+    date
+    tools/wait-for-it.sh -h oracle -p 1521 -t 100
+    date
     docker cp tools/travis/oracle_setup.sh oracle:/
     docker exec -it oracle /bin/bash -c "/oracle_setup.sh"
     docker run -d --link oracle:oracle --link activemq:activemq --name=rucio rucio/rucio
@@ -33,7 +35,9 @@ if [[ $RDBMS == "oracle" ]]; then
 elif [[ $RDBMS == "mysql" ]]; then
     docker run --name=mysql -e MYSQL_ROOT_PASSWORD=secret -e MYSQL_ROOT_HOST=% -d mysql/mysql-server:5.7
     docker run --name=activemq -d webcenter/activemq:latest
-    /usr/local/bin/wait-for-it.sh -h mysql -p 3306 -t 100
+    date
+    tools/wait-for-it.sh -h mysql -p 3306 -t 100
+    date
     docker run -d --link mysql:mysql --link activemq:activemq --name=rucio rucio/rucio
     docker exec -it rucio cp /opt/rucio/etc/docker/travis/rucio_mysql.cfg /opt/rucio/etc/rucio.cfg
     docker exec -it rucio cp /opt/rucio/etc/docker/travis/alembic_mysql.ini /opt/rucio/etc/alembic.ini
@@ -43,7 +47,9 @@ elif [[ $RDBMS == "mysql" ]]; then
 elif [[ $RDBMS == "postgres" ]]; then
     docker run --name=postgres -e POSTGRES_PASSWORD=secret -d postgres -c 'max_connections=300'
     docker run --name=activemq -d webcenter/activemq:latest
-    /usr/local/bin/wait-for-it.sh -h postgres -p 5432 -t 100
+    date
+    tools/wait-for-it.sh -h postgres -p 5432 -t 100
+    date
     docker run -d --link postgres:postgres --link activemq:activemq --name=rucio rucio/rucio
     docker exec -it rucio cp /opt/rucio/etc/docker/travis/rucio_postgres.cfg /opt/rucio/etc/rucio.cfg
     docker exec -it rucio cp /opt/rucio/etc/docker/travis/alembic_postgres.ini /opt/rucio/etc/alembic.ini
