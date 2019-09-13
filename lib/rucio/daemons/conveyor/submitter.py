a9c8195bc73ac404c4e9c96304f2fbafe47b1da0
maatthias <maatthias@gmail.com>
2019-09-13 11:00:55 -0400

    conveyor.submitter

--- lib/rucio/daemons/conveyor/submitter.py
+++ lib/rucio/daemons/conveyor/submitter.py
@@ -64,7 +64,8 @@ logging.basicConfig(stream=sys.stdout,
 graceful_stop = threading.Event()
 
 USER_TRANSFERS = config_get('conveyor', 'user_transfers', False, None)
-
+TRANSFER_TOOL = config_get('conveyor', 'transfertool', False, None)
+TRANSFER_TYPE = config_get('conveyor', 'transfertype', False, 'single')
 
 def submitter(once=False, rses=None, mock=False,
               bulk=100, group_bulk=1, group_policy='rule', source_strategy=None,
@@ -175,18 +176,40 @@ def submitter(once=False, rses=None, mock=False,
 
                 logging.info('%s Starting to submit transfers for %s', prepend_str, activity)
 
-                for external_host in grouped_jobs:
-                    if not user_transfer:
-                        for job in grouped_jobs[external_host]:
-                            # submit transfers
-                            submit_transfer(external_host=external_host, job=job, submitter='transfer_submitter',
-                                            logging_prepend_str=prepend_str, timeout=timeout)
-                    else:
-                        for _, jobs in iteritems(grouped_jobs[external_host]):
-                            # submit transfers
-                            for job in jobs:
+                if TRANSFER_TOOL == 'fts3':
+                    for external_host in grouped_jobs:
+                        if not user_transfer:
+                            for job in grouped_jobs[external_host]:
+                                # submit transfers
                                 submit_transfer(external_host=external_host, job=job, submitter='transfer_submitter',
-                                                logging_prepend_str=prepend_str, timeout=timeout, user_transfer_job=user_transfer)
+                                                logging_prepend_str=prepend_str, timeout=timeout)
+                        else:
+                            for _, jobs in iteritems(grouped_jobs[external_host]):
+                                # submit transfers
+                                for job in jobs:
+                                    submit_transfer(external_host=external_host, job=job, submitter='transfer_submitter',
+                                                    logging_prepend_str=prepend_str, timeout=timeout, user_transfer_job=user_transfer)
+                elif TRANSFER_TOOL == 'globus':
+                    if TRANSFER_TYPE == 'bulk':
+                        # build bulk job file list per external host to send to submit_transfer
+                        for external_host in grouped_jobs:
+                            # pad the job with job_params; irrelevant for globus but needed for further rucio parsing
+                            submitjob = {'files': [], 'job_params': grouped_jobs[''][0].get('job_params')}
+                            for job in grouped_jobs[external_host]:
+                                 submitjob.get('files').append(job.get('files')[0])
+                            logging.debug('submitjob: %s' % submitjob)
+                            submit_transfer(external_host=external_host, job=submitjob, submitter='transfer_submitter', logging_prepend_str=prepend_str, timeout=timeout)
+                    else:
+                        # build single job files and individually send to submit_transfer
+                        job_params = grouped_jobs[''][0].get('job_params') if grouped_jobs else None
+                        for external_host in grouped_jobs:
+                            for job in grouped_jobs[external_host]:
+                                for file in job['files']:
+                                    singlejob = {'files': [file], 'job_params': job_params}
+                                    logging.debug('singlejob: %s' % singlejob)
+                                    submit_transfer(external_host=external_host, job=singlejob, submitter='transfer_submitter', logging_prepend_str=prepend_str, timeout=timeout)
+                else:
+                    logging.error(prepend_str + 'Unknown transfer tool')
 
                 if len(transfers) < group_bulk:
                     logging.info('%s Only %s transfers for %s which is less than group bulk %s, sleep %s seconds', prepend_str, len(transfers), activity, group_bulk, sleep_time)
