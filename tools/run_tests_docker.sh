6510b3e97f59b74cec38b5435a73e7f229256f8f
Mario Lassnig <mario.lassnig@cern.ch>
2019-09-23 17:05:59 +0200

    add new script to automatically create test RSEs; Fix #2805

--- tools/run_tests_docker.sh
+++ tools/run_tests_docker.sh
@@ -27,14 +27,16 @@ function usage {
   echo ''
   echo '  -h    Show usage.'
   echo '  -i    Do only the initialization.'
+  echo '  -r    Activate default RSEs (XRD1, XRD2, XRD3)'
   exit
 }
 
-while getopts hi opt
+while getopts hir opt
 do
   case "$opt" in
     h) usage;;
     i) init_only="true";;
+    r) activate_rse="true";;
   esac
 done
 
@@ -91,6 +93,12 @@ if [ $? != 0 ]; then
     echo 'Failed to bootstrap!'
     exit 1
 fi
+
+if test ${activate_rse}; then
+    echo 'Activating default RSEs (XRD1, XRD2, XRD3)'
+    tools/docker_activate_rses.sh
+fi
+
 
 if test ${init_only}; then
     exit
