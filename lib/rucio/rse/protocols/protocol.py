63fcf0fd2b041b1fe3b05d71ebe403bbc6fe4d74
James Perry <j.perry@epcc.ed.ac.uk>
2019-10-16 10:52:14 +0100

    Allow policy packages to register surl and lfn2pfn algorithms

--- lib/rucio/rse/protocols/protocol.py
+++ lib/rucio/rse/protocols/protocol.py
@@ -203,6 +203,9 @@ class RSEDeterministicTranslation(object):
 
             :returns: RSE specific URI of the physical file
         """
+        # ensure that policy package is loaded in case it registers algorithms
+        import rucio.common.schema
+
         algorithm = self.rse_attributes.get('lfn2pfn_algorithm', 'default')
         if algorithm == 'default':
             algorithm = RSEDeterministicTranslation._DEFAULT_LFN2PFN
