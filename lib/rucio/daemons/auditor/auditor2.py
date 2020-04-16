4fe26c2cd9cd972637ca23fab28b27237c54be6f
Dimitrios Christidis <dimitrios.christidis@cern.ch>
2020-04-16 10:17:08 +0200

    Consistency: Implement core consistency function #3437

--- lib/rucio/daemons/auditor/auditor2.py
+++ lib/rucio/daemons/auditor/auditor2.py
@@ -18,6 +18,65 @@
 # - Dimitrios Christidis <dimitrios.christidis@cern.ch>, 2020
 
 
+class Result:
+    """Tags used for the results of the consistency check."""
+    DARK = 'DARK'
+    LOST = 'LOST'
+
+
+def consistency(rucio_dump_before, storage_dump, rucio_dump_after, output):
+    """Perform a consistency check.
+
+    All parameters should be ``str``s with the paths to the appropriate
+    files.
+
+    The storage and Rucio dumps must be sorted and not contain any
+    duplicate entries.  Each line must contain solely the local path of
+    a file (i.e. by stripping the common base from the absolute path on
+    the RSE).
+
+    The results are written to the file located at ``output`` in CSV
+    format.  The first field is a tag and the second is the path.
+
+    The table that follows shows all possible outcomes of the
+    comparison.  Only the ones marked with (*) are written to the
+    output file.  'B' and 'A' are the Rucio dumps generated before and
+    after the storage dump 'S'.
+
+            +---------------+------------+
+            |  Set          |  Result    |
+            +===============+============+
+            |  B - (A | S)  |  Deleted   |
+            +---------------+------------+
+            |  (B & S) - A  |  Deleted   |
+            +---------------+------------+
+            |  (B & A) - S  |  LOST (*)  |
+            +---------------+------------+
+            |  B & A & S    |  OK        |
+            +---------------+------------+
+            |  (B | A) - S  |  DARK (*)  |
+            +---------------+------------+
+            |  A - (B | S)  |  New       |
+            +---------------+------------+
+            |  (A & S) - B  |  New       |
+            +---------------+------------+
+    """
+    with open(rucio_dump_before) as fh_rucio_before,\
+            open(storage_dump) as fh_storage,\
+            open(rucio_dump_after) as fh_rucio_after,\
+            open(output, 'w') as fh_output:
+
+        for item in merge((line.rstrip() for line in fh_rucio_before),
+                          (line.rstrip() for line in fh_storage),
+                          (line.rstrip() for line in fh_rucio_after)):
+
+            path, found = item
+            if not found[0] and found[1] and not found[2]:
+                print(Result.DARK, path, sep=',', file=fh_output)
+            if found[0] and not found[1] and found[2]:
+                print(Result.LOST, path, sep=',', file=fh_output)
+
+
 def merge(*args):
     r"""Merge and compare iterables.
 
