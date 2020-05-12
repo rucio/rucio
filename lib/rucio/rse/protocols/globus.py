46ad981d6a0a1f5357ddc519ee2d321b05fbbe00
Matt Snyder <msnyder@bnl.gov>
2020-05-12 15:13:31 -0400

    Transfers: GlobusRSEProtocol Modifications #3584

--- lib/rucio/rse/protocols/globus.py
+++ lib/rucio/rse/protocols/globus.py
@@ -52,140 +52,6 @@ logging.basicConfig(stream=sys.stdout,
                     format='%(asctime)s\t%(process)d\t%(levelname)s\t%(message)s')
 
 
-class RSEDeterministicTranslation(object):
-    """
-    Execute the logic for translating a LFN to a path.
-    """
-
-    _LFN2PFN_ALGORITHMS = {}
-    _DEFAULT_LFN2PFN = "hash"
-
-    def __init__(self, rse=None, rse_attributes=None, protocol_attributes=None):
-        """
-        Initialize a translator object from the RSE, its attributes, and the protocol-specific
-        attributes.
-
-        :param rse: Name of RSE for this translation.
-        :param rse_attributes: A dictionary of RSE-specific attributes for use in the translation.
-        :param protocol_attributes: A dictionary of RSE/protocol-specific attributes.
-        """
-        self.rse = rse
-        self.rse_attributes = rse_attributes if rse_attributes else {}
-        self.protocol_attributes = protocol_attributes if protocol_attributes else {}
-
-    @classmethod
-    def supports(cls, name):
-        """
-        Check to see if a specific algorithm is supported.
-
-        :param name: Name of the deterministic algorithm.
-        :returns: True if `name` is an algorithm supported by the translator class, False otherwise.
-        """
-        return name in cls._LFN2PFN_ALGORITHMS
-
-    @staticmethod
-    def register(lfn2pfn_callable, name=None):
-        """
-        Provided a callable function, register it as one of the valid LFN2PFN algorithms.
-
-        The callable will receive five arguments:
-         - scope: Scope of the LFN.
-         - name: LFN's path name
-         - rse: RSE name the translation is being done for.
-         - rse_attributes: Attributes of the RSE.
-         - protocol_attributes: Attributes of the RSE's protocol
-        The return value should be the last part of the PFN - it will be appended to the
-        rest of the URL.
-
-        :param lfn2pfn_callable: Callable function to use for generating paths.
-        :param name: Algorithm name used for registration.  If None, then `lfn2pfn_callable.__name__` is used.
-        """
-        if name is None:
-            name = lfn2pfn_callable.__name__
-        RSEDeterministicTranslation._LFN2PFN_ALGORITHMS[name] = lfn2pfn_callable
-
-    @staticmethod
-    def __hash(scope, name, rse, rse_attrs, protocol_attrs):
-        """
-        Given a LFN, turn it into a sub-directory structure using a hash function.
-
-        This takes the MD5 of the LFN and uses the first four characters as a subdirectory
-        name.
-
-        :param scope: Scope of the LFN.
-        :param name: File name of the LFN.
-        :param rse: RSE for PFN (ignored)
-        :param rse_attrs: RSE attributes for PFN (ignored)
-        :param protocol_attrs: RSE protocol attributes for PFN (ignored)
-        :returns: Path for use in the PFN generation.
-        """
-        del rse
-        del rse_attrs
-        del protocol_attrs
-        hstr = hashlib.md5('%s:%s' % (scope, name)).hexdigest()
-        if scope.startswith('user') or scope.startswith('group'):
-            scope = scope.replace('.', '/')
-        return '%s/%s/%s/%s' % (scope, hstr[0:2], hstr[2:4], name)
-
-    @staticmethod
-    def __identity(scope, name, rse, rse_attrs, protocol_attrs):
-        """
-        Given a LFN, convert it directly to a path using the mapping:
-
-            scope:path -> scope/path
-
-        :param scope: Scope of the LFN.
-        :param name: File name of the LFN.
-        :param rse: RSE for PFN (ignored)
-        :param rse_attrs: RSE attributes for PFN (ignored)
-        :param protocol_attrs: RSE protocol attributes for PFN (ignored)
-        :returns: Path for use in the PFN generation.
-        """
-        del rse
-        del rse_attrs
-        del protocol_attrs
-        if scope.startswith('user') or scope.startswith('group'):
-            scope = scope.replace('.', '/')
-        return '%s/%s' % (scope, name)
-
-    @classmethod
-    def _module_init_(cls):
-        """
-        Initialize the class object on first module load.
-        """
-        cls.register(cls.__hash, "hash")
-        cls.register(cls.__identity, "identity")
-        policy_module = None
-        try:
-            policy_module = config.config_get('policy', 'lfn2pfn_module')
-        except (NoOptionError, NoSectionError):
-            pass
-        if policy_module:
-            # TODO: The import of importlib is done like this due to a dependency issue with python 2.6 and incompatibility of the module with py3.x
-            # More information https://github.com/rucio/rucio/issues/875
-            import importlib
-            importlib.import_module(policy_module)
-
-        cls._DEFAULT_LFN2PFN = config.get_lfn2pfn_algorithm_default()
-
-    def path(self, scope, name):
-        """ Transforms the logical file name into a PFN's path.
-
-            :param lfn: filename
-            :param scope: scope
-
-            :returns: RSE specific URI of the physical file
-        """
-        algorithm = self.rse_attributes.get('lfn2pfn_algorithm', 'default')
-        if algorithm == 'default':
-            algorithm = RSEDeterministicTranslation._DEFAULT_LFN2PFN
-        algorithm_callable = RSEDeterministicTranslation._LFN2PFN_ALGORITHMS[algorithm]
-        return algorithm_callable(scope, name, self.rse, self.rse_attributes, self.protocol_attributes)
-
-
-RSEDeterministicTranslation._module_init_()  # pylint: disable=protected-access
-
-
 class GlobusRSEProtocol(RSEProtocol):
     """ This class is to support Globus as a Rucio RSE protocol.  Inherits from abstract base class RSEProtocol."""
 
@@ -194,23 +60,10 @@ class GlobusRSEProtocol(RSEProtocol):
 
             :param props: Properties of the requested protocol
         """
+        # super(GlobusRSEProtocol, self).__init__(protocol_attr, rse_settings, logger=logger)
         self.attributes = protocol_attr
-        self.translator = None
-        self.renaming = True
-        self.overwrite = False
         self.rse = rse_settings
         self.globus_endpoint_id = get_rse_attribute(key='globus_endpoint_id', rse_id=self.rse.get('id'))
-        if self.rse['deterministic']:
-            self.translator = RSEDeterministicTranslation(self.rse['rse'], rse_settings, self.attributes)
-            if getattr(rsemanager, 'CLIENT_MODE', None) and \
-                    not RSEDeterministicTranslation.supports(self.rse.get('lfn2pfn_algorithm')):
-                # Remote server has an algorithm we don't understand; always make the server do the lookup.
-                setattr(self, 'lfns2pfns', self.__lfns2pfns_client)
-        else:
-            if getattr(rsemanager, 'CLIENT_MODE', None):
-                setattr(self, 'lfns2pfns', self.__lfns2pfns_client)
-            if getattr(rsemanager, 'SERVER_MODE', None):
-                setattr(self, '_get_path', self._get_path_nondeterministic_server)
 
     def lfns2pfns(self, lfns):
         """
