19290aed8df4ad516e52889426d3386c3ec24756
Nick Smith <nick.smith@cern.ch>
2020-05-13 08:49:13 -0500

    flake8 issues

--- lib/rucio/transfertool/mock.py
+++ lib/rucio/transfertool/mock.py
@@ -5,16 +5,18 @@ import uuid
 class MockTransfertool(Transfertool):
     """
     Mock implementation of a Rucio transfertool
+
+    This is not actually used anywhere at the moment
     """
 
     def __init__(self, external_host, token=None):
-        super(FTS3Transfertool, self).__init__(external_host)
+        super(MockTransfertool, self).__init__(external_host)
 
     def submit(self, files, job_params, timeout=None):
         return uuid.uuid1()
 
     def query(self, transfer_ids, details=False, timeout=None):
-        return [{'status': 'ok', 'idontknowthefields': True}]
+        return [{'status': 'ok'}]
 
     def cancel(self, transfer_ids, timeout=None):
         return True
