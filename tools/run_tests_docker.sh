0cddd16b812cbbe79a70313a58a7319a0b86dd48
patrick-austin <patrick.austin@stfc.ac.uk>
2020-06-03 08:07:34 +0000

    internal user-interaction: Clean up failed flush attempts #25

--- tools/run_tests_docker.sh
+++ tools/run_tests_docker.sh
@@ -42,9 +42,6 @@ do
 done
 
 echo 'Clearing memcache'
-echo 'flush_all' | nc localhost 11211
-echo 'flush_all' | ncat localhost 11211
-echo 'flush_all' | netcat localhost 11211
 echo flush_all > /dev/tcp/127.0.0.1/11211
 
 echo 'Graceful restart of Apache'
