71a507ac45a8265ad851dc7f4c0425275d1ec042
Benedikt Ziemons <benedikt.ziemons@cern.ch>
2020-06-09 12:49:17 +0200

    Release management: Move to GitHub Actions; #3417
    Add workflow to run all tests in Docker. Tests are run according to
    the matrix in etc/docker/test/matrix.yml
    
    Start Date:  Mon Apr 20 15:41:26 2020 +0200
    
    Added multi_vo test suite

---
+++ tools/test/create_changelist.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Copyright 2020 CERN for the benefit of the ATLAS collaboration.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors:
+# - Benedikt Ziemons <benedikt.ziemons@cern.ch>, 2020
+
+set -eo pipefail
+IFS=$'\n\t'
+
+if [[ -z "$BASE_BRANCH" ]]; then
+    echo "BASE_BRANCH environment variable needs to be set"
+    exit 2
+fi
+
+# change directory to main repository directory
+cd `dirname $0`/../..
+
+rm -f changed_files.txt
+git diff --name-status HEAD $(git merge-base HEAD $BASE_BRANCH) | grep -E 'bin/|\.py$' | grep -v '^A' | grep -v 'conf.py' | cut -f 2 | paste -sd " " - > changed_files.txt || touch changed_files.txt
