b799ba890577a53e3260687619c5c9816d9734b1
Benedikt Ziemons <benedikt.ziemons@cern.ch>
2020-06-29 14:27:41 +0200

    Core: Refactor integrity error catch; #3777

--- lib/rucio/core/scope.py
+++ lib/rucio/core/scope.py
@@ -1,9 +1,16 @@
-# Copyright European Organization for Nuclear Research (CERN)
+# Copyright 2012-2020 CERN for the benefit of the ATLAS collaboration.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# You may not use this file except in compliance with the License.
+# you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# http://www.apache.org/licenses/LICENSE-2.0
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 # Authors:
 # - Thomas Beermann, <thomas.beermann@cern.ch>, 2012
@@ -14,6 +21,7 @@
 # - Hannes Hansen, <hannes.jakob.hansen@cern.ch>, 2019
 # - Andrew Lister, <andrew.lister@stfc.ac.uk>, 2019
 # - Patrick Austin, <patrick.austin@stfc.ac.uk>, 2020
+# - Benedikt Ziemons <benedikt.ziemons@cern.ch>, 2020
 #
 # PY3K COMPATIBLE
 
@@ -49,11 +57,11 @@ def add_scope(scope, account, session=None):
         new_scope.save(session=session)
     except IntegrityError as e:
         if match('.*IntegrityError.*ORA-00001: unique constraint.*SCOPES_PK.*violated.*', e.args[0]) \
-           or match('.*IntegrityError.*Duplicate entry.*for key.*', e.args[0]) \
-           or match('.*IntegrityError.*UNIQUE constraint failed: scopes.scope.*', e.args[0]) \
-           or match('.*IntegrityError.*duplicate key value violates unique constraint.*', e.args[0]) \
-           or match('.*UniqueViolation.*duplicate key value violates unique constraint.*', e.args[0]) \
-           or match('.*sqlite3.IntegrityError.*is not unique.*', e.args[0]):
+                or match('.*IntegrityError.*Duplicate entry.*for key.*', e.args[0]) \
+                or match('.*IntegrityError.*UNIQUE constraint failed: scopes.scope.*', e.args[0]) \
+                or match('.*IntegrityError.*duplicate key value violates unique constraint.*', e.args[0]) \
+                or match('.*UniqueViolation.*duplicate key value violates unique constraint.*', e.args[0]) \
+                or match('.*IntegrityError.*columns? .*not unique.*', e.args[0]):
             raise Duplicate('Scope \'%s\' already exists!' % scope)
         else:
             raise RucioException(e)
