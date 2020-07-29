64da799df2f3753926173b5c519fa736af4a6e0a
Benedikt Ziemons <benedikt.ziemons@cern.ch>
2020-07-29 23:36:13 +0200

    Core & Internals: Fix server error when adding RSE; #3420
    The error was:
    TypeError: %x format: an integer is required, not UUID

--- lib/rucio/db/sqla/types.py
+++ lib/rucio/db/sqla/types.py
@@ -1,13 +1,24 @@
-# Copyright European Organization for Nuclear Research (CERN)
+# Copyright 2015-2020 CERN for the benefit of the ATLAS collaboration.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# You may not use this file except in compliance with the License.
-# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 # Authors:
-# - Vincent Garonne, <vincent.garonne@cern.ch>, 2013
-# - Mario Lassnig, <mario.lassnig@cern.ch>, 2013-2018
-# - Andrew Lister, <andrew.lister@stfc.ac.uk>, 2019
+# - Vincent Garonne <vincent.garonne@cern.ch>, 2015
+# - asket <asket.agarwal96@gmail.com>, 2018
+# - Hannes Hansen <hannes.jakob.hansen@cern.ch>, 2018
+# - Mario Lassnig <mario.lassnig@cern.ch>, 2018-2019
+# - Andrew Lister <andrew.lister@stfc.ac.uk>, 2019
+# - Benedikt Ziemons <benedikt.ziemons@cern.ch>, 2020
 #
 # PY3K COMPATIBLE
 
@@ -58,10 +69,10 @@ class GUID(TypeDecorator):
             return uuid.UUID(value).bytes
         else:
             if not isinstance(value, uuid.UUID):
-                return "%.32x" % uuid.UUID(value)
+                return "%.32x" % uuid.UUID(value).int
             else:
                 # hexstring
-                return "%.32x" % value
+                return "%.32x" % value.int
 
     def process_result_value(self, value, dialect):
         if value is None:
