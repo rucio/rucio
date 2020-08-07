ba6176553da1a0903878783255f2352e674c2315
Martin Barisits <martin.barisits@cern.ch>
2020-08-07 11:30:44 +0200

    Release: Tools cleanup in the repo; Closes #3917

--- tools/configure_git.sh
+++ tools/configure_git.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-# Copyright European Organization for Nuclear Research (CERN)
+# Copyright 2012-2020 CERN for the benefit of the ATLAS collaboration.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# You may not use this file except in compliance with the License.
-# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 # Authors:
-# Vincent Garonne, <vincent.garonne@cern.ch>, 2012-2015
-# Martin Barisits, <martin.barisits@cern.ch>, 2017
+# - Vincent Garonne <vincent.garonne@cern.ch>, 2012-2015
+# - Joaquin Bogado <jbogado@linti.unlp.edu.ar>, 2015
+# - Martin Barisits <martin.barisits@cern.ch>, 2017-2020
 
 git remote add upstream https://github.com/rucio/rucio.git
 
-cp tools/pre-commit .git/hooks/pre-commit
-chmod +x .git/hooks/pre-commit
 cp tools/prepare-commit-msg .git/hooks/prepare-commit-msg
 chmod +x .git/hooks/prepare-commit-msg
