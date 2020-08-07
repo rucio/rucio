64e63846243a0e25b99f6ca2c2ed5b6b8b86cbdd
Martin Barisits <martin.barisits@cern.ch>
2020-08-07 11:30:44 +0200

    Release: Tools cleanup in the repo; Closes #3917

--- tools/restart_apache.sh
+++
@@ -1,9 +0,0 @@
-source .venv/bin/activate
-sudo rm /var/log/apache2/*
-sudo rm /tmp/rucio.db /tmp/mock-fts.db
-python tools/reset_database.py -t
-sudo /etc/init.d/apache2 restart
-sudo chmod 777 /tmp/rucio.db
-python tools/sync_meta.py
-python tools/sync_rses.py
-sudo tail -f /var/log/apache2/*
--- tools/test_auth.sh
+++
@@ -1,7 +0,0 @@
-curl -v -i --cacert /opt/rucio/etc/web/CERN-bundle.pem -H "X-Rucio-Account: root" -H "X-Rucio-Username: ddmlab" -H "X-Rucio-Password: secret" -X GET https://mlassnig-dev.cern.ch/auth/userpass
-
-curl -v -i --cacert /opt/rucio/etc/web/CERN-bundle.pem -H "X-Rucio-Account: root" -E /opt/rucio/etc/web/usercert.pem -X GET https://mlassnig-dev.cern.ch/auth/x509
-
-curl -v -i --cacert /opt/rucio/etc/web/CERN-bundle.pem -H "X-Rucio-Account: root" -E $X509_USER_PROXY -X GET https://mlassnig-dev.cern.ch/auth/x509_proxy
-
-curl -v -i --cacert /opt/rucio/etc/web/CERN-bundle.pem -H "X-Rucio-Account: root" --negotiate -u: -X GET https://mlassnig-dev.cern.ch/auth/gss
