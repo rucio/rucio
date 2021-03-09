fe2c2a24cac1546fc830086c451e597d7434eff1
Ilija Vukotic <ivukotic@cern.ch>
2021-03-09 16:08:24 -0600

    fix test

--- lib/rucio/tests/test_replica.py
+++ lib/rucio/tests/test_replica.py
@@ -50,7 +50,8 @@ from rucio.client.baseclient import BaseClient
 from rucio.client.didclient import DIDClient
 from rucio.client.replicaclient import ReplicaClient
 from rucio.client.ruleclient import RuleClient
-from rucio.common.config import config_get, config_get_bool, config_set
+from rucio.core.config import set as cconfig_set
+from rucio.common.config import config_get, config_get_bool
 from rucio.common.exception import (DataIdentifierNotFound, AccessDenied, UnsupportedOperation,
                                     RucioException, ReplicaIsLocked, ReplicaNotFound, ScopeNotFound,
                                     DatabaseException)
@@ -124,7 +125,7 @@ class TestReplicaCore(unittest.TestCase):
 
         add_replicas(rse_id=rse_id, files=files, account=root)
 
-        # config_set('clientcachemap', 'BLACKMESA', 'AGLT2')
+        cconfig_set('clientcachemap', 'BLACKMESA', 'AGLT2')
 
         for rep in list_replicas(
                 dids=[{'scope': f['scope'], 'name': f['name'], 'type': DIDType.FILE} for f in files],
