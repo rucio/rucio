cd690cd6e286d6bc0d1b5f3112e0dbb69bc5a6ab
sahandilshan <sahandilshansg@gmail.com>
2021-03-20 19:16:00 +0530

    Core & Internals: Add FTSState and FTSCompleteState to common/constants: #4241

--- lib/rucio/common/constants.py
+++ lib/rucio/common/constants.py
@@ -11,6 +11,8 @@
 #
 # PY3K COMPATIBLE
 
+from collections import namedtuple
+
 """
 Constants.
 
@@ -33,3 +35,11 @@ SCHEME_MAP = {'srm': ['srm', 'gsiftp'],
               's3': ['https', 'davs', 's3']}
 
 SUPPORTED_PROTOCOLS = ['gsiftp', 'srm', 'root', 'davs', 'http', 'https', 'file', 's3', 's3+rucio', 's3+https', 'storm']
+
+FTS_STATE_NAMED_TUPLE = namedtuple('FTS_STATE', ['SUBMITTED', 'READY', 'ACTIVE', 'FAILED', 'FINISHED', 'FINISHEDDIRTY',
+                                                'CANCELED'])
+FTS_STATE = FTS_STATE_NAMED_TUPLE('SUBMITTED', 'READY', 'ACTIVE', 'FAILED', 'FINISHED', 'FINISHEDDIRTY', 'CANCELED')
+
+FTS_COMPLETE_STATE_NAMED_TUPLE = namedtuple('FTS_COMPLETE_STATE', ['OK', 'ERROR'])
+FTS_COMPLETE_STATE = FTS_COMPLETE_STATE_NAMED_TUPLE('OK', 'ERROR')
+
