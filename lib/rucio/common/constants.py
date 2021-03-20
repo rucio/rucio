abcb4adde57f55441a69f6a446f6a79e6e5c2eb8
sahandilshan <sahandilshansg@gmail.com>
2021-03-20 19:53:14 +0530

    Core & Internals: Remove FTSStates & FTSCompleteStates from db/sqla/constants: #4241

--- lib/rucio/common/constants.py
+++ lib/rucio/common/constants.py
@@ -36,9 +36,8 @@ SCHEME_MAP = {'srm': ['srm', 'gsiftp'],
 
 SUPPORTED_PROTOCOLS = ['gsiftp', 'srm', 'root', 'davs', 'http', 'https', 'file', 's3', 's3+rucio', 's3+https', 'storm']
 
-FTS_STATE_NAMED_TUPLE = namedtuple('FTS_STATE', ['SUBMITTED', 'READY', 'ACTIVE', 'FAILED', 'FINISHED', 'FINISHEDDIRTY',
-                                                 'CANCELED'])
-FTS_STATE = FTS_STATE_NAMED_TUPLE('SUBMITTED', 'READY', 'ACTIVE', 'FAILED', 'FINISHED', 'FINISHEDDIRTY', 'CANCELED')
+FTS_STATE = namedtuple('FTS_STATE', ['SUBMITTED', 'READY', 'ACTIVE', 'FAILED', 'FINISHED', 'FINISHEDDIRTY',
+                                     'CANCELED'])('SUBMITTED', 'READY', 'ACTIVE', 'FAILED', 'FINISHED', 'FINISHEDDIRTY',
+                                                  'CANCELED')
 
-FTS_COMPLETE_STATE_NAMED_TUPLE = namedtuple('FTS_COMPLETE_STATE', ['OK', 'ERROR'])
-FTS_COMPLETE_STATE = FTS_COMPLETE_STATE_NAMED_TUPLE('OK', 'ERROR')
+FTS_COMPLETE_STATE = namedtuple('FTS_COMPLETE_STATE', ['OK', 'ERROR'])('OK', 'ERROR')
