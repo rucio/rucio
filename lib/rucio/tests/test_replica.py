1e243fe7a1c6856075b32adc52415b9a663f5e89
Radu Carpa <radu.carpa@cern.ch>
2021-04-19 12:03:29 +0200

    test_replica_mixed_protocols: move to pytest and remove dirty flag

--- lib/rucio/tests/test_replica.py
+++ lib/rucio/tests/test_replica.py
@@ -563,46 +563,42 @@ class TestReplicaCore(unittest.TestCase):
                                                 client_location={'site': 'SOMEWHERE'})]
         assert 'root://' in list(replicas[0]['pfns'].keys())[0]
 
-    @pytest.mark.dirty
-    def test_replica_mixed_protocols(self):
-        """ REPLICA (CORE): Test adding replicas with mixed protocol """
 
-        rse = 'APERTURE_%s' % rse_name_generator()
-        rse_id = add_rse(rse, **self.vo)
+def test_replica_mixed_protocols(rse_factory, mock_scope, root_account):
+    """ REPLICA (CORE): Test adding replicas with mixed protocol """
 
-        add_protocol(rse_id, {'scheme': 'root',
-                              'hostname': 'root.aperture.com',
-                              'port': 1409,
-                              'prefix': '//test/chamber/',
-                              'impl': 'rucio.rse.protocols.xrootd.Default',
-                              'domains': {
-                                  'lan': {'read': 1, 'write': 1, 'delete': 1},
-                                  'wan': {'read': 1, 'write': 1, 'delete': 1}}})
-        add_protocol(rse_id, {'scheme': 'http',
-                              'hostname': 'root.aperture.com',
-                              'port': 1409,
-                              'prefix': '//test/chamber/',
-                              'impl': 'rucio.rse.protocols.xrootd.Default',
-                              'domains': {
-                                  'lan': {'read': 1, 'write': 1, 'delete': 1},
-                                  'wan': {'read': 1, 'write': 1, 'delete': 1}}})
+    rse, rse_id = rse_factory.make_rse()
 
-        tmp_scope = InternalScope('mock', **self.vo)
-        root = InternalAccount('root', **self.vo)
+    add_protocol(rse_id, {'scheme': 'root',
+                          'hostname': 'root.aperture.com',
+                          'port': 1409,
+                          'prefix': '//test/chamber/',
+                          'impl': 'rucio.rse.protocols.xrootd.Default',
+                          'domains': {
+                              'lan': {'read': 1, 'write': 1, 'delete': 1},
+                              'wan': {'read': 1, 'write': 1, 'delete': 1}}})
+    add_protocol(rse_id, {'scheme': 'http',
+                          'hostname': 'root.aperture.com',
+                          'port': 1409,
+                          'prefix': '//test/chamber/',
+                          'impl': 'rucio.rse.protocols.xrootd.Default',
+                          'domains': {
+                              'lan': {'read': 1, 'write': 1, 'delete': 1},
+                              'wan': {'read': 1, 'write': 1, 'delete': 1}}})
 
-        files = []
+    files = []
 
-        name = 'element_%s' % generate_uuid()
-        hstr = hashlib.md5(('%s:%s' % (tmp_scope, name)).encode('utf-8')).hexdigest()
-        pfn = 'root://root.aperture.com:1409//test/chamber/mock/%s/%s/%s' % (hstr[0:2], hstr[2:4], name)
-        files.append({'scope': tmp_scope, 'name': name, 'bytes': 1234, 'adler32': 'deadbeef', 'pfn': pfn})
+    name = 'element_%s' % generate_uuid()
+    hstr = hashlib.md5(('%s:%s' % (mock_scope, name)).encode('utf-8')).hexdigest()
+    pfn = 'root://root.aperture.com:1409//test/chamber/mock/%s/%s/%s' % (hstr[0:2], hstr[2:4], name)
+    files.append({'scope': mock_scope, 'name': name, 'bytes': 1234, 'adler32': 'deadbeef', 'pfn': pfn})
 
-        name = 'element_%s' % generate_uuid()
-        hstr = hashlib.md5(('%s:%s' % (tmp_scope, name)).encode('utf-8')).hexdigest()
-        pfn = 'http://root.aperture.com:1409//test/chamber/mock/%s/%s/%s' % (hstr[0:2], hstr[2:4], name)
-        files.append({'scope': tmp_scope, 'name': name, 'bytes': 1234, 'adler32': 'deadbeef', 'pfn': pfn})
+    name = 'element_%s' % generate_uuid()
+    hstr = hashlib.md5(('%s:%s' % (mock_scope, name)).encode('utf-8')).hexdigest()
+    pfn = 'http://root.aperture.com:1409//test/chamber/mock/%s/%s/%s' % (hstr[0:2], hstr[2:4], name)
+    files.append({'scope': mock_scope, 'name': name, 'bytes': 1234, 'adler32': 'deadbeef', 'pfn': pfn})
 
-        add_replicas(rse_id=rse_id, files=files, account=root)
+    add_replicas(rse_id=rse_id, files=files, account=root_account)
 
 
 def test_set_tombstone_via_core(rse_factory, mock_scope, root_account):
