ef1751bebe63c04def1b16b2f876876cca376dbc
lucascottolavina <scotto@lpnhe.in2p3.fr>
2021-04-22 21:15:52 +0200

    fixed a missing line necessary to implement the xenon filesystem

--- lib/rucio/rse/protocols/protocol.py
+++ lib/rucio/rse/protocols/protocol.py
@@ -228,6 +228,7 @@ class RSEDeterministicTranslation(object):
         cls.register(cls.__identity, "identity")
         cls.register(cls.__ligo, "ligo")
         cls.register(cls.__belleii, "belleii")
+        cls.register(cls.__xenon, "xenon")
         policy_module = None
         try:
             policy_module = config.config_get('policy', 'lfn2pfn_module')
