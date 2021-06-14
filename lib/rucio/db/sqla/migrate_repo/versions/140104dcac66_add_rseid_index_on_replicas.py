d4a2a58f0677474af1b140fca74e1b6e83fcfe46
joaquinbogado <joaquinbogado@gmail.com>
2021-06-14 12:51:13 -0300

    [Database] rse_id added as index in replicas table

---
+++ lib/rucio/db/sqla/migrate_repo/versions/140104dcac66_add_rseid_index_on_replicas.py
@@ -0,0 +1,44 @@
+# Copyright 2013-2020 CERN for the benefit of the ATLAS collaboration.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors:
+# - Joaquin Bogado <joaquinbogado@gmail.com>, 2021
+
+''' added rse_id index on replicas table '''
+
+from alembic import context
+from alembic.op import create_index, drop_index
+
+
+# Alembic revision identifiers
+revision = '140104dcac66'
+down_revision = 'e138c364ebd0'
+
+
+def upgrade():
+    '''
+    Upgrade the database to this revision
+    '''
+
+    if context.get_context().dialect.name in ['oracle', 'mysql', 'postgresql']:
+        create_index('RSE_ID_IDX', 'replicas', ['rse_id'])
+
+
+def downgrade():
+    '''
+    Downgrade the database to the previous revision
+    '''
+
+    if context.get_context().dialect.name in ['oracle', 'mysql', 'postgresql']:
+        drop_index('RSE_ID_IDX', 'replicas', ['rse_id'])
