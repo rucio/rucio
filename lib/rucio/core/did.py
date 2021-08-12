rucio/rucio

commit b923385b5c9feb848991039f21a573efbffe9bb0

Core: correct redefining of built-in vars. Closes #4466 (#4735)
* Core: change built-in names for dids #4466

* Core: correct redefining of built-in vars #4466

* Fix tests

* Core: change built-in names for dids #4466

* Core: correct redefining of built-in vars #4466

* Fix tests

* Fix redefinitions

* Fix redefinitions again

* Update receiver.py

lib/rucio/core/did.py
@@ -113,14 +113,14 @@ def list_expired_dids(worker_number=None, total_workers=None, limit=None, sessio
 
 
 @transactional_session
-def add_did(scope, name, type, account, statuses=None, meta=None, rules=None,
+def add_did(scope, name, did_type, account, statuses=None, meta=None, rules=None,
             lifetime=None, dids=None, rse_id=None, session=None):
     """
     Add data identifier.
 
     :param scope: The scope name.
     :param name: The data identifier name.
-    :param type: The data identifier type.
+    :param did_type: The data identifier type.
     :param account: The account owner.
     :param statuses: Dictionary with statuses, e.g.g {'monotonic':True}.
     :meta: Meta-data associated with the data identifier is represented using key/value pairs in a dictionary.
@@ -130,7 +130,7 @@ def add_did(scope, name, type, account, statuses=None, meta=None, rules=None,
     :param rse_id: The RSE id when registering replicas.
     :param session: The database session in use.
     """
-    return add_dids(dids=[{'scope': scope, 'name': name, 'type': type,
+    return add_dids(dids=[{'scope': scope, 'name': name, 'type': did_type,
                            'statuses': statuses or {}, 'meta': meta or {},
                            'rules': rules, 'lifetime': lifetime,
                            'dids': dids, 'rse_id': rse_id}],
@@ -1105,16 +1105,16 @@ def list_files(scope, name, long=False, session=None):
                         filter(and_(models.DataIdentifierAssociation.scope == s,
                                     models.DataIdentifierAssociation.name == n))
 
-                    for child_scope, child_name, child_type, bytes, adler32, guid, events, lumiblocknr in query.yield_per(500):
+                    for child_scope, child_name, child_type, bytes_, adler32, guid, events, lumiblocknr in query.yield_per(500):
                         if long:
                             yield {'scope': child_scope, 'name': child_name,
-                                   'bytes': bytes, 'adler32': adler32,
+                                   'bytes': bytes_, 'adler32': adler32,
                                    'guid': guid and guid.upper(),
                                    'events': events,
                                    'lumiblocknr': lumiblocknr}
                         else:
                             yield {'scope': child_scope, 'name': child_name,
-                                   'bytes': bytes, 'adler32': adler32,
+                                   'bytes': bytes_, 'adler32': adler32,
                                    'guid': guid and guid.upper(),
                                    'events': events}
                 else:
@@ -1199,16 +1199,16 @@ def get_did(scope, name, dynamic=False, session=None):
                     'md5': result.md5, 'adler32': result.adler32}
         else:
             if dynamic:
-                bytes, length, events = __resolve_bytes_length_events_did(scope=scope, name=name, session=session)
+                bytes_, length, events = __resolve_bytes_length_events_did(scope=scope, name=name, session=session)
                 # replace None value for bytes with zero
-                if bytes is None:
-                    bytes = 0
+                if bytes_ is None:
+                    bytes_ = 0
             else:
-                bytes, length = result.bytes, result.length
+                bytes_, length = result.bytes, result.length
             return {'scope': result.scope, 'name': result.name, 'type': result.did_type,
                     'account': result.account, 'open': result.is_open,
                     'monotonic': result.monotonic, 'expired_at': result.expired_at,
-                    'length': length, 'bytes': bytes}
+                    'length': length, 'bytes': bytes_}
     except NoResultFound:
         raise exception.DataIdentifierNotFound("Data identifier '%(scope)s:%(name)s' not found" % locals())
 
@@ -1258,7 +1258,7 @@ def get_files(files, session=None):
 
 
 @transactional_session
-def set_metadata(scope, name, key, value, type=None, did=None,
+def set_metadata(scope, name, key, value, did_type=None, did=None,
                  recursive=False, session=None):
     """
     Add metadata to data identifier.
@@ -1403,14 +1403,14 @@ def set_status(scope, name, session=None, **kwargs):
 
 
 @stream_session
-def list_dids(scope, filters, type='collection', ignore_case=False, limit=None,
+def list_dids(scope, filters, did_type='collection', ignore_case=False, limit=None,
               offset=None, long=False, recursive=False, session=None):
     """
     Search data identifiers
 
     :param scope: the scope name.
     :param filters: dictionary of attributes by which the results should be filtered.
-    :param type: the type of the did: all(container, dataset, file), collection(dataset or container), dataset, container, file.
+    :param did_type: the type of the did: all(container, dataset, file), collection(dataset or container), dataset, container, file.
     :param ignore_case: ignore case distinctions.
     :param limit: limit number.
     :param offset: offset number.
@@ -1419,7 +1419,7 @@ def list_dids(scope, filters, type='collection', ignore_case=False, limit=None,
     :param recursive: Recursively list DIDs content.
     """
     types = ['all', 'collection', 'container', 'dataset', 'file']
-    if type not in types:
+    if did_type not in types:
         raise exception.UnsupportedOperation("Valid type are: %(types)s" % locals())
 
     query = session.query(models.DataIdentifier.scope,
@@ -1432,18 +1432,18 @@ def list_dids(scope, filters, type='collection', ignore_case=False, limit=None,
     # Exclude suppressed dids
     query = query.filter(models.DataIdentifier.suppressed != true())
 
-    if type == 'all':
+    if did_type == 'all':
         query = query.filter(or_(models.DataIdentifier.did_type == DIDType.CONTAINER,
                                  models.DataIdentifier.did_type == DIDType.DATASET,
                                  models.DataIdentifier.did_type == DIDType.FILE))
-    elif type.lower() == 'collection':
+    elif did_type.lower() == 'collection':
         query = query.filter(or_(models.DataIdentifier.did_type == DIDType.CONTAINER,
                                  models.DataIdentifier.did_type == DIDType.DATASET))
-    elif type.lower() == 'container':
+    elif did_type.lower() == 'container':
         query = query.filter(models.DataIdentifier.did_type == DIDType.CONTAINER)
-    elif type.lower() == 'dataset':
+    elif did_type.lower() == 'dataset':
         query = query.filter(models.DataIdentifier.did_type == DIDType.DATASET)
-    elif type.lower() == 'file':
+    elif did_type.lower() == 'file':
         query = query.filter(models.DataIdentifier.did_type == DIDType.FILE)
 
     for (k, v) in filters.items():
@@ -1499,37 +1499,37 @@ def list_dids(scope, filters, type='collection', ignore_case=False, limit=None,
         # Get attachted DIDs and save in list because query has to be finished before starting a new one in the recursion
         collections_content = []
         parent_scope = scope
-        for scope, name, did_type, bytes, length in query.yield_per(100):
-            if (did_type == DIDType.CONTAINER or did_type == DIDType.DATASET):
+        for scope, name, type_, bytes_, length in query.yield_per(100):
+            if (type_ == DIDType.CONTAINER or type_ == DIDType.DATASET):
                 collections_content += [did for did in list_content(scope=scope, name=name)]
 
         # List DIDs again to use filter
         for did in collections_content:
             filters['name'] = did['name']
-            for result in list_dids(scope=did['scope'], filters=filters, recursive=True, type=type, limit=limit, offset=offset, long=long, session=session):
+            for result in list_dids(scope=did['scope'], filters=filters, recursive=True, did_type=did_type, limit=limit, offset=offset, long=long, session=session):
                 yield result
 
     if long:
-        for scope, name, did_type, bytes, length in query.yield_per(5):
+        for scope, name, type_, bytes_, length in query.yield_per(5):
             yield {'scope': scope,
                    'name': name,
-                   'did_type': did_type.name,
-                   'bytes': bytes,
+                   'did_type': type_.name,
+                   'bytes': bytes_,
                    'length': length}
     else:
-        for scope, name, did_type, bytes, length in query.yield_per(5):
+        for scope, name, type_, bytes_, length in query.yield_per(5):
             yield name
 
 
 @read_session
-def list_dids_extended(scope, filters, type='collection', ignore_case=False, limit=None,
+def list_dids_extended(scope, filters, did_type='collection', ignore_case=False, limit=None,
                        offset=None, long=False, recursive=False, session=None):
     """
     Search data identifiers
 
     :param scope: the scope name.
     :param filters: dictionary of attributes by which the results should be filtered.
-    :param type: the type of the did: all(container, dataset, file), collection(dataset or container), dataset, container, file.
+    :param did_type: the type of the did: all(container, dataset, file), collection(dataset or container), dataset, container, file.
     :param ignore_case: ignore case distinctions.
     :param limit: limit number.
     :param offset: offset number.
@@ -1537,7 +1537,7 @@ def list_dids_extended(scope, filters, type='collection', ignore_case=False, lim
     :param session: The database session in use.
     :param recursive: Recursively list DIDs content.
     """
-    return did_meta_plugins.list_dids(scope, filters, type, ignore_case, limit, offset, long, recursive, session=session)
+    return did_meta_plugins.list_dids(scope, filters, did_type, ignore_case, limit, offset, long, recursive, session=session)
 
 
 @read_session
@@ -1630,7 +1630,7 @@ def create_did_sample(input_scope, input_name, output_scope, output_name, accoun
     files = [did for did in list_files(scope=input_scope, name=input_name, long=False, session=session)]
     random.shuffle(files)
     output_files = files[:int(nbfiles)]
-    add_did(scope=output_scope, name=output_name, type=DIDType.DATASET, account=account, statuses={}, meta=[], rules=[], lifetime=None, dids=[], rse_id=None, session=session)
+    add_did(scope=output_scope, name=output_name, did_type=DIDType.DATASET, account=account, statuses={}, meta=[], rules=[], lifetime=None, dids=[], rse_id=None, session=session)
     attach_dids(scope=output_scope, name=output_name, dids=output_files, account=account, rse_id=None, session=session)
 
 
@@ -1649,18 +1649,18 @@ def __resolve_bytes_length_events_did(scope, name, session):
     except NoResultFound:
         raise exception.DataIdentifierNotFound("Data identifier '%s:%s' not found" % (scope, name))
 
-    bytes, length, events = 0, 0, 0
+    bytes_, length, events = 0, 0, 0
     if did.did_type == DIDType.FILE:
-        bytes, length, events = did.bytes, 1, did.events
+        bytes_, length, events = did.bytes, 1, did.events
     elif did.did_type == DIDType.DATASET:
         try:
-            length, bytes, events = session.query(func.count(models.DataIdentifierAssociation.scope),
-                                                  func.sum(models.DataIdentifierAssociation.bytes),
-                                                  func.sum(models.DataIdentifierAssociation.events)).\
+            length, bytes_, events = session.query(func.count(models.DataIdentifierAssociation.scope),
+                                                   func.sum(models.DataIdentifierAssociation.bytes),
+                                                   func.sum(models.DataIdentifierAssociation.events)).\
                 filter_by(scope=scope, name=name).\
                 one()
         except NoResultFound:
-            length, bytes, events = 0, 0, 0
+            length, bytes_, events = 0, 0, 0
 
     elif did.did_type == DIDType.CONTAINER:
         for dataset in list_child_datasets(scope=scope, name=name, session=session):
@@ -1673,10 +1673,10 @@ def __resolve_bytes_length_events_did(scope, name, session):
             except NoResultFound:
                 tmp_length, tmp_bytes, tmp_events = 0, 0, 0
 
-            bytes += tmp_bytes or 0
+            bytes_ += tmp_bytes or 0
             length += tmp_length or 0
             events += tmp_events or 0
-    return (bytes, length, events)
+    return (bytes_, length, events)
 
 
 @transactional_session

lib/rucio/api/did.py
@@ -46,13 +46,13 @@ from rucio.core.rse import get_rse_id
 from rucio.db.sqla.constants import DIDType
 
 
-def list_dids(scope, filters, type='collection', ignore_case=False, limit=None, offset=None, long=False, recursive=False, vo='def'):
+def list_dids(scope, filters, did_type='collection', ignore_case=False, limit=None, offset=None, long=False, recursive=False, vo='def'):
     """
     List dids in a scope.
 
     :param scope: The scope name.
     :param pattern: The wildcard pattern.
-    :param type:  The type of the did: all(container, dataset, file), collection(dataset or container), dataset, container
+    :param did_type:  The type of the did: all(container, dataset, file), collection(dataset or container), dataset, container
     :param ignore_case: Ignore case distinctions.
     :param limit: The maximum number of DIDs returned.
     :param offset: Offset number.
@@ -69,20 +69,20 @@ def list_dids(scope, filters, type='collection', ignore_case=False, limit=None, 
     if 'scope' in filters:
         filters['scope'] = InternalScope(filters['scope'], vo=vo)
 
-    result = did.list_dids(scope=scope, filters=filters, type=type, ignore_case=ignore_case,
+    result = did.list_dids(scope=scope, filters=filters, did_type=did_type, ignore_case=ignore_case,
                            limit=limit, offset=offset, long=long, recursive=recursive)
 
     for d in result:
         yield api_update_return_dict(d)
 
 
-def list_dids_extended(scope, filters, type='collection', ignore_case=False, limit=None, offset=None, long=False, recursive=False, vo='def'):
+def list_dids_extended(scope, filters, did_type='collection', ignore_case=False, limit=None, offset=None, long=False, recursive=False, vo='def'):
     """
     List dids in a scope.
 
     :param scope: The scope name.
     :param pattern: The wildcard pattern.
-    :param type:  The type of the did: all(container, dataset, file), collection(dataset or container), dataset, container
+    :param did_type:  The type of the did: all(container, dataset, file), collection(dataset or container), dataset, container
     :param ignore_case: Ignore case distinctions.
     :param limit: The maximum number of DIDs returned.
     :param offset: Offset number.
@@ -97,20 +97,20 @@ def list_dids_extended(scope, filters, type='collection', ignore_case=False, lim
     if 'scope' in filters:
         filters['scope'] = InternalScope(filters['scope'], vo=vo)
 
-    result = did.list_dids_extended(scope=scope, filters=filters, type=type, ignore_case=ignore_case,
+    result = did.list_dids_extended(scope=scope, filters=filters, did_type=did_type, ignore_case=ignore_case,
                                     limit=limit, offset=offset, long=long, recursive=recursive)
 
     for d in result:
         yield api_update_return_dict(d)
 
 
-def add_did(scope, name, type, issuer, account=None, statuses={}, meta={}, rules=[], lifetime=None, dids=[], rse=None, vo='def'):
+def add_did(scope, name, did_type, issuer, account=None, statuses={}, meta={}, rules=[], lifetime=None, dids=[], rse=None, vo='def'):
     """
     Add data did.
 
     :param scope: The scope name.
     :param name: The data identifier name.
-    :param type: The data identifier type.
+    :param did_type: The data identifier type.
     :param issuer: The issuer account.
     :param account: The account owner. If None, then issuer is selected as owner.
     :param statuses: Dictionary with statuses, e.g.g {'monotonic':True}.
@@ -121,11 +121,11 @@ def add_did(scope, name, type, issuer, account=None, statuses={}, meta={}, rules
     :param rse: The RSE name when registering replicas.
     :param vo: The VO to act on.
     """
-    v_did = {'name': name, 'type': type.upper(), 'scope': scope}
+    v_did = {'name': name, 'type': did_type.upper(), 'scope': scope}
     validate_schema(name='did', obj=v_did, vo=vo)
     validate_schema(name='dids', obj=dids, vo=vo)
     validate_schema(name='rse', obj=rse, vo=vo)
-    kwargs = {'scope': scope, 'name': name, 'type': type, 'issuer': issuer, 'account': account, 'statuses': statuses, 'meta': meta, 'rules': rules, 'lifetime': lifetime}
+    kwargs = {'scope': scope, 'name': name, 'type': did_type, 'issuer': issuer, 'account': account, 'statuses': statuses, 'meta': meta, 'rules': rules, 'lifetime': lifetime}
     if not rucio.api.permission.has_permission(issuer=issuer, vo=vo, action='add_did', kwargs=kwargs):
         raise rucio.common.exception.AccessDenied('Account %s can not add data identifier to scope %s' % (issuer, scope))
 
@@ -142,7 +142,7 @@ def add_did(scope, name, type, issuer, account=None, statuses={}, meta={}, rules
     if rse is not None:
         rse_id = get_rse_id(rse=rse, vo=vo)
 
-    if type == 'DATASET':
+    if did_type == 'DATASET':
         # naming_convention validation
         extra_meta = naming_convention.validate_name(scope=scope, name=name, did_type='D')
 
@@ -155,9 +155,9 @@ def add_did(scope, name, type, issuer, account=None, statuses={}, meta={}, rules
                 raise rucio.common.exception.InvalidObject("Provided metadata %s doesn't match the naming convention: %s != %s" % (k, meta[k], extra_meta[k]))
 
         # Validate metadata
-        meta_core.validate_meta(meta=meta, did_type=DIDType[type.upper()])
+        meta_core.validate_meta(meta=meta, did_type=DIDType[did_type.upper()])
 
-    return did.add_did(scope=scope, name=name, type=DIDType[type.upper()], account=account or issuer,
+    return did.add_did(scope=scope, name=name, did_type=DIDType[did_type.upper()], account=account or issuer,
                        statuses=statuses, meta=meta, rules=rules, lifetime=lifetime,
                        dids=dids, rse_id=rse_id)
 
@@ -285,17 +285,17 @@ def detach_dids(scope, name, dids, issuer, vo='def'):
     return did.detach_dids(scope=scope, name=name, dids=dids)
 
 
-def list_new_dids(type=None, thread=None, total_threads=None, chunk_size=1000, vo='def'):
+def list_new_dids(did_type=None, thread=None, total_threads=None, chunk_size=1000, vo='def'):
     """
     List recent identifiers.
 
-    :param type : The DID type.
+    :param did_type : The DID type.
     :param thread: The assigned thread for this necromancer.
     :param total_threads: The total number of threads of all necromancers.
     :param chunk_size: Number of requests to return per yield.
     :param vo: The VO to act on.
     """
-    dids = did.list_new_dids(did_type=type and DIDType[type.upper()], thread=thread, total_threads=total_threads, chunk_size=chunk_size)
+    dids = did.list_new_dids(did_type=did_type and DIDType[did_type.upper()], thread=thread, total_threads=total_threads, chunk_size=chunk_size)
     for d in dids:
         if d['scope'].vo == vo:
             yield api_update_return_dict(d)

commit 68e03a79d5b018fe4f8352d44a9d10b6e33e4a83

fixed bugs introduced from #4735

lib/rucio/core/did.py
@@ -1475,7 +1475,6 @@ def list_dids(scope, filters, did_type='collection', ignore_case=False, limit=No
     if recursive:
         # Get attached DIDs and save in list because query has to be finished before starting a new one in the recursion
         collections_content = []
-        parent_scope = scope
         for scope, name, type_, bytes_, length in query.yield_per(100):
             if (type_ == DIDType.CONTAINER or type_ == DIDType.DATASET):
                 collections_content += [did for did in list_content(scope=scope, name=name)]

lib/rucio/tests/test_filter_engine.py
@@ -141,7 +141,7 @@ class TestFilterEngineReal(unittest.TestCase):
 
     def _create_tmp_DID(self, type='DATASET'):
         did_name = 'fe_test_did_%s' % generate_uuid()
-        add_did(scope=self.tmp_scope, name=did_name, type='DATASET', account=self.root)
+        add_did(scope=self.tmp_scope, name=did_name, did_type='DATASET', account=self.root)
         return did_name
 
     @read_session