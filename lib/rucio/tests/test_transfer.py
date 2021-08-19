fb766f3b809c5d32b2d17b680284d928983a2529
Radu Carpa <radu.carpa@cern.ch>
2021-08-19 18:39:16 +0200

    Transfers: further merge stager and submitter. #857
    Transfers retrievals from database and processing is now done by the
    same function for both stager and submitter.
    Inside this function, we don't create anymore the fts-specific
    "legacy_definition". Stager and submitter uses different bring_online
    and copy_pin_lifetime, so initialize it directly in the needed place.
    That also means that no fts-specific code is anymore in the path of
    globus transfer submission.
    
    To achieve this goal, parameter passing and return values of transfer
    core had to be adapted. Which means some minimal changes to tests
    (test_s3 is being fixed in this PR, it skipped all assert statements).
    
    As transfer core now groups by external_host, monitoring counters/timers
    had to be adjusted. Also, removed the upper layer of grouping
    (external_host) from bulk_group_transfers: they are already grouped.
    
    Minor unrelated adjustement:
    - "include_multihop" was removed from transfer core, just check that
    multihop_rses is not empty.

--- lib/rucio/tests/test_transfer.py
+++ lib/rucio/tests/test_transfer.py
@@ -21,7 +21,7 @@ import pytest
 from rucio.common.exception import NoDistance
 from rucio.core.distance import add_distance
 from rucio.core.replica import add_replicas
-from rucio.core.transfer import get_hops, get_transfer_requests_and_source_replicas
+from rucio.core.transfer import get_hops, next_transfers_to_submit
 from rucio.core import rule as rule_core
 from rucio.core import request as request_core
 from rucio.core import rse as rse_core
@@ -82,9 +82,9 @@ def test_get_hops(rse_factory):
     with pytest.raises(NoDistance):
         get_hops(source_rse_id=rse1_id, dest_rse_id=rse0_id)
     with pytest.raises(NoDistance):
-        get_hops(source_rse_id=rse0_id, dest_rse_id=rse1_id, include_multihop=True, multihop_rses=all_rses)
+        get_hops(source_rse_id=rse0_id, dest_rse_id=rse1_id, multihop_rses=all_rses)
     with pytest.raises(NoDistance):
-        get_hops(source_rse_id=rse1_id, dest_rse_id=rse0_id, include_multihop=True, multihop_rses=all_rses)
+        get_hops(source_rse_id=rse1_id, dest_rse_id=rse0_id, multihop_rses=all_rses)
 
     # A single hop path must be found between two directly connected RSE
     [hop] = get_hops(source_rse_id=rse1_id, dest_rse_id=rse2_id)
@@ -95,19 +95,19 @@ def test_get_hops(rse_factory):
     with pytest.raises(NoDistance):
         get_hops(source_rse_id=rse3_id, dest_rse_id=rse2_id)
 
-    # Multihop_rses argument empty (not set), no path will be computed
+    # No multihop rses given, multihop disabled
     with pytest.raises(NoDistance):
-        get_hops(source_rse_id=rse3_id, dest_rse_id=rse2_id, include_multihop=True)
+        get_hops(source_rse_id=rse3_id, dest_rse_id=rse2_id, multihop_rses=[])
 
     # The shortest multihop path will be computed
-    [hop1, hop2] = get_hops(source_rse_id=rse3_id, dest_rse_id=rse2_id, include_multihop=True, multihop_rses=all_rses)
+    [hop1, hop2] = get_hops(source_rse_id=rse3_id, dest_rse_id=rse2_id, multihop_rses=all_rses)
     assert hop1['source_rse_id'] == rse3_id
     assert hop1['dest_rse_id'] == rse4_id
     assert hop2['source_rse_id'] == rse4_id
     assert hop2['dest_rse_id'] == rse2_id
 
     # multihop_rses doesn't contain the RSE needed for the shortest path. Return a longer path
-    [hop1, hop2] = get_hops(source_rse_id=rse1_id, dest_rse_id=rse4_id, include_multihop=True, multihop_rses=[rse3_id])
+    [hop1, hop2] = get_hops(source_rse_id=rse1_id, dest_rse_id=rse4_id, multihop_rses=[rse3_id])
     assert hop1['source_rse_id'] == rse1_id
     assert hop1['dest_rse_id'] == rse3_id
     assert hop2['source_rse_id'] == rse3_id
@@ -115,26 +115,26 @@ def test_get_hops(rse_factory):
 
     # A link with cost only in one direction will not be used in the opposite direction
     with pytest.raises(NoDistance):
-        get_hops(source_rse_id=rse6_id, dest_rse_id=rse5_id, include_multihop=True, multihop_rses=all_rses)
-    [hop1, hop2] = get_hops(source_rse_id=rse4_id, dest_rse_id=rse3_id, include_multihop=True, multihop_rses=all_rses)
+        get_hops(source_rse_id=rse6_id, dest_rse_id=rse5_id, multihop_rses=all_rses)
+    [hop1, hop2] = get_hops(source_rse_id=rse4_id, dest_rse_id=rse3_id, multihop_rses=all_rses)
     assert hop1['source_rse_id'] == rse4_id
     assert hop2['source_rse_id'] == rse5_id
     assert hop2['dest_rse_id'] == rse3_id
 
     # A longer path is preferred over a shorter one with high intermediate cost
-    [hop1, hop2, hop3] = get_hops(source_rse_id=rse3_id, dest_rse_id=rse6_id, include_multihop=True, multihop_rses=all_rses)
+    [hop1, hop2, hop3] = get_hops(source_rse_id=rse3_id, dest_rse_id=rse6_id, multihop_rses=all_rses)
     assert hop1['source_rse_id'] == rse3_id
     assert hop2['source_rse_id'] == rse4_id
     assert hop3['source_rse_id'] == rse5_id
     assert hop3['dest_rse_id'] == rse6_id
 
     # A link with no cost is ignored. Both for direct connection and multihop paths
-    [hop1, hop2, hop3] = get_hops(source_rse_id=rse2_id, dest_rse_id=rse6_id, include_multihop=True, multihop_rses=all_rses)
+    [hop1, hop2, hop3] = get_hops(source_rse_id=rse2_id, dest_rse_id=rse6_id, multihop_rses=all_rses)
     assert hop1['source_rse_id'] == rse2_id
     assert hop2['source_rse_id'] == rse4_id
     assert hop3['source_rse_id'] == rse5_id
     assert hop3['dest_rse_id'] == rse6_id
-    [hop1, hop2, hop3, hop4] = get_hops(source_rse_id=rse1_id, dest_rse_id=rse6_id, include_multihop=True, multihop_rses=all_rses)
+    [hop1, hop2, hop3, hop4] = get_hops(source_rse_id=rse1_id, dest_rse_id=rse6_id, multihop_rses=all_rses)
     assert hop1['source_rse_id'] == rse1_id
     assert hop2['source_rse_id'] == rse2_id
     assert hop3['source_rse_id'] == rse4_id
@@ -180,9 +180,7 @@ def test_disk_vs_tape_priority(rse_factory, root_account, mock_scope):
                 save(session=session, flush=False)
 
     # On equal priority and distance, disk should be preferred over tape. Both disk sources will be returned
-    transfers, _reqs_no_source, _reqs_scheme_mismatch, _reqs_only_tape_source = get_transfer_requests_and_source_replicas(rses=all_rses)
-    assert len(transfers) == 1
-    transfer = next(iter(transfers.values()))
+    [[_host, [transfer]]] = next_transfers_to_submit(rses=all_rses).items()
     assert len(transfer[0]['sources']) == 2
     assert transfer[0]['sources'][0][0] in (disk1_rse_name, disk2_rse_name)
 
@@ -190,24 +188,20 @@ def test_disk_vs_tape_priority(rse_factory, root_account, mock_scope):
     # Multiple tape sources are not allowed. Only one tape RSE source must be returned.
     __fake_source_ranking(disk1_rse_id, -1)
     __fake_source_ranking(disk2_rse_id, -1)
-    transfers, _reqs_no_source, _reqs_scheme_mismatch, _reqs_only_tape_source = get_transfer_requests_and_source_replicas(rses=all_rses)
+    [[_host, transfers]] = next_transfers_to_submit(rses=all_rses).items()
     assert len(transfers) == 1
-    transfer = next(iter(transfers.values()))
+    transfer = transfers[0]
     assert len(transfer[0]['sources']) == 1
     assert transfer[0]['sources'][0][0] in (tape1_rse_name, tape2_rse_name)
 
     # On equal source ranking, but different distance; the smaller distance is preferred
-    transfers, _reqs_no_source, _reqs_scheme_mismatch, _reqs_only_tape_source = get_transfer_requests_and_source_replicas(rses=all_rses)
-    assert len(transfers) == 1
-    transfer = next(iter(transfers.values()))
+    [[_host, [transfer]]] = next_transfers_to_submit(rses=all_rses).items()
     assert len(transfer[0]['sources']) == 1
     assert transfer[0]['sources'][0][0] == tape2_rse_name
 
     # On different source ranking, the bigger ranking is preferred
     __fake_source_ranking(tape2_rse_id, -1)
-    transfers, _reqs_no_source, _reqs_scheme_mismatch, _reqs_only_tape_source = get_transfer_requests_and_source_replicas(rses=all_rses)
-    assert len(transfers) == 1
-    transfer = next(iter(transfers.values()))
+    [[_host, [transfer]]] = next_transfers_to_submit(rses=all_rses).items()
     assert len(transfer[0]['sources']) == 1
     assert transfer[0]['sources'][0][0] == tape1_rse_name
 
@@ -234,10 +228,7 @@ def test_multihop_requests_created(rse_factory, did_factory, root_account, core_
     did = did_factory.upload_test_file(rs0_name)
     rule_core.add_rule(dids=[did], account=root_account, copies=1, rse_expression=dst_rse_name, grouping='ALL', weight=None, lifetime=None, locked=False, subscription_id=None)
 
-    transfers, _reqs_no_source, _reqs_scheme_mismatch, _reqs_only_tape_source = get_transfer_requests_and_source_replicas(rses=rse_factory.created_rses)
-    assert len(transfers) == 1
-    transfer = next(iter(transfers.values()))
-    assert transfer[1].get('parent_request') == transfer[0].rws.request_id
+    [[_host, [transfer]]] = next_transfers_to_submit(rses=rse_factory.created_rses).items()
     # the intermediate request was correctly created
     assert request_core.get_request_by_did(rse_id=intermediate_rse_id, **did)
 
@@ -286,12 +277,10 @@ def test_singlehop_vs_multihop_priority(rse_factory, root_account, mock_scope, c
     rule_core.add_rule(dids=[did], account=root_account, copies=1, rse_expression=rse3_name, grouping='ALL', weight=None, lifetime=None, locked=False, subscription_id=None)
 
     # The singlehop must be prioritized
-    transfers, _reqs_no_source, _reqs_scheme_mismatch, _reqs_only_tape_source = get_transfer_requests_and_source_replicas(rses=rse_factory.created_rses)
-    assert len(transfers) == 1
-    transfer = next(iter(transfers.values()))
+    [[_host, [transfer]]] = next_transfers_to_submit(rses=rse_factory.created_rses).items()
     assert len(transfer) == 1
-    assert transfer[0]['file_metadata']['src_rse_id'] == rse2_id
-    assert transfer[0]['file_metadata']['dest_rse_id'] == rse3_id
+    assert transfer[0].src.rse.id == rse2_id
+    assert transfer[0].dst.rse.id == rse3_id
 
     # add same file to two source RSEs
     file = {'scope': mock_scope, 'name': 'lfn.' + generate_uuid(), 'type': 'FILE', 'bytes': 1, 'adler32': 'beefdead'}
@@ -302,6 +291,6 @@ def test_singlehop_vs_multihop_priority(rse_factory, root_account, mock_scope, c
     rule_core.add_rule(dids=[did], account=root_account, copies=1, rse_expression=rse3_name, grouping='ALL', weight=None, lifetime=None, locked=False, subscription_id=None)
 
     # The multihop must be prioritized
-    transfers, _reqs_no_source, _reqs_scheme_mismatch, _reqs_only_tape_source = get_transfer_requests_and_source_replicas(rses=rse_factory.created_rses)
-    transfer = next(iter(t for t in transfers.values() if t[0].rws.name == file['name']))
+    [[_host, transfers]] = next_transfers_to_submit(rses=rse_factory.created_rses).items()
+    transfer = next(iter(t for t in transfers if t[0].rws.name == file['name']))
     assert len(transfer) == 2
