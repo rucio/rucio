188311f9d000ec10eef29a8c64323e76d2b24f9f
jdierkes <joel.dierkes@cern.ch>
2021-10-22 15:19:52 +0200

    Patch documentation (#4903)
    * Api: Enhance documentation
    
    * Documentation: Return a non-zero exit code for modified header
    
    If a header in a python file is modified, the `tools/add_header` script with the
    `--check` argument will now return a non-zero exit code. This means that
    `tools/add_header` can be used as a pre-commit check or inside the CI/CD.

--- lib/rucio/api/account_limit.py
+++ lib/rucio/api/account_limit.py
@@ -1,18 +1,27 @@
-# Copyright European Organization for Nuclear Research (CERN)
+# -*- coding: utf-8 -*-
+# Copyright 2014-2021 CERN
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# You may not use this file except in compliance with the License.
-# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 # Authors:
-# - Martin Barisits, <martin.barisits@cern.ch>, 2014
-# - Cedric Serfon, <cedric.serfon@cern.ch>, 2015
-# - Andrew Lister, <andrew.lister@stfc.ac.uk>, 2019
-# - Hannes Hansen, <hannes.jakob.hansen@cern.ch>, 2019
-# - Eli Chadwick, <eli.chadwick@stfc.ac.uk>, 2020
-# - Patrick Austin, <patrick.austin@stfc.ac.uk>, 2020
-#
-# PY3K COMPATIBLE
+# - Martin Barisits <martin.barisits@cern.ch>, 2014
+# - Cedric Serfon <cedric.serfon@cern.ch>, 2015
+# - Hannes Hansen <hannes.jakob.hansen@cern.ch>, 2018-2019
+# - Andrew Lister <andrew.lister@stfc.ac.uk>, 2019
+# - Eli Chadwick <eli.chadwick@stfc.ac.uk>, 2020
+# - Patrick Austin <patrick.austin@stfc.ac.uk>, 2020
+# - David Población Criado <david.poblacion.criado@cern.ch>, 2021
+# - Joel Dierkes <joel.dierkes@cern.ch>, 2021
 
 import rucio.api.permission
 import rucio.common.exception
@@ -219,7 +228,7 @@ def get_local_account_usage(account, rse, issuer, vo='def'):
     :param issuer:   The issuer account.
     :param vo:       The VO to act on.
 
-    :returns:        List of dicts {'rse_id', 'bytes_used', 'files_used', 'bytes_limit'}
+    :returns:        List of dicts {'rse_id', 'rse', 'bytes', 'files', 'bytes_limit', 'bytes_remaining'}
     """
 
     rse_id = None
@@ -247,7 +256,7 @@ def get_global_account_usage(account, rse_expression, issuer, vo='def'):
     :param issuer:          The issuer account.
     :param vo:              The VO to act on.
 
-    :returns:        List of dicts {'rse_id', 'bytes_used', 'files_used', 'bytes_limit'}
+    :returns:               List of dicts {'rse_id', 'rse', 'bytes', 'files', 'bytes_limit', 'bytes_remaining'}
     """
 
     kwargs = {'account': account, 'rse_expression': rse_expression}
