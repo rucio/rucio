8a6e5dd020631a93e239dfad6e308234aaf17ef8
Rob Barnsley <rob.barnsley@skao.int>
2021-11-25 08:41:58 -0800

    metadata: commenting and variable name changes for metadata plugin __init__; Fix #3750

--- lib/rucio/core/did_meta_plugins/__init__.py
+++ lib/rucio/core/did_meta_plugins/__init__.py
@@ -16,11 +16,11 @@
 # Authors:
 # - Aristeidis Fkiaras <aristeidis.fkiaras@cern.ch>, 2020
 # - Benedikt Ziemons <benedikt.ziemons@cern.ch>, 2020-2021
+# - Rob Barnsley <rob.barnsley@skao.int>, 2021
 
 import importlib
 
-from rucio.common import config
-from rucio.common.exception import PolicyPackageNotFound, InvalidMetadata
+from rucio.common import config, exception
 from rucio.db.sqla.session import read_session
 
 try:
@@ -28,136 +28,150 @@ try:
 except ImportError:
     from configparser import NoOptionError, NoSectionError
 
-HARDCODED_METADATA_HANDLER_MODULE = "rucio.core.did_meta_plugins.did_column_meta.DidColumnMeta"
-FALLBACK_METADATA_HANDLER_MODULE = "rucio.core.did_meta_plugins.json_meta.JSONDidMeta"
+# Set default modules.
+#
+# - Hardcoded metadata is tied to column fields in the "dids" table. As such, types are restricted to column types and 
+#   additional fields cannot be created.
+# - Custom metadata refers to any plugin that has been written to interface with Rucio's metadata plugin system.
+# 
+# By default, the custom metadata plugin is set to the prepackaged json module. This utilises a separate table, 
+# "did_meta", allowing users to add custom metadata key/value pairs of any type.
+#
+HARDCODED_METADATA_PLUGIN_MODULE_PATH = "rucio.core.did_meta_plugins.did_column_meta.DidColumnMeta"
+FALLBACK_CUSTOM_METADATA_PLUGIN_MODULE_PATH = "rucio.core.did_meta_plugins.json_meta.JSONDidMeta"
 
+# Overwrite these defaults if plugins are set in the configuration file.
+#
 if config.config_has_section('metadata'):
     try:
-        METADATA_HANDLER_MODULES = config.config_get('metadata', 'plugins')
+        CUSTOM_METADATA_PLUGIN_MODULE_PATHS = config.config_get('metadata', 'plugins')
     except (NoOptionError, NoSectionError):
-        METADATA_HANDLER_MODULES = FALLBACK_METADATA_HANDLER_MODULE
+        CUSTOM_METADATA_PLUGIN_MODULE_PATHS = FALLBACK_CUSTOM_METADATA_PLUGIN_MODULE_PATH
 else:
-    METADATA_HANDLER_MODULES = FALLBACK_METADATA_HANDLER_MODULE
+    CUSTOM_METADATA_PLUGIN_MODULE_PATHS = FALLBACK_CUSTOM_METADATA_PLUGIN_MODULE_PATH
 
-META_MODULE_PATHS = [HARDCODED_METADATA_HANDLER_MODULE] + METADATA_HANDLER_MODULES.split(",")
+METADATA_PLUGIN_MODULE_PATHS = [HARDCODED_METADATA_PLUGIN_MODULE_PATH] + CUSTOM_METADATA_PLUGIN_MODULE_PATHS.split(",")
 
-METADATA_HANDLERS = []
-for meta_module_path in META_MODULE_PATHS:
+# Build a list of available plugin modules.
+#
+# Note that the order of this list is important. As the hardcoded metadata plugin module is always first, hardcoded key 
+# retrieval and setting will always take precedence over custom plugins, i.e. it is not possible to set a custom key with 
+# the same name as those in the hardcoded list.
+#
+# Another consequence of this is that if set_metadata() is called with multiple plugins specified, the first to return 
+# True to manages_key() will be used.
+# 
+METADATA_PLUGIN_MODULES = []
+for meta_module_path in METADATA_PLUGIN_MODULE_PATHS:
     try:
         base_module = ".".join(meta_module_path.split(".")[:-1])
         base_class = meta_module_path.split(".")[-1]
-        meta_handler_module = getattr(importlib.import_module(base_module), base_class)()
-        METADATA_HANDLERS.append(meta_handler_module)
+        metadata_plugin_module = getattr(importlib.import_module(base_module), base_class)()
+        METADATA_PLUGIN_MODULES.append(metadata_plugin_module)
     except ImportError:
-        raise PolicyPackageNotFound('Module ' + meta_module_path + ' not found')
+        raise exception.PolicyPackageNotFound('Module ' + meta_module_path + ' not found')
 
 
 def get_metadata(scope, name, plugin="DID_COLUMN", session=None):
     """
     Gets the metadata for given did.
-    This method has been adapted to bring the metadata from diffrent metadata storages. (HARDCODED or GENERIC for now)
-    If the filter is "ALL", will return the metadata from all available metadata storages. Else filter can be used to
-    only return the metadata of a particular storage.
+
+    This method retrieves metadata using different metadata plugins.
+
+    If [plugin] is set to "all", metadata from all available metadata plugins will be returned, 
+    else [plugin] can be used to only return the metadata using a specific plugin.
 
     :param scope: The scope of the did.
-    :param name: The name of the did.
-    :param filter: (optional) Filter down to specific metadata storages [ALL|HARDCODED|GENERIC]
+    :param name: The data identifier name.
+    :param plugin: (optional) Filter specific metadata plugins.
 
     :returns: List of metadata for did.
     """
-    if plugin == "ALL":
-        all_meta = {}
-
-        for meta_handler in METADATA_HANDLERS:
-            metadata = meta_handler.get_metadata(scope, name, session=session)
-            all_meta.update(metadata)
-
-        return all_meta
+    if plugin.lower() == "all":
+        all_metadata= {}
+        for metadata_plugin in METADATA_PLUGIN_MODULES:
+            metadata = metadata_plugin.get_metadata(scope, name, session=session)
+            all_metadata.update(metadata)
+        return all_metadata
     else:
-        for meta_handler in METADATA_HANDLERS:
-            if meta_handler.get_plugin_name().lower() == plugin.lower():
-                return meta_handler.get_metadata(scope, name, session=session)
-
-        raise NotImplementedError('Metadata plugin %s is not enabled on the server.' % plugin)
+        for metadata_plugin in METADATA_PLUGIN_MODULES:
+            if metadata_plugin.get_plugin_name().lower() == plugin.lower():
+                return metadata_plugin.get_metadata(scope, name, session=session)  
+    raise NotImplementedError('Metadata plugin "%s" is not enabled on the server.' % plugin)
 
 
 def set_metadata(scope, name, key, value, recursive=False, session=None):
     """
-    Sets the metadata for a given did.
-
-    To decide which metadata store to use, it is checking the configuration of the server and wether the key exists
-    as hardcoded.
+    Sets metadata for a given did.
 
     :param scope: The scope of the did.
-    :param name: The name of the did.
-    :param key: Key of the metadata.
-    :param value: Value of the metadata.
-    :param recursive: (Optional) Option to propagate the metadata change to content.
-    :param session: (Optional) The database session in use.
+    :param name: The data identifier name.
+    :param key: Metadata key.
+    :param value: Metadata value.
+    :param recursive: (optional) Propagate the metadata change recursively to content.
+    :param session: (optional) The database session in use.
     """
-    meta_was_set = False
-    for meta_handler in METADATA_HANDLERS:
-        if meta_handler.manages_key(key, session=session):
-            meta_handler.set_metadata(scope, name, key, value, recursive, session=session)
-            meta_was_set = True
+    # Sequentially check if each metadata plugin manages this key. Note that the order of [METADATA_PLUGIN_MODULES] 
+    # means that the key is always checked for existence in the hardcoded list first.
+    metadata_was_set = False
+    for metadata_plugin in METADATA_PLUGIN_MODULES:
+        if metadata_plugin.manages_key(key, session=session):
+            metadata_plugin.set_metadata(scope, name, key, value, recursive, session=session)
+            metadata_was_set = True
             break
 
-    if not meta_was_set:
-        raise InvalidMetadata('No plugin accepts metadata key %s on DID %s:%s' % (key, scope, name))
+    if not metadata_was_set:
+        raise exception.InvalidMetadata('No plugin manages metadata key %s for DID %s:%s' % (key, scope, name))
 
 
-def set_metadata_bulk(scope, name, meta, recursive=False, session=None):
+def set_metadata_bulk(scope, name, metadata, recursive=False, session=None):
     """
-    Sets the metadata for a given did.
-
-    To decide which metadata store to use, it is checking the
-    configuration of the server and assigns each key-value to the
-    correct plugin by checking them in order of METADATA_HANDLERS.
+    Bulk sets metadata for a given did.
 
     :param scope: The scope name.
     :param name: The data identifier name.
-    :param meta: The key-value mapping of metadata to set.
-    :param recursive: (Optional) Option to propagate the metadata change to content.
-    :param session: (Optional) The database session in use.
+    :param metadata: The key-value mapping of metadata to set.
+    :param recursive: (optional) Propagate the metadata change recursively to content.
+    :param session: (optional) The database session in use.
     """
-    denied_keys = list()
-    if not isinstance(meta, dict):
-        # always convert to dict, so that .items() can be called
-        meta = dict(meta)
-    meta_handler_keys = {meta_handler: [] for meta_handler in METADATA_HANDLERS}
+    unmanaged_keys = list()
+    if not isinstance(metadata, dict):
+        metadata = dict(metadata)
+    metadata_plugin_keys = {metadata_plugin: [] for metadata_plugin in METADATA_PLUGIN_MODULES}
 
-    for key, value in meta.items():
-        meta_is_included = False
-        # using METADATA_HANDLERS here to ensure the order of plugins applied
-        for meta_handler in METADATA_HANDLERS:
-            if meta_handler.manages_key(key, session=session):
-                meta_handler_keys[meta_handler].append(key)
-                meta_is_included = True
+    # Iterate through all keys, sequentially checking if each metadata plugin manages the considered key. If it 
+    # does, add it to the list in the plugin's entry in {metadata_plugin_keys}. Note that the order of 
+    # [METADATA_PLUGIN_MODULES] means that the key is always checked for existence in the hardcoded list first.
+    for key in metadata.keys():
+        metadata_is_included = False
+        for metadata_plugin in METADATA_PLUGIN_MODULES:
+            if metadata_plugin.manages_key(key, session=session):
+                metadata_plugin_keys[metadata_plugin].append(key)
+                metadata_is_included = True
                 break
+        if not metadata_is_included:
+            unmanaged_keys.append(key)
+    if unmanaged_keys:
+        raise exception.InvalidMetadata('No plugin manages metadata keys %s on DID %s:%s' % (unmanaged_keys, scope, name))
 
-        if not meta_is_included:
-            denied_keys.append(key)
-
-    if denied_keys:
-        raise InvalidMetadata('No plugin accepted metadata keys %s on DID %s:%s' % (denied_keys, scope, name))
-
-    for meta_handler, key_list in meta_handler_keys.items():
-        if key_list:
-            pluginmeta = {key: meta[key] for key in key_list}
-            meta_handler.set_metadata_bulk(scope, name, meta=pluginmeta, recursive=recursive, session=session)
+    # For each plugin, set the metadata.
+    for metadata_plugin, keys_managed_by_this_plugin in metadata_plugin_keys.items():
+        if keys_managed_by_this_plugin:
+            this_plugin_metadata = {key: metadata[key] for key in keys_managed_by_this_plugin}
+            metadata_plugin.set_metadata_bulk(scope, name, metadata=this_plugin_metadata, recursive=recursive, session=session)
 
 
 def delete_metadata(scope, name, key, session=None):
     """
-    Deletes the metadata stored for the given key. Currently only works for JSON metadata store
+    Deletes metadata stored for a given key.
 
     :param scope: The scope of the did.
     :param name: The name of the did.
     :param key: Key of the metadata.
     """
-    for meta_handler in METADATA_HANDLERS:
-        if meta_handler.manages_key(key, session=session):
-            meta_handler.delete_metadata(scope, name, key, session=session)
+    for metadata_plugin in METADATA_PLUGIN_MODULES:
+        if metadata_plugin.manages_key(key, session=session):
+            metadata_plugin.delete_metadata(scope, name, key, session=session)
 
 
 @read_session
@@ -165,37 +179,37 @@ def list_dids(scope=None, filters=None, did_type='collection', ignore_case=False
               offset=None, long=False, recursive=False, session=None):
     """
     List dids according to metadata.
+
     Either all of the metadata in the query should belong in the hardcoded ones, or none at all.
     A mixture of hardcoded and generic metadata is not supported at the moment.
 
-    :param scope: the scope name.
-    :param filters: dictionary of attributes by which the results should be filtered.
-    :param did_type: the type of the did: all(container, dataset, file), collection(dataset or container), dataset, container, file.
-    :param ignore_case: ignore case distinctions.
-    :param limit: limit number.
-    :param offset: offset number.
+    :param scope: The scope of the did.
+    :param filters: Dictionary of attributes by which the results should be filtered.
+    :param did_type: The type of the did: all(container, dataset, file), collection(dataset or container), dataset, container, file.
+    :param ignore_case: Ignore case distinctions.
+    :param limit: Limit number.
+    :param offset: Offset number.
     :param long: Long format option to display more information for each DID.
     :param recursive: Recursively list DIDs content.
     :param session: The database session in use.
 
-    :returns: List of dids.
+    :returns: List of dids satisfying metadata criteria.
     """
-    meta_handler_to_use = None
-    # Ensure that a single handler manages all the keys of the query
-    for key in filters:
-        if meta_handler_to_use is None:
-            for meta_handler in METADATA_HANDLERS:
-                if meta_handler.manages_key(key, session=session):
-                    meta_handler_to_use = meta_handler
+    # Set [metadata_plugin_to_use] to be the first key's plugin... 
+    metadata_plugin_to_use = None
+    for key in filters.keys():
+        if metadata_plugin_to_use is None:
+            for metadata_plugin in METADATA_PLUGIN_MODULES:
+                if metadata_plugin.manages_key(key, session=session):
+                    metadata_plugin_to_use = metadata_plugin
                     break
-        else:
-            if not meta_handler_to_use.manages_key(key, session=session):
-                # Mix case, difficult, slow and will probably blow up memory
-                raise NotImplementedError('Filter keys used do not all belong on the same metadata plugin.')
+        else:   # ... then check that this plugin manages the rest of the keys.
+            if not metadata_plugin_to_use.manages_key(key, session=session):
+                raise NotImplementedError('Filter keys used do not all belong to the same metadata plugin.')
 
-    if meta_handler_to_use:
-        return meta_handler_to_use.list_dids(scope=scope, filters=filters, did_type=did_type,
-                                             ignore_case=ignore_case, limit=limit,
-                                             offset=offset, long=long, recursive=recursive, session=session)
+    if metadata_plugin_to_use:
+        return metadata_plugin_to_use.list_dids(scope=scope, filters=filters, did_type=did_type,
+                                                ignore_case=ignore_case, limit=limit,
+                                                offset=offset, long=long, recursive=recursive, session=session)
     else:
-        raise NotImplementedError('There is no metadata plugin that manages the filter you used.')
+        raise NotImplementedError('There is no metadata plugin that manages the filter keys you requested.')
