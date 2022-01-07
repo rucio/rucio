rucio/rucio

commit 2ed62932aa5aee130df4799da91ed17607892791

Downloadclient: excluding tape replicas and reporting them to user - … (#4196)
* Downloadclient: excluding tape replicas and reporting them to user - #4193

* Downloadclient: listing rses resulted into exception due to empty set - #4193

lib/rucio/client/downloadclient.py
@@ -1057,6 +1057,15 @@ class DownloadClient:
         """
         logger = self.logger
         merged_items_with_sources = []
+
+        # if excluding tapes, we need to list them first
+        tape_rses = []
+        if self.is_tape_excluded:
+            try:
+                tape_rses = [endp['rse'] for endp in self.client.list_rses(rse_expression='istape=true')]
+            except:
+                logger.debug('No tapes found.')
+
         for item in merged_items:
             # since we're using metalink we need to explicitly give all schemes
             schemes = item.get('force_scheme')
@@ -1064,10 +1073,8 @@ class DownloadClient:
                 schemes = schemes if isinstance(schemes, list) else [schemes]
             logger.debug('schemes: %s' % schemes)
 
-            # extend RSE expression to exclude tape RSEs for non-admin accounts
+            # RSE expression, still with tape endpoints included
             rse_expression = item.get('rse')
-            if self.is_tape_excluded:
-                rse_expression = '*\istape=true' if not rse_expression else '(%s)\istape=true' % rse_expression  # NOQA: W605
             logger.debug('rse_expression: %s' % rse_expression)
 
             # get PFNs of files and datasets
@@ -1093,6 +1100,16 @@ class DownloadClient:
                     logger.error('DID does not exist: %s' % input_did)
                     # TODO: store did directly as DIDType object
                     file_items.append({'did': str(input_did), 'adler32': None, 'md5': None, 'sources': [], 'parent_dids': set()})
+
+            # filtering out tape sources
+            if self.is_tape_excluded:
+                for item in file_items:
+                    sources = item['sources']
+                    for src in item['sources']:
+                        if src in tape_rses:
+                            sources.remove(src)
+                    if not sources:
+                        logger.warning('Requested did {} has only replicas on tape. No files will be download.'.format(item['did']))
 
             nrandom = item.get('nrandom')
             if nrandom:

commit 3fa7f8d5c548c012f3279957132c7eaa4a242404

Clients: Download client does not filter out tape sources Fix #5122
The download client should not download files from a tape RSE if the user is not
privileged.

The code to check that had a bug introduced in #4196, so the user could read
from tape RSEs without restriction. This commit fixes the issue.

lib/rucio/client/downloadclient.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018-2021 CERN
+# Copyright 2018-2022 CERN
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -33,7 +33,7 @@
 # - Rakshita Varadarajan <rakshitajps@gmail.com>, 2021
 # - David Población Criado <david.poblacion.criado@cern.ch>, 2021
 # - Cedric Serfon <cedric.serfon@cern.ch>, 2021
-# - Joel Dierkes <joel.dierkes@cern.ch>, 2021
+# - Joel Dierkes <joel.dierkes@cern.ch>, 2021-2022
 
 from __future__ import division
 
@@ -1205,7 +1205,7 @@ class DownloadClient:
                 for file_item in file_items:
                     unfiltered_sources = copy.copy(file_item['sources'])
                     for src in unfiltered_sources:
-                        if src in tape_rses:
+                        if src['rse'] in tape_rses:
                             file_item['sources'].remove(src)
                     if unfiltered_sources and not file_item['sources']:
                         logger(logging.WARNING, 'The requested DID {} only has replicas on tape. Direct download from tape is prohibited. '

lib/rucio/tests/test_download.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2019-2021 CERN
+# Copyright 2019-2022 CERN
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -24,7 +24,7 @@
 # - Radu Carpa <radu.carpa@cern.ch>, 2021
 # - Martin Barisits <martin.barisits@cern.ch>, 2021
 # - Rakshita Varadarajan <rakshitajps@gmail.com>, 2021
-# - Joel Dierkes <joel.dierkes@cern.ch>, 2021
+# - Joel Dierkes <joel.dierkes@cern.ch>, 2021-2022
 
 import logging
 import os
@@ -723,3 +723,16 @@ def test_download_file_with_supported_protocol_from_config(rse_factory, did_fact
             patch('rucio.rse.protocols.%s.Default.close' % supported_impl):
         download_client.download_dids([{'did': did_str, 'impl': supported_impl}])
         mock_get.assert_called()
+
+
+def test_download_exclude_tape(rse_factory, did_factory, download_client):
+    """Client: Do not download from a tape rse."""
+    rse, rse_id = rse_factory.make_posix_rse()
+    did = did_factory.upload_test_file(rse)
+    did_str = '%s:%s' % (did['scope'], did['name'])
+
+    # We can not mock the server core code here, so mock the API
+    with patch('rucio.client.rseclient.RSEClient.list_rses', return_value=[{'rse': rse}]), \
+         TemporaryDirectory() as tmp_dir, \
+         pytest.raises(NoFilesDownloaded):
+        download_client.download_dids([{'did': did_str, 'base_dir': tmp_dir}])