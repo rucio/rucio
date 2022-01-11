86ba812f264b6aa56c8c7408a2d40b901ca76a2b
Mayank Sharma <imptodefeat@gmail.com>
2022-01-11 17:43:08 +0100

    Traces: update ipv6 and ipv4 validation in trace schemas #5074 (#5083)
    * update ipv6 and ipv4 validation in trace schemas
    * Introduce jsonschema format checkers and remove ip validation regex
    * traces: use stdlib for ip address checks #5074

--- lib/rucio/core/trace.py
+++ lib/rucio/core/trace.py
@@ -34,11 +34,12 @@ import random
 import socket
 
 import stomp
-from jsonschema import validate, ValidationError
+import ipaddress
+from jsonschema import validate, ValidationError, draft7_format_checker
 
 from rucio.common.config import config_get, config_get_int
 from rucio.common.exception import InvalidObject
-from rucio.common.schema.generic import SCOPE, RSE, UUID, TIME_ENTRY, IP, CLIENT_STATE
+from rucio.common.schema.generic import SCOPE, RSE, UUID, TIME_ENTRY, IPv4orIPv6, CLIENT_STATE
 from rucio.core.monitor import record_counter
 
 CONFIG_COMMON_LOGLEVEL = getattr(logging, config_get('common', 'loglevel', raise_exception=False, default='DEBUG').upper())
@@ -94,7 +95,7 @@ TOUCH_SCHEMA = {
         "dataset": {"type": ["string", "null"]},
         "traceTimeentry": TIME_ENTRY,
         "traceTimeentryUnix": {"type": "number"},
-        "traceIp": IP,
+        "traceIp": IPv4orIPv6,
         "traceId": UUID,
         "localSite": RSE,
         "remoteSite": RSE,
@@ -123,7 +124,7 @@ UPLOAD_SCHEMA = {
         "transferEnd": {"type": "number"},
         "traceTimeentry": TIME_ENTRY,
         "traceTimeentryUnix": {"type": "number"},
-        "traceIp": IP,
+        "traceIp": IPv4orIPv6,
         "traceId": UUID,
         "vo": {"type": "string"},
         "stateReason": {"type": "string"},
@@ -159,7 +160,7 @@ DOWNLOAD_SCHEMA = {
         "transferEnd": {"type": "number"},
         "traceTimeentry": TIME_ENTRY,
         "traceTimeentryUnix": {"type": "number"},
-        "traceIp": IP,
+        "traceIp": IPv4orIPv6,
         "traceId": UUID,
         "vo": {"type": "string"},
         "usrdn": {"type": "string"},
@@ -187,7 +188,7 @@ GET_SCHEMA = {
         "name": {"type": "string"},
         "traceTimeentry": TIME_ENTRY,
         "traceTimeentryUnix": {"type": "number"},
-        "traceIp": IP,
+        "traceIp": IPv4orIPv6,
         "traceId": UUID,
         "usrdn": {"type": "string"},
     },
@@ -205,6 +206,25 @@ SCHEMAS = {
     'get_sm_a': GET_SCHEMA,
     'sm_get_a': GET_SCHEMA
 }
+
+FORMAT_CHECKER = draft7_format_checker
+
+
+@FORMAT_CHECKER.checks(format="ipv4_or_ipv6")
+def ip_format_checker(value: str) -> bool:
+    """
+    Validates IPv4 or IPv6 string values. json schemas can use `ipv4_or_ipv6` as a valid `format` argument
+    """
+    try:
+        ipaddress.ip_address(value)
+    except ValueError:
+        LOGGER.debug(f"{value} is not a valid IPv4 or IPv6 address and raises an errors upon validation.")
+        result = False
+    else:
+        result = True
+    finally:
+        return result
+
 
 logging.getLogger("stomp").setLevel(logging.CRITICAL)
 
@@ -282,6 +302,6 @@ def validate_schema(obj):
 
     try:
         if obj and 'eventType' in obj:
-            validate(obj, SCHEMAS.get(obj['eventType'].lower()))
+            validate(obj, SCHEMAS.get(obj['eventType'].lower()), format_checker=FORMAT_CHECKER)
     except ValidationError as error:
         raise InvalidObject(error)
