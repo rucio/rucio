54eac0acf906c93c5a816b06b288ad8ac049964d
Radu Carpa <radu.carpa@cern.ch>
2022-01-27 15:58:04 +0100

    Replicas: fix geoip sorting. Closes #5133
    The bug was introduced in #4968: _sort_pfns_by_priority was called,
    but its return value wasn't used to actually sort the replicas.
    
    Add a test using a small geoip-city test database. To add this test,
    the database download/extraction/usage code was refactored a bit,
    but there weren't any particular issues with that code.

---
+++ lib/rucio/tests/inputs/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 CERN
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors:
+# - Radu Carpa <radu.carpa@cern.ch>, 2022
+
+import os
+from pathlib import Path
+
+# GeoLite2-City-Test.mmdb downloaded on 26/Jan/2022 from ./test-data/ in
+# https://github.com/maxmind/MaxMind-DB/tree/2f0ef0249245c7f19feffa366793a6fffd529701/
+# Check ./source-data/GeoLite2-City-Test.json in this repository for IPs to use in tests
+GEOIP_LITE2_CITY_TEST_DB = Path(os.path.abspath(__file__)).parent / 'GeoLite2-City-Test.tar.gz'
