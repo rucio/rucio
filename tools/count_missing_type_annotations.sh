979e59161bca99614aa897df8f48ef246075316b
Joel Dierkes <joel.dierkes@cern.ch>
2022-04-14 09:55:34 +0200

    Add the `count_missing_python_type_annotations` script #4455
    With rucio 1.29 python2 got deprecated. This deprecation enables us to introduce
    new features used by python3. One of these features is type annotations on the
    code itself.
    
    Type hints allow us to run a static type checker like `mypy` and analyze the
    code for inconsistencies. Having a strongly typed codebase enables us to avoid
    type inconsistency bugs, and it makes the code more readable, thus potentially
    shorten the time to add new features.
    
    The plan is to integrate a job in the CI/CD, which reports an error if the
    number of missing type annotations is increased by a PR. This is the result of
    newly written code, which is not annotated. This forces the user to annotate
    their (or potentially some other) code.
    
    This commit introduces the `count_missing_python_type_annotations_rucio`
    functions, which uses `flake8` and the flake8 plugin `flake8-annotations` to
    count the number of missing type annotations.
    
    `mypy` enables us to check the type annotations for inconsistencies. However, we
    can not use `mypy` to count the number of missing type annotations, since it
    caps the number of reported errors to 200. We can use `mypy` in a later step to
    check the annotations for consistency.
    
    Certain directories are excluded from the type checking:
    
    - The test directory: Type checking the tests is not our goal right now. It
      decreases the time to write test code while providing very little additive.
    
    - The db migration directory: The db migration should focus on the database, not
      the implementation. We could enable this in the future, but it has no priority
      right now.
    
    - The tools directory: Scripts should be written quickly and should not
      necessarily require typed annotations. This can also be changed in the future,
      to enable type-save scripts.
    
    - The bin directory: A strategy is required here on how to handle type
      annotations properly.

---
+++ tools/count_missing_type_annotations.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+# Copyright CERN since 2022
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Script to count the number of missing python type annotations in the project.
+
+set -e
+
+
+source $(dirname "$0")/count_missing_type_annotations_utils.sh
+
+
+TMP_FILE_NAME=python_type_annotations_report.txt
+create_missing_python_type_annotations_report $TMP_FILE_NAME
+NUMBER_MISSING_TYPE_ANNOTATIONS=$(wc -l < $TMP_FILE_NAME)
+
+echo "Number of missing annotations: $NUMBER_MISSING_TYPE_ANNOTATIONS"
+
+rm $TMP_FILE_NAME
