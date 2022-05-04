5d13bdfffcc0f6bc90334fdec03b0ac37aa3dcd5
Radu Carpa <radu.carpa@cern.ch>
2022-05-04 15:50:59 +0200

    Rules: de-indent code in judges
    In each daemon:
     - Remove useless temporary "if True:"
     - Remove else after "if ... return" and de-indent

--- lib/rucio/daemons/judge/injector.py
+++ lib/rucio/daemons/judge/injector.py
@@ -61,78 +61,77 @@ def rule_injector(once=False, sleep_time=60):
 def run_once(paused_rules, heartbeat_handler, **_kwargs):
     worker_number, total_workers, logger = heartbeat_handler.live()
 
-    if True:  # TODO: de-indent in next commit
-        try:
-            start = time.time()
+    try:
+        start = time.time()
 
-            # Refresh paused rules
-            iter_paused_rules = deepcopy(paused_rules)
-            for key in iter_paused_rules:
-                if datetime.utcnow() > paused_rules[key]:
-                    del paused_rules[key]
+        # Refresh paused rules
+        iter_paused_rules = deepcopy(paused_rules)
+        for key in iter_paused_rules:
+            if datetime.utcnow() > paused_rules[key]:
+                del paused_rules[key]
 
-            rules = get_injected_rules(total_workers=total_workers,
-                                       worker_number=worker_number,
-                                       limit=100,
-                                       blocked_rules=[key for key in paused_rules])
-            logger(logging.DEBUG, 'index query time %f fetch size is %d' % (time.time() - start, len(rules)))
+        rules = get_injected_rules(total_workers=total_workers,
+                                   worker_number=worker_number,
+                                   limit=100,
+                                   blocked_rules=[key for key in paused_rules])
+        logger(logging.DEBUG, 'index query time %f fetch size is %d' % (time.time() - start, len(rules)))
 
-            if not rules:
-                logger(logging.DEBUG, 'did not get any work (paused_rules=%s)' % str(len(paused_rules)))
-                return
-            else:
-                for rule in rules:
-                    rule_id = rule[0]
-                    logger(logging.INFO, 'Injecting rule %s' % rule_id)
-                    if graceful_stop.is_set():
-                        break
-                    try:
-                        start = time.time()
-                        inject_rule(rule_id=rule_id, logger=logger)
-                        logger(logging.DEBUG, 'injection of %s took %f' % (rule_id, time.time() - start))
-                    except (DatabaseException, DatabaseError) as e:
-                        if match('.*ORA-00054.*', str(e.args[0])):
-                            paused_rules[rule_id] = datetime.utcnow() + timedelta(seconds=randint(60, 600))
-                            record_counter('rule.judge.exceptions.{exception}', labels={'exception': 'LocksDetected'})
-                            logger(logging.WARNING, 'Locks detected for %s' % rule_id)
-                        elif match('.*QueuePool.*', str(e.args[0])):
-                            logger(logging.WARNING, 'DatabaseException', exc_info=True)
-                            record_counter('rule.judge.exceptions.{exception}', labels={'exception': e.__class__.__name__})
-                        elif match('.*ORA-03135.*', str(e.args[0])):
-                            logger(logging.WARNING, 'DatabaseException', exc_info=True)
-                            record_counter('rule.judge.exceptions.{exception}', labels={'exception': e.__class__.__name__})
-                        else:
-                            logger(logging.ERROR, 'DatabaseException', exc_info=True)
-                            record_counter('rule.judge.exceptions.{exception}', labels={'exception': e.__class__.__name__})
-                    except (RSEWriteBlocked) as e:
-                        paused_rules[rule_id] = datetime.utcnow() + timedelta(seconds=randint(60, 600))
-                        logger(logging.WARNING, 'RSEWriteBlocked for rule %s' % rule_id)
-                        record_counter('rule.judge.exceptions.{exception}', labels={'exception': e.__class__.__name__})
-                    except ReplicationRuleCreationTemporaryFailed as e:
-                        paused_rules[rule_id] = datetime.utcnow() + timedelta(seconds=randint(60, 600))
-                        logger(logging.WARNING, 'ReplicationRuleCreationTemporaryFailed for rule %s' % rule_id)
-                        record_counter('rule.judge.exceptions.{exception}', labels={'exception': e.__class__.__name__})
-                    except RuleNotFound:
-                        pass
-                    except InsufficientAccountLimit:
-                        # A rule with InsufficientAccountLimit on injection hangs there potentially forever
-                        # It should be marked as SUSPENDED
-                        logger(logging.INFO, 'Marking rule %s as SUSPENDED due to InsufficientAccountLimit' % rule_id)
-                        update_rule(rule_id=rule_id, options={'state': 'SUSPENDED'})
+        if not rules:
+            logger(logging.DEBUG, 'did not get any work (paused_rules=%s)' % str(len(paused_rules)))
+            return
 
-        except (DatabaseException, DatabaseError) as e:
-            if match('.*QueuePool.*', str(e.args[0])):
-                logger(logging.WARNING, 'DatabaseException', exc_info=True)
+        for rule in rules:
+            rule_id = rule[0]
+            logger(logging.INFO, 'Injecting rule %s' % rule_id)
+            if graceful_stop.is_set():
+                break
+            try:
+                start = time.time()
+                inject_rule(rule_id=rule_id, logger=logger)
+                logger(logging.DEBUG, 'injection of %s took %f' % (rule_id, time.time() - start))
+            except (DatabaseException, DatabaseError) as e:
+                if match('.*ORA-00054.*', str(e.args[0])):
+                    paused_rules[rule_id] = datetime.utcnow() + timedelta(seconds=randint(60, 600))
+                    record_counter('rule.judge.exceptions.{exception}', labels={'exception': 'LocksDetected'})
+                    logger(logging.WARNING, 'Locks detected for %s' % rule_id)
+                elif match('.*QueuePool.*', str(e.args[0])):
+                    logger(logging.WARNING, 'DatabaseException', exc_info=True)
+                    record_counter('rule.judge.exceptions.{exception}', labels={'exception': e.__class__.__name__})
+                elif match('.*ORA-03135.*', str(e.args[0])):
+                    logger(logging.WARNING, 'DatabaseException', exc_info=True)
+                    record_counter('rule.judge.exceptions.{exception}', labels={'exception': e.__class__.__name__})
+                else:
+                    logger(logging.ERROR, 'DatabaseException', exc_info=True)
+                    record_counter('rule.judge.exceptions.{exception}', labels={'exception': e.__class__.__name__})
+            except (RSEWriteBlocked) as e:
+                paused_rules[rule_id] = datetime.utcnow() + timedelta(seconds=randint(60, 600))
+                logger(logging.WARNING, 'RSEWriteBlocked for rule %s' % rule_id)
                 record_counter('rule.judge.exceptions.{exception}', labels={'exception': e.__class__.__name__})
-            elif match('.*ORA-03135.*', str(e.args[0])):
-                logger(logging.WARNING, 'DatabaseException', exc_info=True)
+            except ReplicationRuleCreationTemporaryFailed as e:
+                paused_rules[rule_id] = datetime.utcnow() + timedelta(seconds=randint(60, 600))
+                logger(logging.WARNING, 'ReplicationRuleCreationTemporaryFailed for rule %s' % rule_id)
                 record_counter('rule.judge.exceptions.{exception}', labels={'exception': e.__class__.__name__})
-            else:
-                logger(logging.CRITICAL, 'DatabaseException', exc_info=True)
-                record_counter('rule.judge.exceptions.{exception}', labels={'exception': e.__class__.__name__})
-        except Exception as e:
-            logger(logging.CRITICAL, 'Exception', exc_info=True)
+            except RuleNotFound:
+                pass
+            except InsufficientAccountLimit:
+                # A rule with InsufficientAccountLimit on injection hangs there potentially forever
+                # It should be marked as SUSPENDED
+                logger(logging.INFO, 'Marking rule %s as SUSPENDED due to InsufficientAccountLimit' % rule_id)
+                update_rule(rule_id=rule_id, options={'state': 'SUSPENDED'})
+
+    except (DatabaseException, DatabaseError) as e:
+        if match('.*QueuePool.*', str(e.args[0])):
+            logger(logging.WARNING, 'DatabaseException', exc_info=True)
             record_counter('rule.judge.exceptions.{exception}', labels={'exception': e.__class__.__name__})
+        elif match('.*ORA-03135.*', str(e.args[0])):
+            logger(logging.WARNING, 'DatabaseException', exc_info=True)
+            record_counter('rule.judge.exceptions.{exception}', labels={'exception': e.__class__.__name__})
+        else:
+            logger(logging.CRITICAL, 'DatabaseException', exc_info=True)
+            record_counter('rule.judge.exceptions.{exception}', labels={'exception': e.__class__.__name__})
+    except Exception as e:
+        logger(logging.CRITICAL, 'Exception', exc_info=True)
+        record_counter('rule.judge.exceptions.{exception}', labels={'exception': e.__class__.__name__})
 
 
 def stop(signum=None, frame=None):
