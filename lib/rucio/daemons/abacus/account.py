390860e0fe54b9f607117fad0fa3bcc8952960f4
Radu Carpa <radu.carpa@cern.ch>
2022-05-10 11:21:02 +0200

    Metadata: fix code indentation in abacus daemons

--- lib/rucio/daemons/abacus/account.py
+++ lib/rucio/daemons/abacus/account.py
@@ -49,25 +49,23 @@ def account_update(once=False, sleep_time=10):
 def run_once(heartbeat_handler, **_kwargs):
     worker_number, total_workers, logger = heartbeat_handler.live()
 
-    if True:
-        if True:
-            start = time.time()  # NOQA
-            account_rse_ids = get_updated_account_counters(total_workers=total_workers,
-                                                           worker_number=worker_number)
-            logger(logging.DEBUG, 'Index query time %f size=%d' % (time.time() - start, len(account_rse_ids)))
+    start = time.time()  # NOQA
+    account_rse_ids = get_updated_account_counters(total_workers=total_workers,
+                                                   worker_number=worker_number)
+    logger(logging.DEBUG, 'Index query time %f size=%d' % (time.time() - start, len(account_rse_ids)))
 
-            # If the list is empty, sent the worker to sleep
-            if not account_rse_ids:
-                logger(logging.INFO, 'did not get any work')
-                return
-            else:
-                for account_rse_id in account_rse_ids:
-                    worker_number, total_workers, logger = heartbeat_handler.live()
-                    if graceful_stop.is_set():
-                        break
-                    start_time = time.time()
-                    update_account_counter(account=account_rse_id[0], rse_id=account_rse_id[1])
-                    logger(logging.DEBUG, 'update of account-rse counter "%s-%s" took %f' % (account_rse_id[0], account_rse_id[1], time.time() - start_time))
+    # If the list is empty, sent the worker to sleep
+    if not account_rse_ids:
+        logger(logging.INFO, 'did not get any work')
+        return
+
+    for account_rse_id in account_rse_ids:
+        worker_number, total_workers, logger = heartbeat_handler.live()
+        if graceful_stop.is_set():
+            break
+        start_time = time.time()
+        update_account_counter(account=account_rse_id[0], rse_id=account_rse_id[1])
+        logger(logging.DEBUG, 'update of account-rse counter "%s-%s" took %f' % (account_rse_id[0], account_rse_id[1], time.time() - start_time))
 
 
 def stop(signum=None, frame=None):
