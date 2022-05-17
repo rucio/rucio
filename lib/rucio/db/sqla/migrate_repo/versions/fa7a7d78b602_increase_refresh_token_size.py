ad9d486bc010c1029ebdda4c31a4b3336e374606
Rizart Dona <rizart.dona@gmail.com>
2022-05-18 01:08:55 +0200

    Authentication & Authorisation : increase refresh token size #5437

---
+++ lib/rucio/db/sqla/migrate_repo/versions/fa7a7d78b602_increase_refresh_token_size.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# Copyright European Organization for Nuclear Research (CERN) since 2012
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+''' increase refresh token size '''
+
+import sqlalchemy as sa
+
+from alembic import context
+from alembic.op import alter_column
+
+# Alembic revision identifiers
+revision = 'fa7a7d78b602'
+down_revision = 'fe1a65b176c9'
+
+
+def upgrade():
+    '''
+    Upgrade the database to this revision
+    '''
+
+    if context.get_context().dialect.name in ['oracle', 'mysql', 'postgresql']:
+        alter_column('tokens', 'refresh_token',
+                existing_type=sa.String(length=315),
+                type_=sa.String(length=3072))
+
+
+def downgrade():
+    '''
+    Downgrade the database to the previous revision
+    '''
+    pass
