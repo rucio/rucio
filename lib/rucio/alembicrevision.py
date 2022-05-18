8c3bbefe5ab74cbe8847e703c865f4e5457e0a9b
Rizart Dona <rizart.dona@gmail.com>
2022-05-18 08:16:12 +0200

    Authentication & Authorisation : fix alembic logic #5437

--- lib/rucio/alembicrevision.py
+++ lib/rucio/alembicrevision.py
@@ -13,4 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ALEMBIC_REVISION = '1677d4d803c8'  # the current alembic head revision
+ALEMBIC_REVISION = 'fa7a7d78b602'  # the current alembic head revision
