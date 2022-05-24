d34eb764e7d47e4dee9e02f47f8e55e938f40064
Martin Barisits <martin.barisits@cern.ch>
2022-05-24 10:42:17 +0200

    Core: Add heartbeat handler to undertaker; #5443

--- lib/rucio/daemons/undertaker/undertaker.py
+++ lib/rucio/daemons/undertaker/undertaker.py
@@ -17,107 +17,100 @@
 Undertaker is a daemon to manage expired did.
 '''
 
+import functools
 import logging
-import os
-import socket
 import threading
-import time
 import traceback
 from copy import deepcopy
 from datetime import datetime, timedelta
 from random import randint
 from re import match
+from typing import Tuple, Dict
 
 from sqlalchemy.exc import DatabaseError
 
 import rucio.db.sqla.util
 from rucio.common.exception import DatabaseException, UnsupportedOperation, RuleNotFound
-from rucio.common.logging import setup_logging, formatted_logger
+from rucio.common.logging import setup_logging
 from rucio.common.types import InternalAccount
-from rucio.common.utils import chunks, daemon_sleep
+from rucio.common.utils import chunks
 from rucio.core.did import list_expired_dids, delete_dids
-from rucio.core.heartbeat import live, die, sanity_check
 from rucio.core.monitor import record_counter
+from rucio.daemons.common import run_daemon, HeartbeatHandler
 
 logging.getLogger("requests").setLevel(logging.CRITICAL)
 
-GRACEFUL_STOP = threading.Event()
+graceful_stop = threading.Event()
 
 
-def undertaker(worker_number=1, total_workers=1, chunk_size=5, once=False, sleep_time=60):
+def undertaker(once: bool = False, sleep_time: int = 60, chunk_size: int = 10):
     """
     Main loop to select and delete dids.
     """
-    logging.info('Undertaker(%s): starting', worker_number)
-    logging.info('Undertaker(%s): started', worker_number)
     executable = 'undertaker'
-    hostname = socket.gethostname()
-    pid = os.getpid()
-    thread = threading.current_thread()
-    sanity_check(executable=executable, hostname=hostname)
-
     paused_dids = {}  # {(scope, name): datetime}
+    run_daemon(
+        once=once,
+        graceful_stop=graceful_stop,
+        executable=executable,
+        logger_prefix=executable,
+        partition_wait_time=1,
+        sleep_time=sleep_time,
+        run_once_fnc=functools.partial(
+            run_once,
+            paused_dids=paused_dids,
+            chunk_size=chunk_size,
+        )
+    )
 
-    while not GRACEFUL_STOP.is_set():
-        try:
-            heartbeat = live(executable=executable, hostname=hostname, pid=pid, thread=thread, older_than=6000)
-            prepend_str = 'undertaker [%i/%i] : ' % (heartbeat['assign_thread'], heartbeat['nr_threads'])
-            logger = formatted_logger(logging.log, prepend_str + '%s')
-            logger(logging.INFO, 'Live gives {0[heartbeat]}'.format(locals()))
 
-            start = time.time()
+def run_once(paused_dids: Dict[Tuple, datetime], chunk_size: int, heartbeat_handler: HeartbeatHandler, **_kwargs):
+    worker_number, total_workers, logger = heartbeat_handler.live()
 
-            # Refresh paused dids
-            iter_paused_dids = deepcopy(paused_dids)
-            for key in iter_paused_dids:
-                if datetime.utcnow() > paused_dids[key]:
-                    del paused_dids[key]
+    try:
+        # Refresh paused dids
+        iter_paused_dids = deepcopy(paused_dids)
+        for key in iter_paused_dids:
+            if datetime.utcnow() > paused_dids[key]:
+                del paused_dids[key]
 
-            dids = list_expired_dids(worker_number=heartbeat['assign_thread'], total_workers=heartbeat['nr_threads'], limit=10000)
+        dids = list_expired_dids(worker_number=worker_number, total_workers=total_workers, limit=10000)
 
-            dids = [did for did in dids if (did['scope'], did['name']) not in paused_dids]
+        dids = [did for did in dids if (did['scope'], did['name']) not in paused_dids]
 
-            if not dids and not once:
-                logger(logging.INFO, 'Nothing to do. sleep 60.')
-                daemon_sleep(start_time=start, sleep_time=sleep_time, graceful_stop=GRACEFUL_STOP)
-                continue
+        if not dids:
+            logger(logging.INFO, 'did not get any work')
+            return
 
-            for chunk in chunks(dids, chunk_size):
-                try:
-                    logger(logging.INFO, 'Receive %s dids to delete', len(chunk))
-                    delete_dids(dids=chunk, account=InternalAccount('root', vo='def'), expire_rules=True)
-                    logger(logging.INFO, 'Delete %s dids', len(chunk))
-                    record_counter(name='undertaker.delete_dids', delta=len(chunk))
-                except RuleNotFound as error:
-                    logger(logging.ERROR, error)
-                except (DatabaseException, DatabaseError, UnsupportedOperation) as e:
-                    if match('.*ORA-00054.*', str(e.args[0])) or match('.*55P03.*', str(e.args[0])) or match('.*3572.*', str(e.args[0])):
-                        for did in chunk:
-                            paused_dids[(did['scope'], did['name'])] = datetime.utcnow() + timedelta(seconds=randint(600, 2400))
-                        record_counter('undertaker.delete_dids.exceptions.{exception}', labels={'exception': 'LocksDetected'})
-                        logger(logging.WARNING, 'Locks detected for chunk')
-                    else:
-                        logger(logging.ERROR, 'Got database error %s.', str(e))
-        except:
-            logging.critical(traceback.format_exc())
-            time.sleep(1)
-
-        if once:
-            break
-
-    die(executable=executable, hostname=hostname, pid=pid, thread=thread)
-    logging.info('Undertaker(%s): graceful stop requested', worker_number)
-    logging.info('Undertaker(%s): graceful stop done', worker_number)
+        for chunk in chunks(dids, chunk_size):
+            _, _, logger = heartbeat_handler.live()
+            try:
+                logger(logging.INFO, 'Receive %s dids to delete', len(chunk))
+                delete_dids(dids=chunk, account=InternalAccount('root', vo='def'), expire_rules=True)
+                logger(logging.INFO, 'Delete %s dids', len(chunk))
+                record_counter(name='undertaker.delete_dids', delta=len(chunk))
+            except RuleNotFound as error:
+                logger(logging.ERROR, error)
+            except (DatabaseException, DatabaseError, UnsupportedOperation) as e:
+                if match('.*ORA-00054.*', str(e.args[0])) or match('.*55P03.*', str(e.args[0])) or match('.*3572.*', str(e.args[0])):
+                    for did in chunk:
+                        paused_dids[(did['scope'], did['name'])] = datetime.utcnow() + timedelta(seconds=randint(600, 2400))
+                    record_counter('undertaker.delete_dids.exceptions.{exception}', labels={'exception': 'LocksDetected'})
+                    logger(logging.WARNING, 'Locks detected for chunk')
+                else:
+                    logger(logging.ERROR, 'Got database error %s.', str(e))
+    except:
+        logging.critical(traceback.format_exc())
 
 
 def stop(signum=None, frame=None):
     """
     Graceful exit.
     """
-    GRACEFUL_STOP.set()
+    graceful_stop.set()
 
 
-def run(once=False, total_workers=1, chunk_size=10, sleep_time=60):
+def run(once: bool = False, total_workers: int = 1, chunk_size: int = 10, sleep_time: int = 60):
     """
     Starts up the undertaker threads.
     """
@@ -126,13 +119,15 @@ def run(once=False, total_workers=1, chunk_size=10, sleep_time=60):
     if rucio.db.sqla.util.is_old_db():
         raise DatabaseException('Database was not updated, daemon won\'t start')
 
-    logging.info('main: starting threads')
-    threads = [threading.Thread(target=undertaker, kwargs={'worker_number': i, 'total_workers': total_workers,
-                                                           'once': once, 'chunk_size': chunk_size,
-                                                           'sleep_time': sleep_time}) for i in range(0, total_workers)]
-    [t.start() for t in threads]
-    logging.info('main: waiting for interrupts')
+    if once:
+        undertaker(once)
+    else:
+        logging.info('main: starting threads')
+        threads = [threading.Thread(target=undertaker, kwargs={'once': once, 'chunk_size': chunk_size,
+                                                               'sleep_time': sleep_time}) for i in range(0, total_workers)]
+        [t.start() for t in threads]
+        logging.info('main: waiting for interrupts')
 
-    # Interruptible joins require a timeout.
-    while threads[0].is_alive():
-        [t.join(timeout=3.14) for t in threads]
+        # Interruptible joins require a timeout.
+        while threads[0].is_alive():
+            [t.join(timeout=3.14) for t in threads]
