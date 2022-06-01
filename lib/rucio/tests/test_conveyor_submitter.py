2540ee13e6bfe6a4332371b19553a3d69f989041
Joel Dierkes <joel.dierkes@ovgu.de>
2022-06-01 15:23:22 +0200

    Documentation: Resolve the file headers #5455
    The `add_header` script got modified. This commit resolves the headers
    to the newest state.

--- lib/rucio/tests/test_conveyor_submitter.py
+++ lib/rucio/tests/test_conveyor_submitter.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright CERN since 2021
+# Copyright European Organization for Nuclear Research (CERN) since 2012
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
