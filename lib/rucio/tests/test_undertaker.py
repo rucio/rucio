a83e226d73cead47cab4d812cf79d3481e671ecf
Radu Carpa <radu.carpa@cern.ch>
2022-07-13 12:49:03 +0200

    Core & Internals: duplicate delete_dids code
    One of them will be moved to use temporary tables

--- lib/rucio/tests/test_undertaker.py
+++ lib/rucio/tests/test_undertaker.py
@@ -13,13 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import unittest
 from datetime import datetime, timedelta
 from logging import getLogger
 
 import pytest
 
-from rucio.common.config import config_get_bool
 from rucio.common.policy import get_policy
 from rucio.common.types import InternalAccount, InternalScope
 from rucio.common.utils import generate_uuid
@@ -32,60 +30,56 @@ from rucio.daemons.judge.cleaner import rule_cleaner
 from rucio.daemons.undertaker.undertaker import undertaker
 from rucio.db.sqla.util import json_implemented
 from rucio.tests.common import rse_name_generator
-from rucio.tests.common_server import get_vo
 
 LOG = getLogger(__name__)
 
 
 @pytest.mark.dirty
 @pytest.mark.noparallel(reason='uses pre-defined rses; runs undertaker, which impacts other tests')
-class TestUndertaker(unittest.TestCase):
+class TestUndertaker:
 
-    def setUp(self):
-        if config_get_bool('common', 'multi_vo', raise_exception=False, default=False):
-            self.vo = {'vo': get_vo()}
-        else:
-            self.vo = {}
-
-    def test_undertaker(self):
+    @pytest.mark.parametrize("file_config_mock", [
+        # Run test twice: with, and without, temp tables
+        {"overrides": [('core', 'use_temp_tables', 'True')]},
+        {"overrides": [('core', 'use_temp_tables', 'False')]},
+    ], indirect=True)
+    def test_undertaker(self, vo, mock_scope, root_account, file_config_mock):
         """ UNDERTAKER (CORE): Test the undertaker. """
-        tmp_scope = InternalScope('mock', **self.vo)
-        jdoe = InternalAccount('jdoe', **self.vo)
-        root = InternalAccount('root', **self.vo)
+        jdoe = InternalAccount('jdoe', vo=vo)
 
         nbdatasets = 5
         nbfiles = 5
         rse = 'MOCK'
-        rse_id = get_rse_id('MOCK', **self.vo)
+        rse_id = get_rse_id('MOCK', vo=vo)
 
         set_local_account_limit(jdoe, rse_id, -1)
 
         dsns1 = [{'name': 'dsn_%s' % generate_uuid(),
-                  'scope': tmp_scope,
+                  'scope': mock_scope,
                   'type': 'DATASET',
                   'lifetime': -1} for _ in range(nbdatasets)]
 
         dsns2 = [{'name': 'dsn_%s' % generate_uuid(),
-                  'scope': tmp_scope,
+                  'scope': mock_scope,
                   'type': 'DATASET',
                   'lifetime': -1,
                   'rules': [{'account': jdoe, 'copies': 1,
                              'rse_expression': rse,
                              'grouping': 'DATASET'}]} for _ in range(nbdatasets)]
 
-        add_dids(dids=dsns1 + dsns2, account=root)
+        add_dids(dids=dsns1 + dsns2, account=root_account)
 
         # arbitrary keys do not work without JSON support (sqlite, Oracle < 12)
         if json_implemented():
             # Add generic metadata on did
-            set_metadata(tmp_scope, dsns1[0]['name'], "test_key", "test_value")
+            set_metadata(mock_scope, dsns1[0]['name'], "test_key", "test_value")
 
         replicas = list()
         for dsn in dsns1 + dsns2:
-            files = [{'scope': tmp_scope, 'name': 'file_%s' % generate_uuid(),
+            files = [{'scope': mock_scope, 'name': 'file_%s' % generate_uuid(),
                       'bytes': 1, 'adler32': '0cc737eb',
                       'tombstone': datetime.utcnow() + timedelta(weeks=2), 'meta': {'events': 10}} for _ in range(nbfiles)]
-            attach_dids(scope=tmp_scope, name=dsn['name'], rse_id=rse_id, dids=files, account=root)
+            attach_dids(scope=mock_scope, name=dsn['name'], rse_id=rse_id, dids=files, account=root_account)
             replicas += files
 
         add_rules(dids=dsns1, rules=[{'account': jdoe, 'copies': 1, 'rse_expression': rse, 'grouping': 'DATASET'}])
@@ -96,61 +90,67 @@ class TestUndertaker(unittest.TestCase):
         for replica in replicas:
             assert get_replica(scope=replica['scope'], name=replica['name'], rse_id=rse_id)['tombstone'] is not None
 
-    def test_list_expired_dids_with_locked_rules(self):
+    @pytest.mark.parametrize("file_config_mock", [
+        # Run test twice: with, and without, temp tables
+        {"overrides": [('core', 'use_temp_tables', 'True')]},
+        {"overrides": [('core', 'use_temp_tables', 'False')]},
+    ], indirect=True)
+    def test_list_expired_dids_with_locked_rules(self, vo, mock_scope, root_account, file_config_mock):
         """ UNDERTAKER (CORE): Test that the undertaker does not list expired dids with locked rules"""
-        tmp_scope = InternalScope('mock', **self.vo)
-        jdoe = InternalAccount('jdoe', **self.vo)
-        root = InternalAccount('root', **self.vo)
+        jdoe = InternalAccount('jdoe', vo=vo)
 
         # Add quota
-        set_local_account_limit(jdoe, get_rse_id('MOCK', **self.vo), -1)
+        set_local_account_limit(jdoe, get_rse_id('MOCK', vo=vo), -1)
 
         dsn = {'name': 'dsn_%s' % generate_uuid(),
-               'scope': tmp_scope,
+               'scope': mock_scope,
                'type': 'DATASET',
                'lifetime': -1,
                'rules': [{'account': jdoe, 'copies': 1,
                           'rse_expression': 'MOCK', 'locked': True,
                           'grouping': 'DATASET'}]}
 
-        add_dids(dids=[dsn], account=root)
+        add_dids(dids=[dsn], account=root_account)
 
         for did in list_expired_dids(limit=1000):
             assert(did['scope'] != dsn['scope'] and did['name'] != dsn['name'])
 
-    def test_atlas_archival_policy(self):
+    @pytest.mark.parametrize("file_config_mock", [
+        # Run test twice: with, and without, temp tables
+        {"overrides": [('core', 'use_temp_tables', 'True')]},
+        {"overrides": [('core', 'use_temp_tables', 'False')]},
+    ], indirect=True)
+    def test_atlas_archival_policy(self, vo, mock_scope, root_account, file_config_mock):
         """ UNDERTAKER (CORE): Test the atlas archival policy. """
         if get_policy() != 'atlas':
             LOG.info("Skipping atlas-specific test")
             return
 
-        tmp_scope = InternalScope('mock', **self.vo)
-        jdoe = InternalAccount('jdoe', **self.vo)
-        root = InternalAccount('root', **self.vo)
+        jdoe = InternalAccount('jdoe', vo=vo)
 
         nbdatasets = 5
         nbfiles = 5
 
         rse = 'LOCALGROUPDISK_%s' % rse_name_generator()
-        rse_id = add_rse(rse, **self.vo)
+        rse_id = add_rse(rse, vo=vo)
 
         set_local_account_limit(jdoe, rse_id, -1)
 
         dsns2 = [{'name': 'dsn_%s' % generate_uuid(),
-                  'scope': tmp_scope,
+                  'scope': mock_scope,
                   'type': 'DATASET',
                   'lifetime': -1,
                   'rules': [{'account': jdoe, 'copies': 1,
                              'rse_expression': rse,
                              'grouping': 'DATASET'}]} for _ in range(nbdatasets)]
 
-        add_dids(dids=dsns2, account=root)
+        add_dids(dids=dsns2, account=root_account)
 
         replicas = list()
         for dsn in dsns2:
-            files = [{'scope': tmp_scope, 'name': 'file_%s' % generate_uuid(), 'bytes': 1,
+            files = [{'scope': mock_scope, 'name': 'file_%s' % generate_uuid(), 'bytes': 1,
                       'adler32': '0cc737eb', 'tombstone': datetime.utcnow() + timedelta(weeks=2), 'meta': {'events': 10}} for _ in range(nbfiles)]
-            attach_dids(scope=tmp_scope, name=dsn['name'], rse_id=rse_id, dids=files, account=root)
+            attach_dids(scope=mock_scope, name=dsn['name'], rse_id=rse_id, dids=files, account=root_account)
             replicas += files
 
         undertaker(once=True)
@@ -159,8 +159,8 @@ class TestUndertaker(unittest.TestCase):
             assert(get_replica(scope=replica['scope'], name=replica['name'], rse_id=rse_id)['tombstone'] is None)
 
         for dsn in dsns2:
-            assert(get_did(scope=InternalScope('archive', **self.vo), name=dsn['name'])['name'] == dsn['name'])
-            assert(len([x for x in list_rules(filters={'scope': InternalScope('archive', **self.vo), 'name': dsn['name']})]) == 1)
+            assert(get_did(scope=InternalScope('archive', vo=vo), name=dsn['name'])['name'] == dsn['name'])
+            assert(len([x for x in list_rules(filters={'scope': InternalScope('archive', vo=vo), 'name': dsn['name']})]) == 1)
 
 
 @pytest.mark.noparallel(reason='runs undertaker, which impacts other tests')
@@ -170,11 +170,15 @@ class TestUndertaker(unittest.TestCase):
 @pytest.mark.parametrize("caches_mock", [{"caches_to_mock": [
     'rucio.core.config.REGION',
 ]}], indirect=True)
-def test_removal_all_replicas2(rse_factory, root_account, mock_scope, core_config_mock, caches_mock):
+@pytest.mark.parametrize("file_config_mock", [
+    # Run test twice: with, and without, temp tables
+    {"overrides": [('core', 'use_temp_tables', 'True')]},
+    {"overrides": [('core', 'use_temp_tables', 'False')]},
+], indirect=True)
+def test_removal_all_replicas2(rse_factory, root_account, mock_scope, core_config_mock, caches_mock, file_config_mock):
     """ UNDERTAKER (CORE): Test the undertaker is setting Epoch tombstone on all the replicas. """
     rse1, rse1_id = rse_factory.make_posix_rse()
     rse2, rse2_id = rse_factory.make_posix_rse()
-    dst_rse_name, dst_rse_id = rse_factory.make_posix_rse()
 
     set_local_account_limit(root_account, rse1_id, -1)
     set_local_account_limit(root_account, rse2_id, -1)
