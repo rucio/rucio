6d65643af1eaa39d92158de047c502ffd0207df2
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2022-07-22 11:25:41 +0200

    Tools: Pyright static type checking; Fix #5727
    This commit introduces the Pyright type checker to the CI pipeline. The
    CI check works in a similar way to the current "flake8-annotations"
    check, in that it compares the number of typing errors before and after
    applying an incoming pull request. The check fails if the number of
    errors has increased. The added value of this check is to check for
    typing errors, instead of just counting missing annotations as in the
    "flake8-annotations" check. These two checks are orthogonal to each
    other and serve different purposes. Pyright to a certain degree does
    type inference, but produces better results in presence of type
    annotations. The annotations themselves serve little value if there is
    no type checker to use them except for increased readability. Therefore,
    the "flake8-annotations" check will continue to be active.

---
+++ tools/run_pyright/__main__.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright European Organization for Nuclear Research (CERN) since 2012
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+from argparse import ArgumentParser
+
+from . import generate
+from . import compare
+
+
+def parse_arguments():
+    parser = ArgumentParser('tools/run_pyright.sh', description="Generate and compare Pyright typing reports.")
+    subparser = parser.add_subparsers(dest='command')
+    subparser.required = True
+
+    parse_generate = subparser.add_parser('generate')
+    generate.setup_parser(parse_generate)
+
+    parse_compare = subparser.add_parser('compare')
+    compare.setup_parser(parse_compare)
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+    exit_code = args.func(args)
+    sys.exit(exit_code)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tools/run_pyright/compare.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+# Copyright European Organization for Nuclear Research (CERN) since 2012
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from argparse import ArgumentParser, Namespace
+from collections import Counter
+from pathlib import Path
+from typing import List, Tuple
+
+from .models import Report, ReportDiagnostic, ReportDiagnosticWithoutRange, Severity
+from .utils import group_by, load_json
+
+
+def setup_parser(parser: ArgumentParser) -> None:
+    parser.description = """
+    Compares two Pyright reports and outputs newly introduced warnings and errors.
+
+    If new errors are introduced (or warnings with --Werror) the exit code is 2,
+    otherwise 0.
+    """
+    parser.add_argument('old', type=Path, help='First report of comparison.')
+    parser.add_argument('new', type=Path, help='Second report of comparison.')
+    parser.add_argument('--Werror', action='store_true', help='Treat warnings as errors.')
+    parser.set_defaults(func=compare)
+
+
+def compare(args: Namespace) -> int:
+    """Compares two reports to find new warnings and errors."""
+    old_report = Report.from_dict(load_json(args.old))
+    new_report = Report.from_dict(load_json(args.new))
+    new_diagnostics = _compare_reports(new_report, old_report)
+
+    print_regressions(new_diagnostics, new_report)
+
+    num_errors = sum(count for err, count in new_diagnostics if err.severity == Severity.ERROR)
+    num_warnings = sum(count for err, count in new_diagnostics if err.severity == Severity.WARNING)
+
+    print('Summary:')
+    print(f'    {num_errors} new errors.')
+    print(f'    {num_warnings} new warnings.')
+
+    if args.Werror:
+        num_errors += num_warnings
+
+    return 2 if num_errors else 0
+
+
+def _compare_reports(new_report: Report, old_report: Report):
+    """Counts instances of each diagnostic and returns those which have increased in the latest report."""
+    old_count = Counter(map(ReportDiagnostic.without_range, old_report.diagnostics))
+    new_count = Counter(map(ReportDiagnostic.without_range, new_report.diagnostics))
+
+    diff = new_count
+    diff.subtract(old_count)
+
+    new_diagnostics = [(err, count) for err, count in diff.most_common() if count > 0]
+    return new_diagnostics
+
+
+def _indent(text: str, prefix: str) -> str:
+    """Prepends `prefix` to each line in `text` except the first."""
+    return text.replace('\n', '\n' + prefix)
+
+
+def print_regressions(collection: List[Tuple[ReportDiagnosticWithoutRange, int]], report: Report) -> None:
+    """Takes the output of `_compare_reports` and prints it in a human-readable way."""
+    all_problems = group_by(report.diagnostics, key=lambda elem: elem.without_range())
+    diagnostics_by_file = group_by(collection, key=lambda elem: elem[0].file)
+
+    for file, diagnostics in diagnostics_by_file.items():
+        num_diagnostics = sum(count for _, count in diagnostics)
+        print(f'Found {num_diagnostics} new problems in {file}')
+        for diagnostic, count in diagnostics:
+            candidate_line_list: List[str] = []
+            for candidate in all_problems.get(diagnostic, []):
+                if candidate.range_start_line == candidate.range_end_line:
+                    candidate_line_list.append(f'{candidate.range_start_line+1}')
+                else:
+                    candidate_line_list.append(f'{candidate.range_start_line+1}-{candidate.range_end_line+1}')
+
+            prefix = f'  - {count} {diagnostic.severity.value}s with message'
+            message = _indent(diagnostic.message, ' ' * (len(prefix) + 4))
+            candidate_lines = ', '.join(candidate_line_list)
+
+            print(f'{prefix} """{message}""".')
+            print(f'    Candidates: line {candidate_lines}')
