9a84c1e3056081ba583c737c038424bc506a506c
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2022-07-22 11:25:41 +0200

    Tools: Pyright static type checking; Fix #5727
    This commit introduces the Pyright type checker to the CI pipeline. The
    CI check works in a similar way to the current "flake8-annotations"
    check, in that it compares the number of typing errors before and after
    applying an incoming pull request. The check fails if the number of
    errors has increased. The added value of this check is to check for
    typing errors, instead of just counting missing annotations as in the
    "flake8-annotations" check. These two checks are orthogonal to each
    other and serve different purposes. Pyright to a certain degree does
    type inference, but produces better results in presence of type
    annotations. The annotations themselves serve little value if there is
    no type checker to use them except for increased readability. Therefore,
    the "flake8-annotations" check will continue to be active.

---
+++ tools/run_pyright/generate.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+# Copyright European Organization for Nuclear Research (CERN) since 2012
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
+import json
+import subprocess
+import sys
+
+from .models import ReportDict, Report
+from .utils import save_json
+
+
+PATHS = (
+    'lib/',
+)
+
+
+def setup_parser(parser: ArgumentParser) -> None:
+    parser.description = """
+    Invokes Pyright to generate a report of current typing errors and warnings.
+    """
+    parser.add_argument('out', type=Path, help='Store the Pyright report at this path.')
+    parser.set_defaults(func=generate)
+
+
+def generate(args: Namespace) -> int:
+    """Generate a Pyright report and save it at the specified path."""
+    reportdict = _run_pyright()
+
+    save_json(args.out, reportdict)
+
+    report = Report.from_dict(reportdict)
+
+    print('Summary:')
+    print(f'    {report.summary.num_files} files checked.')
+    print(f'    {report.summary.num_errors} errors.')
+    print(f'    {report.summary.num_warnings} warnings.')
+    print(f'    {report.summary.num_information} notes.')
+    print(f'    Duration: {report.summary.time_seconds:.1f} seconds.')
+
+    return 0
+
+
+def _run_pyright() -> ReportDict:
+    """Runs the pyright type-checker and returns its output as json."""
+    cmdline = ['pyright', '--outputjson', *PATHS]
+    try:
+        process = subprocess.run(cmdline, stdout=subprocess.PIPE)
+        return json.loads(process.stdout)
+    except FileNotFoundError as ex:
+        print('Error running pyright.'
+              ' This could be due to pyright not being installed on your system,'
+              ' in which case it may be installed using `npm install --global pyright`.\n'
+              'Additional details:', ex,
+              file=sys.stderr)
+        sys.exit(1)
---
+++ tools/run_pyright/models.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+# Copyright European Organization for Nuclear Research (CERN) since 2012
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from enum import Enum
+from typing import Any, Dict, List
+from dataclasses import dataclass
+
+
+ReportDict = Dict[str, Any]
+
+
+class Severity(Enum):
+    INFORMATION = 'information'
+    WARNING = 'warning'
+    ERROR = 'error'
+
+
+@dataclass(frozen=True)
+class ReportDiagnosticWithoutRange:
+    severity: Severity
+    file: str
+    rule: str
+    message: str
+
+
+@dataclass(frozen=True)
+class ReportDiagnostic:
+    severity: Severity
+    file: str
+    rule: str
+    message: str
+    range_start_line: int
+    range_start_char: int
+    range_end_line: int
+    range_end_char: int
+
+    @classmethod
+    def from_dict(cls, obj: Dict[str, Any]):
+        return cls(
+            severity=Severity(obj['severity']),
+            file=obj['file'],
+            rule=obj['rule'],
+            message=obj['message'],
+            range_start_line=obj['range']['start']['line'],
+            range_start_char=obj['range']['start']['character'],
+            range_end_line=obj['range']['end']['line'],
+            range_end_char=obj['range']['end']['character'],
+        )  # type: ignore
+
+    def without_range(self) -> ReportDiagnosticWithoutRange:
+        return ReportDiagnosticWithoutRange(
+            self.severity, self.file, self.rule, self.message
+        )  # type: ignore
+
+
+@dataclass
+class ReportSummary:
+    num_files: int
+    num_errors: int
+    num_warnings: int
+    num_information: int
+    time_seconds: float
+
+    @classmethod
+    def from_dict(cls, obj: Dict[str, Any]):
+        return cls(
+            num_files=obj['filesAnalyzed'],
+            num_errors=obj['errorCount'],
+            num_warnings=obj['warningCount'],
+            num_information=obj['informationCount'],
+            time_seconds=obj['timeInSec']
+        )
+
+
+@dataclass
+class Report:
+    summary: ReportSummary
+    diagnostics: List[ReportDiagnostic]
+
+    @classmethod
+    def from_dict(cls, obj: ReportDict):
+        return cls(
+            summary=ReportSummary.from_dict(obj['summary']),
+            diagnostics=list(map(ReportDiagnostic.from_dict, obj['generalDiagnostics'])),
+        )
