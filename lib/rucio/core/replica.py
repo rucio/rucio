7404122821edd0d7b88e663e3a35addd72cbb67c
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2022-08-08 14:46:07 +0200

    Core: Fix usage of unused parameter `younger_than` in `core/replica.py` #5847
    Signed-off-by: Joel Dierkes <joel.dierkes@cern.ch>

--- lib/rucio/core/replica.py
+++ lib/rucio/core/replica.py
@@ -3974,7 +3974,7 @@ def get_suspicious_files(rse_expression, available_elsewhere, filter_=None, logg
     if not isinstance(nattempts, int):
         nattempts = 0
     if not isinstance(younger_than, datetime):
-        younger_than = datetime.now() - timedelta(days=10)
+        younger_than = datetime.now() - timedelta(days=younger_than if younger_than is not None else 10)
 
     # assembling exclude_states_clause
     exclude_states_clause = []
