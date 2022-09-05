4a0891ab894c3d5c3ab610492744bb4c67c69aa3
Radu Carpa <radu.carpa@cern.ch>
2022-09-05 10:49:30 +0200

    Tests: use rse_factory in judge tests
    Judges tests leave leftover requests in the databse.
    Because of that, other tests fail. In particular,
    the submitter tests which rely on the default submitter
    "bulk" of 100.
    Until now things "worked" because test_requests.py was
    blast-removing all requests from the dabase. Effectively
    "cleaning" up behind judge tests. But that behavior was
    removed from test_requests.py in a previous commit.

--- lib/rucio/tests/test_judge_cleaner.py
+++ lib/rucio/tests/test_judge_cleaner.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import unittest
-
 import pytest
 
 from rucio.common.config import config_get_bool
@@ -23,7 +21,7 @@ from rucio.common.utils import generate_uuid as uuid
 from rucio.core.account_limit import set_local_account_limit
 from rucio.core.did import add_did, attach_dids
 from rucio.core.lock import get_replica_locks
-from rucio.core.rse import add_rse_attribute, get_rse_id
+from rucio.core.rse import add_rse_attribute
 from rucio.core.rule import add_rule, update_rule
 from rucio.daemons.judge.cleaner import rule_cleaner
 from rucio.db.sqla.constants import DIDType
@@ -31,8 +29,14 @@ from rucio.tests.common_server import get_vo
 from rucio.tests.test_rule import create_files, tag_generator
 
 
+@pytest.fixture(scope="class")
+def setup_class(request, rse_factory_unittest):
+    request.cls.setUpClass()
+
+
 @pytest.mark.noparallel(reason='uses pre-defined RSE, sets account limits, adds global rse attributes')
-class TestJudgeCleaner(unittest.TestCase):
+@pytest.mark.usefixtures("setup_class")
+class TestJudgeCleaner:
 
     @classmethod
     def setUpClass(cls):
@@ -42,15 +46,10 @@ class TestJudgeCleaner(unittest.TestCase):
             cls.vo = {}
 
         # Add test RSE
-        cls.rse1 = 'MOCK'
-        cls.rse3 = 'MOCK3'
-        cls.rse4 = 'MOCK4'
-        cls.rse5 = 'MOCK5'
-
-        cls.rse1_id = get_rse_id(rse=cls.rse1, **cls.vo)
-        cls.rse3_id = get_rse_id(rse=cls.rse3, **cls.vo)
-        cls.rse4_id = get_rse_id(rse=cls.rse4, **cls.vo)
-        cls.rse5_id = get_rse_id(rse=cls.rse5, **cls.vo)
+        cls.rse1, cls.rse1_id = cls.rse_factory.make_mock_rse()
+        cls.rse3, cls.rse3_id = cls.rse_factory.make_mock_rse()
+        cls.rse4, cls.rse4_id = cls.rse_factory.make_mock_rse()
+        cls.rse5, cls.rse5_id = cls.rse_factory.make_mock_rse()
 
         # Add Tags
         cls.T1 = tag_generator()
