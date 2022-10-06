6d341c0e8c1d13ebf616e56961eb47ac3b265caf
Radu Carpa <radu.carpa@cern.ch>
2022-10-06 12:59:57 +0200

    Tests: migrate test_qos to pytest

--- lib/rucio/tests/test_qos.py
+++ lib/rucio/tests/test_qos.py
@@ -13,78 +13,59 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import unittest
-
-from rucio.client.rseclient import RSEClient
-from rucio.common.config import config_get_bool
 from rucio.core.rse import update_rse, get_rse
-from rucio.tests.common import rse_name_generator
-from rucio.tests.common_server import get_vo
 
 
-class TestQoS(unittest.TestCase):
+class TestQoS:
 
-    @classmethod
-    def setUpClass(cls):
-        if config_get_bool('common', 'multi_vo', raise_exception=False, default=False):
-            cls.vo = {'vo': get_vo()}
-        else:
-            cls.vo = {}
-
-        cls.rse_client = RSEClient()
-        cls.tmp_rse_name = rse_name_generator()
-        cls.rse_client.add_rse(cls.tmp_rse_name, vo=cls.vo)
-        cls.tmp_rse = cls.rse_client.get_rse(cls.tmp_rse_name)['id']
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.rse_client.delete_rse(cls.tmp_rse_name)
-
-    def test_update_and_remove_rse_qos_class(self):
+    def test_update_and_remove_rse_qos_class(self, rse_factory):
         """ QoS (CORE): Update and remove QoS class for RSE """
+        rse_name, rse_id = rse_factory.make_mock_rse()
 
-        update_rse(self.tmp_rse, {'qos_class': 'fast_and_expensive'})
-        rse = get_rse(self.tmp_rse)
+        update_rse(rse_id, {'qos_class': 'fast_and_expensive'})
+        rse = get_rse(rse_id)
         assert rse['qos_class'] == 'fast_and_expensive'
 
-        update_rse(self.tmp_rse, {'qos_class': 'slow_but_cheap'})
-        rse = get_rse(self.tmp_rse)
+        update_rse(rse_id, {'qos_class': 'slow_but_cheap'})
+        rse = get_rse(rse_id)
         assert rse['qos_class'] == 'slow_but_cheap'
 
-        update_rse(self.tmp_rse, {'qos_class': None})
-        rse = get_rse(self.tmp_rse)
+        update_rse(rse_id, {'qos_class': None})
+        rse = get_rse(rse_id)
         assert rse['qos_class'] is None
 
-    def test_update_and_remove_rse_qos_class_client(self):
+    def test_update_and_remove_rse_qos_class_client(self, rse_client, rse_factory):
         """ QoS (CLIENT): Update and remove QoS class for RSE """
+        rse_name, rse_id = rse_factory.make_mock_rse()
 
-        self.rse_client.update_rse(self.tmp_rse_name, {'qos_class': 'fast_and_expensive'})
-        rse = self.rse_client.get_rse(self.tmp_rse_name)
+        rse_client.update_rse(rse_name, {'qos_class': 'fast_and_expensive'})
+        rse = rse_client.get_rse(rse_name)
         assert rse['qos_class'] == 'fast_and_expensive'
 
-        self.rse_client.update_rse(self.tmp_rse_name, {'qos_class': 'slow_but_cheap'})
-        rse = self.rse_client.get_rse(self.tmp_rse_name)
+        rse_client.update_rse(rse_name, {'qos_class': 'slow_but_cheap'})
+        rse = rse_client.get_rse(rse_name)
         assert rse['qos_class'] == 'slow_but_cheap'
 
-        self.rse_client.update_rse(self.tmp_rse_name, {'qos_class': None})
-        rse = self.rse_client.get_rse(self.tmp_rse_name)
+        rse_client.update_rse(rse_name, {'qos_class': None})
+        rse = rse_client.get_rse(rse_name)
         assert rse['qos_class'] is None
 
-    def test_qos_policies(self):
+    def test_qos_policies(self, rse_client, rse_factory):
         """ QoS (CLIENT): Add QoS policy for RSE """
+        rse_name, rse_id = rse_factory.make_mock_rse()
 
-        self.rse_client.add_qos_policy(self.tmp_rse_name, 'FOO')
-        policies = self.rse_client.list_qos_policies(self.tmp_rse_name)
+        rse_client.add_qos_policy(rse_name, 'FOO')
+        policies = rse_client.list_qos_policies(rse_name)
         assert policies == ['FOO']
 
-        self.rse_client.add_qos_policy(self.tmp_rse_name, 'BAR')
-        policies = sorted(self.rse_client.list_qos_policies(self.tmp_rse_name))
+        rse_client.add_qos_policy(rse_name, 'BAR')
+        policies = sorted(rse_client.list_qos_policies(rse_name))
         assert policies == ['BAR', 'FOO']
 
-        self.rse_client.delete_qos_policy(self.tmp_rse_name, 'BAR')
-        policies = self.rse_client.list_qos_policies(self.tmp_rse_name)
+        rse_client.delete_qos_policy(rse_name, 'BAR')
+        policies = rse_client.list_qos_policies(rse_name)
         assert policies == ['FOO']
 
-        self.rse_client.delete_qos_policy(self.tmp_rse_name, 'FOO')
-        policies = self.rse_client.list_qos_policies(self.tmp_rse_name)
+        rse_client.delete_qos_policy(rse_name, 'FOO')
+        policies = rse_client.list_qos_policies(rse_name)
         assert policies == []
