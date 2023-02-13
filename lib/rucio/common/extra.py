e852b1697ed2e59fb0355127c290241051ac1694
Martin Barisits <martin.barisits@cern.ch>
2023-02-13 16:36:43 +0100

    Revert "Common/Client: Server SSL Expiry Error Message; Fix #3866"
    This reverts commit 469e8c2a656b62f7c1c1de52d33cba211fe218c2.

--- lib/rucio/common/extra.py
+++ lib/rucio/common/extra.py
@@ -32,9 +32,6 @@ def import_extras(module_list):
                 warnings.filterwarnings('ignore', 'Blowfish has been deprecated', module='paramiko')
                 # TODO: deprecated python 2 and 3.6 too ...
                 warnings.filterwarnings('ignore', 'Python .* is no longer supported', module='paramiko')
-                # TODO: fix cryptography module import
-                warnings.filterwarnings('ignore', 'Cryptography module throws DeprecationWarning for 3.6', module='cryptography')
-                # import
                 out[mod] = importlib.import_module(mod)
         except ImportError:
             pass
