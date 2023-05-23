a044964c65a82c47d0452bf544addfbb23f852ce
Radu Carpa <radu.carpa@cern.ch>
2023-05-23 10:15:43 +0200

    Tests: avoid code duplication in test_request.py

--- lib/rucio/tests/test_request.py
+++ lib/rucio/tests/test_request.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 
 from datetime import datetime
+from typing import Type, Union
 
 import pytest
 
@@ -122,56 +123,51 @@ def test_queue_requests_state(vo, file_config_mock, rse_factory, mock_scope, roo
     assert request['state'] == target_state
 
 
-class TestRequestCoreList:
+@pytest.mark.parametrize(
+    "model,list_fnc", [
+        (models.Request, list_requests),
+        (models.RequestHistory, list_requests_history),
+    ]
+)
+def test_core_list(model: Union[Type[models.Request], Type[models.RequestHistory]], list_fnc, rse_factory, db_session):
+    """ REQUEST (CORE): Test listing requests and request history via the core"""
+    _, source_rse_id = rse_factory.make_mock_rse(session=db_session)
+    _, source_rse_id2 = rse_factory.make_mock_rse(session=db_session)
+    _, dest_rse_id = rse_factory.make_mock_rse(session=db_session)
+    _, dest_rse_id2 = rse_factory.make_mock_rse(session=db_session)
+    model(state=constants.RequestState.WAITING, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id).save(session=db_session)
+    model(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id2, dest_rse_id=dest_rse_id).save(session=db_session)
+    model(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id2).save(session=db_session)
+    model(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id).save(session=db_session)
+    model(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id).save(session=db_session)
 
-    def test_list_requests(self, rse_factory, db_session):
-        """ REQUEST (CORE): list requests """
-        _, source_rse_id = rse_factory.make_mock_rse(session=db_session)
-        _, source_rse_id2 = rse_factory.make_mock_rse(session=db_session)
-        _, dest_rse_id = rse_factory.make_mock_rse(session=db_session)
-        _, dest_rse_id2 = rse_factory.make_mock_rse(session=db_session)
-        models.Request(state=constants.RequestState.WAITING, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id).save(session=db_session)
-        models.Request(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id2, dest_rse_id=dest_rse_id).save(session=db_session)
-        models.Request(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id2).save(session=db_session)
-        models.Request(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id).save(session=db_session)
-        models.Request(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id).save(session=db_session)
+    requests = [request for request in list_fnc([source_rse_id], [dest_rse_id], [constants.RequestState.SUBMITTED], session=db_session)]
+    assert len(requests) == 2
 
-        requests = [request for request in list_requests([source_rse_id], [dest_rse_id], [constants.RequestState.SUBMITTED], session=db_session)]
-        assert len(requests) == 2
+    requests = [request for request in list_fnc([source_rse_id, source_rse_id2], [dest_rse_id], [constants.RequestState.SUBMITTED], session=db_session)]
+    assert len(requests) == 3
 
-        requests = [request for request in list_requests([source_rse_id, source_rse_id2], [dest_rse_id], [constants.RequestState.SUBMITTED], session=db_session)]
-        assert len(requests) == 3
-
-        requests = [request for request in list_requests([source_rse_id], [dest_rse_id], [constants.RequestState.QUEUED], session=db_session)]
-        assert len(requests) == 0
-
-
-class TestRequestHistoryCoreList:
-
-    def test_list_requests_history(self, rse_factory, db_session):
-        _, source_rse_id = rse_factory.make_mock_rse(session=db_session)
-        _, source_rse_id2 = rse_factory.make_mock_rse(session=db_session)
-        _, dest_rse_id = rse_factory.make_mock_rse(session=db_session)
-        _, dest_rse_id2 = rse_factory.make_mock_rse(session=db_session)
-        """ REQUEST (CORE): list requests history"""
-        models.RequestHistory(state=constants.RequestState.WAITING, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id).save(session=db_session)
-        models.RequestHistory(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id2, dest_rse_id=dest_rse_id).save(session=db_session)
-        models.RequestHistory(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id2).save(session=db_session)
-        models.RequestHistory(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id).save(session=db_session)
-        models.RequestHistory(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id).save(session=db_session)
-
-        requests = [request for request in list_requests_history([source_rse_id], [dest_rse_id], [constants.RequestState.SUBMITTED], session=db_session)]
-        assert len(requests) == 2
-
-        requests = [request for request in list_requests_history([source_rse_id, source_rse_id2], [dest_rse_id], [constants.RequestState.SUBMITTED], session=db_session)]
-        assert len(requests) == 3
-
-        requests = [request for request in list_requests_history([source_rse_id], [dest_rse_id], [constants.RequestState.QUEUED], session=db_session)]
-        assert len(requests) == 0
+    requests = [request for request in list_fnc([source_rse_id], [dest_rse_id], [constants.RequestState.QUEUED], session=db_session)]
+    assert len(requests) == 0
 
 
-def test_list_requests(vo, rest_client, auth_token, rse_factory, tag_factory, db_session):
-    """ REQUEST (REST): list requests """
+@pytest.mark.parametrize(
+    "model,api_endpoint", [
+        (models.Request, '/requests/list'),
+        (models.RequestHistory, '/requests/history/list'),
+    ]
+)
+def test_api_list(
+        vo,
+        model: Union[Type[models.Request], Type[models.RequestHistory]],
+        api_endpoint: str,
+        rest_client,
+        auth_token,
+        rse_factory,
+        tag_factory,
+        db_session
+):
+    """ REQUEST (REST): Test listing requests and request history via the api"""
     source_rse, source_rse_id = rse_factory.make_mock_rse(session=db_session)
     source_rse2, source_rse_id2 = rse_factory.make_mock_rse(session=db_session)
     source_rse3, source_rse_id3 = rse_factory.make_mock_rse(session=db_session)
@@ -190,17 +186,17 @@ def test_list_requests(vo, rest_client, auth_token, rse_factory, tag_factory, db
     name1 = generate_uuid()
     name2 = generate_uuid()
     name3 = generate_uuid()
-    models.Request(state=constants.RequestState.WAITING, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id, name=name3).save(session=db_session)
-    models.Request(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id2, dest_rse_id=dest_rse_id, name=name1).save(session=db_session)
-    models.Request(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id2, name=name1).save(session=db_session)
-    models.Request(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id, name=name1).save(session=db_session)
-    models.Request(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id, name=name2).save(session=db_session)
-    models.Request(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id3, dest_rse_id=dest_rse_id, name=name2).save(session=db_session)
+    model(state=constants.RequestState.WAITING, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id, name=name3).save(session=db_session)
+    model(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id2, dest_rse_id=dest_rse_id, name=name1).save(session=db_session)
+    model(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id2, name=name1).save(session=db_session)
+    model(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id, name=name1).save(session=db_session)
+    model(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id, name=name2).save(session=db_session)
+    model(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id3, dest_rse_id=dest_rse_id, name=name2).save(session=db_session)
     db_session.commit()
 
     def check_correct_api(params, expected_requests):
         headers_dict = {'X-Rucio-Type': 'user', 'X-Rucio-Account': 'root'}
-        response = rest_client.get('/requests/list', query_string=params, headers=headers(auth(auth_token), vohdr(vo), hdrdict(headers_dict)))
+        response = rest_client.get(api_endpoint, query_string=params, headers=headers(auth(auth_token), vohdr(vo), hdrdict(headers_dict)))
         assert response.status_code == 200
         requests = set()
         for request in response.get_data(as_text=True).split('\n')[:-1]:
@@ -210,129 +206,7 @@ def test_list_requests(vo, rest_client, auth_token, rse_factory, tag_factory, db
 
     def check_error_api(params, exception_class, exception_message, code):
         headers_dict = {'X-Rucio-Type': 'user', 'X-Rucio-Account': 'root'}
-        response = rest_client.get('/requests/list', query_string=params, headers=headers(auth(auth_token), vohdr(vo), hdrdict(headers_dict)))
-        assert response.status_code == code
-        body = parse_response(response.get_data(as_text=True))
-        assert body['ExceptionClass'] == exception_class
-        assert body['ExceptionMessage'] == exception_message
-
-    params = {'src_rse': source_rse, 'dst_rse': dest_rse, 'request_states': 'S'}
-    expected_requests = set()
-    expected_requests.add(('SUBMITTED', source_rse_id, dest_rse_id, name1))
-    expected_requests.add(('SUBMITTED', source_rse_id, dest_rse_id, name2))
-    check_correct_api(params, expected_requests)
-
-    params = {'src_rse': source_rse, 'dst_rse': dest_rse, 'request_states': 'Q'}
-    expected_requests = set([])
-    check_correct_api(params, expected_requests)
-
-    params = {'src_rse': source_rse2, 'dst_rse': dest_rse, 'request_states': 'S'}
-    expected_requests = set()
-    expected_requests.add(('SUBMITTED', source_rse_id2, dest_rse_id, name1))
-    check_correct_api(params, expected_requests)
-
-    params = {'src_rse': source_rse, 'dst_rse': dest_rse2, 'request_states': 'S'}
-    expected_requests = set()
-    expected_requests.add(('SUBMITTED', source_rse_id, dest_rse_id2, name1))
-    check_correct_api(params, expected_requests)
-
-    params = {'src_site': source_site, 'dst_site': dst_site, 'request_states': 'S'}
-    expected_requests = set()
-    expected_requests.add(('SUBMITTED', source_rse_id, dest_rse_id, name1))
-    expected_requests.add(('SUBMITTED', source_rse_id, dest_rse_id, name2))
-    # check correct resolution of site attribute to multiple RSE
-    expected_requests.add(('SUBMITTED', source_rse_id3, dest_rse_id, name2))
-    check_correct_api(params, expected_requests)
-
-    params = {'src_site': source_site, 'dst_site': dst_site, 'request_states': 'S,W,Q'}
-    expected_requests = set()
-    expected_requests.add(('SUBMITTED', source_rse_id, dest_rse_id, name1))
-    expected_requests.add(('SUBMITTED', source_rse_id, dest_rse_id, name2))
-    expected_requests.add(('WAITING', source_rse_id, dest_rse_id, name3))
-    expected_requests.add(('SUBMITTED', source_rse_id3, dest_rse_id, name2))
-    check_correct_api(params, expected_requests)
-
-    params = {'src_site': source_site2, 'dst_site': dst_site, 'request_states': 'S'}
-    expected_requests = set()
-    expected_requests.add(('SUBMITTED', source_rse_id2, dest_rse_id, name1))
-    check_correct_api(params, expected_requests)
-
-    params = {'src_site': source_site, 'dst_site': dst_site2, 'request_states': 'S'}
-    expected_requests = set()
-    expected_requests.add(('SUBMITTED', source_rse_id, dest_rse_id2, name1))
-    check_correct_api(params, expected_requests)
-
-    params = {'src_site': source_site, 'dst_site': dst_site2, 'request_states': 'S,W,Q'}
-    expected_requests = set()
-    expected_requests.add(('SUBMITTED', source_rse_id, dest_rse_id2, name1))
-    check_correct_api(params, expected_requests)
-
-    params = {}
-    check_error_api(params, 'MissingParameter', 'Request state is missing', 400)
-
-    params = {'request_states': 'unkown', 'dst_rse': dest_rse, 'src_rse': source_rse}
-    check_error_api(params, 'Invalid', 'Request state value is invalid', 400)
-
-    params = {'request_states': 'S', 'src_rse': source_rse}
-    check_error_api(params, 'MissingParameter', 'Destination RSE is missing', 400)
-
-    params = {'request_states': 'S', 'dst_rse': source_rse}
-    check_error_api(params, 'MissingParameter', 'Source RSE is missing', 400)
-
-    params = {'request_states': 'S', 'src_rse': source_rse, 'dst_site': dst_site}
-    check_error_api(params, 'MissingParameter', 'Destination RSE is missing', 400)
-
-    params = {'request_states': 'S', 'src_site': source_site}
-    check_error_api(params, 'MissingParameter', 'Destination site is missing', 400)
-
-    params = {'request_states': 'S', 'dst_site': dst_site}
-    check_error_api(params, 'MissingParameter', 'Source site is missing', 400)
-
-    params = {'request_states': 'S', 'src_site': source_site, 'dst_site': 'unknown'}
-    check_error_api(params, 'NotFound', 'Could not resolve site name unknown to RSE', 404)
-
-
-def test_list_requests_history(vo, rest_client, auth_token, rse_factory, tag_factory, db_session):
-    """ REQUEST (REST): list requests history """
-    source_rse, source_rse_id = rse_factory.make_mock_rse(session=db_session)
-    source_rse2, source_rse_id2 = rse_factory.make_mock_rse(session=db_session)
-    source_rse3, source_rse_id3 = rse_factory.make_mock_rse(session=db_session)
-    dest_rse, dest_rse_id = rse_factory.make_mock_rse(session=db_session)
-    dest_rse2, dest_rse_id2 = rse_factory.make_mock_rse(session=db_session)
-    source_site = tag_factory.new_tag()
-    source_site2 = tag_factory.new_tag()
-    dst_site = tag_factory.new_tag()
-    dst_site2 = tag_factory.new_tag()
-    add_rse_attribute(source_rse_id, 'site', source_site, session=db_session)
-    add_rse_attribute(source_rse_id2, 'site', source_site2, session=db_session)
-    add_rse_attribute(source_rse_id3, 'site', source_site, session=db_session)
-    add_rse_attribute(dest_rse_id, 'site', dst_site, session=db_session)
-    add_rse_attribute(dest_rse_id2, 'site', dst_site2, session=db_session)
-
-    name1 = generate_uuid()
-    name2 = generate_uuid()
-    name3 = generate_uuid()
-    models.RequestHistory(state=constants.RequestState.WAITING, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id, name=name3).save(session=db_session)
-    models.RequestHistory(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id2, dest_rse_id=dest_rse_id, name=name1).save(session=db_session)
-    models.RequestHistory(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id2, name=name1).save(session=db_session)
-    models.RequestHistory(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id, name=name1).save(session=db_session)
-    models.RequestHistory(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id, dest_rse_id=dest_rse_id, name=name2).save(session=db_session)
-    models.RequestHistory(state=constants.RequestState.SUBMITTED, source_rse_id=source_rse_id3, dest_rse_id=dest_rse_id, name=name2).save(session=db_session)
-    db_session.commit()
-
-    def check_correct_api(params, expected_requests):
-        headers_dict = {'X-Rucio-Type': 'user', 'X-Rucio-Account': 'root'}
-        response = rest_client.get('/requests/history/list', query_string=params, headers=headers(auth(auth_token), vohdr(vo), hdrdict(headers_dict)))
-        assert response.status_code == 200
-        requests = set()
-        for request in response.get_data(as_text=True).split('\n')[:-1]:
-            request = parse_response(request)
-            requests.add((request['state'], request['source_rse_id'], request['dest_rse_id'], request['name']))
-        assert requests == expected_requests
-
-    def check_error_api(params, exception_class, exception_message, code):
-        headers_dict = {'X-Rucio-Type': 'user', 'X-Rucio-Account': 'root'}
-        response = rest_client.get('/requests/history/list', query_string=params, headers=headers(auth(auth_token), vohdr(vo), hdrdict(headers_dict)))
+        response = rest_client.get(api_endpoint, query_string=params, headers=headers(auth(auth_token), vohdr(vo), hdrdict(headers_dict)))
         assert response.status_code == code
         body = parse_response(response.get_data(as_text=True))
         assert body['ExceptionClass'] == exception_class
