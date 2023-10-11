c0fb4e27140b1b163cbbd16aff5e4b2a41af4a79
Radu Carpa <radu.carpa@cern.ch>
2023-10-11 14:01:16 +0200

    Tests: use sqlalchemy to wait for database readiness
    It's much more generic this way. This will probably allow to
    completely get rid of the before_script.sh later.

--- tools/test/before_script.sh
+++ tools/test/before_script.sh
@@ -29,22 +29,7 @@ RESTART_HTTPD=0
 
 if [ $RDBMS == "oracle" ]; then
     docker $CONTAINER_RUNTIME_ARGS cp tools/test/oracle_setup.sh ${CON_DB}:/
-    date
-    ORACLE_STARTUP_STRING="DATABASE IS READY TO USE"
-    for i in {1..60}; do
-        sleep 2
-        cont=$(bash -c 'docker '"$CONTAINER_RUNTIME_ARGS"' logs '"$CON_DB"' | grep "'"$ORACLE_STARTUP_STRING"'" | wc -l')
-        [ "$cont" -eq "1" ] && break
-    done
-    date
-    if [ "$cont" -ne "1" ]; then
-        echo "Oracle did not start up in time."
-        docker $CONTAINER_RUNTIME_ARGS logs $CON_DB || true
-        exit 1
-    fi
-    sleep 3
     docker $CONTAINER_RUNTIME_ARGS exec $CON_DB /oracle_setup.sh
-    sleep 3
     docker $CONTAINER_RUNTIME_ARGS exec $CON_RUCIO python3 tools/merge_rucio_configs.py \
         -s /usr/local/src/rucio/etc/docker/test/extra/rucio_autotests_common.cfg  \
            /usr/local/src/rucio/etc/docker/test/extra/rucio_oracle.cfg  \
@@ -54,17 +39,6 @@ if [ $RDBMS == "oracle" ]; then
     RESTART_HTTPD=1
 
 elif [ $RDBMS == "mysql8" ]; then
-    date
-    for i in {1..30}; do
-        sleep 4
-        cont=$(bash -c 'ping=`docker '"$CONTAINER_RUNTIME_ARGS"' exec '"$CON_DB"' mysqladmin --user=rucio --password=rucio ping`; echo $ping 1>&2; echo $ping | grep "mysqld is alive" 1>&2; echo $?')
-        [ "$cont" -eq "0" ] && break
-    done
-    date
-    if [ "$cont" -ne "0" ]; then
-        echo Could not connect to MySQL in time.
-        exit 1
-    fi
     docker $CONTAINER_RUNTIME_ARGS exec $CON_RUCIO python3 tools/merge_rucio_configs.py \
         -s /usr/local/src/rucio/etc/docker/test/extra/rucio_autotests_common.cfg  \
            /usr/local/src/rucio/etc/docker/test/extra/rucio_mysql8.cfg  \
@@ -74,17 +48,6 @@ elif [ $RDBMS == "mysql8" ]; then
     RESTART_HTTPD=1
 
 elif [ $RDBMS == "postgres14" ]; then
-    date
-    for i in {1..30}; do
-        sleep 1
-        cont=$(bash -c 'docker '"$CONTAINER_RUNTIME_ARGS"' exec '"$CON_DB"' pg_isready 1>&2; echo $?')
-        [ "$cont" -eq "0" ] && break
-    done
-    date
-    if [ "$cont" -ne "0" ]; then
-        echo Could not connect to Postgres in time.
-        exit 1
-    fi
     if [ $SUITE == "multi_vo" ]; then
         docker $CONTAINER_RUNTIME_ARGS exec $CON_RUCIO mkdir -p /opt/rucio/etc/multi_vo/tst/etc
         docker $CONTAINER_RUNTIME_ARGS exec $CON_RUCIO mkdir -p /opt/rucio/etc/multi_vo/ts2/etc
@@ -115,6 +78,13 @@ elif [ $RDBMS == "sqlite" ]; then
         -d /opt/rucio/etc/rucio.cfg
     docker $CONTAINER_RUNTIME_ARGS exec $CON_RUCIO cp /usr/local/src/rucio/etc/docker/test/extra/alembic_sqlite.ini /opt/rucio/etc/alembic.ini
     RESTART_HTTPD=1
+fi
+
+echo 'Waiting for database to be ready'
+if ! python3 -c "from rucio.db.sqla.session import wait_for_database; wait_for_database()"
+then
+    echo 'Cannot access database'
+    exit 1
 fi
 
 if [ "$RESTART_HTTPD" == "1" ]; then
