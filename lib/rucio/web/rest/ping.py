caa3198faaa691e61a90b7c08f5798084de743e9
Radu Carpa <radu.carpa@cern.ch>
2023-11-29 15:12:32 +0100

    API: allow exposing /ping on a separate rest endpoint. Closes #6394
    This is to allow health checks in kubernetes to be done on a different
    port. Otherwise things don't play nicely with the proxy protocol

---
+++ lib/rucio/web/rest/ping.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright European Organization for Nuclear Research (CERN) since 2012
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from flask import Flask
+
+from rucio.common.logging import setup_logging
+from rucio.web.rest.flaskapi.v1.ping import blueprint as metrics_blueprint
+
+# Allow to run the /ping endpoint as a separate application on a separate PORT
+
+setup_logging()
+application = Flask(__name__)
+application.register_blueprint(metrics_blueprint(standalone=True))
+
+if __name__ == '__main__':
+    application.run()
