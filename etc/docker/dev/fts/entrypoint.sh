542c4bccffff2ae159f71507e8ea152b89bd6728
Radu Carpa <radu.carpa@cern.ch>
2024-01-15 13:53:39 +0100

    Tests: bring back full stager test
    fts_qos is a drop-in substitute to fts_bringonline.

--- etc/docker/dev/fts/entrypoint.sh
+++ etc/docker/dev/fts/entrypoint.sh
@@ -37,5 +37,6 @@ update-ca-trust
 
 # startup the FTS services
 /usr/sbin/fts_server               # main FTS server daemonizes
+/usr/sbin/fts_qos                  # for the stager tests
 /usr/sbin/fts_msg_bulk             # daemon to send messages to activemq
 /usr/sbin/httpd -DFOREGROUND       # FTS REST frontend & FTSMON
