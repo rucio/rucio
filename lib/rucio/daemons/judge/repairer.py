986282a23ec14eecf4ba3edd9b316988a2d2ac01
rdimaio <riccardodimaio11@gmail.com>
2024-03-13 21:46:18 +0100

    Rules: return rule IDs instead of single-element rule tuples

--- lib/rucio/daemons/judge/repairer.py
+++ lib/rucio/daemons/judge/repairer.py
@@ -92,7 +92,6 @@ def run_once(paused_rules, delta, heartbeat_handler, **_kwargs):
 
     for rule_id in rules:
         _, _, logger = heartbeat_handler.live()
-        rule_id = rule_id[0]
         logger(logging.INFO, 'Repairing rule %s' % (rule_id))
         if graceful_stop.is_set():
             break
