2e7c0354c63483e0c1c41c3b65803c8836503fcb
rdimaio <riccardodimaio11@gmail.com>
2024-03-13 13:43:45 +0100

    Typing: Add type annotations to exporter.py; #6454

--- lib/rucio/core/exporter.py
+++ lib/rucio/core/exporter.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 from rucio.core import rse as rse_module, distance as distance_module
 from rucio.db.sqla.session import transactional_session
@@ -23,7 +23,7 @@ if TYPE_CHECKING:
 
 
 @transactional_session
-def export_rses(vo='def', *, session: "Session"):
+def export_rses(vo: str = 'def', *, session: "Session") -> dict[str, dict[str, Any]]:
     """
     Export RSE data.
 
@@ -39,7 +39,7 @@ def export_rses(vo='def', *, session: "Session"):
 
 
 @transactional_session
-def export_data(vo='def', distance=True, *, session: "Session"):
+def export_data(vo: str = 'def', distance: bool = True, *, session: "Session") -> dict[str, Any]:
     """
     Export data.
 
