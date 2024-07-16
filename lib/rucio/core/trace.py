91b3c6e085a1297feef97f008bc3af409608450b
rdimaio <riccardodimaio11@gmail.com>
2024-07-16 13:21:31 +0200

    Core & Internals: Pass cause to ValidationError constructor to fix error

--- lib/rucio/core/trace.py
+++ lib/rucio/core/trace.py
@@ -349,8 +349,9 @@ def validate_schema(obj):
         if obj and 'eventType' in obj:
             event_type = SCHEMAS.get(obj['eventType'].lower())
             if not event_type:
-                validation_error = ValidationError(message=f"Trace schema for eventType {obj['eventType']} is not currently supported.")
-                validation_error.cause = "SCHEMA_NOT_FOUND"
+                validation_error = ValidationError(
+                    message=f"Trace schema for eventType {obj['eventType']} is not currently supported.",
+                    cause="SCHEMA_NOT_FOUND")
                 raise validation_error
             validate(obj, SCHEMAS.get(obj['eventType'].lower()), format_checker=FORMAT_CHECKER)
     except ValidationError as error:
