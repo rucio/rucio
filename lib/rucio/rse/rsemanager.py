67a7f1bb55b4acdb33143ee5c1356fbb87bb7c04
rdimaio <riccardodimaio11@gmail.com>
2024-07-19 15:09:15 +0200

    RSE: Change checks in RSEManager from type check to instance check

--- lib/rucio/rse/rsemanager.py
+++ lib/rucio/rse/rsemanager.py
@@ -276,7 +276,8 @@ def exists(rse_settings: types.RSESettingsDict, files, domain='wan', scheme=None
     except:
         pass
 
-    files = [files] if not type(files) is list else files
+    if not isinstance(files, list):
+        files = [files]
     for f in files:
         exists = None
         if isinstance(f, STRING_TYPES):
@@ -343,7 +344,9 @@ def upload(rse_settings: types.RSESettingsDict, lfns, domain='wan', source_dir=N
     protocol.connect()
     protocol_delete = create_protocol(rse_settings, 'delete', domain=domain, auth_token=auth_token, logger=logger, impl=impl)
     protocol_delete.connect()
-    lfns = [lfns] if not type(lfns) is list else lfns
+
+    if not isinstance(lfns, list):
+        lfns = [lfns]
     for lfn in lfns:
         base_name = lfn.get('filename', lfn['name'])
         name = lfn.get('name', base_name)
@@ -527,7 +530,8 @@ def delete(rse_settings: types.RSESettingsDict, lfns, domain='wan', auth_token=N
     protocol = create_protocol(rse_settings, 'delete', domain=domain, auth_token=auth_token, logger=logger, impl=impl)
     protocol.connect()
 
-    lfns = [lfns] if not type(lfns) is list else lfns
+    if not isinstance(lfns, list):
+        lfns = [lfns]
     for lfn in lfns:
         pfn = list(protocol.lfns2pfns(lfn).values())[0]
         try:
@@ -578,7 +582,8 @@ def rename(rse_settings: types.RSESettingsDict, files, domain='wan', auth_token=
     protocol = create_protocol(rse_settings, 'write', domain=domain, auth_token=auth_token, logger=logger, impl=impl)
     protocol.connect()
 
-    files = [files] if not type(files) is list else files
+    if not isinstance(files, list):
+        files = [files]
     for f in files:
         pfn = None
         new_pfn = None
