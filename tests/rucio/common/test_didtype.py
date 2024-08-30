79038705038f6d80b00402a0655def35d491fc5e
rdimaio <riccardodimaio11@gmail.com>
2024-08-30 15:23:25 +0200

    Testing: DIDType, parametrize and refactor existing test; #7008

--- tests/rucio/common/test_didtype.py
+++ tests/rucio/common/test_didtype.py
@@ -12,79 +12,64 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import pytest
+
 from rucio.common.didtype import DID
 from rucio.common.exception import DIDError
 
 
 class TestDIDType:
-    def _test_did(self, did, cmp_str, diff_half_did=DID(scope='diff'), diff_full_did=DID('sdiff:ndiff'), is_valid=True):
-        stat = 0
-        did_as_str = str(did)
-        if did == diff_half_did:
-            stat = 1
-        if did == diff_full_did:
-            stat += 2
-        if did != cmp_str:
-            stat += 4
-        if did_as_str != cmp_str:
-            stat += 8
-        if did_as_str != did:
-            stat += 16
-        if did.is_valid_format() != is_valid:
-            stat += 32
+    @pytest.mark.parametrize(
+        'input_did,expected_scope,expected_name',
+        [
+            (DID(), '', ''),
+            (DID('test.scope:test.name'), 'test.scope', 'test.name'),
+            (DID('user.implicit.scope.in.name'), 'user.implicit', 'user.implicit.scope.in.name'),
+            (DID('test.scope', 'test.name'), 'test.scope', 'test.name'),
+            (DID(['test.scope', 'test.name']), 'test.scope', 'test.name'),
+            (DID(('test.scope', 'test.name')), 'test.scope', 'test.name'),
+            (DID({'scope': 'test.scope', 'name': 'test.name'}), 'test.scope', 'test.name'),
+            (DID(scope='test.scope', name='test.name'), 'test.scope', 'test.name'),
+            (DID(scope='test.scope'), 'test.scope', ''),
+            (DID(name='test.name'), '', 'test.name'),
+            (DID(name='user.kw.implicit.scope'), 'user.kw', 'user.kw.implicit.scope'),
+            (DID(did=['test.scope', 'test.name']), 'test.scope', 'test.name'),
+            (DID(did=('test.scope', 'test.name')), 'test.scope', 'test.name'),
+            (DID(did={'scope': 'test.scope', 'name': 'test.name'}), 'test.scope', 'test.name'),
+            (DID('test.scope', name='test.name'), 'test.scope', 'test.name'),
+            (DID('test.name', scope='test.scope'), 'test.scope', 'test.name'),
+        ],
+        ids=[
+            'empty_did',
+            'string',
+            'string_implicit_scope_in_name',
+            'args',
+            'list',
+            'tuple',
+            'dict',
+            'kwarg_scope_name',
+            'single_kwarg_scope',
+            'single_kwarg_name',
+            'kwarg_implicit_scope_in_name',
+            'kwarg_list',
+            'kwarg_tuple',
+            'kwarg_dict',
+            'arg_scope_kwarg_name',
+            'arg_name_kwarg_scope'
+        ])
+    def test_did_type_success(self, input_did, expected_scope, expected_name):
+        assert input_did.scope == expected_scope
+        assert input_did.name == expected_name
 
-        if stat != 0:
-            self.success = False
-        return (stat, cmp_str)
-
-    def test_did_type(self):
-        ''' Test DIDType '''
-        self.success = True
-        try:
-            self._test_did(DID(), '')
-            self._test_did(DID('scope:name.did.str'), 'scope:name.did.str')
-            self._test_did(DID('user.implicit.scope.in.name'), 'user.implicit:user.implicit.scope.in.name')
-            self._test_did(DID('custom.scope', 'custom.name'), 'custom.scope:custom.name')
-            self._test_did(DID(['list.scope', 'list.name']), 'list.scope:list.name')
-            self._test_did(DID(('tuple.scope', 'tuple.name')), 'tuple.scope:tuple.name')
-            self._test_did(DID({'scope': 'dict.scope', 'name': 'dict.name'}), 'dict.scope:dict.name')
-            self._test_did(DID(scope='kw.scope'), 'kw.scope')
-            self._test_did(DID(name='kw.name'), 'kw.name')
-            self._test_did(DID(name='user.kw.implicit.scope'), 'user.kw:user.kw.implicit.scope')
-            self._test_did(DID(scope='kw.scope', name='kw.name'), 'kw.scope:kw.name')
-            self._test_did(DID(did={'scope': 'kw.did.scope', 'name': 'kw.did.name'}), 'kw.did.scope:kw.did.name')
-            self._test_did(DID(did=['kw.list.scope', 'kw.list.name']), 'kw.list.scope:kw.list.name')
-            self._test_did(DID(did=('kw.tuple.scope', 'kw.tuple.name')), 'kw.tuple.scope:kw.tuple.name')
-            self._test_did(DID('arg.scope', name='kwarg.name'), 'arg.scope:kwarg.name')
-            self._test_did(DID('arg.name', scope='kwarg.scope'), 'kwarg.scope:arg.name')
-            x = DID('scope.copy:name.test')
-            y = DID(x)
-            if x != y:
-                print('Copy failed: {} != {}'.format(x, y))
-                self.success = False
-        except DIDError as err:
-            print('Exception: {}'.format(err))
-            self.success = False
-
-        try:
+    def test_non_implicit_single_string(self):
+        with pytest.raises(DIDError, match='Error using DID type\nDetails: Object construction from non-splitable string is ambigious'):
             DID('non.implicit.single.string')
-            print('Exception for invalid DID did not work!')
-            self.success = False
-        except DIDError:
-            pass
 
-        try:
+    def test_copy(self):
+        x = DID('test.scope:test.name')
+        y = DID(x)
+        assert x == y
+
+    def test_invalid_format_during_construction(self):
+        with pytest.raises(DIDError, match='Error using DID type\nDetails: Object has invalid format after construction: invalid:user.implicit:user:invalid'):
             DID('invalid', 'user.implicit:user:invalid')
-            print('Exception for invalid DID did not work!')
-            self.success = False
-        except DIDError:
-            pass
-
-        try:
-            DID('user.implicit:user:invalid')
-            print('Exception for invalid DID did not work!')
-            self.success = False
-        except DIDError:
-            pass
-
-        assert self.success
