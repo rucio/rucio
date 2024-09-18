82bf11d4a030895e119e21f56048d9e4b7e6ea5f
Erling Mathias Staff <erlingstaff@gmail.com>
2024-09-18 14:53:45 +0200

    Testing: Add type annotations to db/sqla/sautils.py; #7021

--- lib/rucio/db/sqla/sautils.py
+++ lib/rucio/db/sqla/sautils.py
@@ -17,18 +17,26 @@ SQLAlchemy utilities
 
 '''
 
+from typing import TYPE_CHECKING, Any, Union
+
 from sqlalchemy.ext.compiler import compiles
 from sqlalchemy.sql.expression import ClauseElement, Executable
 
+if TYPE_CHECKING:
+    from sqlalchemy import Table
+    from sqlalchemy.engine.interfaces import SQLCompiler
+    from sqlalchemy.sql.schema import Column
+    from sqlalchemy.sql.selectable import Select
+
 
 class InsertFromSelect(Executable, ClauseElement):
-    def __init__(self, insert_spec, select):
+    def __init__(self, insert_spec: "Union[Table, list[Column]]", select: "Select") -> None:
         self.insert_spec = insert_spec
         self.select = select
 
 
 @compiles(InsertFromSelect)
-def visit_insert_from_select(element, compiler, **kw):
+def visit_insert_from_select(element: InsertFromSelect, compiler: "SQLCompiler", **kw: Any) -> str:
     if isinstance(element.insert_spec, list):
         columns = []
         for column in element.insert_spec:
