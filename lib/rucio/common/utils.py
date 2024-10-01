ae34c9a094aa6913e87ed193f6419634b0ae67a4
rdimaio <riccardodimaio11@gmail.com>
2024-10-01 16:16:42 -0700

    Common: fix Paramiko module check (assign None instead of False)

--- lib/rucio/common/utils.py
+++ lib/rucio/common/utils.py
@@ -54,7 +54,7 @@ if EXTRA_MODULES['paramiko']:
     try:
         from paramiko import RSAKey
     except Exception:
-        EXTRA_MODULES['paramiko'] = False
+        EXTRA_MODULES['paramiko'] = None
 
 if TYPE_CHECKING:
     from collections.abc import Callable, Iterable, Iterator, Mapping, Sequence
