fff39f10c27148d7894c3685774b784775b0dde5
maany <imptodefeat@gmail.com>
2024-12-02 14:41:23 +0100

    tests: use `docker compose` over `docker-compose` in vo tests #6818

--- tools/test/run_tests.py
+++ tools/test/run_tests.py
@@ -280,7 +280,7 @@ def run_with_httpd(
     logs_dir: pathlib.Path,
     tests: list[str],
 ) -> bool:
-    compose_version = int(run('docker compose', 'version', '--short', return_stdout=True).decode().split('.')[0])
+    compose_version = int(run('docker', 'compose', 'version', '--short', return_stdout=True).decode().split('.')[0])
 
     with (NamedTemporaryFile() as compose_override_file):
         compose_override_content = yaml.dump({
@@ -308,7 +308,7 @@ def run_with_httpd(
         rucio_container = None
         try:
             # Start docker compose
-            run('docker compose', '-p', project, *up_down_args, 'up', '-d')
+            run('docker', 'compose', '-p', project, *up_down_args, 'up', '-d')
 
             # Retrieve container names from docker compose
             # or use pre-defined names for old, v1, docker compose
@@ -316,7 +316,7 @@ def run_with_httpd(
             if compose_version > 1:
                 rucio_container = next(filter(
                     lambda c: c['Service'] == 'rucio',
-                    json.loads(run('docker compose', '-p', project, 'ps', '--format', 'json', return_stdout=True))
+                    json.loads(run('docker', 'compose', '-p', project, 'ps', '--format', 'json', return_stdout=True))
                 ), {}).get('Name', rucio_container)
 
             # Running test.sh
@@ -356,7 +356,7 @@ def run_with_httpd(
                             file=sys.stderr,
                             flush=True,
                         )
-            run('docker compose', '-p', project, *up_down_args, 'down', '-t', '30', check=False)
+            run('docker', 'compose', '-p', project, *up_down_args, 'down', '-t', '30', check=False)
         return False
 
 
