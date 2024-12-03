d667d29d5b36271157b99c3efa51dbce05dedb36
rdimaio <riccardodimaio11@gmail.com>
2024-12-03 13:46:03 +0100

    Dependencies: Move packaging install_requires dependency to clients table

--- setuputil.py
+++ setuputil.py
@@ -26,6 +26,7 @@ clients_requirements_table = {
         'requests',
         'urllib3',
         'dogpile-cache',
+        'packaging',
         'tabulate',
         'jsonschema',
         'dataclasses',
@@ -70,7 +71,6 @@ server_requirements_table = {
         'boto3',
         'sqlalchemy',
         'alembic',
-        'packaging'
         'pymemcache',
         'python-dateutil',
         'stomp-py',
