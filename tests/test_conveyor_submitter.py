rucio/rucio

commit a814398e6d9b2c0b4d2a18490b4fbedb7142881e

Transfers: Skip submitting requests from expired rules #6505
When the Cleaner handles an expired rule, it deletes requests in state
QUEUED (and states prior to it) and it tries to cancel (to FTS) requests
in state SUBMITTED.  The Submitter, however, tries to submit QUEUED
requests.  This can create two problems: (1) contention between the two
daemons and (2) inefficiency (submitting a request only to cancel it
shortly after).

This commit adjusts the underlying database query to skip requests whose
directly-associated rule has expired.

There are two somewhat special cases that won’t be fully handled:
(1) requests without any rule associated to them (e.g. multi-hop
transfers, replica recovery) and (2) request which are reused (if there
are two or more rules that result in a transfer of the same file to the
same destination, then the first request to be created by one rule is
reused by the others).

lib/rucio/core/request.py
@@ -31,6 +31,7 @@ from sqlalchemy import and_, delete, exists, insert, or_, select, update
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import aliased
 from sqlalchemy.sql.expression import asc, false, func, null, true
+from sqlalchemy.sql.functions import coalesce
 
 from rucio.common.config import config_get_bool, config_get_int
 from rucio.common.exception import InvalidRSEExpression, RequestNotFound, RucioException, UnsupportedOperation
@@ -451,12 +452,12 @@ def list_and_mark_transfer_requests_and_source_replicas(
 
     if partition_hash_var is None:
         partition_hash_var = 'requests.id'
-
     if request_state is None:
         request_state = RequestState.QUEUED
-
     if request_type is None:
         request_type = [RequestType.TRANSFER]
+
+    now = datetime.datetime.utcnow()
 
     sub_requests = select(
         models.Request.id,
@@ -483,6 +484,11 @@ def list_and_mark_transfer_requests_and_source_replicas(
     ).where(
         models.Request.state == request_state,
         models.Request.request_type.in_(request_type)
+    ).outerjoin(
+        models.ReplicationRule,
+        models.Request.rule_id == models.ReplicationRule.id
+    ).where(
+        coalesce(models.ReplicationRule.expires_at, now) >= now
     ).join(
         models.RSE,
         models.RSE.id == models.Request.dest_rse_id

tests/test_conveyor_submitter.py
@@ -94,6 +94,27 @@ def test_request_submitted_in_order(rse_factory, did_factory, root_account):
 
 
 @pytest.mark.noparallel(groups=[NoParallelGroups.SUBMITTER])
+def test_skip_requests_from_expired_rules(rse_factory, did_factory, root_account):
+    src_rse_name, src_rse_id = rse_factory.make_posix_rse()
+    dst_rse_name, dst_rse_id = rse_factory.make_posix_rse()
+    distance_core.add_distance(src_rse_id, dst_rse_id, distance=10)
+
+    did = did_factory.upload_test_file(rse_name=src_rse_name)
+    rule = rule_core.add_rule(dids=[did], account=root_account, copies=1,
+                              rse_expression=dst_rse_name, grouping='ALL',
+                              weight=None, lifetime=-1, locked=False,
+                              subscription_id=None)[0]
+    request = request_core.get_request_by_did(rse_id=dst_rse_id, **did)
+    assert request_core.get_request(request_id=request['id'])['state'] == RequestState.QUEUED
+
+    submitter(once=True,
+              rses=[{'id': rse_id} for rse_id in (src_rse_id, dst_rse_id)],
+              partition_wait_time=None, transfertools=['mock'],
+              transfertype='single', filter_transfertool=None)
+    assert request_core.get_request(request_id=request['id'])['state'] == RequestState.QUEUED
+
+
+@pytest.mark.noparallel(groups=[NoParallelGroups.SUBMITTER])
 @pytest.mark.parametrize("core_config_mock", [
     # Run test twice: with, and without, temp tables
     {

commit d2029396109b0bd4a8c1226bb769fcd82c2c7e5f

Transfers: Do not ignore requests from expired rules with a child #7379
Commit a814398e6 introduced a bug in the selection of requests for
submission. The motivation was to avoid the Submitter processing
requests associated with replication rules that the Cleaner is working
on, to the best of our ability.

When moving a replication rule, a new rule is created and the original
rule is updated: its expiration is set to the current time and its child
is set to the new rule. The Cleaner works on rules that are expired, but
ignores the ones with a child rule.

As such, the Submitter should not ignore requests from expired
replication rules that have a child, mainly because there is no risk of
contention with the Cleaner. Such a logic may have still been desirable,
were it not for the bug: if the child ends up reusing requests from its
parent, then such requests become silently stuck, with no obvious way to
identify them, nor an easy procedure to cure them.

The use of COALESCE() was a cool trick to simplify the query, at the
cost of readability. This time, the entire WHERE clause is made out of
simple predicates and logical operators. Note that in SQL there is no
guaranteed short-circuit evaluation. Consequently, the clause must be
more verbose than one would expect.

lib/rucio/core/request.py
@@ -29,7 +29,6 @@ from sqlalchemy import and_, delete, exists, insert, or_, select, update
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import aliased
 from sqlalchemy.sql.expression import asc, false, func, null, true
-from sqlalchemy.sql.functions import coalesce
 
 from rucio.common.config import config_get_bool, config_get_int
 from rucio.common.constants import RseAttr
@@ -494,8 +493,6 @@ def list_and_mark_transfer_requests_and_source_replicas(
     if request_type is None:
         request_type = [RequestType.TRANSFER]
 
-    now = datetime.datetime.utcnow()
-
     sub_requests = select(
         models.Request.id,
         models.Request.request_type,
@@ -527,7 +524,11 @@ def list_and_mark_transfer_requests_and_source_replicas(
         models.ReplicationRule,
         models.Request.rule_id == models.ReplicationRule.id
     ).where(
-        coalesce(models.ReplicationRule.expires_at, now) >= now
+        or_(models.ReplicationRule.child_rule_id != null(),
+            and_(models.ReplicationRule.child_rule_id == null(),
+                 models.ReplicationRule.expires_at == null()),
+            and_(models.ReplicationRule.child_rule_id == null(),
+                 models.ReplicationRule.expires_at > datetime.datetime.utcnow()))
     ).join(
         models.RSE,
         models.RSE.id == models.Request.dest_rse_id

tests/test_conveyor_submitter.py
@@ -108,14 +108,14 @@ def test_skip_requests_from_expired_rules(rse_factory, did_factory, root_account
     distance_core.add_distance(src_rse_id, dst_rse_id, distance=10)
 
     did = did_factory.upload_test_file(rse_name=src_rse_name)
-    rule_core.add_rule(dids=[did],
-                       account=root_account,
-                       copies=1,
-                       rse_expression=dst_rse_name, grouping='ALL',
-                       weight=None,
-                       lifetime=-1,
-                       locked=False,
-                       subscription_id=None)[0]
+    rule = rule_core.add_rule(dids=[did],
+                              account=root_account,
+                              copies=1,
+                              rse_expression=dst_rse_name, grouping='ALL',
+                              weight=None,
+                              lifetime=-1,
+                              locked=False,
+                              subscription_id=None)[0]
     request = request_core.get_request_by_did(rse_id=dst_rse_id, **did)
     assert request_core.get_request(request_id=request['id'])['state'] == RequestState.QUEUED
 
@@ -124,6 +124,13 @@ def test_skip_requests_from_expired_rules(rse_factory, did_factory, root_account
               partition_wait_time=None, transfertools=['mock'],
               transfertype='single', filter_transfertool=None)
     assert request_core.get_request(request_id=request['id'])['state'] == RequestState.QUEUED
+
+    rule_core.move_rule(rule_id=rule, rse_expression=f'{dst_rse_name}&*')
+    submitter(once=True,
+              rses=[{'id': rse_id} for rse_id in (src_rse_id, dst_rse_id)],
+              partition_wait_time=None, transfertools=['mock'],
+              transfertype='single', filter_transfertool=None)
+    assert request_core.get_request(request_id=request['id'])['state'] == RequestState.SUBMITTED
 
 
 @pytest.mark.noparallel(groups=[NoParallelGroups.SUBMITTER])