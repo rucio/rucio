b3e3edc0f9f8397da478cafceaae9ae2b2314ca3
Karanjot Singh <karanjot.singh@cern.ch>
2025-03-27 14:38:10 +0100

    release: Fix TypeError in min_value by correctly filtering None values
    Signed-off-by: Karanjot Singh <karanjot.singh@cern.ch>

--- lib/rucio/common/dumper/consistency.py
+++ lib/rucio/common/dumper/consistency.py
@@ -171,7 +171,7 @@ def min_value(*values):
     values_without_none = cast('list[str]', [value for value in values if value is not None])
     if len(values_without_none) == 0:
         raise ValueError("Input contains 0 non-null values.")
-    return min(values)
+    return min(values_without_none)
 
 
 def split_if_not_none(value, sep=',', fields=2):
