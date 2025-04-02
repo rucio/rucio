4b7559ab8f81ebd0263ad0c003ee98eb9c534e8d
rdimaio <riccardodimaio11@gmail.com>
2025-04-02 13:21:52 +0200

    Database: gateway/importer, migrate to session context manager; #7409

--- lib/rucio/gateway/importer.py
+++ lib/rucio/gateway/importer.py
@@ -12,35 +12,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import TYPE_CHECKING, Any
+from typing import Any
 
 from rucio.common import exception
 from rucio.common.schema import validate_schema
 from rucio.common.types import InternalAccount
 from rucio.core import importer
-from rucio.db.sqla.session import transactional_session
+from rucio.db.sqla.constants import DatabaseOperationType
+from rucio.db.sqla.session import db_session
 from rucio.gateway import permission
 
-if TYPE_CHECKING:
-    from sqlalchemy.orm import Session
 
-
-@transactional_session
-def import_data(data: dict[str, Any], issuer: str, vo: str = 'def', *, session: "Session") -> None:
+def import_data(data: dict[str, Any], issuer: str, vo: str = 'def') -> None:
     """
     Import data to add/update/delete records in Rucio.
 
     :param data: data to be imported.
     :param issuer: the issuer.
     :param vo: the VO of the issuer.
-    :param session: The database session in use.
     """
     kwargs = {'issuer': issuer}
     validate_schema(name='import', obj=data, vo=vo)
-    auth_result = permission.has_permission(issuer=issuer, vo=vo, action='import', kwargs=kwargs, session=session)
-    if not auth_result.allowed:
-        raise exception.AccessDenied('Account %s can not import data. %s' % (issuer, auth_result.message))
 
-    for account in data.get('accounts', []):
-        account['account'] = InternalAccount(account['account'], vo=vo)
-    return importer.import_data(data, vo=vo, session=session)
+    with db_session(DatabaseOperationType.WRITE) as session:
+        auth_result = permission.has_permission(issuer=issuer, vo=vo, action='import', kwargs=kwargs, session=session)
+        if not auth_result.allowed:
+            raise exception.AccessDenied('Account %s can not import data. %s' % (issuer, auth_result.message))
+
+        for account in data.get('accounts', []):
+            account['account'] = InternalAccount(account['account'], vo=vo)
+        return importer.import_data(data, vo=vo, session=session)
