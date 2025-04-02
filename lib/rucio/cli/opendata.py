af453f8a1a3a714d7406ae8655182994548283e6
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2025-04-02 12:53:17 +0200

    opendata

--- lib/rucio/cli/opendata.py
+++ lib/rucio/cli/opendata.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import click
 import json
+
+import click
 
 from rucio.common.exception import RucioException
 
