e59fd8bc978a4d940f3eba8016b42cea9eef1ff3
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2025-04-04 10:27:44 +0200

    fix imports

--- lib/rucio/cli/opendata.py
+++ lib/rucio/cli/opendata.py
@@ -13,12 +13,11 @@
 # limitations under the License.
 
 import json
+from typing import TYPE_CHECKING
 
 import click
 
 from rucio.common.exception import RucioException
-
-from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from click import Context
