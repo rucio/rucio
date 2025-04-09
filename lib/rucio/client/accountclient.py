e87696b22eed9cde12a59a5f6a311145a1c675ee
voetberg <magpie127@gmail.com>
2025-04-09 15:05:26 -0500

    REST & API: Remove AccountLimitClient #7482

--- lib/rucio/client/accountclient.py
+++ lib/rucio/client/accountclient.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 from json import dumps
-from typing import TYPE_CHECKING, Any, Optional
+from typing import TYPE_CHECKING, Any, Literal, Optional
 from urllib.parse import quote_plus
 
 from requests.status_codes import codes
@@ -347,7 +347,7 @@ class AccountClient(BaseClient):
             exc_cls, exc_msg = self._get_exception(headers=res.headers, status_code=res.status_code, data=res.content)
             raise exc_cls(exc_msg)
 
-    def get_account_limits(self, account: str, rse_expression: str, locality: str) -> dict[str, Any]:
+    def get_account_limits(self, account: str, rse_expression: str, locality: Literal['local', 'global']) -> dict[str, Any]:
         """
         Return the correct account limits for the given locality.
 
@@ -375,16 +375,26 @@ class AccountClient(BaseClient):
             account: str,
             rse: str,
             bytes_: int,
-            locality: str
+            locality: Literal['local', 'global']
     ) -> bool:
         """
         Sets an account limit for a given limit scope.
 
-        :param account: The name of the account.
-        :param rse:     The rse name.
-        :param bytes_:   An integer with the limit in bytes.
-        :param locality: The scope of the account limit. 'local' or 'global'.
-        :return:        True if quota was created successfully else False.
+        Parameters
+        ----------
+        account :
+            The name of the account.
+        rse :
+            The rse name.
+        bytes_ :
+            The limit in bytes.
+        locality :
+            The scope of the account limit.
+
+        Returns
+        -------
+
+            True if quota was created successfully, False otherwise.
         """
 
         if locality == 'local':
@@ -399,15 +409,24 @@ class AccountClient(BaseClient):
             self,
             account: str,
             rse: str,
-            locality: str
+            locality: Literal['local', 'global']
     ) -> bool:
         """
         Deletes an account limit for a given limit scope.
 
-        :param account: The name of the account.
-        :param rse:     The rse name.
-        :param locality: The scope of the account limit. 'local' or 'global'.
-        :return:        True if quota was created successfully else False.
+        Parameters
+        ----------
+        account :
+            The name of the account.
+        rse :
+            The rse name.
+        locality :
+            The scope of the account limit.
+
+        Returns
+        -------
+
+            True if quota was deleted successfully, False otherwise.
         """
 
         if locality == 'local':
@@ -504,10 +523,19 @@ class AccountClient(BaseClient):
         """
         Sends the request to set an account limit for an account.
 
-        :param account: The name of the account.
-        :param rse:     The rse name.
-        :param bytes_:   An integer with the limit in bytes.
-        :return:        True if quota was created successfully else False.
+        Parameters
+        ----------
+        account :
+            The name of the account.
+        rse :
+            The rse name.
+        bytes_ :
+            The limit in bytes.
+
+        Returns
+        -------
+
+            True if quota was created successfully, False otherwise.
         """
         data = dumps({'bytes': bytes_})
         path = '/'.join([self.ACCOUNTS_BASEURL, account, "limits", 'local', rse])
@@ -529,11 +557,22 @@ class AccountClient(BaseClient):
         """
         Sends the request to remove an account limit.
 
-        :param account: The name of the account.
-        :param rse_expression: The rse expression.
+        Parameters
+        ----------
+        account :
+            The name of the account.
+        rse :
+            The rse name.
 
-        :return: True if quota was removed successfully. False otherwise.
-        :raises AccountNotFound: if account doesn't exist.
+        Returns
+        -------
+
+            True if quota was removed successfully, False otherwise.
+
+        Raises
+        ------
+        AccountNotFound
+            If account doesn't exist.
         """
 
         path = '/'.join([self.ACCOUNTS_BASEURL, account, "limits", "local", rse])
@@ -556,10 +595,19 @@ class AccountClient(BaseClient):
         """
         Sends the request to set a global account limit for an account.
 
-        :param account:        The name of the account.
-        :param rse_expression: The rse expression.
-        :param bytes_:          An integer with the limit in bytes.
-        :return:               True if quota was created successfully else False.
+        Parameters
+        ----------
+        account :
+            The name of the account.
+        rse_expression :
+            The rse expression.
+        bytes_ :
+            The limit in bytes.
+
+        Returns
+        -------
+
+            True if quota was created successfully, False otherwise.
         """
 
         data = dumps({'bytes': bytes_})
@@ -582,11 +630,22 @@ class AccountClient(BaseClient):
         """
         Sends the request to remove a global account limit.
 
-        :param account:        The name of the account.
-        :param rse_expression: The rse expression.
+        Parameters
+        ----------
+        account :
+            The name of the account.
+        rse_expression :
+            The rse expression.
 
-        :return: True if quota was removed successfully. False otherwise.
-        :raises AccountNotFound: if account doesn't exist.
+        Returns
+        -------
+
+            True if quota was removed successfully, False otherwise.
+
+        Raises
+        ------
+        AccountNotFound
+            If account doesn't exist.
         """
 
         path = '/'.join([self.ACCOUNTS_BASEURL, account, "limits", 'global', quote_plus(rse_expression)])
