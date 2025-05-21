22ff4ac0cb8e7f124e56b3280501c2c5052105ae
haozturk <h.ozturk@cern.ch>
2025-05-21 16:57:39 +0200

    Client: Include the transfer limit functions in the python client

--- lib/rucio/client/requestclient.py
+++ lib/rucio/client/requestclient.py
@@ -12,13 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import TYPE_CHECKING, Any, Optional
+from json import dumps
+from typing import TYPE_CHECKING, Any, Literal, Optional
 from urllib.parse import quote_plus
 
 from requests.status_codes import codes
 
 from rucio.client.baseclient import BaseClient, choice
 from rucio.common.utils import build_url
+from rucio.db.sqla.constants import TransferLimitDirection
 
 if TYPE_CHECKING:
     from collections.abc import Iterator, Sequence
@@ -149,3 +151,82 @@ class RequestClient(BaseClient):
         else:
             exc_cls, exc_msg = self._get_exception(headers=r.headers, status_code=r.status_code, data=r.content)
             raise exc_cls(exc_msg)
+
+    def list_transfer_limits(
+            self
+    ) -> 'Iterator[dict[str, Any]]':
+        """Returns all the transfer limits
+
+        :returns: transfer limits
+        """
+        path = '/'.join([self.REQUEST_BASEURL, 'transfer_limits'])
+        url = build_url(choice(self.list_hosts), path=path)
+        r = self._send_request(url, type_='GET')
+
+        if r.status_code == codes.ok:
+            return self._load_json_data(r)
+        else:
+            exc_cls, exc_msg = self._get_exception(headers=r.headers, status_code=r.status_code, data=r.content)
+            raise exc_cls(exc_msg)
+
+    def set_transfer_limit(
+            self,
+            rse_expression: str,
+            activity: Optional[str] = None,
+            direction: TransferLimitDirection = TransferLimitDirection.DESTINATION,
+            max_transfers: Optional[int] = None,
+            volume: Optional[int] = None,
+            deadline: Optional[int] = None,
+            strategy: Optional[str] = None,
+            transfers: Optional[int] = None,
+            waitings: Optional[int] = None,
+    ) -> Literal[True]:
+        """Set the transfer limit for a given RSE
+
+        :param rse_expression: RSE expression string.
+        :param activity: The activity.
+        :param direction: The direction in which this limit applies (source/destination)
+        :param max_transfers: Maximum transfers.
+        :param volume: Maximum transfer volume in bytes.
+        :param deadline: Maximum waiting time in hours until a datasets gets released.
+        :param strategy: defines how to handle datasets: `fifo` (each file released separately) or `grouped_fifo` (wait for the entire dataset to fit)
+        :param transfers: Current number of active transfers
+        :param waitings: Current number of waiting transfers
+        
+        :returns: True if the transfer limit was deleted
+        """
+        path = '/'.join([self.REQUEST_BASEURL, 'transfer_limits'])
+        url = build_url(choice(self.list_hosts), path=path)
+        data = dumps({'rse_expression': rse_expression, 'activity': activity, 'direction': direction.value,
+                'max_transfers': max_transfers, 'volume': volume, 'deadline': deadline, 'strategy': strategy,
+                'transfers': transfers, 'waitings': waitings})
+        r = self._send_request(url, type_='PUT', data=data)
+
+        if r.status_code == codes.created:
+            return True
+        exc_cls, exc_msg = self._get_exception(headers=r.headers, status_code=r.status_code, data=r.content)
+        raise exc_cls(exc_msg)
+
+    def delete_transfer_limit(
+            self,
+            rse_expression: str,
+            activity: Optional[str] = None,
+            direction: TransferLimitDirection = TransferLimitDirection.DESTINATION
+    ) -> Literal[True]:
+        """Delete the transfer limit for a given RSE
+
+        :param rse_expression: RSE expression string.
+        :param activity: The activity.
+        :param direction: The direction in which this limit applies (source/destination)
+
+        :returns: True if the transfer limit was deleted
+        """
+        path = '/'.join([self.REQUEST_BASEURL, 'transfer_limits'])
+        url = build_url(choice(self.list_hosts), path=path)
+        data = dumps({'rse_expression': rse_expression, 'activity': activity, 'direction': direction.value})
+        r = self._send_request(url, type_='DEL', data=data)
+
+        if r.status_code == codes.ok:
+            return True
+        exc_cls, exc_msg = self._get_exception(headers=r.headers, status_code=r.status_code, data=r.content)
+        raise exc_cls(exc_msg)
