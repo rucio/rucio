89558408b9a175489ab19d5349725ad5081eabcf
rdimaio <riccardodimaio11@gmail.com>
2025-05-27 13:47:48 +0200

    Database: gateway/account, migrate to session context manager; #7409

--- lib/rucio/gateway/account.py
+++ lib/rucio/gateway/account.py
@@ -22,27 +22,22 @@ from rucio.common.types import InternalAccount
 from rucio.common.utils import gateway_update_return_dict
 from rucio.core import account as account_core
 from rucio.core.rse import get_rse_id
-from rucio.db.sqla.constants import AccountType
-from rucio.db.sqla.session import read_session, stream_session, transactional_session
+from rucio.db.sqla.constants import AccountType, DatabaseOperationType
+from rucio.db.sqla.session import db_session
 
 if TYPE_CHECKING:
     from collections.abc import Iterator
-
-    from sqlalchemy.orm import Session
 
     from rucio.common.types import AccountAttributesDict, IdentityDict, UsageDict
     from rucio.db.sqla.models import Account
 
 
-@transactional_session
 def add_account(
     account: str,
     type_: str,
     email: str,
     issuer: str,
     vo: str = 'def',
-    *,
-    session: "Session"
 ) -> None:
     """
     Creates an account with the provided account name, contact information, etc.
@@ -53,29 +48,27 @@ def add_account(
 
     :param issuer: The issuer account_core.
     :param vo: The VO to act on.
-    :param session: The database session in use.
 
     """
 
     validate_schema(name='account', obj=account, vo=vo)
 
     kwargs = {'account': account, 'type': type_}
-    auth_result = rucio.gateway.permission.has_permission(issuer=issuer, vo=vo, action='add_account', kwargs=kwargs, session=session)
-    if not auth_result.allowed:
-        raise rucio.common.exception.AccessDenied('Account %s can not add account. %s' % (issuer, auth_result.message))
 
-    internal_account = InternalAccount(account, vo=vo)
+    with db_session(DatabaseOperationType.WRITE) as session:
+        auth_result = rucio.gateway.permission.has_permission(issuer=issuer, vo=vo, action='add_account', kwargs=kwargs, session=session)
+        if not auth_result.allowed:
+            raise rucio.common.exception.AccessDenied('Account %s can not add account. %s' % (issuer, auth_result.message))
 
-    account_core.add_account(internal_account, AccountType[type_.upper()], email, session=session)
+        internal_account = InternalAccount(account, vo=vo)
+
+        account_core.add_account(internal_account, AccountType[type_.upper()], email, session=session)
 
 
-@transactional_session
 def del_account(
     account: str,
     issuer: str,
     vo: str = 'def',
-    *,
-    session: "Session"
 ) -> None:
     """
     Disables an account with the provided account name.
@@ -83,25 +76,22 @@ def del_account(
     :param account: The account name.
     :param issuer: The issuer account.
     :param vo: The VO to act on.
-    :param session: The database session in use.
 
     """
     kwargs = {'account': account}
-    auth_result = rucio.gateway.permission.has_permission(issuer=issuer, vo=vo, action='del_account', kwargs=kwargs, session=session)
-    if not auth_result.allowed:
-        raise rucio.common.exception.AccessDenied('Account %s can not delete account. %s' % (issuer, auth_result.message))
+    with db_session(DatabaseOperationType.WRITE) as session:
+        auth_result = rucio.gateway.permission.has_permission(issuer=issuer, vo=vo, action='del_account', kwargs=kwargs, session=session)
+        if not auth_result.allowed:
+            raise rucio.common.exception.AccessDenied('Account %s can not delete account. %s' % (issuer, auth_result.message))
 
-    internal_account = InternalAccount(account, vo=vo)
+        internal_account = InternalAccount(account, vo=vo)
 
-    account_core.del_account(internal_account, session=session)
+        account_core.del_account(internal_account, session=session)
 
 
-@read_session
 def get_account_info(
     account: str,
     vo: str = 'def',
-    *,
-    session: "Session"
 ) -> "Account":
     """
     Returns the info like the statistics information associated to an account_core.
@@ -109,25 +99,23 @@ def get_account_info(
     :param account: The account name.
     :returns: A list with all account information.
     :param vo: The VO to act on.
-    :param session: The database session in use.
+
     """
 
     internal_account = InternalAccount(account, vo=vo)
 
-    acc = account_core.get_account(internal_account, session=session)
-    acc.account = acc.account.external
-    return acc
+    with db_session(DatabaseOperationType.READ) as session:
+        acc = account_core.get_account(internal_account, session=session)
+        acc.account = acc.account.external
+        return acc
 
 
-@transactional_session
 def update_account(
     account: str,
     key: str,
     value: Any,
     issuer: str = 'root',
     vo: str = 'def',
-    *,
-    session: "Session"
 ) -> None:
     """ Update a property of an account_core.
 
@@ -136,21 +124,21 @@ def update_account(
     :param value: Property value.
     :param issuer: The issuer account
     :param vo: The VO to act on.
-    :param session: The database session in use.
+
     """
     validate_schema(name='account', obj=account, vo=vo)
     kwargs = {}
-    auth_result = rucio.gateway.permission.has_permission(issuer=issuer, vo=vo, action='update_account', kwargs=kwargs, session=session)
-    if not auth_result.allowed:
-        raise rucio.common.exception.AccessDenied('Account %s can not change %s  of the account. %s' % (issuer, key, auth_result.message))
+    with db_session(DatabaseOperationType.WRITE) as session:
+        auth_result = rucio.gateway.permission.has_permission(issuer=issuer, vo=vo, action='update_account', kwargs=kwargs, session=session)
+        if not auth_result.allowed:
+            raise rucio.common.exception.AccessDenied('Account %s can not change %s  of the account. %s' % (issuer, key, auth_result.message))
 
-    internal_account = InternalAccount(account, vo=vo)
+        internal_account = InternalAccount(account, vo=vo)
 
-    return account_core.update_account(internal_account, key, value, session=session)
+        return account_core.update_account(internal_account, key, value, session=session)
 
 
-@stream_session
-def list_accounts(filter_: Optional[dict[str, Any]] = None, vo: str = 'def', *, session: "Session") -> 'Iterator[dict[str, Any]]':
+def list_accounts(filter_: Optional[dict[str, Any]] = None, vo: str = 'def') -> 'Iterator[dict[str, Any]]':
     """
     Lists all the Rucio account names.
 
@@ -158,7 +146,6 @@ def list_accounts(filter_: Optional[dict[str, Any]] = None, vo: str = 'def', *, 
 
     :param filter_: Dictionary of attributes by which the input data should be filtered
     :param vo: The VO to act on.
-    :param session: The database session in use.
 
     :returns: List of all accounts.
     """
@@ -169,80 +156,73 @@ def list_accounts(filter_: Optional[dict[str, Any]] = None, vo: str = 'def', *, 
         filter_['account'] = InternalAccount(filter_['account'], vo=vo)
     else:
         filter_['account'] = InternalAccount(account='*', vo=vo)
-    for result in account_core.list_accounts(filter_=filter_, session=session):
-        yield gateway_update_return_dict(result, session=session)
+
+    with db_session(DatabaseOperationType.READ) as session:
+        for result in account_core.list_accounts(filter_=filter_, session=session):
+            yield gateway_update_return_dict(result, session=session)
 
 
-@read_session
 def account_exists(
     account: str,
     vo: str = 'def',
-    *,
-    session: "Session"
 ) -> bool:
     """
     Checks to see if account exists. This procedure does not check it's status.
 
     :param account: Name of the account.
     :param vo: The VO to act on.
-    :param session: The database session in use.
+
     :returns: True if found, otherwise false.
     """
 
     internal_account = InternalAccount(account, vo=vo)
 
-    return account_core.account_exists(internal_account, session=session)
+    with db_session(DatabaseOperationType.READ) as session:
+        return account_core.account_exists(internal_account, session=session)
 
 
-@read_session
 def list_identities(
     account: str,
     vo: str = 'def',
-    *,
-    session: "Session"
 ) -> list["IdentityDict"]:
     """
     List all identities on an account_core.
 
     :param account: The account name.
     :param vo: The VO to act on.
-    :param session: The database session in use.
+
     """
 
     internal_account = InternalAccount(account, vo=vo)
 
-    return account_core.list_identities(internal_account, session=session)
+    with db_session(DatabaseOperationType.READ) as session:
+        return account_core.list_identities(internal_account, session=session)
 
 
-@read_session
 def list_account_attributes(
     account: str,
     vo: str = 'def',
-    *,
-    session: "Session"
 ) -> list["AccountAttributesDict"]:
     """
     Returns all the attributes for the given account.
 
     :param account: The account name.
     :param vo: The VO to act on
-    :param session: The database session in use.
+
     """
 
     internal_account = InternalAccount(account, vo=vo)
 
-    return account_core.list_account_attributes(internal_account, session=session)
+    with db_session(DatabaseOperationType.READ) as session:
+        return account_core.list_account_attributes(internal_account, session=session)
 
 
-@transactional_session
 def add_account_attribute(
     key: str,
     value: Any,
     account: str,
     issuer: str,
     vo: str = 'def',
-    *,
-    session: "Session"
 ) -> None:
     """
     Add an attribute to an account.
@@ -252,29 +232,27 @@ def add_account_attribute(
     :param account: The account name.
     :param issuer: The issuer account.
     :param vo: The VO to act on.
-    :param session: The database session in use.
+
     """
     validate_schema(name='account_attribute', obj=key, vo=vo)
     validate_schema(name='account_attribute', obj=value, vo=vo)
 
     kwargs = {'account': account, 'key': key, 'value': value}
-    auth_result = rucio.gateway.permission.has_permission(issuer=issuer, vo=vo, action='add_attribute', kwargs=kwargs, session=session)
-    if not auth_result.allowed:
-        raise rucio.common.exception.AccessDenied('Account %s can not add attributes. %s' % (issuer, auth_result.message))
+    with db_session(DatabaseOperationType.WRITE) as session:
+        auth_result = rucio.gateway.permission.has_permission(issuer=issuer, vo=vo, action='add_attribute', kwargs=kwargs, session=session)
+        if not auth_result.allowed:
+            raise rucio.common.exception.AccessDenied('Account %s can not add attributes. %s' % (issuer, auth_result.message))
 
-    internal_account = InternalAccount(account, vo=vo)
+        internal_account = InternalAccount(account, vo=vo)
 
-    account_core.add_account_attribute(internal_account, key, value, session=session)
+        account_core.add_account_attribute(internal_account, key, value, session=session)
 
 
-@transactional_session
 def del_account_attribute(
     key: str,
     account: str,
     issuer: str,
     vo: str = 'def',
-    *,
-    session: "Session"
 ) -> None:
     """
     Delete an attribute to an account.
@@ -283,26 +261,24 @@ def del_account_attribute(
     :param account: The account name.
     :param issuer: The issuer account.
     :param vo: The VO to act on.
-    :param session: The database session in use.
+
     """
     kwargs = {'account': account, 'key': key}
-    auth_result = rucio.gateway.permission.has_permission(issuer=issuer, vo=vo, action='del_attribute', kwargs=kwargs, session=session)
-    if not auth_result.allowed:
-        raise rucio.common.exception.AccessDenied('Account %s can not delete attribute. %s' % (issuer, auth_result.message))
+    with db_session(DatabaseOperationType.WRITE) as session:
+        auth_result = rucio.gateway.permission.has_permission(issuer=issuer, vo=vo, action='del_attribute', kwargs=kwargs, session=session)
+        if not auth_result.allowed:
+            raise rucio.common.exception.AccessDenied('Account %s can not delete attribute. %s' % (issuer, auth_result.message))
 
-    internal_account = InternalAccount(account, vo=vo)
+        internal_account = InternalAccount(account, vo=vo)
 
-    account_core.del_account_attribute(internal_account, key, session=session)
+        account_core.del_account_attribute(internal_account, key, session=session)
 
 
-@read_session
 def get_usage(
     rse: str,
     account: str,
     issuer: str,
     vo: str = 'def',
-    *,
-    session: "Session"
 ) -> "UsageDict":
     """
     Returns current values of the specified counter, or raises CounterNotFound if the counter does not exist.
@@ -311,23 +287,21 @@ def get_usage(
     :param account:          The account name.
     :param issuer:           The issuer account.
     :param vo:               The VO to act on.
-    :param session:          The database session in use.
+
     :returns:                A dictionary with total and bytes.
     """
-    rse_id = get_rse_id(rse=rse, vo=vo, session=session)
-    internal_account = InternalAccount(account, vo=vo)
+    with db_session(DatabaseOperationType.READ) as session:
+        rse_id = get_rse_id(rse=rse, vo=vo, session=session)
+        internal_account = InternalAccount(account, vo=vo)
 
-    return account_core.get_usage(rse_id, internal_account, session=session)
+        return account_core.get_usage(rse_id, internal_account, session=session)
 
 
-@read_session
 def get_usage_history(
     rse: str,
     account: str,
     issuer: str,
     vo: str = 'def',
-    *,
-    session: "Session"
 ) -> list["UsageDict"]:
     """
     Returns historical values of the specified counter, or raises CounterNotFound if the counter does not exist.
@@ -336,10 +310,11 @@ def get_usage_history(
     :param account:          The account name.
     :param issuer:           The issuer account.
     :param vo:               The VO to act on.
-    :param session:          The database session in use.
+
     :returns:                A dictionary with total and bytes.
     """
-    rse_id = get_rse_id(rse=rse, vo=vo, session=session)
-    internal_account = InternalAccount(account, vo=vo)
+    with db_session(DatabaseOperationType.READ) as session:
+        rse_id = get_rse_id(rse=rse, vo=vo, session=session)
+        internal_account = InternalAccount(account, vo=vo)
 
-    return account_core.get_usage_history(rse_id, internal_account, session=session)
+        return account_core.get_usage_history(rse_id, internal_account, session=session)
