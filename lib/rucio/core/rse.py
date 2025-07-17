1fd4d1d6ab47d493339c66fce924e54f0e860ebc
Dimitrios Christidis <dimitrios.christidis@cern.ch>
2025-07-17 10:58:52 +0200

    Core: Clarify meaning of RSEProtocolPriorityError #6312
    There is no implemented limit to how large a protocol priority can be.
    Thus, update the one test which suggested the opposite.

--- lib/rucio/core/rse.py
+++ lib/rucio/core/rse.py
@@ -1449,7 +1449,7 @@ def add_protocol(
     :raises RSENotFound: If RSE is not found.
     :raises RSEOperationNotSupported: If no scheme supported the requested operation for the given RSE.
     :raises RSEProtocolDomainNotSupported: If an undefined domain was provided.
-    :raises RSEProtocolPriorityError: If the provided priority for the scheme is to big or below zero.
+    :raises RSEProtocolPriorityError: If the provided priority for the protocol is below zero.
     :raises Duplicate: If scheme with identifier, hostname and port already exists
                        for the given RSE.
     """
@@ -1673,7 +1673,7 @@ def update_protocols(
     :raises RSEProtocolNotSupported: If no matching protocol was found for the given RSE.
     :raises RSEOperationNotSupported: If no protocol supported the requested operation for the given RSE.
     :raises RSEProtocolDomainNotSupported: If an undefined domain was provided.
-    :raises RSEProtocolPriorityError: If the provided priority for the protocol is too big or below zero.
+    :raises RSEProtocolPriorityError: If the provided priority for the protocol is below zero.
     :raises KeyNotFound: Invalid data for update provided.
     :raises Duplicate: If protocol with identifier, hostname and port already exists
                        for the given RSE.
