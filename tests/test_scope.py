3f889162f0c8b3e8bfe53792df69f25a4105e9ab
voetberg <magpie127@gmail.com>
2025-10-09 11:27:00 -0500

    Testing: Adding tests for updating ownership of scope #7830

--- tests/test_scope.py
+++ tests/test_scope.py
@@ -17,9 +17,9 @@ from json import loads
 import pytest
 
 from rucio.common.exception import AccountNotFound, Duplicate, InvalidObject, ScopeNotFound
-from rucio.common.types import InternalScope
+from rucio.common.types import InternalAccount, InternalScope
 from rucio.common.utils import generate_uuid as uuid
-from rucio.core.scope import add_scope, get_scopes, is_scope_owner
+from rucio.core.scope import add_scope, get_scopes, is_scope_owner, update_scope
 from rucio.tests.common import account_name_generator, auth, hdrdict, headers, scope_name_generator
 
 
@@ -40,6 +40,22 @@ class TestScopeCoreApi:
         add_scope(scope=scope, account=jdoe_account)
         answer = is_scope_owner(scope=scope, account=jdoe_account)
         assert answer is True
+
+    def test_change_scope_owner(self, vo, jdoe_account, random_account):
+        """ SCOPE (CORE): Give the scope a different owner"""
+        scope = InternalScope(scope_name_generator(), vo=vo)
+        add_scope(scope=scope, account=jdoe_account)
+
+        update_scope(scope=scope, account=random_account)
+        assert is_scope_owner(scope, account=random_account)
+
+        account = InternalAccount(account_name_generator())
+        with pytest.raises(AccountNotFound):
+            update_scope(scope=scope, account=account)
+
+        scope = InternalScope(scope_name_generator())
+        with pytest.raises(ScopeNotFound):
+            update_scope(scope=scope, account=random_account)
 
 
 def test_scope_success(rest_client, auth_token):
@@ -74,6 +90,28 @@ def test_scope_duplicate(rest_client, auth_token):
     assert response.status_code == 201
     response = rest_client.post('/accounts/%s/scopes/%s' % (acntusr, scopeusr), headers=headers(auth(auth_token)))
     assert response.status_code == 409
+
+
+def test_scope_change_ownership(rest_client, auth_token, random_account_factory):
+    """ SCOPE (REST): Send a post to change the existing scope's owner """
+    og_owner = random_account_factory()
+    scope = scope_name_generator()
+    response = rest_client.post(f'/accounts/{og_owner}/scopes/{scope}', headers=headers(auth(auth_token)))
+    assert response.status_code == 201
+
+    new_owner = random_account_factory()
+    response = rest_client.put(f"/scopes/{new_owner}/{scope}", headers=headers(auth(auth_token)))
+    assert response.status_code == 201
+
+    # Try to do it without sufficient permissions
+    new_owner = random_account_factory()
+    response = rest_client.put(f"/scopes/{new_owner}/{scope}", headers=headers(auth("fake_token")))
+    assert response.status_code == 401
+
+    # Try it with an account that doesn't exist
+    new_owner = account_name_generator()
+    response = rest_client.put(f"/scopes/{new_owner}/{scope}", headers=headers(auth(auth_token)))
+    assert response.status_code == 404
 
 
 def test_list_scope(rest_client, auth_token):
@@ -183,3 +221,12 @@ class TestScopeClient:
             item = list(filter(lambda d: d['scope'] == scope, listed_scopes))[0]
             assert item['scope'] == scope
             assert item['account'] == account
+
+    def test_update_scope_owner(self, scope_factory, random_account_factory, rucio_client, vo):
+        initial_account = random_account_factory().external
+        scope, _ = scope_factory(vos=[vo], account_name=initial_account)
+        assert scope in rucio_client.list_scopes_for_account(initial_account)
+
+        new_account = random_account_factory().external
+        assert rucio_client.update_scope_ownership(new_account, scope)
+        assert scope in rucio_client.list_scopes_for_account(new_account)
