e2735c10ad263b6189516ae2163210b7222599e5
voetberg <magpie127@gmail.com>
2025-10-09 11:30:10 -0500

    Core: Method to update scope ownership #7830

--- lib/rucio/core/scope.py
+++ lib/rucio/core/scope.py
@@ -16,10 +16,11 @@ from re import match
 from traceback import format_exc
 from typing import TYPE_CHECKING, Any, Literal, Optional
 
-from sqlalchemy import and_, select
+from sqlalchemy import and_, select, update
 from sqlalchemy.exc import IntegrityError
 
-from rucio.common.exception import AccountNotFound, Duplicate, RucioException, VONotFound
+import rucio.core.account as account_core
+from rucio.common.exception import AccountNotFound, Duplicate, RucioException, ScopeNotFound, VONotFound
 from rucio.core.vo import vo_exists
 from rucio.db.sqla import models
 from rucio.db.sqla.constants import AccountStatus, ScopeStatus
@@ -29,6 +30,8 @@ if TYPE_CHECKING:
     from collections.abc import Iterable
 
     from sqlalchemy.orm import Session
+
+    from rucio.common.types import InternalAccount, InternalScope
 
 
 @transactional_session
@@ -186,3 +189,33 @@ def is_scope_owner(scope, account, *, session: "Session"):
              models.Scope.account == account)
     )
     return bool(session.execute(stmt).scalar())
+
+
+@transactional_session
+def update_scope(scope: "InternalScope", account: "InternalAccount", *, session: "Session") -> None:
+    """ Give the scope a new owner
+
+    :param scope: the name for the existing scope.
+    :param account: the account to add the scope to.
+    :param session: The database session in use.
+    """
+
+    if not vo_exists(vo=scope.vo, session=session):
+        raise VONotFound('VO {} not found'.format(scope.vo))
+
+    # Verify both the scope and account exist
+    account_core.get_account(account, session=session)
+    if not check_scope(scope):
+        raise ScopeNotFound
+
+    stmt = update(
+        models.Scope
+    ).where(
+        models.Scope.scope == scope
+    ).values({
+        models.Scope.account: account
+    })
+    try:
+        session.execute(stmt)
+    except Exception:
+        raise RucioException(str(format_exc()))
