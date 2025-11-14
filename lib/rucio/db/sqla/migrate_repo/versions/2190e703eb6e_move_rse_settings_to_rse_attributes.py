9475037ad9db920c379ce98d85ee730d2ebc364f
Dimitris Xenakis <d.xenakis@ieee.org>
2025-11-15 01:53:29 +0200

    Database: Replace `get_schema` usage with `get_effective_schema` in Alembic migration `2190e703eb6e`

--- lib/rucio/db/sqla/migrate_repo/versions/2190e703eb6e_move_rse_settings_to_rse_attributes.py
+++ lib/rucio/db/sqla/migrate_repo/versions/2190e703eb6e_move_rse_settings_to_rse_attributes.py
@@ -15,10 +15,9 @@
 """ move rse settings to rse attributes """
 
 import sqlalchemy as sa
-from alembic import context
 from alembic.op import get_bind
 
-from rucio.db.sqla.migrate_repo import is_current_dialect
+from rucio.db.sqla.migrate_repo import get_effective_schema, is_current_dialect
 from rucio.db.sqla.types import GUID, BooleanString
 
 # Alembic revision identifiers
@@ -38,12 +37,6 @@ def get_changed_rse_settings():
     ]
 
 
-def get_schema():
-    return context.get_context().version_table_schema \
-        if context.get_context().version_table_schema \
-        else ""
-
-
 def get_rse_attr_association():
     return sa.sql.table(
         "rse_attr_map",
@@ -52,7 +45,7 @@ def get_rse_attr_association():
         sa.Column("value", BooleanString(255)),
         sa.Column("created_at", sa.DateTime),
         sa.Column("updated_at", sa.DateTime),
-        schema=get_schema(),
+        schema=get_effective_schema(),
     )
 
 
@@ -69,7 +62,7 @@ def upgrade():
                 sa.Column(setting, setting_datatype),
                 sa.Column("created_at", sa.DateTime),
                 sa.Column("updated_at", sa.DateTime),
-                schema=get_schema(),
+                schema=get_effective_schema(),
             )
 
             select_stmt = (
@@ -102,7 +95,7 @@ def downgrade():
                 "rses",
                 sa.Column("id", GUID()),
                 sa.Column(setting, setting_datatype),
-                schema=get_schema(),
+                schema=get_effective_schema(),
             )
 
             rse_attr_association = sa.sql.table(
@@ -110,7 +103,7 @@ def downgrade():
                 sa.Column("rse_id", GUID()),
                 sa.Column("key", sa.String(255)),
                 sa.Column("value", BooleanString(255)),
-                schema=get_schema(),
+                schema=get_effective_schema(),
             )
 
             # Oracle needs the sub-query, since multi-table updates are not supported.
