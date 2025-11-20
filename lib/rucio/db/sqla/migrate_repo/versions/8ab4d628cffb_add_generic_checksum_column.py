2116cc9062fa1891e5c3f040f68ec01f1ecd92d1
martynia <janusz.martyniak@googlemail.com>
2025-11-20 12:32:41 +0100

    feat: alembic migration:

---
+++ lib/rucio/db/sqla/migrate_repo/versions/8ab4d628cffb_add_generic_checksum_column.py
@@ -0,0 +1,57 @@
+# Copyright European Organization for Nuclear Research (CERN) since 2012
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""add generic checksum column"""    # noqa: D400, D415
+
+import sqlalchemy as sa
+from alembic import context
+from alembic.op import add_column, drop_column
+
+# Alembic revision identifiers
+revision = '8ab4d628cffb'
+down_revision = 'a62db546a1f1'
+
+
+def upgrade():
+    """Upgrade the database to this revision."""
+
+    if context.get_context().dialect.name in ['oracle', 'mysql', 'postgresql']:
+        schema = context.get_context().version_table_schema if context.get_context().version_table_schema else ''
+        add_column('replicas', sa.Column('checksum', sa.JSON(), nullable=True), schema=schema)
+        add_column('dids', sa.Column('checksum', sa.JSON(), nullable=True), schema=schema)
+        add_column('deleted_dids', sa.Column('checksum', sa.JSON(), nullable=True), schema=schema)
+        add_column('quarantined_replicas', sa.Column('checksum', sa.JSON(), nullable=True), schema=schema)
+        add_column('quarantined_replicas_history', sa.Column('checksum', sa.JSON(), nullable=True), schema=schema)
+        add_column('contents', sa.Column('checksum', sa.JSON(), nullable=True), schema=schema)
+        add_column('archive_contents', sa.Column('checksum', sa.JSON(), nullable=True), schema=schema)
+        add_column('archive_contents_history', sa.Column('checksum', sa.JSON(), nullable=True), schema=schema)
+        add_column('contents_history', sa.Column('checksum', sa.JSON(), nullable=True), schema=schema)
+        add_column('requests', sa.Column('checksum', sa.JSON(), nullable=True), schema=schema)
+        add_column('requests_history', sa.Column('checksum', sa.JSON(), nullable=True), schema=schema)
+
+
+def downgrade():
+    """Downgrade the database to the previous revision."""
+    schema = context.get_context().version_table_schema if context.get_context().version_table_schema else ''
+    drop_column('replicas', 'availability', schema=schema)
+    drop_column('dids', 'availability', schema=schema)
+    drop_column('deleted_dids', 'availability', schema=schema)
+    drop_column('quarantined_replicas', 'availability', schema=schema)
+    drop_column('quarantined_replicas_history', 'availability', schema=schema)
+    drop_column('contents', 'availability', schema=schema)
+    drop_column('archive_contents', 'availability', schema=schema)
+    drop_column('archive_contents_history', 'availability', schema=schema)
+    drop_column('contents_history', 'availability', schema=schema)
+    drop_column('requests', 'availability', schema=schema)
+    drop_column('requests_history', 'availability', schema=schema)
