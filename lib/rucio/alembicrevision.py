7507fa0a6a0e80ec4f47169e96f360786520f2bb
Dimitrios Christidis <dimitrios.christidis@cern.ch>
2025-11-20 10:50:16 +0100

    feat(Database): Add rules deleted_at column
    The new column matters only for the `rules_history` table. It is added
    to the other two for consistency.
    
    Issue: #8227

--- lib/rucio/alembicrevision.py
+++ lib/rucio/alembicrevision.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ALEMBIC_REVISION = 'a7e76cf4881d'  # the current alembic head revision
+ALEMBIC_REVISION = '0548e199f091'  # the current alembic head revision
