8f19edf3c6a2a092e006dda01f8be276df503d3a
voetberg <magpie127@gmail.com>
2026-02-03 08:47:57 -0600

    refactor(Documentation): Change RSEAttr attrs to RSEAttrObj Objects
    * Adds the documentation referencing  https://github.com/rucio/documentation/blob/main/docs/operator/configuration_parameters.md#rse-attributes
    * Adds types referencing the 'str_like_attrs' and 'bool_like_attrs' lists
    
    Issue: rucio/documentation#551

--- lib/rucio/common/constants.py
+++ lib/rucio/common/constants.py
@@ -139,54 +139,429 @@ class RseAttr:
     This class acts as a namespace containing all RSE attributes referenced in
     the Rucio source code. Setting them affects Rucio's behaviour in some way.
     """
+    ARCHIVE_TIMEOUT = RSEAttrObj(
+        name='archive_timeout',
+        type_=str,
+        doc=(
+            "Only used for transfers with a tape destination. Controls the number "
+            "of seconds the FTS3 transfer manager will wait for the tape archival "
+            "of the file to go `FAILED` or `FINISHED`.\n"
+            "Default: *None* (no default)."
+        ),
+    )
 
-    ARCHIVE_TIMEOUT = 'archive_timeout'
-    ASSOCIATED_SITES = 'associated_sites'
-    AUTO_APPROVE_BYTES = 'auto_approve_bytes'
-    AUTO_APPROVE_FILES = 'auto_approve_files'
-    BITTORRENT_TRACKER_ADDR = 'bittorrent_tracker_addr'
-    BLOCK_MANUAL_APPROVAL = 'block_manual_approval'
-    COUNTRY = 'country'
-    DECOMMISSION = 'decommission'
-    DEFAULT_ACCOUNT_LIMIT_BYTES = 'default_account_limit_bytes'
-    FTS = 'fts'
-    GLOBUS_ENDPOINT_ID = 'globus_endpoint_id'
-    GREEDYDELETION = 'greedyDeletion'
-    IS_OBJECT_STORE = 'is_object_store'
-    LFN2PFN_ALGORITHM = 'lfn2pfn_algorithm'
-    MAXIMUM_PIN_LIFETIME = 'maximum_pin_lifetime'
-    MULTIHOP_TOMBSTONE_DELAY = 'multihop_tombstone_delay'
-    NAMING_CONVENTION = 'naming_convention'
-    OIDC_BASE_PATH = 'oidc_base_path'
-    OIDC_SUPPORT = 'oidc_support'
-    PHYSGROUP = 'physgroup'
-    QBITTORRENT_MANAGEMENT_ADDRESS = 'qbittorrent_management_address'
-    RESTRICTED_READ = 'restricted_read'
-    RESTRICTED_WRITE = 'restricted_write'
-    RULE_APPROVERS = 'rule_approvers'
-    S3_URL_STYLE = 's3_url_style'
-    SIGN_URL = 'sign_url'
-    SIMULATE_MULTIRANGE = 'simulate_multirange'
-    SITE = 'site'
-    SKIP_UPLOAD_STAT = 'skip_upload_stat'
-    SOURCE_FOR_TOTAL_SPACE = 'source_for_total_space'
-    SOURCE_FOR_USED_SPACE = 'source_for_used_space'
-    STAGING_BUFFER = 'staging_buffer'
-    STAGING_REQUIRED = 'staging_required'
-    STRICT_COPY = 'strict_copy'
-    TOMBSTONE_DELAY = 'tombstone_delay'
-    TYPE = 'type'
-    USE_IPV4 = 'use_ipv4'
-    VERIFY_CHECKSUM = 'verify_checksum'
+    ASSOCIATED_SITES = RSEAttrObj(
+        name='associated_sites',
+        type_=str,
+        doc=(
+            "Separated by commas. Used for chaining of subscriptions so that "
+            "transfers to one RSE will also be mirrored to associated_sites.\n"
+            "Default: *None* (no default)."
+        ),
+    )
 
-    # The following RSE attributes are exclusively used in the permission layer
-    # and are likely VO-specific.
+    AUTO_APPROVE_BYTES = RSEAttrObj(
+        name='auto_approve_bytes',
+        type_=str,
+        doc=(
+            "Upper limit for the size in bytes of a DID for which rules will be "
+            "automatically approved. Example: `500GB`.\n"
+            "Default: *None* (no default)."
+        ),
+    )
 
-    BLOCK_MANUAL_APPROVE = 'block_manual_approve'
-    CMS_TYPE = 'cms_type'
-    DEFAULT_LIMIT_FILES = 'default_limit_files'
-    QUOTA_APPROVERS = 'quota_approvers'
-    RULE_DELETERS = 'rule_deleters'
+    AUTO_APPROVE_FILES = RSEAttrObj(
+        name='auto_approve_files',
+        type_=str,
+        doc=(
+            "Upper limit for the number of files covered by a rule which will be "
+            "automatically approved.\n"
+            "Default: *None* (no default)."
+        ),
+    )
+
+    BLOCK_MANUAL_APPROVAL = RSEAttrObj(
+        name='block_manual_approval',
+        type_=bool,
+        doc=(
+            "Disable manual rule approval for this RSE.\n"
+            "Default: ``False``."
+        ),
+    )
+
+    BITTORRENT_TRACKER_ADDR = RSEAttrObj(
+        name='bittorrent_tracker_addr',
+        type_=str,
+        doc=(
+            "Used to configure the URL of the bittorrent tracker API when using the "
+            "torrent transfer manager.\n"
+            "Default: *None* (no default)."
+        ),
+    )
+
+    CHECKSUM_KEY = RSEAttrObj(
+        name='checksum_key',
+        type_=str,
+        doc=(
+            "Used to specify an alternate RSE attribute to search for supported "
+            "checksums beyond those with global support (ADLER32, MD5).\n"
+            "Default: ``supported_checksums``."
+        ),
+    )
+
+    COUNTRY = RSEAttrObj(
+        name='country',
+        type_=str,
+        doc=(
+            "Used for country‑level granularity of RSE selectors.\n"
+            "Default: *None* (no default)."
+        ),
+    )
+
+    DECOMMISSION = RSEAttrObj(
+        name='decommission',
+        type_=bool,
+        doc=(
+            "Indicates to the RSE Decommissioning Daemon that this RSE is to be "
+            "decommissioned.\n"
+            "Default: ``False``."
+        ),
+    )
+
+    DEFAULT_ACCOUNT_LIMIT_BYTES = RSEAttrObj('default_account_limit_bytes', str, "")
+
+    FTS = RSEAttrObj(
+        name='fts',
+        type_=str,
+        doc=(
+            "Specify the REST API URL of the FTS3 transfer manager.\n"
+            "Default: *None* (no default)."
+        ),
+    )
+
+    GREEDYDELETION = RSEAttrObj(
+        name='greedyDeletion',
+        type_=bool,
+        doc=(
+            "Allow files without a rule locking them to be deleted by a Reaper daemon. "
+            "Default behaviour only marks a file for deletion when there is no space "
+            "on an RSE for a new required file.\n"
+            "Default: ``False``."
+        ),
+    )
+
+    GLOBUS_ENDPOINT_ID = RSEAttrObj(
+        name='globus_endpoint_id',
+        type_=str,
+        doc=(
+            "Specify the REST API URL of the Globus transfer manager.\n"
+            "Default: *None* (no default)."
+        ),
+    )
+
+    HOP_PENALTY = RSEAttrObj(
+        name='hop_penalty',
+        type_=str,
+        doc=(
+            "Usage cost of this RSE as an intermediate in multihop transfers. Overrides "
+            "the global ``transfers/hop_penalty`` configuration for this particular RSE.\n"
+            "Requires ``available_for_multihop`` to be ``True``.\n"
+            "Default: *None* (no default)."
+        ),
+    )
+
+    IS_OBJECT_STORE = RSEAttrObj(
+        name='is_object_store',
+        type_=bool,
+        doc=(
+            "Control the auditor daemon's behavior. Instead of dumping all files, list "
+            "them by date.\n"
+            "Default: ``False``."
+        ),
+    )
+
+    LFN2PFN_ALGORITHM = RSEAttrObj(
+        name='lfn2pfn_algorithm',
+        type_=str,
+        doc=(
+            "Name of the algorithm to be used for generating paths to files on the "
+            "storage. Must be defined in the configured policy package.\n"
+            "Default: ``default``."
+        ),
+    )
+    MAXIMUM_PIN_LIFETIME = RSEAttrObj('maximum_pin_lifetime', type_=str, doc='')
+
+    MOCK = RSEAttrObj(
+        name='mock',
+        type_=bool,
+        doc=(
+            "Flag used for test/mock RSEs.\n"
+            "Default: ``False``."
+        ),
+    )
+
+    MULTIHOP_TOMBSTONE_DELAY = RSEAttrObj(
+        name='multihop_tombstone_delay',
+        type_=str,
+        doc=(
+            "Delay before a multihop transfer intermediate rule is to be deleted (seconds).\n"
+            "Default: ``7200``."
+        ),
+    )
+
+    NAMING_CONVENTION = RSEAttrObj(
+        name='naming_convention',
+        type_=str,
+        doc=(
+            "Name of the algorithm in the configured policy package which is to be used "
+            "to validate DIDs on this RSE.\n"
+            "Default: ``None``."
+        ),
+    )
+
+    OIDC_SUPPORT = RSEAttrObj(
+        name='oidc_support',
+        type_=bool,
+        doc=(
+            "Specifies that the RSE supports OIDC authentication for FTS3 transfers.\n"
+            "Default: ``False``."
+        ),
+    )
+
+    PHYSGROUP = RSEAttrObj(
+        name='physgroup',
+        type_=str,
+        doc=(
+            "Used for grouping of rules by CERN experiments.\n"
+            "Default: ``.``."
+        ),
+    )
+
+    QBITTORRENT_MANAGEMENT_ADDRESS = RSEAttrObj(
+        name='qbittorrent_management_address',
+        type_=str,
+        doc=(
+            "Used to configure the URL of the bittorrent management API when using the "
+            "torrent transfer manager.\n"
+            "Default: *None* (no default)."
+        ),
+    )
+
+    QUOTA_APPROVERS = RSEAttrObj(
+        name='quota_approvers',
+        type_=str,
+        doc=(
+            "List of Rucio users (comma‑separated) that can approve quota changes. "
+            "Permission‑layer only; typically CERN‑specific.\n"
+            "Default: ``None``."
+        ),
+    )
+
+    RESTRICTED_READ = RSEAttrObj(
+        name='restricted_read',
+        type_=bool,
+        doc=(
+            "If ``True``, only allow transfers **FROM** this RSE if started by an account "
+            "with admin privileges.\n"
+            "Default: ``False``."
+        ),
+    )
+
+    RESTRICTED_WRITE = RSEAttrObj(
+        name='restricted_write',
+        type_=bool,
+        doc=(
+            "If ``True``, only allow transfers **TO** this RSE if started by an account "
+            "with admin privileges.\n"
+            "Default: ``False``."
+        ),
+    )
+
+    RULE_APPROVERS = RSEAttrObj(
+        name='rule_approvers',
+        type_=str,
+        doc=(
+            "List of Rucio users (comma‑separated) that will be notified by email to approve "
+            "rules on this RSE.\n"
+            "Default: ``None``."
+        ),
+    )
+
+    RULE_DELETERS = RSEAttrObj(
+        name='rule_deleters',
+        type_=str,
+        doc=(
+            "List of Rucio users (comma‑separated) that can delete rules. Permission‑layer only; "
+            "typically CERN‑specific.\n"
+            "Default: ``None``."
+        ),
+    )
+
+    SIGN_URL = RSEAttrObj(
+        name='sign_url',
+        type_=str,
+        doc=(
+            "Controls if URLs for uploads and transfers are to be cryptographically signed "
+            "by an external service.\n"
+            "Default: *None* (no default)."
+        ),
+    )
+
+    SIMULATE_MULTIRANGE = RSEAttrObj(
+        name='simulate_multirange',
+        type_=str,
+        doc=(
+            "Control the number of connections for multirange byte requests on commercial cloud "
+            "storage. Multirange is not supported on S3.\n"
+            "Default: *None* (no default)."
+        ),
+    )
+
+    SITE = RSEAttrObj(
+        name='site',
+        type_=str,
+        doc=(
+            "Used to determine if downloads/transfers are local or wide‑area network.\n"
+            "Default: *None* (no default)."
+        ),
+    )
+
+    SKIP_UPLOAD_STAT = RSEAttrObj(
+        name='skip_upload_stat',
+        type_=bool,
+        doc=(
+            "Disables the use of a GFAL ``stat`` when calling ``rucio upload`` for this RSE.\n"
+            "Default: ``False``."
+        ),
+    )
+
+    SOURCE_FOR_TOTAL_SPACE = RSEAttrObj(
+        name='source_for_total_space',
+        type_=str,
+        doc=(
+            "Used to specify where Rucio should obtain storage capacity information from.\n"
+            "Default: ``storage``."
+        ),
+    )
+
+    SOURCE_FOR_USED_SPACE = RSEAttrObj(
+        name='source_for_used_space',
+        type_=str,
+        doc=(
+            "Used to specify where Rucio should obtain storage usage information from.\n"
+            "Default: ``storage``."
+        ),
+    )
+
+    STAGING_BUFFER = RSEAttrObj(
+        name='staging_buffer',
+        type_=str,
+        doc=(
+            "Used with ``TAPE`` RSEs to specify to which RSE a file on tape should be "
+            "transferred to as a disk buffer. Destination RSE should have ``staging_area: True``.\n"
+            "Default: *None* (no default)."
+        ),
+    )
+
+    STAGING_REQUIRED = RSEAttrObj(
+        name='staging_required',
+        type_=bool,
+        doc=(
+            "Duplicates the ``rse_type`` RSE setting. Specifies that files on this RSE will "
+            "require staging from high‑latency storage.\n"
+            "Default: ``False``."
+        ),
+    )
+
+    STRICT_COPY = RSEAttrObj(
+        name='strict_copy',
+        type_=bool,
+        doc=(
+            "Instructs the transfer manager to **ONLY** copy the file, disabling all validation "
+            "checks such as ``PROPFIND`` and checksumming.\n"
+            "Default: ``False``."
+        ),
+    )
+
+    S3_URL_STYLE = RSEAttrObj(
+        name='s3_url_style',
+        type_=str,
+        doc=(
+            "For S3 storage elements [specify](https://docs.aws.amazon.com/AmazonS3/latest/userguide/VirtualHosting.html) `path` or `host`."
+            "Default: *None* (no default)."
+        ),
+    )
+
+    TIER = RSEAttrObj(
+        name='tier',
+        type_=str,
+        doc=(
+            "Datacenter tier (1‑4).\n"
+            "Default: *None* (no default)."
+        ),
+    )
+
+    TOMBSTONE_DELAY = RSEAttrObj(name='tombstone_delay', type_=str, doc='Delay before a rule is to be deleted (seconds)')
+
+    TYPE = RSEAttrObj(
+        name='type',
+        type_=str,
+        doc=(
+            "Values: ``{LOCALGROUPDISK, LOCALGROUPTAPE, GROUPDISK, SCRATCHDISK, MOCK, "
+            "TEST, DATADISK}``.\n"
+            "Default: ``.``."
+        ),
+    )
+
+    USE_IPV4 = RSEAttrObj(
+        name='use_ipv4',
+        type_=bool,
+        doc=(
+            "Force the transfer manager to use IPv4 for transfers to this RSE.\n"
+            "Default: ``False``."
+        ),
+    )
+
+    VERIFY_CHECKSUM = RSEAttrObj(
+        name='verify_checksum',
+        type_=bool,
+        doc=(
+            "Control if checksum is to be queried on transfer source and destination to "
+            "confirm successful transfers.\n"
+            "Default: ``True``."
+        ),
+    )
+
+    # VO Specific
+
+    BLOCK_MANUAL_APPROVE = RSEAttrObj(
+        name='block_manual_approve',
+        type_=bool,
+        doc=(
+            "VO‑specific duplicate of ``block_manual_approval``.\n"
+            "Default: ``False``."
+        ),
+    )
+
+    CMS_TYPE = RSEAttrObj(
+        name='cms_type',
+        type_=str,
+        doc=(
+            "VO‑specific attribute used by CMS.\n"
+            "Default: *None* (no default)."
+        ),
+    )
+
+    DEFAULT_LIMIT_FILES = RSEAttrObj(
+        name='default_limit_files',
+        type_=str,
+        doc=(
+            "VO‑specific default limit on number of files.\n"
+            "Default: *None* (no default)."
+        ),
+    )
+    OIDC_BASE_PATH = RSEAttrObj(name="oidc_base_path", type_=str, doc="")
+
 
 
 # Literal types to allow overloading of functions with RSE attributes in their signature.
