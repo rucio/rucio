03a997f013a7f637e8e818b510530a0e63b7d3d1
martynia <janusz.martyniak@googlemail.com>
2026-03-06 14:35:13 +0000

    feat(Replicas): add deletable replica counting, refactor, fix tests
    Closes: #4261

--- tests/test_abacus_rse_expired.py
+++ tests/test_abacus_rse_expired.py
@@ -12,12 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from datetime import datetime, timedelta, timezone
+
 import pytest
 from sqlalchemy import delete
 
 from rucio.core.replica import add_replica, get_replica, set_tombstone
 from rucio.core.rse import get_rse_usage
-from rucio.daemons.abacus.rse_obsolete import run
+from rucio.daemons.abacus.rse_expired import run
 from rucio.db.sqla import models
 from rucio.db.sqla.constants import OBSOLETE
 from rucio.db.sqla.session import get_session
@@ -46,16 +48,31 @@ class TestAbacusRSEObsolete:
             set_tombstone(rse_id, mock_scope, name)
             assert get_replica(rse_id, mock_scope, name)['tombstone'] == OBSOLETE
 
+        # add one more expired replica on the RSE
+        name3 = did_name_generator('file')
+        add_replica(rse_id, mock_scope, name3, 27, root_account)
+        assert get_replica(rse_id, mock_scope, name3)['tombstone'] is None
+        set_tombstone(rse_id, mock_scope, name3, datetime.now(timezone.utc) - timedelta(hours=1))
+        assert get_replica(rse_id, mock_scope, name3)['tombstone'] is not None
         # now one more replica, on a different RSE, but not obsolete:
         _, rse_id2 = rse_factory.make_mock_rse()
         name2 = did_name_generator('file')
         add_replica(rse_id2, mock_scope, name2, 4, root_account)
         assert get_replica(rse_id2, mock_scope, name2)['tombstone'] is None
-
-        run(once=True)
-        res = get_rse_usage(rse_id, 'obsolete')[0]  # 3 files, 12 bytes
-        assert res['used'] == 12
-        assert res['files'] == 3
-        res = get_rse_usage(rse_id2, 'obsolete')[0]  # 0 files, 0 bytes
-        assert res['used'] == 0
-        assert res['files'] == 0
+        # usage is not accumulated:
+        for _ in range(2):
+            run(once=True, obsolete=True)
+            res = get_rse_usage(rse_id, 'obsolete')[0]  # 3 files, 12 bytes
+            assert res['used'] == 12
+            assert res['files'] == 3
+            run(once=True)
+            res = get_rse_usage(rse_id, 'expired')[0]  # 4 files (OBSOLETE included), 37 bytes
+            assert res['used'] == 39
+            assert res['files'] == 4
+            # rse_id2 undisturbed ..
+            res = get_rse_usage(rse_id2, 'obsolete')[0]  # 0 files, 0 bytes
+            assert res['used'] == 0
+            assert res['files'] == 0
+            res = get_rse_usage(rse_id2, 'expired')[0]  # 0 files, 0 bytes
+            assert res['used'] == 0
+            assert res['files'] == 0
