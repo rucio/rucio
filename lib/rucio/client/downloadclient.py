rucio/rucio

commit 64b62d69bb75ff33a66e1c1ab557ae125309534a

Common: Refactor execute to single return signature better typehints; Fix #5942
The function rucio/common/utils.py::execute is of great importance for
the testing workflows, since it allows for shell commands to be run and
their outputs to be used. The issue was that the function had two return
signatures, depending on the value of a keyword argument
(blocking=True). The different return signatures cause linting errors.

The function was refactored to have a single return signature, removing
the keyword argument and only treating the case where blocking=True. The
one situation in which the function 'execute' is called with
blocking=False has been handled by substituting the body of the
'execute' function into what was the function call.

lib/rucio/client/downloadclient.py
@@ -22,6 +22,7 @@ import random
 import shutil
 import signal
 import time
+import subprocess
 
 from queue import Queue, Empty, deque
 from threading import Thread
@@ -831,7 +832,13 @@ class DownloadClient:
             try:
                 to_exec = cmd % (os.getpid(), rpc_secret, port)
                 logger(logging.DEBUG, to_exec)
-                rpcproc = execute(to_exec, False)
+                rpcproc = subprocess.Popen(
+                    cmd,
+                    shell=True,
+                    stdin=subprocess.PIPE,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE
+                )
             except Exception as error:
                 raise RucioException('Failed to execute aria2c!', error)
 

lib/rucio/common/utils.py
@@ -33,6 +33,7 @@ import subprocess
 import tempfile
 import threading
 import time
+from typing import Tuple
 import zlib
 from collections import OrderedDict
 from enum import Enum
@@ -434,7 +435,7 @@ def parse_response(data):
     return json.loads(data, object_hook=datetime_parser)
 
 
-def execute(cmd, blocking=True):
+def execute(cmd) -> Tuple[int, str, str]:
     """
     Executes a command in a subprocess. Returns a tuple
     of (exitcode, out, err), where out is the string output
@@ -450,12 +451,10 @@ def execute(cmd, blocking=True):
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
 
-    if blocking:
-        result = process.communicate()
-        (out, err) = result
-        exitcode = process.returncode
-        return exitcode, out.decode(encoding='utf-8'), err.decode(encoding='utf-8')
-    return process
+    result = process.communicate()
+    (out, err) = result
+    exitcode = process.returncode
+    return exitcode, out.decode(encoding='utf-8'), err.decode(encoding='utf-8')
 
 
 def rse_supported_protocol_operations():

commit 6076413b1078387a1aaf1b6c78a186f3a7b43c3f

fix(Clients): fix running aria2c command in DownloadClient
The command that was passed to subprocess.Popen had string formatting markers, which were not replaced.
Broken since 64b62d69bb75ff33a66e1c1ab557ae125309534a

Closes: #8776

lib/rucio/client/downloadclient.py
@@ -1092,21 +1092,21 @@ class DownloadClient:
         logger = self.logger
         from xmlrpc.client import ServerProxy as RPCServerProxy
 
-        cmd = 'aria2c '\
-              '--enable-rpc '\
-              '--certificate=$X509_USER_PROXY '\
-              '--private-key=$X509_USER_PROXY '\
-              '--ca-certificate=/etc/pki/tls/certs/CERN-bundle.pem '\
-              '--quiet=true '\
-              '--allow-overwrite=true '\
-              '--auto-file-renaming=false '\
-              '--stop-with-process=%d '\
-              '--rpc-secret=%s '\
-              '--rpc-listen-all=false '\
-              '--rpc-max-request-size=100M '\
-              '--connect-timeout=5 '\
-              '--rpc-listen-port=%d'
-
+        args = [
+            'aria2c',
+            '--enable-rpc',
+            '--certificate=' + os.environ.get('X509_USER_PROXY', ''),
+            '--private-key=' + os.environ.get('X509_USER_PROXY', ''),
+            '--ca-certificate=/etc/pki/tls/certs/CERN-bundle.pem',
+            '--quiet=true',
+            '--allow-overwrite=true',
+            '--auto-file-renaming=false',
+            f'--stop-with-process={os.getpid()}',
+            f'--rpc-secret={rpc_secret}',
+            '--rpc-listen-all=false',
+            '--rpc-max-request-size=100M',
+            '--connect-timeout=5',
+        ]
         logger(logging.INFO, 'Starting aria2c rpc server...')
 
         # trying up to 3 random ports
@@ -1114,11 +1114,11 @@ class DownloadClient:
             port = random.randint(1024, 65534)  # noqa: S311
             logger(logging.DEBUG, 'Trying to start rpc server on port: %d' % port)
             try:
-                to_exec = cmd % (os.getpid(), rpc_secret, port)
+                to_exec = list(args)
+                to_exec.append(f'--rpc-listen-port={port}')
                 logger(logging.DEBUG, to_exec)
                 rpcproc = subprocess.Popen(
-                    cmd,
-                    shell=True,
+                    to_exec,
                     stdin=subprocess.PIPE,
                     stdout=subprocess.PIPE,
                     stderr=subprocess.PIPE